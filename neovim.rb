eaa4f2b9ce99eeddef0b21c7ba3f913d36d038eb
Cameron Eagans <cweagans@gmail.com>
2014-02-23 14:08:33 -0700

    Adding neovim formula for Homebrew

---
+++ neovim.rb
@@ -0,0 +1,15 @@
+require 'formula'
+
+class Neovim < Formula
+  homepage 'http://neovim.org'
+  head 'https://github.com/neovim/neovim.git'
+
+  depends_on 'md5sha1sum'
+  depends_on 'cmake'
+  depends_on 'libtool'
+  depends_on 'automake'
+
+  def install
+    system "make", "PREFIX=#{prefix}", "cmake"
+  end
+end
