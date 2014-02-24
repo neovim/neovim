00ca93fb5037173897f2055a8dc9af143c031931
Rich Wareham <rjw57@cam.ac.uk>
2014-02-24 20:57:04 +0000

    automatically discover libuv in CMake
    Idiomatically discover if libuv is installed.

---
+++ cmake/FindLibUV.cmake
@@ -0,0 +1,24 @@
+# - Try to find libuv
+# Once done, this will define
+#
+#  LibUV_FOUND - system has libuv
+#  LibUV_INCLUDE_DIRS - the libuv include directories
+#  LibUV_LIBRARIES - link these to use libuv
+
+include(LibFindMacros)
+
+# Include dir
+find_path(LibUV_INCLUDE_DIR
+    NAMES uv.h
+)
+
+# The library itself
+find_library(LibUV_LIBRARY
+    NAMES uv
+)
+
+# Set the include dir variables and the libraries and let libfind_process do the rest.
+# NOTE: Singular variables for this library, plural for libraries this this lib depends on.
+set(LibUV_PROCESS_INCLUDES LibUV_INCLUDE_DIR)
+set(LibUV_PROCESS_LIBS LibUV_LIBRARY)
+libfind_process(LibUV)
