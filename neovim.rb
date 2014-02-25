e08d2aea3e6d86f0c8aac6e2d75182a729662fd7
andrewmchen <andrewmchen@berkeley.edu>
2014-02-25 13:32:55 -0800

    Fixed brew install instructions in README.md and changed dependencies in brew script.

--- neovim.rb
+++ neovim.rb
@@ -4,10 +4,10 @@ class Neovim < Formula
   homepage 'http://neovim.org'
   head 'https://github.com/neovim/neovim.git'
 
-  depends_on 'md5sha1sum'
   depends_on 'cmake'
   depends_on 'libtool'
   depends_on 'automake'
+  depends_on 'libuv'
 
   def install
     system "make", "PREFIX=#{prefix}", "cmake"
