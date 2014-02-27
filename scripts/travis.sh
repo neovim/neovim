c492c5e36d26bbc631d1e1de6c04a89a30552fd1
Thiago de Arruda <tpadilha84@gmail.com>
2014-02-26 21:38:21 -0300

    Fix travis script
    It was broke because the ls command failed when no valgrind.* file were found

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -5,7 +5,7 @@ make cmake CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$PWD/dist"
 make
 echo "Running tests with valgrind..."
 if ! make test > /dev/null 2>&1; then
-	failed=$(ls src/testdir/valgrind.*)
+	failed=$(ls src/testdir/valgrind.* || true)
 	if [ -n "$failed" ]; then
 		echo "Memory leak detected" >&2 
 		cat src/testdir/valgrind.*
