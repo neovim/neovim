c4e43d423e1bbb9c2c76a5ec8f12a2f681d259d0
simendsjo <none@none>
2014-02-28 12:37:21 +0100

    Some cleanup of growable arrays: * moved declarations closer to where they are used * assertions * documentation * added const where possible * some early returns to avoid doing unecessary work
    --HG--
    branch : gaarray.h-cleanup-dev

--- src/garray.h
+++ src/garray.h
@@ -6,8 +6,8 @@ void ga_clear_strings __ARGS((garray_T *gap));
 void ga_init __ARGS((garray_T *gap));
 void ga_init2 __ARGS((garray_T *gap, int itemsize, int growsize));
 int ga_grow __ARGS((garray_T *gap, int n));
-char_u *ga_concat_strings __ARGS((garray_T *gap));
-void ga_concat __ARGS((garray_T *gap, char_u *s));
+char_u *ga_concat_strings __ARGS((garray_T const *const gap));
+void ga_concat __ARGS((garray_T *gap, char_u const *const s));
 void ga_append __ARGS((garray_T *gap, int c));
 void append_ga_line __ARGS((garray_T *gap));
 
