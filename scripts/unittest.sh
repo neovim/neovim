c379c88e54b2d5049d85c39a0ec9a432e2c8491b
Simen Endsjø <simendsjo@gmail.com>
2014-03-06 12:15:28 +0100

    Quicker way to run a single unittest file

--- scripts/unittest.sh
+++ scripts/unittest.sh
@@ -3,4 +3,16 @@
 (cd "$pkgroot/build" && make) || exit 1
 eval "$(luarocks path)"
 
-busted --pattern=.moon ./test
+if [ $# == 0 ]; then # Assume full test
+  busted --pattern=.moon ./test
+else
+  # Assume a list of unittest files
+  for filename; do
+    filename=./test/unit/$filename.moon
+    if [ ! -f $filename ]; then
+      echo "Not found: $filename"
+    else
+      busted $filename
+    fi
+  done
+fi
