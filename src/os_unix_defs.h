5958c10f3e8d1a8bbbad3dfecf2da2c821ec2002
Stefan Natchev <stefan.natchev@gmail.com>
2014-03-14 10:11:34 -0400

    include sys/time.h as it was causing problems with undef'ed structs on Darwin
    Not sure if the implications of this, and looking for comments

--- src/os_unix_defs.h
+++ src/os_unix_defs.h
@@ -89,13 +89,8 @@
 # endif
 #endif
 
-#if !defined(HAVE_SYS_TIME_H) || defined(TIME_WITH_SYS_TIME)
-# include <time.h>          /* on some systems time.h should not be
-                               included together with sys/time.h */
-#endif
-#ifdef HAVE_SYS_TIME_H
-# include <sys/time.h>
-#endif
+#include <time.h>
+#include <sys/time.h>
 
 #include <signal.h>
 
