86180787fa8ee414dddfd370c81742543bc5dde4
John Szakmeister <john@szakmeister.net>
2014-03-29 10:01:18 -0400

    Drop the deb-src for clang.
    The llvm.org/apt repo currently has an issue with the source packages,
    and we don't need it on Travis anyways.

--- scripts/travis-setup.sh
+++ scripts/travis-setup.sh
@@ -7,10 +7,10 @@ wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key | apt-key add -
 
 cat > /etc/apt/sources.list.d/clang.list << "EOF"
 deb http://llvm.org/apt/precise/ llvm-toolchain-precise main
-deb-src http://llvm.org/apt/precise/ llvm-toolchain-precise main
+# deb-src http://llvm.org/apt/precise/ llvm-toolchain-precise main
 # 3.4
 deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.4 main
-deb-src http://llvm.org/apt/precise/ llvm-toolchain-precise-3.4 main
+# deb-src http://llvm.org/apt/precise/ llvm-toolchain-precise-3.4 main
 # Common
 deb http://ppa.launchpad.net/ubuntu-toolchain-r/test/ubuntu precise main
 EOF
