49ad606cd802c51bed02d94924c6718ed943ff62
Thomas Wienecke <wienecke.t@gmail.com>
2014-04-10 19:17:24 +0200

    Remove undefined function server_to_input_buf.

--- src/main.h
+++ src/main.h
@@ -10,7 +10,6 @@ void mainerr_arg_missing(char_u *str);
 void time_push(void *tv_rel, void *tv_start);
 void time_pop(void *tp);
 void time_msg(char *mesg, void *tv_start);
-void server_to_input_buf(char_u *str);
 char_u *eval_client_expr_to_string(char_u *expr);
 char_u *serverConvert(char_u *client_enc, char_u *data, char_u **tofree);
 
