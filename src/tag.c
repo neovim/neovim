bbcf2be54bd294fa046d0a6b00b5e3e7fcb3910f
Marco Hinz <mh.codebro@gmail.com>
2014-04-14 15:47:59 +0200

    vim-patch:7.4.240
    Problem:  ":tjump" shows "\n" as "\\n".
    Solution: Skip over "\" that escapes a backslash. (Gary Johnson)
    
    https://code.google.com/p/vim/source/detail?r=8d1ba0a23588932d22ad37cbd87ae3bbd4bfeff8

--- src/tag.c
+++ src/tag.c
@@ -645,9 +645,11 @@ do_tag (
               break;
             msg_advance(15);
 
-            /* skip backslash used for escaping command char */
-            if (*p == '\\' && *(p + 1) == *tagp.command)
+            // Skip backslash used for escaping a command char or a backslash.
+            if (*p == '\\' && (*(p + 1) == *tagp.command
+                               || *(p + 1) == '\\')) {
               ++p;
+            }
 
             if (*p == TAB) {
               msg_putchar(' ');
