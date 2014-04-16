096eadbcbb224e9d0febacfad571a9c25934f151
Pavel Platto <hinidu@gmail.com>
2014-04-16 08:58:09 +0300

    Added os_isatty

--- src/os/input.h
+++ src/os/input.h
@@ -13,5 +13,11 @@ int os_inchar(uint8_t *, int, int32_t, int);
 bool os_char_avail(void);
 void os_breakcheck(void);
 
+/// Test whether a file descriptor refers to a terminal.
+/// 
+/// @param fd File descriptor.
+/// @return `true` if file descriptor refers to a terminal.
+bool os_isatty(int fd);
+
 #endif  // NEOVIM_OS_INPUT_H
 
