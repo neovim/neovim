6a63b0986f20e09817c4273f42aa7c64236b2e91
Marco Hinz <mh.codebro@gmail.com>
2014-04-23 19:09:45 +0200

    vim-patch:7.4.261
    Problem:  When updating the window involves a regexp pattern, an
              interactive substitute to replace a "\n" with a line
              break fails. (Ingo Karkat)
    
    Solution: Set reg_line_lbr in vim_regsub() and vim_regsub_multi().
    
    https://code.google.com/p/vim/source/detail?r=43c6cd07c8defd8505acbe479c6970764c08e6f9

--- src/version.c
+++ src/version.c
@@ -202,6 +202,13 @@ static char *(features[]) = {
 
 static int included_patches[] = {
   // Add new patch number below this line
+  //265,
+  //264,
+  //263,
+  //262,
+  261,
+  //260,
+  //259,
   //258,
   //257,
   //256,
