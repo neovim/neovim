81ac66b06414fa995f387871878068a53ed7097a
Marco Hinz <mh.codebro@gmail.com>
2014-04-29 15:01:06 +0200

    vim-patch:7.4.266
    Problem:  Test 62 fails.
    Solution: Set the language to C. (Christian Brabandt)
    
    https://code.google.com/p/vim/source/detail?r=8f84e906d454a95d3167678a745dde9de442b604

--- src/version.c
+++ src/version.c
@@ -202,6 +202,11 @@ static char *(features[]) = {
 
 static int included_patches[] = {
   // Add new patch number below this line
+  //270,
+  //269,
+  //268,
+  //267,
+  266,
   265,
   264,
   //263,
