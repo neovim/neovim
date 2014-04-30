7cb20fd1b09a5c263ca3aef57d87f09084fb7830
John Szakmeister <john@szakmeister.net>
2014-04-30 05:10:37 -0400

    Generate a lua module to help pass build-related settings.
    This allows us to avoid hard-coding paths and using environment
    variables to communicate key information to unit tests, which fits
    with the overall goal of making sure that folks driving CMake directly
    can continue to do out-of-tree builds.

--- cmake/RunUnittests.cmake
+++ cmake/RunUnittests.cmake
@@ -1,13 +1,11 @@
 get_filename_component(BUSTED_DIR ${BUSTED_PRG} PATH)
 set(ENV{PATH} "${BUSTED_DIR}:$ENV{PATH}")
-set(ENV{NVIM_TEST_LIB} ${NVIM_TEST_LIB})
-set(ENV{TEST_INCLUDES} ${TEST_INCLUDES})
 if(DEFINED ENV{TEST_FILE})
   set(TEST_DIR $ENV{TEST_FILE})
 endif()
 
 execute_process(
-  COMMAND ${BUSTED_PRG} -o ${BUSTED_OUTPUT_TYPE} --pattern=.moon ${TEST_DIR}
+  COMMAND ${BUSTED_PRG} -o ${BUSTED_OUTPUT_TYPE} --lpath=${BUILD_DIR}/?.lua --pattern=.moon ${TEST_DIR}
   WORKING_DIRECTORY ${WORKING_DIR}
   RESULT_VARIABLE res)
 
