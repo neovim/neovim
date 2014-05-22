e47d735e1cafb13b5affedd0a73adba8aa01514c
oni-link <knil.ino@gmail.com>
2014-05-22 11:20:45 +0200

    vim-patch:7.4.281
    Problem:    When a session file has more than one tabpage and 'showtabline' is
                one the positions may be slightly off.
    Solution:   Set 'showtabline' to two while positioning windows.
    
    https://code.google.com/p/vim/source/detail?r=24c90f1fec859b54cf2b854b98c4c9e614c46061

--- src/nvim/version.c
+++ src/nvim/version.c
@@ -201,6 +201,13 @@ static char *(features[]) = {
 
 static int included_patches[] = {
   // Add new patch number below this line
+  281,
+  //280,
+  //279,
+  //278,
+  //277,
+  //276,
+  //275,
   274,
   //273,
   272,
