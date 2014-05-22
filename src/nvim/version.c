806d3e66de6438961c4b83654c0b61a5799bd491
oni-link <knil.ino@gmail.com>
2014-05-22 10:59:59 +0200

    vim-patch:7.4.280
    Problem:    When using a session file the relative position of the cursor is
                not restored if there is another tab. (Nobuhiro Takasaki)
    Solution:   Update w_wrow before calculating the fraction.
    
    https://code.google.com/p/vim/source/detail?r=daf7e98675cf395e1ef96f8040567affb2782a11

--- src/nvim/version.c
+++ src/nvim/version.c
@@ -201,6 +201,12 @@ static char *(features[]) = {
 
 static int included_patches[] = {
   // Add new patch number below this line
+  280,
+  //279,
+  //278,
+  //277,
+  //276,
+  //275,
   274,
   //273,
   272,
