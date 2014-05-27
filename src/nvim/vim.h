c2ba4e48ef1a9e4208043316dd2a26a6a5b91fad
Brandon Coleman <metrix1978@gmail.com>
2014-05-27 01:32:41 -0500

    move update_screen() flags to screen.h

--- src/nvim/vim.h
+++ src/nvim/vim.h
@@ -133,19 +133,6 @@ typedef uint32_t u8char_T;
 #endif
 
 /*
- * flags for update_screen()
- * The higher the value, the higher the priority
- */
-#define VALID                   10  /* buffer not changed, or changes marked
-                                       with b_mod_* */
-#define INVERTED                20  /* redisplay inverted part that changed */
-#define INVERTED_ALL            25  /* redisplay whole inverted part */
-#define REDRAW_TOP              30  /* display first w_upd_rows screen lines */
-#define SOME_VALID              35  /* like NOT_VALID but may scroll */
-#define NOT_VALID               40  /* buffer needs complete redraw */
-#define CLEAR                   50  /* screen messed up, clear it */
-
-/*
  * Flags for w_valid.
  * These are set when something in a window structure becomes invalid, except
  * when the cursor is moved.  Call check_cursor_moved() before testing one of
