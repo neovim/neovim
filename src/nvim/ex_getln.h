80bfa3821224891b192791a971b2e9f0ed9f7b03
Brandon Coleman <metrix1978@gmail.com>
2014-05-28 08:37:25 -0500

    move WILD_* defines to ex_getln.h

--- src/nvim/ex_getln.h
+++ src/nvim/ex_getln.h
@@ -1,5 +1,26 @@
 #ifndef NVIM_EX_GETLN_H
 #define NVIM_EX_GETLN_H
+
+/* Values for nextwild() and ExpandOne().  See ExpandOne() for meaning. */
+#define WILD_FREE               1
+#define WILD_EXPAND_FREE        2
+#define WILD_EXPAND_KEEP        3
+#define WILD_NEXT               4
+#define WILD_PREV               5
+#define WILD_ALL                6
+#define WILD_LONGEST            7
+#define WILD_ALL_KEEP           8
+
+#define WILD_LIST_NOTFOUND      1
+#define WILD_HOME_REPLACE       2
+#define WILD_USE_NL             4
+#define WILD_NO_BEEP            8
+#define WILD_ADD_SLASH          16
+#define WILD_KEEP_ALL           32
+#define WILD_SILENT             64
+#define WILD_ESCAPE             128
+#define WILD_ICASE              256
+
 /* ex_getln.c */
 char_u *getcmdline(int firstc, long count, int indent);
 char_u *getcmdline_prompt(int firstc, char_u *prompt, int attr,
