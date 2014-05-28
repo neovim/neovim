79f98bd429952c471257d14f49acb4502a65a987
Brandon Coleman <metrix1978@gmail.com>
2014-05-28 07:45:43 -0500

    move HL_* variables to syntax.h

--- src/nvim/syntax.h
+++ src/nvim/syntax.h
@@ -5,6 +5,19 @@
 
 typedef int guicolor_T;
 
+/*
+ * Terminal highlighting attribute bits.
+ * Attributes above HL_ALL are used for syntax highlighting.
+ */
+#define HL_NORMAL               0x00
+#define HL_INVERSE              0x01
+#define HL_BOLD                 0x02
+#define HL_ITALIC               0x04
+#define HL_UNDERLINE            0x08
+#define HL_UNDERCURL            0x10
+#define HL_STANDOUT             0x20
+#define HL_ALL                  0x3f
+
 #ifdef INCLUDE_GENERATED_DECLARATIONS
 # include "syntax.h.generated.h"
 #endif
