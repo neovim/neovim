6d3ce133b0e6e2cfb95008912822c6e67ec97fbf
Rui Abreu Ferreira <raf-ep@gmx.com>
2014-06-08 19:57:18 +0100

    Use putenv for os_setenv
    - In systems where setenv is not available, os_setenv() can use
      putenv

--- src/nvim/os/env.c
+++ src/nvim/os/env.c
@@ -34,7 +34,22 @@ const char *os_getenv(const char *name)
 
 int os_setenv(const char *name, const char *value, int overwrite)
 {
+#ifdef HAVE_SETENV
   return setenv(name, value, overwrite);
+#else
+  // Use putenv, instead of setenv - this will leak memory
+  // but in some systems only putenv is available
+  if (!overwrite && os_getenv(name) != NULL) {
+    return 0;
+  }
+
+  size_t len = strlen(name) + 1 + strlen(value) + 1;
+  char *env = xmalloc(len);
+  xstrlcpy(env, name, len);
+  STRCAT(env, "=");
+  STRCAT(env, value);
+  return putenv(env);
+#endif
 }
 
 /// Unset environment variable
