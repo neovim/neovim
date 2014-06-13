95822abdb06b4466aaa9a5481a65d3b3ddf28005
Will Stamper <epmatsw@gmail.com>
2014-06-12 20:47:05 -0500

    vim-patch:7.4.295
    Problem:    Various typos, bad white space and unclear comments.
    Solution:   Fix typos.  Improve white space. Update comments.
    
    https://code.google.com/p/vim/source/detail?r=662ae48e7e246a63d38c9f3165b15b62252edaee

--- src/nvim/macros.h
+++ src/nvim/macros.h
@@ -135,7 +135,7 @@
 /* Advance multi-byte pointer, do not skip over composing chars. */
 # define mb_cptr_adv(p)     p += \
   enc_utf8 ? utf_ptr2len(p) : has_mbyte ? (*mb_ptr2len)(p) : 1
-/* Backup multi-byte pointer. */
+/* Backup multi-byte pointer. Only use with "p" > "s" ! */
 # define mb_ptr_back(s, p)  p -= has_mbyte ? ((*mb_head_off)(s, p - 1) + 1) : 1
 /* get length of multi-byte char, not including composing chars */
 # define mb_cptr2len(p)     (enc_utf8 ? utf_ptr2len(p) : (*mb_ptr2len)(p))
