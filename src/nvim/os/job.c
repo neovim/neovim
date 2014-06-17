fe89ebb12339a53d10ebebcdf47602533075a72f
Thiago de Arruda <tpadilha84@gmail.com>
2014-06-17 10:01:44 -0300

    wstream: Refactor wstream_new_buffer/wstream_write
    - Removed 'copy' parameter from `wstream_new_buffer`. Callers simply pass a
      copy of the buffer if required.
    - Added a callback parameter, which is used to notify callers when the data is
      successfully written. The callback is also used to free the buffer(if
      required) and is compatible with `free` from the standard library.

--- src/nvim/os/job.c
+++ src/nvim/os/job.c
@@ -258,7 +258,7 @@ bool job_write(int id, char *data, uint32_t len)
     return false;
   }
 
-  if (!wstream_write(job->in, wstream_new_buffer(data, len, false))) {
+  if (!wstream_write(job->in, wstream_new_buffer(data, len, free))) {
     job_stop(job->id);
     return false;
   }
