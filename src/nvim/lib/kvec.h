caf2fb84808add8edf204c8a46abedd5aa26ab29
Thiago de Arruda <tpadilha84@gmail.com>
2014-06-18 11:31:47 -0300

    api: Enable usage of Arrays and Dictionaries as [k]vectors
    - Rename a/n/m to items/size/capactity in kvec.h
    - Add capactity field to Arrays/Dictionaries

--- src/nvim/lib/kvec.h
+++ src/nvim/lib/kvec.h
@@ -53,39 +53,39 @@ int main() {
 
 #define kv_roundup32(x) (--(x), (x)|=(x)>>1, (x)|=(x)>>2, (x)|=(x)>>4, (x)|=(x)>>8, (x)|=(x)>>16, ++(x))
 
-#define kvec_t(type) struct { size_t n, m; type *a; }
-#define kv_init(v) ((v).n = (v).m = 0, (v).a = 0)
-#define kv_destroy(v) free((v).a)
-#define kv_A(v, i) ((v).a[(i)])
-#define kv_pop(v) ((v).a[--(v).n])
-#define kv_size(v) ((v).n)
-#define kv_max(v) ((v).m)
+#define kvec_t(type) struct { size_t size, capacity; type *items; }
+#define kv_init(v) ((v).size = (v).capacity = 0, (v).items = 0)
+#define kv_destroy(v) free((v).items)
+#define kv_A(v, i) ((v).items[(i)])
+#define kv_pop(v) ((v).items[--(v).size])
+#define kv_size(v) ((v).size)
+#define kv_max(v) ((v).capacity)
 
-#define kv_resize(type, v, s)  ((v).m = (s), (v).a = (type*)xrealloc((v).a, sizeof(type) * (v).m))
+#define kv_resize(type, v, s)  ((v).capacity = (s), (v).items = (type*)xrealloc((v).items, sizeof(type) * (v).capacity))
 
 #define kv_copy(type, v1, v0) do {							\
-		if ((v1).m < (v0).n) kv_resize(type, v1, (v0).n);	\
-		(v1).n = (v0).n;									\
-		memcpy((v1).a, (v0).a, sizeof(type) * (v0).n);		\
+		if ((v1).capacity < (v0).size) kv_resize(type, v1, (v0).size);	\
+		(v1).size = (v0).size;									\
+		memcpy((v1).items, (v0).items, sizeof(type) * (v0).size);		\
 	} while (0)												\
 
 #define kv_push(type, v, x) do {									\
-		if ((v).n == (v).m) {										\
-			(v).m = (v).m? (v).m<<1 : 2;							\
-			(v).a = (type*)xrealloc((v).a, sizeof(type) * (v).m);	\
+		if ((v).size == (v).capacity) {										\
+			(v).capacity = (v).capacity? (v).capacity<<1 : 8;							\
+			(v).items = (type*)xrealloc((v).items, sizeof(type) * (v).capacity);	\
 		}															\
-		(v).a[(v).n++] = (x);										\
+		(v).items[(v).size++] = (x);										\
 	} while (0)
 
-#define kv_pushp(type, v) (((v).n == (v).m)?							\
-						   ((v).m = ((v).m? (v).m<<1 : 2),				\
-							(v).a = (type*)xrealloc((v).a, sizeof(type) * (v).m), 0)	\
-						   : 0), ((v).a + ((v).n++))
+#define kv_pushp(type, v) (((v).size == (v).capacity)?							\
+						   ((v).capacity = ((v).capacity? (v).capacity<<1 : 8),				\
+							(v).items = (type*)xrealloc((v).items, sizeof(type) * (v).capacity), 0)	\
+						   : 0), ((v).items + ((v).size++))
 
-#define kv_a(type, v, i) (((v).m <= (size_t)(i)? \
-						  ((v).m = (v).n = (i) + 1, kv_roundup32((v).m), \
-						   (v).a = (type*)xrealloc((v).a, sizeof(type) * (v).m), 0) \
-						  : (v).n <= (size_t)(i)? (v).n = (i) + 1 \
-						  : 0), (v).a[(i)])
+#define kv_a(type, v, i) (((v).capacity <= (size_t)(i)? \
+						  ((v).capacity = (v).size = (i) + 1, kv_roundup32((v).capacity), \
+						   (v).items = (type*)xrealloc((v).items, sizeof(type) * (v).capacity), 0) \
+						  : (v).size <= (size_t)(i)? (v).size = (i) + 1 \
+						  : 0), (v).items[(i)])
 
 #endif
