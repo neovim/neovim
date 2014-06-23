neovim/neovim

commit 94a6f16cf121b29be8bf8a881a6465bbb07c5f73

api:buffer Refactor range checks in `buffer_set_slice`
* Get rid of the old range checks for variables and move the new checks to the
  beginning of the function.

src/nvim/api/buffer.c
@@ -166,8 +166,19 @@ void buffer_set_slice(Buffer buffer,
   buf_T *save_curbuf = NULL;
   win_T *save_curwin = NULL;
   tabpage_T *save_curtab = NULL;
+
   size_t new_len = replacement.size;
+  if (!new_len <= SIZE_MAX) {
+    set_api_error("Too many lines to add", err);
+    return;
+  }
+
   size_t old_len = (size_t)(end - start);
+  if (!buf->b_ml.ml_line_count + (new_len - old_len) <= LONG_MAX) {
+    set_api_error("Exceding maximum line number", err);
+    return;
+  }
+
   ssize_t extra = 0;  // lines added to text, can be negative
   char **lines = (new_len != 0) ? xmalloc(new_len * sizeof(char *)) : NULL;
 
@@ -204,12 +215,6 @@ void buffer_set_slice(Buffer buffer,
   // less memory allocation and freeing.
   size_t to_replace = old_len < new_len ? old_len : new_len;
 
-  if ((int64_t)to_replace > LONG_MAX - start) {
-    to_replace = LONG_MAX - start;
-    set_api_error("Index value is too high", err);
-    goto end;
-  }
-
   for (size_t i = 0; i < to_replace; i++) {
     int64_t lnum = start + (int64_t)i;
 
@@ -220,11 +225,6 @@ void buffer_set_slice(Buffer buffer,
     // Mark lines that haven't been passed to the buffer as they need
     // to be freed later
     lines[i] = NULL;
-  }
-
-  if ((int64_t)to_replace > LONG_MAX - start + 1) {
-    set_api_error("Index value is too high", err);
-    goto end;
   }
 
   // Now we may need to insert the remaining new old_len

commit 89088fe37d9a21c1fd7c6c372e3f91b8acaf8b2c

api: Fix operator precedence
Problem:
  - commit 94a6f16cf121b29be8bf8a881a6465bbb07c5f73 introduced an error
    regarding operator precedence in two if clauses resulting in broken
    program logic.

Solution:
  - Use parantheses to tell the compiler the desired precedence.

src/nvim/api/buffer.c
@@ -168,13 +168,13 @@ void buffer_set_slice(Buffer buffer,
   tabpage_T *save_curtab = NULL;
 
   size_t new_len = replacement.size;
-  if (!new_len <= SIZE_MAX) {
+  if (!(new_len <= SIZE_MAX)) {
     set_api_error("Too many lines to add", err);
     return;
   }
 
   size_t old_len = (size_t)(end - start);
-  if (!buf->b_ml.ml_line_count + (new_len - old_len) <= LONG_MAX) {
+  if (!(buf->b_ml.ml_line_count + (new_len - old_len)) <= LONG_MAX) {
     set_api_error("Exceding maximum line number", err);
     return;
   }