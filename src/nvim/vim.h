6d306b38d00db1dfe856857b0d177d37aa649248
Rui Abreu Ferreira <raf-ep@gmx.com>
2014-06-25 21:03:09 +0100

    Remove include for sys/select.h in vim.h
    - There is no need to include sys/select.h anymore
    - Removed HAVE_SYS_SELECT_H from config.h.in

--- src/nvim/vim.h
+++ src/nvim/vim.h
@@ -109,11 +109,6 @@ typedef uint32_t u8char_T;
 #include <wctype.h>
 #include <stdarg.h>
 
-#if defined(HAVE_SYS_SELECT_H) && \
-  (!defined(HAVE_SYS_TIME_H) || defined(SYS_SELECT_WITH_SYS_TIME))
-# include <sys/select.h>
-#endif
-
 /* ================ end of the header file puzzle =============== */
 
 #ifdef HAVE_WORKING_LIBINTL
