91bbc563ba92dadac97ef5dd5de7b40e48884730
Xu Cheng <xucheng@me.com>
2014-07-08 20:53:31 +0800

    neovim.rb: modernize the style
    https://github.com/styleguide/ruby

--- neovim.rb
+++ neovim.rb
@@ -1,13 +1,13 @@
-require 'formula'
+require "formula"
 
 class Neovim < Formula
-  homepage 'http://neovim.org'
-  head 'https://github.com/neovim/neovim.git'
+  homepage "http://neovim.org"
+  head "https://github.com/neovim/neovim.git"
 
-  depends_on 'md5sha1sum'
-  depends_on 'cmake'
-  depends_on 'libtool'
-  depends_on 'automake'
+  depends_on "md5sha1sum"
+  depends_on "cmake"
+  depends_on "libtool"
+  depends_on "automake"
 
   def install
     ENV.deparallelize
