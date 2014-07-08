ccd3f9ce67543c45f341b05adf8f85e42bc0fdce
Xu Cheng <xucheng@me.com>
2014-07-08 20:55:07 +0800

    neovim.rb: improve dependency
    1. remove unnecessary deps `md5sha1sum`
    2. add `=> :build`
    3. add missing deps `autoconf`

--- neovim.rb
+++ neovim.rb
@@ -4,10 +4,10 @@ class Neovim < Formula
   homepage "http://neovim.org"
   head "https://github.com/neovim/neovim.git"
 
-  depends_on "md5sha1sum"
-  depends_on "cmake"
-  depends_on "libtool"
-  depends_on "automake"
+  depends_on "cmake" => :build
+  depends_on "libtool" => :build
+  depends_on "automake" => :build
+  depends_on "autoconf" => :build
 
   def install
     ENV.deparallelize
