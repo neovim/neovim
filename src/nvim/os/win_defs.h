0fdfef99c320f1deada1ec2d0a31163c343aad5d
Rui Abreu Ferreira <raf-ep@gmx.com>
2014-07-09 17:24:12 +0100

    Add includes to win_defs.h
    - Add includes for stdio.h, winsock2.h
      and time.h

--- src/nvim/os/win_defs.h
+++ src/nvim/os/win_defs.h
@@ -1,8 +1,13 @@
 #ifndef NVIM_OS_WIN_DEFS_H
 #define NVIM_OS_WIN_DEFS_H
 
+// winsock2.h must be before windows.h - or so says Mingw
+#include <winsock2.h>
 #include <windows.h>
 #include <uv.h>
+
+#include <stdio.h>
+#include <time.h>
 
 #define TEMP_DIR_NAMES {"$TMP", "$TEMP", "$USERPROFILE", ""}
 #define TEMP_FILE_PATH_MAXLEN _MAX_PATH
