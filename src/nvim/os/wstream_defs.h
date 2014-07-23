06cc046e30936cc78fb4c3ca03b7e4333bf397f8
Nicolas Hillegeer <nicolas@hillegeer.com>
2014-07-23 14:20:36 +0200

    wstream: write completion callback
    Now modules using the wstream can find out what's happening to their writes.

--- src/nvim/os/wstream_defs.h
+++ src/nvim/os/wstream_defs.h
@@ -5,5 +5,17 @@ typedef struct wbuffer WBuffer;
 typedef struct wstream WStream;
 typedef void (*wbuffer_data_finalizer)(void *data);
 
+/// Type of function called when the WStream has information about a write
+/// request.
+///
+/// @param wstream The `WStream` instance
+/// @param data User-defined data
+/// @param pending The number of write requests that are still pending
+/// @param status 0 on success, anything else indicates failure
+typedef void (*wstream_cb)(WStream *wstream,
+                           void *data,
+                           size_t pending,
+                           int status);
+
 #endif  // NVIM_OS_WSTREAM_DEFS_H
 
