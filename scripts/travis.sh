543b39ffc8483174a13ea6cccf954fb2c7685d44
Florian Walch <florian@fwalch.com>
2014-08-06 11:21:02 +0200

    travis.sh: Update sources before installing packages.

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -166,12 +166,13 @@ elif [ "$TRAVIS_BUILD_TYPE" = "clint" ]; then
     ./scripts/clint.sh
 elif [ "$TRAVIS_BUILD_TYPE" = "api/python" ]; then
     set_environment /opt/neovim-deps
-    $MAKE_CMD
+    sudo apt-get update
     sudo apt-get install expect valgrind
+    $MAKE_CMD
     git clone --depth=1 -b master git://github.com/neovim/python-client
     cd python-client
-  sudo pip install .
-  sudo pip install nose
+    sudo pip install .
+    sudo pip install nose
     test_cmd="nosetests --verbosity=2"
     nvim_cmd="valgrind -q --track-origins=yes --leak-check=yes --suppressions=$suppressions --log-file=$tmpdir/valgrind-%p.log ../build/bin/nvim -u NONE"
     if ! ../scripts/run-api-tests.exp "$test_cmd" "$nvim_cmd"; then
