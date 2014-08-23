bbaf699b6b13ae7e8de2fc147a50de6a00a99a30
Florian Walch <florian@fwalch.com>
2014-08-23 22:29:28 +0200

    CMake: Use DESTDIR for helptags generation.

--- cmake/GenerateHelptags.cmake
+++ cmake/GenerateHelptags.cmake
@@ -1,4 +1,8 @@
-message(STATUS "Generating helptags.")
+file(TO_CMAKE_PATH
+  "$ENV{DESTDIR}/${CMAKE_INSTALL_PREFIX}/share/nvim/runtime/doc"
+  HELPTAGS_WORKING_DIRECTORY)
+
+message(STATUS "Generating helptags in ${HELPTAGS_WORKING_DIRECTORY}.")
 
 execute_process(
   COMMAND "${CMAKE_CURRENT_BINARY_DIR}/bin/nvim"
@@ -6,7 +10,8 @@ execute_process(
     -esX
     -c "helptags ++t ."
     -c quit
-  WORKING_DIRECTORY "${CMAKE_INSTALL_PREFIX}/share/nvim/runtime/doc"
+  WORKING_DIRECTORY "${HELPTAGS_WORKING_DIRECTORY}"
+  OUTPUT_VARIABLE err
   ERROR_VARIABLE err
   RESULT_VARIABLE res)
 
