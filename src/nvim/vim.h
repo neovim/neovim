4a88d4d6c1361090485095c69f9174657af158b6
Scott Prager <splinterofchaos@gmail.com>
2014-08-25 22:47:39 -0400

    Macroize xstpncpy.

--- src/nvim/vim.h
+++ src/nvim/vim.h
@@ -263,6 +263,7 @@ enum {
 #define STRNCPY(d, s, n)    strncpy((char *)(d), (char *)(s), (size_t)(n))
 #define STRLCPY(d, s, n)    xstrlcpy((char *)(d), (char *)(s), (size_t)(n))
 #define STPCPY(d, s)        xstpcpy((char *)(d), (char *)(s))
+#define STPNCPY(d, s, l)    xstpncpy((char *)(d), (char *)(s), (size_t)(l))
 #define STRCMP(d, s)        strcmp((char *)(d), (char *)(s))
 #define STRNCMP(d, s, n)    strncmp((char *)(d), (char *)(s), (size_t)(n))
 #ifdef HAVE_STRCASECMP
