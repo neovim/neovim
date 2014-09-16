f5320f645b325546f13289a708ba2efce3204c87
Shougo Matsushita <Shougo.Matsu@gmail.com>
2014-09-16 19:23:11 +0900

    vim-patch:7.4.368
    Problem:    Restoring the window sizes after closing the command line window
                doesn't work properly if there are nested splits.
    Solution:   Restore the sizes twice. (Hirohito Higashi)
    
    https://code.google.com/p/vim/source/detail?r=v7-4-368

--- src/nvim/window.c
+++ src/nvim/window.c
@@ -3800,10 +3800,15 @@ void win_size_save(garray_T *gap)
 void win_size_restore(garray_T *gap)
 {
   if (win_count() * 2 == gap->ga_len) {
-    int i = 0;
-    FOR_ALL_WINDOWS(wp) {
-      frame_setwidth(wp->w_frame, ((int *)gap->ga_data)[i++]);
-      win_setheight_win(((int *)gap->ga_data)[i++], wp);
+    /* The order matters, because frames contain other frames, but it's
+     * difficult to get right. The easy way out is to do it twice. */
+    for (int j = 0; j < 2; ++j)
+    {
+      int i = 0;
+      FOR_ALL_WINDOWS(wp) {
+        frame_setwidth(wp->w_frame, ((int *)gap->ga_data)[i++]);
+        win_setheight_win(((int *)gap->ga_data)[i++], wp);
+      }
     }
     /* recompute the window positions */
     (void)win_comp_pos();
