1fe6dc03aa924110cca27fd993c584ed1b3c7c83
Shougo Matsushita <Shougo.Matsu@gmail.com>
2014-09-18 08:05:08 +0900

    vim-patch:7.4.374
    Problem:    Character after "fb" command not mapped if it might be a composing
                character.
    Solution:   Don't disable mapping when looking for a composing character.
                (Jacob Niehus)
    
    https://code.google.com/p/vim/source/detail?r=v7-4-374

--- src/nvim/normal.c
+++ src/nvim/normal.c
@@ -845,7 +845,10 @@ getcount:
 
       /* When getting a text character and the next character is a
        * multi-byte character, it could be a composing character.
-       * However, don't wait for it to arrive. */
+       * However, don't wait for it to arrive. Also, do enable mapping,
+       * because if it's put back with vungetc() it's too late to apply
+       * mapping. */
+      no_mapping--;
       while (enc_utf8 && lang && (c = vpeekc()) > 0
              && (c >= 0x100 || MB_BYTE2LEN(vpeekc()) > 1)) {
         c = plain_vgetc();
@@ -857,6 +860,7 @@ getcount:
         else
           ca.ncharC2 = c;
       }
+      no_mapping++;
     }
     --no_mapping;
     --allow_keys;
