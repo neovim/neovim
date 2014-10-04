b80d8039670ff3405a7f5ea9b6b4444f5c337d18
Justin M. Keyes <justinkz@gmail.com>
2014-10-04 18:08:11 +0000

    version: remove "Included patches" message
    It is meaningless to Nvim.

--- src/nvim/version.c
+++ src/nvim/version.c
@@ -744,48 +744,17 @@ static void list_features(void)
 
 void list_version(void)
 {
-  int i;
-  int first;
-  char *s = "";
-
   // When adding features here, don't forget to update the list of
   // internal variables in eval.c!
   MSG(longVersion);
 
-  // Print the list of patch numbers if there is at least one.
-  // Print a range when patches are consecutive: "1-10, 12, 15-40, 42-45"
-  if (included_patches[0] != 0) {
-    MSG_PUTS(_("\nIncluded patches: "));
-    first = -1;
-
-    // find last one
-    for (i = 0; included_patches[i] != 0; ++i) {}
-
-    while (--i >= 0) {
-      if (first < 0) {
-        first = included_patches[i];
-      }
-
-      if ((i == 0) || (included_patches[i - 1] != included_patches[i] + 1)) {
-        MSG_PUTS(s);
-        s = ", ";
-        msg_outnum((long)first);
-
-        if (first != included_patches[i]) {
-          MSG_PUTS("-");
-          msg_outnum((long)included_patches[i]);
-        }
-        first = -1;
-      }
-    }
-  }
-
   // Print the list of extra patch descriptions if there is at least one.
+  char *s = "";
   if (extra_patches[0] != NULL) {
     MSG_PUTS(_("\nExtra patches: "));
     s = "";
 
-    for (i = 0; extra_patches[i] != NULL; ++i) {
+    for (int i = 0; extra_patches[i] != NULL; ++i) {
       MSG_PUTS(s);
       s = ", ";
       MSG_PUTS(extra_patches[i]);
@@ -809,8 +778,6 @@ void list_version(void)
   }
 #endif  // ifdef HAVE_PATHDEF
 
-  MSG_PUTS(_("\nHuge version "));
-  MSG_PUTS(_("without GUI."));
   version_msg(_("  Features included (+) or not (-):\n"));
 
   list_features();
