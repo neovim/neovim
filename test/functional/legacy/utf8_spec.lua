79cd4a98ecc100694e2e35df7ec05971bd720830
Rainer Borene <rainerborene@gmail.com>
2014-10-22 20:50:54 -0200

    legacy tests: migrate test_utf8

---
+++ test/functional/legacy/utf8_spec.lua
@@ -0,0 +1,31 @@
+-- Tests for Unicode manipulations
+
+local helpers = require('test.functional.helpers')
+local clear, feed, insert = helpers.clear, helpers.feed, helpers.insert
+local execute, expect = helpers.execute, helpers.expect
+
+describe('utf8', function()
+  setup(clear)
+
+  it('is working', function()
+    insert('start:')
+
+    execute('set encoding=utf-8')
+    execute('new')
+    execute('call setline(1, ["aaa", "あああ", "bbb"])')
+
+    -- Visual block Insert adjusts for multi-byte char
+    feed('gg0l<C-V>jjIx<Esc>')
+
+    execute('let r = getline(1, "$")')
+    execute('bwipeout!')
+    execute('$put=r')
+    execute('call garbagecollect(1)')
+
+    expect([[
+      start:
+      axaa
+      xあああ
+      bxbb]])
+  end)
+end)
