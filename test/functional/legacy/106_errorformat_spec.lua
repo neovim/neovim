b40b88689c033e3881a946e74aafdfd6d2e2c49c
Rainer Borene <rainerborene@gmail.com>
2014-10-22 19:53:31 -0200

    legacy tests: migrate test106

---
+++ test/functional/legacy/106_errorformat_spec.lua
@@ -0,0 +1,25 @@
+-- Tests for errorformat.
+
+local helpers = require('test.functional.helpers')
+local clear, feed, insert = helpers.clear, helpers.feed, helpers.insert
+local execute, expect = helpers.execute, helpers.expect
+
+describe('errorformat', function()
+  setup(clear)
+
+  it('is working', function()
+    execute("set efm=%EEEE%m,%WWWW%m,%+CCCC%.%#,%-GGGG%.%#")
+    execute("cgetexpr ['WWWW', 'EEEE', 'CCCC']")
+    execute("$put =strtrans(string(map(getqflist(), '[v:val.text, v:val.valid]')))")
+    execute("cgetexpr ['WWWW', 'GGGG', 'EEEE', 'CCCC']")
+    execute("$put =strtrans(string(map(getqflist(), '[v:val.text, v:val.valid]')))")
+    execute("cgetexpr ['WWWW', 'GGGG', 'ZZZZ', 'EEEE', 'CCCC', 'YYYY']")
+    execute("$put =strtrans(string(map(getqflist(), '[v:val.text, v:val.valid]')))")
+    
+    expect([=[
+      
+      [['W', 1], ['E^@CCCC', 1]]
+      [['W', 1], ['E^@CCCC', 1]]
+      [['W', 1], ['ZZZZ', 0], ['E^@CCCC', 1], ['YYYY', 0]]]=])
+  end)
+end)
