b67ad4f67f3012c2f0165fc0f2874614ab2b796a
Rainer Borene <rainerborene@gmail.com>
2014-10-22 22:35:55 -0200

    legacy tests: migrate test_changelist

---
+++ test/functional/legacy/changelist_spec.lua
@@ -0,0 +1,26 @@
+-- Test changelist position after splitting window
+-- Set 'undolevels' to make changelist for sourced file
+
+local helpers = require('test.functional.helpers')
+local clear, feed, insert = helpers.clear, helpers.feed, helpers.insert
+local execute, expect = helpers.execute, helpers.expect
+
+describe('changelist', function()
+  setup(clear)
+
+  it('is working', function()
+    insert("1\n2")
+
+    feed('Gkylp')
+    execute('set ul=100')
+
+    feed('Gylp')
+    execute('set ul=100')
+
+    feed('gg')
+    execute('vsplit')
+    execute('try', 'normal g;', 'normal ggVGcpass', 'catch', 'normal ggVGcfail', 'endtry')
+
+    expect('pass')
+  end)
+end)
