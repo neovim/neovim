9e41dd07e4f25b7c02975e47fdf0dac65bc08e41
Thiago de Arruda <tpadilha84@gmail.com>
2014-10-29 23:26:30 -0300

    travis: Make sure valgrind report is shown on test failures

--- .ci/gcc.sh
+++ .ci/gcc.sh
@@ -11,8 +11,16 @@ export VALGRIND_LOG="$tmpdir/valgrind-%p.log"
 CMAKE_EXTRA_FLAGS="-DTRAVIS_CI_BUILD=ON -DUSE_GCOV=ON"
 
 $MAKE_CMD CMAKE_EXTRA_FLAGS="${CMAKE_EXTRA_FLAGS}" unittest
-$MAKE_CMD test
+if ! $MAKE_CMD test; then
+	valgrind_check "$tmpdir"
+	exit 1
+fi
 valgrind_check "$tmpdir"
-$MAKE_CMD oldtest
+
+if ! $MAKE_CMD oldtest; then
+	valgrind_check "$tmpdir"
+	exit 1
+fi
+valgrind_check "$tmpdir"
 
 coveralls --encoding iso-8859-1 || echo 'coveralls upload failed.'
