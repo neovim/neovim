d3f81424e5a8de5d184f3ab3e14b632fa70f857d
Thiago de Arruda <tpadilha84@gmail.com>
2014-11-07 02:38:12 -0300

    job: Only force-close stdout/stderr when the job exits
    stdout/stderr should only be closed after the job truly exits, or else we can
    lose data sent by it.

--- test/functional/job/job_spec.lua
+++ test/functional/job/job_spec.lua
@@ -15,6 +15,10 @@ describe('jobs', function()
     return "au! JobActivity xxx call rpcnotify("..channel..", "..expr..")"
   end
 
+  local notify_job = function()
+    return "au! JobActivity xxx call rpcnotify("..channel..", 'j', v:job_data)"
+  end
+
   it('returns 0 when it fails to start', function()
     local status, rv = pcall(eval, "jobstart('', '')")
     eq(false, status)
@@ -55,5 +59,15 @@ describe('jobs', function()
 
   it('will not cause a memory leak if we leave a job running', function()
     nvim('command', "call jobstart('xxx', 'cat', ['-'])")
+  end)
+
+  it('will only emit the "exit" event after "stdout" and "stderr"', function()
+    nvim('command', notify_job())
+    nvim('command', "let j = jobstart('xxx', 'cat', ['-'])")
+    local jobid = nvim('eval', 'j')
+    nvim('eval', 'jobsend(j, "abc\ndef")')
+    nvim('eval', 'jobstop(j)')
+    eq({'notification', 'j', {{jobid, 'stdout', 'abc\ndef'}}}, next_message())
+    eq({'notification', 'j', {{jobid, 'exit'}}}, next_message())
   end)
 end)
