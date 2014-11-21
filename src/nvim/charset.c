c82eb31a5d22353b3a31e247448a44b19a590c05
Felipe Morales <hel.sheep@gmail.com>
2014-11-21 13:21:35 -0300

    vim-patch:7.4.473
    vim-patch:7.4.473
    
    Problem:    Cursor movement is incorrect when there is a number
                column/sign/fold column and 'sbr' is displayed.
    Solution:   Adjust the column for 'sbr'. (Christian Brabandt)
    
    https://code.google.com/p/vim/source/detail?r=v7-4-473

--- src/nvim/charset.c
+++ src/nvim/charset.c
@@ -1058,6 +1058,8 @@ int win_lbr_chartabsize(win_T *wp, char_u *line, char_u *s, colnr_T col, int *he
     if (col >= (colnr_T)wp->w_width) {
       col -= wp->w_width;
       numberextra = wp->w_width - (numberextra - win_col_off2(wp));
+      if (*p_sbr != NUL && col >= (colnr_T)STRLEN(p_sbr))
+        col -= (colnr_T)STRLEN(p_sbr);
       if (numberextra > 0) {
         col = col % numberextra;
       }
