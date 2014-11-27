987ee79bb7ea4a73988f3878a1ff59ac0e97782f
Florian Walch <florian@fwalch.com>
2014-11-27 15:10:36 +0100

    Travis: Prepare for OS X builds.
     * Use bot-ci script at to set up prebuilt dependencies.
     * Make CI scripts more consistent.

--- .ci/common.sh
+++ .ci/common.sh
@@ -35,43 +35,11 @@ check_core_dumps() {
 	done
 }
 
-set_environment() {
-	local prefix="$1/usr"
-	eval $($prefix/bin/luarocks path)
-	export PATH="$prefix/bin:$PATH"
-	export PKG_CONFIG_PATH="$prefix/lib/pkgconfig"
-	export USE_BUNDLED_DEPS=OFF
-}
-
-
-install_prebuilt_deps() {
-	# install prebuilt dependencies
-	if [ ! -d /opt/neovim-deps ]; then
-		cd /opt
-		sudo git clone --depth=1 git://github.com/neovim/deps neovim-deps
-		cd -
-	fi
+setup_prebuilt_deps() {
+	eval "$(curl -Ss https://raw.githubusercontent.com/neovim/bot-ci/master/scripts/travis-setup.sh) deps-${1}"
 }
 
 tmpdir="$(pwd)/tmp"
 rm -rf "$tmpdir"
 mkdir -p "$tmpdir"
 suppressions="$(pwd)/.valgrind.supp"
-
-# Travis reports back that it has 32-cores via /proc/cpuinfo, but it's not
-# what we really have available.  According to their documentation, it only has
-# 1.5 virtual cores.
-# See:
-#   http://docs.travis-ci.com/user/speeding-up-the-build/#Paralellizing-your-build-on-one-VM
-# for more information.
-MAKE_CMD="make -j2"
-
-install_prebuilt_deps
-
-# Pins the version of the java package installed on the Travis VMs
-# and avoids a lengthy upgrade process for them.
-sudo apt-mark hold oracle-java7-installer oracle-java8-installer
-
-sudo apt-get update
-
-export CFLAGS='-DMIN_LOG_LEVEL=0'  # force verification of DLOG macros
