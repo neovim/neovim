00d7ca107e25b1b06b528619847e216f64ef144c
Rui Abreu Ferreira <raf-ep@gmx.com>
2014-12-13 01:49:27 +0000

    Initialise event queues before calling termcapinit()
    - Move queue initialization out of event_init() into
      new function event_early_init()
    - event_early_init() must be called before termcapinit(), and
      event_init() can only be called afterwards
    - Add unit tests to check termcapinit with valid and invalid
      term types

--- src/nvim/main.c
+++ src/nvim/main.c
@@ -266,6 +266,8 @@ int main(int argc, char **argv)
   if (GARGCOUNT > 1 && !silent_mode)
     printf(_("%d files to edit\n"), GARGCOUNT);
 
+  event_early_init();
+
   if (params.want_full_screen && !silent_mode) {
     if (embedded_mode) {
       // In embedded mode don't do terminal-related initializations, assume an
