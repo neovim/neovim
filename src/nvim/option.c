099fdb49a700f851f0165089fc2ca06807591f6a
Florian Walch <florian@fwalch.com>
2015-01-04 21:47:59 +0100

    Change 'history' default to 50.
    Default as of Vim 7.4.336 (not yet ported). Runtime files already
    mention this new default value.

--- src/nvim/option.c
+++ src/nvim/option.c
@@ -887,7 +887,7 @@ static struct vimoption
    SCRIPTID_INIT},
   {"history",     "hi",   P_NUM|P_VIM,
    (char_u *)&p_hi, PV_NONE,
-   {(char_u *)0L, (char_u *)20L} SCRIPTID_INIT},
+   {(char_u *)0L, (char_u *)50L} SCRIPTID_INIT},
   {"hkmap",       "hk",   P_BOOL|P_VI_DEF|P_VIM,
    (char_u *)&p_hkmap, PV_NONE,
    {(char_u *)FALSE, (char_u *)0L} SCRIPTID_INIT},
