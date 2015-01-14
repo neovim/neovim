dad1e39edf7f704dba40b182c7726ef4bc34c502
Eliseo Martínez <eliseomarmol@gmail.com>
2015-01-14 21:47:37 +0100

    Remove long_u: Remove type.

--- src/nvim/types.h
+++ src/nvim/types.h
@@ -13,11 +13,6 @@
 // dummy to pass an ACL to a function
 typedef void *vim_acl_T;
 
-// According to the vanilla Vim docs, long_u needs to be big enough to hold
-// a pointer for the platform. On C99, this is easy to do with the uintptr_t
-// type in lieu of the platform-specific typedefs that existed before.
-typedef uintptr_t long_u;
-
 /*
  * Shorthand for unsigned variables. Many systems, but not all, have u_char
  * already defined, so we use char_u to avoid trouble.
