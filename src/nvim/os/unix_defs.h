032c20078a617252ff9fc5ac03b4c390a6868a4d
Michael Reed <m.reed@mykolab.com>
2015-02-17 01:18:22 -0500

    os_unix_defs && os/unix_defs: Consistently use '~' over '$HOME' #2009
    these path names are ridiculous...
    
    Based on #889, but also remove some unused #defines

--- src/nvim/os/unix_defs.h
+++ src/nvim/os/unix_defs.h
@@ -1,7 +1,7 @@
 #ifndef NVIM_OS_UNIX_DEFS_H
 #define NVIM_OS_UNIX_DEFS_H
 
-#define TEMP_DIR_NAMES {"$TMPDIR", "/tmp", ".", "$HOME"}
+#define TEMP_DIR_NAMES {"$TMPDIR", "/tmp", ".", "~"}
 #define TEMP_FILE_PATH_MAXLEN 256
 
 #endif  // NVIM_OS_UNIX_DEFS_H
