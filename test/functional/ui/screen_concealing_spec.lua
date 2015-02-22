ec9d303a905685cbb7b3d431f24837c8b836ff6a
Clinton McKay <ziesunkownemail@clueless.duh>
2015-02-22 16:00:55 -0700

    Updated screen concealing tests. Found a bug with initial syn-match conceal

--- test/functional/ui/screen_concealing_spec.lua
+++ test/functional/ui/screen_concealing_spec.lua
@@ -4,22 +4,140 @@ local clear, feed, execute = helpers.clear, helpers.feed, helpers.execute
 local insert = helpers.insert
 
 describe('Screen', function()
-  local screen
+  local screen   
   
-  before_each(function()
-    clear()
-    screen = Screen.new()
-    screen:attach()
-    screen:set_default_attr_ignore( {{}, {bold=true, foreground=255}} ) 
-  end)
+  describe("Concealing multiple '&&' With 'syn-match'", function()
+    before_each(function()
+      clear()
+      screen = Screen.new()
+      screen:attach()
+      screen:set_default_attr_ignore( {{}, {bold=true, foreground=255}} ) 
+      insert([[
+        &&
+        &&
+        &&
+        &&
+        &&
+        &&
+        ]])
+      screen:expect([[
+        &&                                                   |
+        &&                                                   |
+        &&                                                   |
+        &&                                                   |
+        &&                                                   |
+        &&                                                   |
+        ^                                                    |
+        ~                                                    |
+        ~                                                    |
+        ~                                                    |
+        ~                                                    |
+        ~                                                    |
+        ~                                                    |
+                                                             |
+      ]])
+      execute("let &conceallevel=1")
+      execute("syn match dAmpersand '[&][&]' conceal cchar=∧")
+    end)
 
-  after_each(function()
-    screen:detach()
-  end)
+    after_each(function()
+      screen:detach()
+    end)
 
-  describe('', function()
-    it('wadup test 1', function()
-
+    -- Begin && concealing tests
+    it('Conceal All &&', function()
+      screen:expect([[
+          {1:∧}                                                    |
+          {1:∧}                                                    |
+          {1:∧}                                                    |
+          {1:∧}                                                    |
+          {1:∧}                                                    |
+          {1:∧}                                                    |
+          ^                                                    |
+          ~                                                    |
+          ~                                                    |
+          ~                                                    |
+          ~                                                    |
+          ~                                                    |
+          ~                                                    |
+          :syn match dAmpersand '[&][&]' conceal cchar=∧       |
+        ]], {[1] = {foreground = Screen.colors.LightGrey, background = Screen.colors.DarkGray}})
+    end)
+    it('Move Cursor Up', function() 
+      feed("k")
+      screen:expect([[
+        {1:∧}                                                    |
+        {1:∧}                                                    |
+        {1:∧}                                                    |
+        {1:∧}                                                    |
+        {1:∧}                                                    |
+        ^&                                                   |
+                                                             |
+        ~                                                    |
+        ~                                                    |
+        ~                                                    |
+        ~                                                    |
+        ~                                                    |
+        ~                                                    |
+        :syn match dAmpersand '[&][&]' conceal cchar=∧       |
+      ]], {[1] = {foreground = Screen.colors.LightGrey, background = Screen.colors.DarkGray}})
+    end)
+    it('Move Cursor to Top', function()
+      feed("gg")
+      screen:expect([[
+        ^&                                                   |
+        {1:∧}                                                    |
+        {1:∧}                                                    |
+        {1:∧}                                                    |
+        {1:∧}                                                    |
+        {1:∧}                                                    |
+                                                             |
+        ~                                                    |
+        ~                                                    |
+        ~                                                    |
+        ~                                                    |
+        ~                                                    |
+        ~                                                    |
+        :syn match dAmpersand '[&][&]' conceal cchar=∧       |
+      ]], {[1] = {foreground = Screen.colors.LightGrey, background = Screen.colors.DarkGray}})
+    end)
+    it('Move Cursor Down', function() 
+      feed("ggj")
+      screen:expect([[
+        {1:∧}                                                    |
+        ^&                                                   |
+        {1:∧}                                                    |
+        {1:∧}                                                    |
+        {1:∧}                                                    |
+        {1:∧}                                                    |
+                                                             |
+        ~                                                    |
+        ~                                                    |
+        ~                                                    |
+        ~                                                    |
+        ~                                                    |
+        ~                                                    |
+        :syn match dAmpersand '[&][&]' conceal cchar=∧       |
+      ]], {[1] = {foreground = Screen.colors.LightGrey, background = Screen.colors.DarkGray}})
+    end)
+    it('Move Cursor to Bottom', function() 
+      feed("ggG")
+      screen:expect([[
+        {1:∧}                                                    |
+        {1:∧}                                                    |
+        {1:∧}                                                    |
+        {1:∧}                                                    |
+        {1:∧}                                                    |
+        {1:∧}                                                    |
+        ^                                                    |
+        ~                                                    |
+        ~                                                    |
+        ~                                                    |
+        ~                                                    |
+        ~                                                    |
+        ~                                                    |
+        :syn match dAmpersand '[&][&]' conceal cchar=∧       |
+      ]], {[1] = {foreground = Screen.colors.LightGrey, background = Screen.colors.DarkGray}})
     end)
   end)
 end)
