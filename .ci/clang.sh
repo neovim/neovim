fc20c0fd264d5e24b2d909724623347ba71fc98e
grtlr <lyzrd17@gmail.com>
2015-04-25 08:49:23 +0200

    Travis: Redo tabs

--- .ci/clang.sh
+++ .ci/clang.sh
@@ -1,20 +1,20 @@
 . "$CI_SCRIPTS/common.sh"
 
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
-    export CC=clang
-    symbolizer=/usr/local/clang-3.4/bin/llvm-symbolizer
-    export ASAN_SYMBOLIZER_PATH=$symbolizer
-    export ASAN_OPTIONS="detect_leaks=1:log_path=$tmpdir/asan"
-    export TSAN_OPTIONS="external_symbolizer_path=$symbolizer:log_path=$tmpdir/tsan"
-    export UBSAN_OPTIONS="log_path=$tmpdir/ubsan" # not sure if this works
-    CMAKE_EXTRA_FLAGS="-DTRAVIS_CI_BUILD=ON \
-        -DUSE_GCOV=ON \
-        -DBUSTED_OUTPUT_TYPE=plainTerminal \
-        -DSANITIZE=ON"
+	export CC=clang
+	symbolizer=/usr/local/clang-3.4/bin/llvm-symbolizer
+	export ASAN_SYMBOLIZER_PATH=$symbolizer
+	export ASAN_OPTIONS="detect_leaks=1:log_path=$tmpdir/asan"
+	export TSAN_OPTIONS="external_symbolizer_path=$symbolizer:log_path=$tmpdir/tsan"
+	export UBSAN_OPTIONS="log_path=$tmpdir/ubsan" # not sure if this works
+	CMAKE_EXTRA_FLAGS="-DTRAVIS_CI_BUILD=ON \
+		-DUSE_GCOV=ON \
+		-DBUSTED_OUTPUT_TYPE=plainTerminal \
+		-DSANITIZE=ON"
 else
-    CMAKE_EXTRA_FLAGS="-DTRAVIS_CI_BUILD=ON \
-        -DUSE_GCOV=ON \
-        -DBUSTED_OUTPUT_TYPE=plainTerminal"
+	CMAKE_EXTRA_FLAGS="-DTRAVIS_CI_BUILD=ON \
+		-DUSE_GCOV=ON \
+		-DBUSTED_OUTPUT_TYPE=plainTerminal"
 fi
 
 # Build and output version info.
@@ -26,16 +26,16 @@ make unittest
 
 # Run functional tests.
 if ! $MAKE_CMD test; then
-    asan_check "$tmpdir"
-    exit 1
+	asan_check "$tmpdir"
+	exit 1
 fi
 asan_check "$tmpdir"
 
 # Run legacy tests.
 if ! $MAKE_CMD oldtest; then
-    reset
-    asan_check "$tmpdir"
-    exit 1
+	reset
+	asan_check "$tmpdir"
+	exit 1
 fi
 asan_check "$tmpdir"
 
