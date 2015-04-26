39dfab4cbf7f8c1aff6c666bbb81db83b8ae2f96
Erik S. V. Jansson <caffeineviking@gmail.com>
2015-04-26 17:35:13 +0200

    Moved ins/del from misc1 to cursor_modify.
    The ins/del functions in misc1 are somewhat related to inserting and
    deleting (modifying) data relative to the cursor position. By moving
    these over to their own file, increased readability and maintainability
    is acheived. Further refactoring is still needed to comply with nvim
    coding standards.

---
+++ src/nvim/cursor_modify.h
@@ -0,0 +1,9 @@
+#ifndef NVIM_CURSOR_MODIFY_H
+#define NVIM_CURSOR_MODIFY_H
+
+#include "nvim/vim.h"
+
+#ifdef INCLUDE_GENERATED_DECLARATIONS
+# include "cursor_modify.h.generated.h"
+#endif
+#endif  // NVIM_CURSOR_MODIFY_H
