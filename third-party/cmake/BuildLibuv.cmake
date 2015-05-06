faafd9b70c87e7af7f7ad9da000b41bbf1875734
Rui Abreu Ferreira <raf-ep@gmx.com>
2015-05-06 01:13:40 +0100

    Build third-party deps in MinGW
    Added recipes to build bundled dependencies in native MinGW
    toolchains - libuv, luajit, luarocks, msgpack

--- third-party/cmake/BuildLibuv.cmake
+++ third-party/cmake/BuildLibuv.cmake
@@ -5,7 +5,7 @@ include(CMakeParseArguments)
 # Failing to pass a command argument will result in no command being run
 function(BuildLibuv)
   cmake_parse_arguments(_libuv
-    ""
+    "BUILD_IN_SOURCE"
     "TARGET"
     "CONFIGURE_COMMAND;BUILD_COMMAND;INSTALL_COMMAND"
     ${ARGN})
@@ -30,6 +30,7 @@ function(BuildLibuv)
       -DTARGET=${_libuv_TARGET}
       -DUSE_EXISTING_SRC_DIR=${USE_EXISTING_SRC_DIR}
       -P ${CMAKE_CURRENT_SOURCE_DIR}/cmake/DownloadAndExtractFile.cmake
+    BUILD_IN_SOURCE ${_libuv_BUILD_IN_SOURCE}
     CONFIGURE_COMMAND "${_libuv_CONFIGURE_COMMAND}"
     BUILD_COMMAND "${_libuv_BUILD_COMMAND}"
     INSTALL_COMMAND "${_libuv_INSTALL_COMMAND}")
@@ -56,6 +57,16 @@ elseif(MINGW AND CMAKE_CROSSCOMPILING)
     CONFIGURE_COMMAND ${UNIX_CFGCMD} --host=${CROSS_TARGET}
     INSTALL_COMMAND ${MAKE_PRG} V=1 install)
 
+elseif(MINGW)
+
+  # Native MinGW
+  BuildLibUv(BUILD_IN_SOURCE
+    BUILD_COMMAND ${CMAKE_MAKE_PROGRAM} -f Makefile.mingw
+    INSTALL_COMMAND ${CMAKE_COMMAND} -E make_directory ${DEPS_INSTALL_DIR}/lib
+      COMMAND ${CMAKE_COMMAND} -E copy ${DEPS_BUILD_DIR}/src/libuv/libuv.a ${DEPS_INSTALL_DIR}/lib
+      COMMAND ${CMAKE_COMMAND} -E make_directory ${DEPS_INSTALL_DIR}/include
+      COMMAND ${CMAKE_COMMAND} -E copy_directory ${DEPS_BUILD_DIR}/src/libuv/include ${DEPS_INSTALL_DIR}/include
+    )
 
 elseif(WIN32 AND MSVC)
 
