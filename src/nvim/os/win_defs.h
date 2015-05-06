2e46765849e504cd961fc98195d5e5dff443f09a
Yamakaky <yamakaky@gmail.com>
2015-05-06 18:30:51 +0200

    Split os_unix_defs.h into os/

--- src/nvim/os/win_defs.h
+++ src/nvim/os/win_defs.h
@@ -6,4 +6,19 @@
 #define TEMP_DIR_NAMES {"$TMP", "$TEMP", "$USERPROFILE", ""}
 #define TEMP_FILE_PATH_MAXLEN _MAX_PATH
 
+// Defines needed to fix the build on Windows:
+// - USR_EXRC_FILE
+// - USR_VIMRC_FILE
+// - VIMINFO_FILE
+// - DFLT_DIR
+// - DFLT_BDIR
+// - DFLT_VDIR
+// - DFLT_RUNTIMEPATH
+// - EXRC_FILE
+// - VIMRC_FILE
+// - SYNTAX_FNAME
+// - DFLT_HELPFILE
+// - SYS_VIMRC_FILE
+// - SPECIAL_WILDCHAR
+
 #endif  // NVIM_OS_WIN_DEFS_H
