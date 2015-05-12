5090d946998d5d407d64d29808fb0821586ddb17
Rui Abreu Ferreira <raf-ep@gmx.com>
2015-05-12 14:59:13 +0100

    Windows: Add has('win32'). #3223
    References #810

--- src/nvim/eval.c
+++ src/nvim/eval.c
@@ -10005,6 +10005,9 @@ static void f_has(typval_T *argvars, typval_T *rettv)
 #ifdef UNIX
     "unix",
 #endif
+#if defined(WIN32)
+    "win32",
+#endif
 #if defined(WIN64) || defined(_WIN64)
     "win64",
 #endif
