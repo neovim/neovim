496715421860c9f401cad4dc1dbf058d9001bdd3
Lucas Hoffmann <l-m-h@web.de>
2015-05-15 19:03:44 +0200

    Convert test12 with scripts/legacy2luatest.pl.

---
+++ test/functional/legacy/012_spec.lua
@@ -0,0 +1,73 @@
+-- Tests for 'directory' option.
+-- - ".", in same dir as file
+-- - "./dir", in directory relative to file
+-- - "dir", in directory relative to current dir
+
+local helpers = require('test.functional.helpers')
+local feed, insert, source = helpers.feed, helpers.insert, helpers.source
+local clear, execute, expect = helpers.clear, helpers.execute, helpers.expect
+
+describe('12', function()
+  setup(clear)
+
+  it('is working', function()
+    insert([=[
+      start of testfile
+      line 2 Abcdefghij
+      line 3 Abcdefghij
+      end of testfile]=])
+
+    execute('so small.vim')
+    execute('set dir=.,~')
+    execute('/start of testfile/,/end of testfile/w! Xtest1')
+    -- Do an ls of the current dir to find the swap file (should not be there).
+    execute('if has("unix")')
+    execute('  !ls .X*.swp >test.out')
+    execute('else')
+    execute('  r !ls X*.swp >test.out')
+    execute('endif')
+    execute('!echo first line >>test.out')
+    execute('e Xtest1')
+    execute('if has("unix")')
+    -- Do an ls of the current dir to find the swap file, remove the leading dot.
+    -- To make the result the same for all systems.
+    execute('  r!ls .X*.swp')
+    execute([[  s/\.*X/X/]])
+    execute('  .w >>test.out')
+    execute('  undo')
+    execute('else')
+    execute('  !ls X*.swp >>test.out')
+    execute('endif')
+    execute('!echo under Xtest1.swp >>test.out')
+    execute('!mkdir Xtest2')
+    execute('set dir=./Xtest2,.,~')
+    execute('e Xtest1')
+    execute('!ls X*.swp >>test.out')
+    execute('!echo under under >>test.out')
+    execute('!ls Xtest2 >>test.out')
+    execute('!echo under Xtest1.swp >>test.out')
+    execute('!mkdir Xtest.je')
+    execute('/start of testfile/,/end of testfile/w! Xtest2/Xtest3')
+    execute('set dir=Xtest.je,~')
+    execute('e Xtest2/Xtest3')
+    execute('swap')
+    execute('!ls Xtest2 >>test.out')
+    execute('!echo under Xtest3 >>test.out')
+    execute('!ls Xtest.je >>test.out')
+    execute('!echo under Xtest3.swp >>test.out')
+    execute('qa!')
+
+    -- Assert buffer contents.
+    expect([=[
+      first line
+      Xtest1.swp
+      under Xtest1.swp
+      under under
+      Xtest1.swp
+      under Xtest1.swp
+      Xtest3
+      under Xtest3
+      Xtest3.swp
+      under Xtest3.swp]=])
+  end)
+end)
