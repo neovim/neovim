b9173e057515f4af53db6e45cfbf28170447e76c
Lucas Hoffmann <l-m-h@web.de>
2015-06-11 01:18:35 +0200

    tests: Modernize migrated legacy test argument_0count.

--- test/functional/legacy/argument_0count_spec.lua
+++ test/functional/legacy/argument_0count_spec.lua
@@ -1,38 +1,28 @@
 -- Tests for :0argadd and :0argedit
 
 local helpers = require('test.functional.helpers')
-local source, clear, expect = helpers.source, helpers.clear, helpers.expect
+local eq, eval, clear, execute =
+  helpers.eq, helpers.eval, helpers.clear, helpers.execute
 
 describe('argument_0count', function()
   setup(clear)
 
   it('is working', function()
-    source([[
-      let arglists = []
-      arga a b c d
-      2argu
-      0arga added
-      call add(arglists, argv())
-      2argu
-      arga third
-      call add(arglists, argv())
-      %argd
-      arga a b c d
-      2argu
-      0arge edited
-      call add(arglists, argv())
-      2argu
-      arga third
-      call add(arglists, argv())
-      call append(0, map(copy(arglists), 'join(v:val, " ")'))
-    ]])
-
-    -- Assert buffer contents.
-    expect([=[
-      added a b c d
-      added a third b c d
-      edited a b c d
-      edited a third b c d
-      ]=])
+    execute('arga a b c d')
+    eq({'a', 'b', 'c', 'd'}, eval('argv()'))
+    execute('2argu')
+    execute('0arga added')
+    eq({'added', 'a', 'b', 'c', 'd'}, eval('argv()'))
+    execute('2argu')
+    execute('arga third')
+    eq({'added', 'a', 'third', 'b', 'c', 'd'}, eval('argv()'))
+    execute('%argd')
+    execute('arga a b c d')
+    execute('2argu')
+    execute('0arge edited')
+    eq({'edited', 'a', 'b', 'c', 'd'}, eval('argv()'))
+    execute('2argu')
+    execute('arga third')
+    eq({'edited', 'a', 'third', 'b', 'c', 'd'}, eval('argv()'))
   end)
 end)
