41ad72fd58aa8b9bf188f2bf6c64c011dc1aa296
Lucas Hoffmann <l-m-h@web.de>
2015-06-17 14:11:01 +0200

    tests: Fix and modernize migrated test 17.

--- test/functional/legacy/017_spec.lua
+++ test/functional/legacy/017_spec.lua
@@ -5,10 +5,23 @@
 local helpers = require('test.functional.helpers')
 local feed, insert, source = helpers.feed, helpers.insert, helpers.source
 local clear, execute, expect = helpers.clear, helpers.execute, helpers.expect
+local write_file = helpers.write_file
+
+local rmdir = function(dir)
+  os.execute('rm -rf '..dir)
+end
 
 describe('17', function()
-  setup(clear)
+  setup(function()
+    clear()
+    write_file('test17a.in', [[
+      This file is just to test "gf" in test 17.
+      The contents is not important.
+      Just testing!
+      ]])
+  end)
   teardown(function()
+    os.remove('test17a.in')
     os.remove('Xbase.a')
     os.remove('Xbase.b')
     os.remove('Xbase.c')
@@ -16,27 +29,21 @@ describe('17', function()
   end)
 
   it('is working', function()
-    insert([=[
+    insert([[
       	${CDIR}/test17a.in
       	$TDIR/test17a.in
-      ]=])
+      ]])
 
-    execute('set isfname=@,48-57,/,.,-,_,+,,,$,:,~,{,}')
-    execute('function! DeleteDirectory(dir)')
-    execute(' if has("win16") || has("win32") || has("win64") || has("dos16") || has("dos32")')
-    execute('  exec "silent !rmdir /Q /S " . a:dir')
-    execute(' else')
-    execute('  exec "silent !rm -rf " . a:dir')
-    execute(' endif')
-    execute('endfun')
-    execute('if has("unix")')
-    execute('let $CDIR = "."')
-    execute('/CDIR')
-    execute('else')
-    execute('let $TDIR = "."')
-    execute('/TDIR')
-    execute('endif')
-    -- Dummy writing for making that sure gf doesn't fail even if the current
+    source([[
+      set isfname=@,48-57,/,.,-,_,+,,,$,:,~,{,}
+      if has("unix")
+        let $CDIR = "."
+        /CDIR
+      else
+        let $TDIR = "."
+        /TDIR
+      endif]])
+    -- Dummy writing for making sure that gf doesn't fail even if the current
     -- file is modified. It can be occurred when executing the following
     -- command directly on Windows without fixing the 'fileformat':
     -- > nmake -f Make_dos.mak test17.out
@@ -47,68 +54,58 @@ describe('17', function()
     execute('brewind')
     -- Check for 'include' without \zs or \ze.
     execute('lang C')
-    execute('call delete("./Xbase.a")')
-    execute('call DeleteDirectory("Xdir1")')
-    execute('!mkdir Xdir1')
-    execute('!mkdir "Xdir1/dir2"')
-    execute('e! Xdir1/dir2/foo.a')
-    feed('i#include   "bar.a"<esc>')
-    execute('w')
-    execute('e Xdir1/dir2/bar.a')
-    feed('i#include      "baz.a"<esc>')
-    execute('w')
-    execute('e Xdir1/dir2/baz.a')
-    feed('i#include            "foo.a"<esc>')
-    execute('w')
+    os.remove('Xbase.a')
+    rmdir('Xdir1')
+    lfs.mkdir('Xdir1')
+    lfs.mkdir('Xdir1/dir2')
+    write_file('Xdir1/dir2/foo.a', '#include   "bar.a"')
+    write_file('Xdir1/dir2/bar.a', '#include      "baz.a"')
+    write_file('Xdir1/dir2/baz.a', '#include            "foo.a"')
+    write_file('Xbase.a', '#include    <foo.a>')
     execute('e Xbase.a')
     execute('set path=Xdir1/dir2')
-    feed('i#include    <foo.a><esc>')
-    execute('w')
     execute('redir! >>test.out')
     execute('checkpath!')
     execute('redir END')
     execute('brewind')
+
     -- Check for 'include' with \zs and \ze.
-    execute('call delete("./Xbase.b")')
-    execute('call DeleteDirectory("Xdir1")')
-    execute('!mkdir Xdir1')
-    execute('!mkdir "Xdir1/dir2"')
-    execute([=[let &include='^\s*%inc\s*/\zs[^/]\+\ze']=])
-    execute('function! DotsToSlashes()')
-    execute([[  return substitute(v:fname, '\.', '/', 'g') . '.b']])
-    execute('endfunction')
-    execute([[let &includeexpr='DotsToSlashes()']])
-    execute('e! Xdir1/dir2/foo.b')
-    feed('i%inc   /bar/<esc>')
-    execute('w')
-    execute('e Xdir1/dir2/bar.b')
-    feed('i%inc      /baz/<esc>')
-    execute('w')
-    execute('e Xdir1/dir2/baz.b')
-    feed('i%inc            /foo/<esc>')
-    execute('w')
+    os.remove('Xbase.b')
+    rmdir('Xdir1')
+    lfs.mkdir('Xdir1')
+    lfs.mkdir('Xdir1/dir2')
+    source([[
+      let &include='^\s*%inc\s*/\zs[^/]\+\ze'
+      function! DotsToSlashes()
+        return substitute(v:fname, '\.', '/', 'g') . '.b'
+      endfunction
+      let &includeexpr='DotsToSlashes()'
+    ]])
+    write_file('Xdir1/dir2/foo.b', '%inc   /bar/')
+    write_file('Xdir1/dir2/bar.b', '%inc      /baz/')
+    write_file('Xdir1/dir2/baz.b', '%inc            /foo/')
+    write_file('Xbase.b', '%inc    /foo/')
     execute('e Xbase.b')
     execute('set path=Xdir1/dir2')
-    feed('i%inc    /foo/<esc>')
-    execute('w')
     execute('redir! >>test.out')
     execute('checkpath!')
     execute('redir END')
     execute('brewind')
     -- Check for 'include' with \zs and no \ze.
-    execute('call delete("./Xbase.c")')
-    execute('call DeleteDirectory("Xdir1")')
-    execute('!mkdir Xdir1')
-    execute('!mkdir "Xdir1/dir2"')
-    execute([=[let &include='^\s*%inc\s*\%([[:upper:]][^[:space:]]*\s\+\)\?\zs\S\+\ze']=])
-    execute('function! StripNewlineChar()')
-    execute([[  if v:fname =~ '\n$']])
-    execute('    return v:fname[:-2]')
-    execute('  endif')
-    execute('  return v:fname')
-    execute('endfunction')
-    execute([[let &includeexpr='StripNewlineChar()']])
-    execute('e! Xdir1/dir2/foo.c')
+    os.remove('Xbase.c')
+    rmdir('Xdir1')
+    lfs.mkdir('Xdir1')
+    lfs.mkdir('Xdir1/dir2')
+    source([=[
+      let &include='^\s*%inc\s*\%([[:upper:]][^[:space:]]*\s\+\)\?\zs\S\+\ze'
+      function! StripNewlineChar()
+        if v:fname =~ '\n$'
+          return v:fname[:-2]
+        endif
+        return v:fname
+      endfunction
+      let &includeexpr='StripNewlineChar()'
+      e! Xdir1/dir2/foo.c]=])
     feed('i%inc   bar.c<esc>')
     execute('w')
     execute('e Xdir1/dir2/bar.c')
@@ -129,13 +126,11 @@ describe('17', function()
     execute('redir END')
     execute('brewind')
     -- Change "\" to "/" for Windows and fix 'fileformat'.
-    execute('e test.out')
+    execute('e! test.out')
     execute([[%s#\\#/#g]])
-    execute('set ff&')
-    execute('w')
 
     -- Assert buffer contents.
-    expect([=[
+    expect([[
       This file is just to test "gf" in test 17.
       The contents is not important.
       Just testing!
@@ -168,6 +163,6 @@ describe('17', function()
         Xdir1/dir2/bar.c -->
           Xdir1/dir2/baz.c
           Xdir1/dir2/baz.c -->
-            foo.c  (Already listed)]=])
+            foo.c  (Already listed)]])
   end)
 end)
