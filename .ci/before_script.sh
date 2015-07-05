c2e26bb7538e091ffd34b3c2b5b0cbbad41c72c8
Florian Walch <florian@fwalch.com>
2015-07-05 21:37:29 +0300

    Travis: Check some configuration variables in before_script.

--- .ci/before_script.sh
+++ .ci/before_script.sh
@@ -10,6 +10,16 @@ fi
 CI_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "${CI_DIR}/common/build.sh"
 
+# Test some of the configuration variables.
+if [[ -n "${GCOV}" ]] && [[ ! $(type -P "${GCOV}") ]]; then
+  echo "\$GCOV: '${GCOV}' is not executable."
+  exit 1
+fi
+if [[ -n "${LLVM_SYMBOLIZER}" ]] && [[ ! $(type -P "${LLVM_SYMBOLIZER}") ]]; then
+  echo "\$LLVM_SYMBOLIZER: '${LLVM_SYMBOLIZER}' is not executable."
+  exit 1
+fi
+
 if [[ "${TRAVIS_OS_NAME}" == osx ]]; then
   # Adds user to a dummy group.
   # That allows to test changing the group of the file by `os_fchown`.
