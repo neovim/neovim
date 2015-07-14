eb1ab7bc20056682f7649ac95ed6b3a77ee1b6d9
Lucas Hoffmann <l-m-h@web.de>
2015-07-14 14:34:57 +0200

    tests: Modernize migrated legacy test 73.
    The output file is removed from the test.  All checks are done directly by
    `expect()`ing the correct files.  Tab characters in the input are escaped as
    `\t` in lua strings for readability.  The setting of visualbell is removed as
    it did not influence the test.  Lastly the use of separate windows for the
    test steps is dropped.

--- test/functional/legacy/073_find_completion_spec.lua
+++ test/functional/legacy/073_find_completion_spec.lua
@@ -5,6 +5,12 @@ local feed, insert, source = helpers.feed, helpers.insert, helpers.source
 local clear, execute, expect = helpers.clear, helpers.execute, helpers.expect
 local write_file = helpers.write_file
 
+local function expect_empty_buffer()
+  -- The space will be removed by helpers.dedent but is needed as dedent will
+  -- throw an error if it can not find the common indent of the given lines.
+  return expect(' ')
+end
+
 describe(':find completion', function()
   setup(function()
     clear()
@@ -18,37 +24,20 @@ describe(':find completion', function()
     -- This test relies on the default setting of 'wildmode' as it was in
     -- plain Vim.
     execute('set wildmode=full')
-    -- This will cause a few errors, do it silently.
-    execute('set visualbell')
-    -- Do all test in a separate window to avoid E211 when we recursively
-    -- delete the Xfind directory during cleanup.
-    execute('new')
     execute('let cwd=getcwd()')
-    execute('let test_out = cwd . "/test.out"')
     execute('cd Xfind')
     execute('set path=')
-    execute('find 	')
-    source([[
-      exec "w! " . test_out
-      close
-      new
-      set path=.
-    ]])
-    execute('find 	')
-    execute('exec "w >>" . test_out')
-    execute('close')
-    execute('new')
+    execute('find \t')
+    expect_empty_buffer()
+    execute('set path=.')
+    execute('find \t')
+    expect_empty_buffer()
     execute('set path=.,,')
-    execute('find 	')
-    execute('exec "w >>" . test_out')
-    execute('close')
-    execute('new')
+    execute('find \t')
+    expect_empty_buffer()
     execute('set path=./**')
-    execute('find 	')
-    execute('exec "w >>" . test_out')
-    execute('close')
-    execute('new')
-    -- We shouldn't find any file at this point, test.out must be empty.
+    execute('find \t')
+    expect_empty_buffer()
     lfs.mkdir('Xfind/in')
     lfs.mkdir('Xfind/in/path')
     write_file('Xfind/file.txt', 'Holy Grail\n')
@@ -57,42 +46,42 @@ describe(':find completion', function()
     write_file('Xfind/in/path/file.txt', 'E.T.\n')
     execute('exec "cd " . cwd')
     execute('set path=Xfind/**')
-    execute('find file	')
-    execute('exec "w >>" . test_out')
-    execute('find file		')
-    execute('exec "w >>" . test_out')
-    execute('find file			')
-    execute('exec "w >>" . test_out')
+    execute('find file\t')
+    expect('Holy Grail')
+    execute('find file\t\t')
+    expect('Jimmy Hoffa')
+    execute('find file\t\t\t')
+    expect('E.T.')
     -- Rerun the previous three find completions, using fullpath in 'path'.
     execute('exec "set path=" . cwd . "/Xfind/**"')
-    execute('find file	')
-    execute('exec "w >>" .  test_out')
-    execute('find file		')
-    execute('exec "w >>" . test_out')
-    execute('find file			')
-    execute('exec "w >>" . test_out')
+    execute('find file\t')
+    expect('Holy Grail')
+    execute('find file\t\t')
+    expect('Jimmy Hoffa')
+    execute('find file\t\t\t')
+    expect('E.T.')
     -- Same steps again, using relative and fullpath items that point to the
-    -- same.  Recursive location.
-    -- This is to test that there are no duplicates in the completion list.
-    execute('exec "set path+=Xfind/**"')
-    execute('find file	')
-    execute('exec "w >>" .  test_out')
-    execute('find file		')
-    execute('exec "w >>" . test_out')
-    execute('find file			')
-    execute('exec "w >>" . test_out')
-    execute('find file		')
+    -- same recursive location.  This is to test that there are no duplicates
+    -- in the completion list.
+    execute('set path+=Xfind/**')
+    execute('find file\t')
+    expect('Holy Grail')
+    execute('find file\t\t')
+    expect('Jimmy Hoffa')
+    execute('find file\t\t\t')
+    expect('E.T.')
+    execute('find file\t\t')
     -- Test find completion for directory of current buffer, which at this
     -- point is Xfind/in/file.txt.
     execute('set path=.')
-    execute('find st	')
-    execute('exec "w >>" .  test_out')
+    execute('find st\t')
+    expect('Another Holy Grail')
     -- Test find completion for empty path item ",," which is the current
     -- directory.
     execute('cd Xfind')
     execute('set path=,,')
-    execute('find f		')
-    execute('exec "w >>" . test_out')
+    execute('find f\t\t')
+    expect('Holy Grail')
     -- Test shortening of
     --
     --   foo/x/bar/voyager.txt
@@ -101,22 +90,20 @@ describe(':find completion', function()
     -- When current directory is above foo/ they should be shortened to (in
     -- order of appearance):
     --
-    --   x/bar/voyager.txt.
-    --   y/bar/voyager.txt.
+    --   x/bar/voyager.txt
+    --   y/bar/voyager.txt
     lfs.mkdir('Xfind/foo')
     lfs.mkdir('Xfind/foo/x')
     lfs.mkdir('Xfind/foo/x/bar')
     lfs.mkdir('Xfind/foo/y')
     lfs.mkdir('Xfind/foo/y/bar')
-    -- We should now be in the Xfind directory.
-    helpers.eq('Xfind', helpers.eval('fnamemodify(getcwd(), ":t")'))
     write_file('Xfind/foo/x/bar/voyager.txt', 'Voyager 1\n')
     write_file('Xfind/foo/y/bar/voyager.txt', 'Voyager 2\n')
     execute('exec "set path=" . cwd . "/Xfind/**"')
-    execute('find voyager	')
-    execute('exec "w >>" . test_out')
-    execute('find voyager		')
-    execute('exec "w >>" . test_out')
+    execute('find voyager\t')
+    expect('Voyager 1')
+    execute('find voyager\t\t')
+    expect('Voyager 2')
 
     -- When current directory is .../foo/y/bar they should be shortened to (in
     -- order of appearance):
@@ -126,67 +113,40 @@ describe(':find completion', function()
     execute('cd foo')
     execute('cd y')
     execute('cd bar')
-    execute('find voyager	')
-    execute('exec "w >> " . test_out')
-    execute('find voyager		')
-    execute('exec "w >> " . test_out')
-    -- Check the opposite too:.
+    execute('find voyager\t')
+    expect('Voyager 2')
+    execute('find voyager\t\t')
+    expect('Voyager 1')
+    -- Check the opposite too:
     execute('cd ..')
     execute('cd ..')
     execute('cd x')
     execute('cd bar')
-    execute('find voyager	')
-    execute('exec "w >> " . test_out')
-    execute('find voyager		')
-    execute('exec "w >> " . test_out')
+    execute('find voyager\t')
+    expect('Voyager 1')
+    execute('find voyager\t\t')
+    expect('Voyager 2')
     -- Check for correct handling of shorten_fname()'s behavior on windows.
     execute('exec "cd " . cwd . "/Xfind/in"')
-    execute('find file	')
-    execute('exec "w >>" . test_out')
+    execute('find file\t')
+    expect('Jimmy Hoffa')
     -- Test for relative to current buffer 'path' item.
     execute('exec "cd " . cwd . "/Xfind/"')
     execute('set path=./path')
     -- Open the file where Jimmy Hoffa is found.
     execute('e in/file.txt')
     -- Find the file containing 'E.T.' in the Xfind/in/path directory.
-    execute('find file	')
-    execute('exec "w >>" . test_out')
+    execute('find file\t')
+    expect('E.T.')
 
     -- Test that completion works when path=.,,.
-
     execute('set path=.,,')
     -- Open Jimmy Hoffa file.
     execute('e in/file.txt')
-    execute('exec "w >>" . test_out')
+    expect('Jimmy Hoffa')
     -- Search for the file containing Holy Grail in same directory as in/path.txt.
-    execute('find stu	')
-    execute('exec "w >>" . test_out')
-    execute('q')
-    execute('exec "cd " . cwd')
-    execute('e test.out')
-
-    -- Assert buffer contents.
-    expect([[
-      Holy Grail
-      Jimmy Hoffa
-      E.T.
-      Holy Grail
-      Jimmy Hoffa
-      E.T.
-      Holy Grail
-      Jimmy Hoffa
-      E.T.
-      Another Holy Grail
-      Holy Grail
-      Voyager 1
-      Voyager 2
-      Voyager 2
-      Voyager 1
-      Voyager 1
-      Voyager 2
-      Jimmy Hoffa
-      E.T.
-      Jimmy Hoffa
-      Another Holy Grail]])
+    execute('find stu\t')
+    expect('Another Holy Grail')
   end)
 end)
+
