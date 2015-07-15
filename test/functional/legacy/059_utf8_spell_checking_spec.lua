c1ad593fc8ae071ac02b9ee4fa180f34f62053b8
Lucas Hoffmann <l-m-h@web.de>
2015-07-15 14:47:27 +0200

    Write Xtest2.aff from lua.

--- test/functional/legacy/059_utf8_spell_checking_spec.lua
+++ test/functional/legacy/059_utf8_spell_checking_spec.lua
@@ -70,6 +70,59 @@ describe("spell checking with 'encoding' set to utf-8", function()
       'MAP c\xe7\n' ..
       'MAP y\xff\xfd\n' ..
       'MAP s\xdf\n')
+    write_file('Xtest2.aff', 
+      'SET ISO8859-1\n' ..
+      '\n' ..
+      'FOL  \xe0\xe1\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9\xea\xeb\xec\xed\xee' ..
+      '\xef\xf0\xf1\xf2\xf3\xf4\xf5\xf6\xf8\xf9\xfa\xfb\xfc\xfd\xfe\xdf' ..
+      '\xff\n' ..
+      'LOW  \xe0\xe1\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9\xea\xeb\xec\xed\xee' ..
+      '\xef\xf0\xf1\xf2\xf3\xf4\xf5\xf6\xf8\xf9\xfa\xfb\xfc\xfd\xfe\xdf' ..
+      '\xff\n' ..
+      'UPP  \xc0\xc1\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xca\xcb\xcc\xcd\xce' ..
+      '\xcf\xd0\xd1\xd2\xd3\xd4\xd5\xd6\xd8\xd9\xda\xdb\xdc\xdd\xde\xdf' ..
+      '\xff\n' ..
+      '\n' ..
+      'PFXPOSTPONE\n' ..
+      '\n' ..
+      'MIDWORD\t\'-\n' ..
+      '\n' ..
+      'KEP =\n' ..
+      'RAR ?\n' ..
+      'BAD !\n' ..
+      '\n' ..
+      '#NOSPLITSUGS\n' ..
+      '\n' ..
+      'PFX I N 1\n' ..
+      'PFX I 0 in .\n' ..
+      '\n' ..
+      'PFX O Y 1\n' ..
+      'PFX O 0 out [a-z]\n' ..
+      '\n' ..
+      'SFX S Y 2\n' ..
+      'SFX S 0 s [^s]\n' ..
+      'SFX S 0 es s\n' ..
+      '\n' ..
+      'SFX N N 3\n' ..
+      'SFX N 0 en [^n]\n' ..
+      'SFX N 0 nen n\n' ..
+      'SFX N 0 n .\n' ..
+      '\n' ..
+      'REP 3\n' ..
+      'REP g ch\n' ..
+      'REP ch g\n' ..
+      'REP svp s.v.p.\n' ..
+      '\n' ..
+      'MAP 9\n' ..
+      'MAP a\xe0\xe1\xe2\xe3\xe4\xe5\n' ..
+      'MAP e\xe8\xe9\xea\xeb\n' ..
+      'MAP i\xec\xed\xee\xef\n' ..
+      'MAP o\xf2\xf3\xf4\xf5\xf6\n' ..
+      'MAP u\xf9\xfa\xfb\xfc\n' ..
+      'MAP n\xf1\n' ..
+      'MAP c\xe7\n' ..
+      'MAP y\xff\xfd\n' ..
+      'MAP s\xdf\n')
   end)
 
   it('is working', function()
@@ -229,55 +282,6 @@ describe("spell checking with 'encoding' set to utf-8", function()
       SAL ZZ-                  _
       SAL Z                    S
       affend_sal
-      
-      2affstart
-      SET ISO8859-1
-      
-      FOL  àáâãäåæçèéêëìíîïðñòóôõöøùúûüýþßÿ
-      LOW  àáâãäåæçèéêëìíîïðñòóôõöøùúûüýþßÿ
-      UPP  ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞßÿ
-      
-      PFXPOSTPONE
-      
-      MIDWORD	'-
-      
-      KEP =
-      RAR ?
-      BAD !
-      
-      #NOSPLITSUGS
-      
-      PFX I N 1
-      PFX I 0 in .
-      
-      PFX O Y 1
-      PFX O 0 out [a-z]
-      
-      SFX S Y 2
-      SFX S 0 s [^s]
-      SFX S 0 es s
-      
-      SFX N N 3
-      SFX N 0 en [^n]
-      SFX N 0 nen n
-      SFX N 0 n .
-      
-      REP 3
-      REP g ch
-      REP ch g
-      REP svp s.v.p.
-      
-      MAP 9
-      MAP aàáâãäå
-      MAP eèéêë
-      MAP iìíîï
-      MAP oòóôõö
-      MAP uùúûü
-      MAP nñ
-      MAP cç
-      MAP yÿý
-      MAP sß
-      2affend
       
       1dicstart
       123456
@@ -553,7 +557,7 @@ describe("spell checking with 'encoding' set to utf-8", function()
         $put =''
         $put ='test '. a:aff . '-' . a:dic
 	"  Generate a .spl file from a .dic and .aff file.
-	if str2nr(a:aff) <= 1
+	if str2nr(a:aff) <= 2
 	  exe '1;/^' . a:aff . 'affstart/+1,/^' . a:aff . 'affend/-1w! Xtest.aff'
 	else
 	  exe '!cp -f Xtest'.a:aff.'.aff Xtest.aff'
