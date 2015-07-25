931539d108d397b88da618585861dce854018af2
ZyX <kp-pav@yandex.ru>
2015-07-26 01:20:13 +0300

    shada: Do not export shada_read_file
    Unlike some wrapper functions it is not used actually, so no need to export it.
    Also removed ShadaPosition typedef which was not used.

--- src/nvim/shada.c
+++ src/nvim/shada.c
@@ -151,6 +151,15 @@ KHASH_SET_INIT_STR(strset)
 /// Common prefix for all “system” errors
 #define SERR "E886: "
 
+/// Flags for shada_read_file and children
+enum {
+  kShaDaWantInfo = 1,       ///< Load non-mark information
+  kShaDaWantMarks = 2,      ///< Load local file marks and change list
+  kShaDaForceit = 4,        ///< Overwrite info already read
+  kShaDaGetOldfiles = 8,    ///< Load v:oldfiles.
+  kShaDaMissingError = 16,  ///< Error out when os_open returns -ENOENT.
+};
+
 /// Possible ShaDa entry types
 ///
 /// @warning Enum values are part of the API and must not be altered.
@@ -776,7 +785,7 @@ static bool shada_disabled(void)
 /// @param[in]  flags  Flags, see kShaDa enum values in shada.h.
 ///
 /// @return FAIL if reading failed for some reason and OK otherwise.
-int shada_read_file(const char *const file, const int flags)
+static int shada_read_file(const char *const file, const int flags)
   FUNC_ATTR_WARN_UNUSED_RESULT
 {
   if (shada_disabled()) {
--- src/nvim/shada.h
+++ src/nvim/shada.h
@@ -1,17 +1,6 @@
 #ifndef NVIM_SHADA_H
 #define NVIM_SHADA_H
 
-typedef long ShadaPosition;
-
-/// Flags for shada_read_file and children
-enum {
-  kShaDaWantInfo = 1,       ///< Load non-mark information
-  kShaDaWantMarks = 2,      ///< Load local file marks and change list
-  kShaDaForceit = 4,        ///< Overwrite info already read
-  kShaDaGetOldfiles = 8,    ///< Load v:oldfiles.
-  kShaDaMissingError = 16,  ///< Error out when os_open returns -ENOENT.
-};
-
 #ifdef INCLUDE_GENERATED_DECLARATIONS
 # include "shada.h.generated.h"
 #endif
