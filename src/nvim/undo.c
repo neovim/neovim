848f39cee5d0f257aafca2bfa2c3ab583796daa6
Wayne Rowcliffe <war1025@gmail.com>
2015-11-12 16:43:15 -0600

    Use WITH_BLOCK_AUTOCMDS in more places

--- src/nvim/undo.c
+++ src/nvim/undo.c
@@ -2061,212 +2061,217 @@ static void u_undoredo(int undo)
   int empty_buffer;                         /* buffer became empty */
   u_header_T  *curhead = curbuf->b_u_curhead;
 
-  /* Don't want autocommands using the undo structures here, they are
-   * invalid till the end. */
-  block_autocmds();
-
 #ifdef U_DEBUG
-  u_check(FALSE);
+    u_check(false);
 #endif
-  old_flags = curhead->uh_flags;
-  new_flags = (curbuf->b_changed ? UH_CHANGED : 0) +
-              ((curbuf->b_ml.ml_flags & ML_EMPTY) ? UH_EMPTYBUF : 0);
-  setpcmark();
 
-  /*
-   * save marks before undo/redo
-   */
-  zero_fmark_additional_data(curbuf->b_namedm);
-  memmove(namedm, curbuf->b_namedm, sizeof(curbuf->b_namedm[0]) * NMARKS);
-  visualinfo = curbuf->b_visual;
-  curbuf->b_op_start.lnum = curbuf->b_ml.ml_line_count;
-  curbuf->b_op_start.col = 0;
-  curbuf->b_op_end.lnum = 0;
-  curbuf->b_op_end.col = 0;
-
-  for (uep = curhead->uh_entry; uep != NULL; uep = nuep) {
-    top = uep->ue_top;
-    bot = uep->ue_bot;
-    if (bot == 0)
-      bot = curbuf->b_ml.ml_line_count + 1;
-    if (top > curbuf->b_ml.ml_line_count || top >= bot
-        || bot > curbuf->b_ml.ml_line_count + 1) {
-      unblock_autocmds();
-      EMSG(_("E438: u_undo: line numbers wrong"));
-      changed();                /* don't want UNCHANGED now */
-      return;
-    }
-
-    oldsize = bot - top - 1;        /* number of lines before undo */
-    newsize = uep->ue_size;         /* number of lines after undo */
-
-    if (top < newlnum) {
-      /* If the saved cursor is somewhere in this undo block, move it to
-       * the remembered position.  Makes "gwap" put the cursor back
-       * where it was. */
-      lnum = curhead->uh_cursor.lnum;
-      if (lnum >= top && lnum <= top + newsize + 1) {
-        curwin->w_cursor = curhead->uh_cursor;
-        newlnum = curwin->w_cursor.lnum - 1;
-      } else {
-        /* Use the first line that actually changed.  Avoids that
-         * undoing auto-formatting puts the cursor in the previous
-         * line. */
-        for (i = 0; i < newsize && i < oldsize; ++i)
-          if (STRCMP(uep->ue_array[i], ml_get(top + 1 + i)) != 0)
-            break;
-        if (i == newsize && newlnum == MAXLNUM && uep->ue_next == NULL) {
-          newlnum = top;
-          curwin->w_cursor.lnum = newlnum + 1;
-        } else if (i < newsize)   {
-          newlnum = top + i;
-          curwin->w_cursor.lnum = newlnum + 1;
-        }
-      }
-    }
-
-    empty_buffer = FALSE;
-
-    /* delete the lines between top and bot and save them in newarray */
-    if (oldsize > 0) {
-      newarray = xmalloc(sizeof(char_u *) * (size_t)oldsize);
-      /* delete backwards, it goes faster in most cases */
-      for (lnum = bot - 1, i = oldsize; --i >= 0; --lnum) {
-        /* what can we do when we run out of memory? */
-        newarray[i] = u_save_line(lnum);
-        /* remember we deleted the last line in the buffer, and a
-         * dummy empty line will be inserted */
-        if (curbuf->b_ml.ml_line_count == 1)
-          empty_buffer = TRUE;
-        ml_delete(lnum, FALSE);
-      }
-    } else
-      newarray = NULL;
-
-    /* insert the lines in u_array between top and bot */
-    if (newsize) {
-      for (lnum = top, i = 0; i < newsize; ++i, ++lnum) {
-        /*
-         * If the file is empty, there is an empty line 1 that we
-         * should get rid of, by replacing it with the new line
-         */
-        if (empty_buffer && lnum == 0)
-          ml_replace((linenr_T)1, uep->ue_array[i], TRUE);
-        else
-          ml_append(lnum, uep->ue_array[i], (colnr_T)0, FALSE);
-        xfree(uep->ue_array[i]);
-      }
-      xfree((char_u *)uep->ue_array);
-    }
-
-    /* adjust marks */
-    if (oldsize != newsize) {
-      mark_adjust(top + 1, top + oldsize, (long)MAXLNUM,
-          (long)newsize - (long)oldsize);
-      if (curbuf->b_op_start.lnum > top + oldsize)
-        curbuf->b_op_start.lnum += newsize - oldsize;
-      if (curbuf->b_op_end.lnum > top + oldsize)
-        curbuf->b_op_end.lnum += newsize - oldsize;
-    }
-
-    changed_lines(top + 1, 0, bot, newsize - oldsize);
-
-    /* set '[ and '] mark */
-    if (top + 1 < curbuf->b_op_start.lnum)
-      curbuf->b_op_start.lnum = top + 1;
-    if (newsize == 0 && top + 1 > curbuf->b_op_end.lnum)
-      curbuf->b_op_end.lnum = top + 1;
-    else if (top + newsize > curbuf->b_op_end.lnum)
-      curbuf->b_op_end.lnum = top + newsize;
-
-    u_newcount += newsize;
-    u_oldcount += oldsize;
-    uep->ue_size = oldsize;
-    uep->ue_array = newarray;
-    uep->ue_bot = top + newsize + 1;
+  // Don't want autocommands using the undo structures here, they are
+  // invalid till the end.
+  WITH_BLOCK_AUTOCMDS({
+    old_flags = curhead->uh_flags;
+    new_flags = (curbuf->b_changed ? UH_CHANGED : 0) +
+                ((curbuf->b_ml.ml_flags & ML_EMPTY) ? UH_EMPTYBUF : 0);
+    setpcmark();
 
     /*
-     * insert this entry in front of the new entry list
+     * save marks before undo/redo
      */
-    nuep = uep->ue_next;
-    uep->ue_next = newlist;
-    newlist = uep;
-  }
+    zero_fmark_additional_data(curbuf->b_namedm);
+    memmove(namedm, curbuf->b_namedm, sizeof(curbuf->b_namedm[0]) * NMARKS);
+    visualinfo = curbuf->b_visual;
+    curbuf->b_op_start.lnum = curbuf->b_ml.ml_line_count;
+    curbuf->b_op_start.col = 0;
+    curbuf->b_op_end.lnum = 0;
+    curbuf->b_op_end.col = 0;
 
-  curhead->uh_entry = newlist;
-  curhead->uh_flags = new_flags;
-  if ((old_flags & UH_EMPTYBUF) && bufempty())
-    curbuf->b_ml.ml_flags |= ML_EMPTY;
-  if (old_flags & UH_CHANGED)
-    changed();
-  else
-    unchanged(curbuf, FALSE);
+    for (uep = curhead->uh_entry; uep != NULL; uep = nuep) {
+      top = uep->ue_top;
+      bot = uep->ue_bot;
+      if (bot == 0)
+        bot = curbuf->b_ml.ml_line_count + 1;
+      if (top > curbuf->b_ml.ml_line_count || top >= bot
+          || bot > curbuf->b_ml.ml_line_count + 1) {
+        // Note: We have to manually unblock autocmds here
+        //       since we are returning before the macro has a
+        //       chance to unblock them.
+        unblock_autocmds();
+        EMSG(_("E438: u_undo: line numbers wrong"));
+        changed();                /* don't want UNCHANGED now */
+        return;
+      }
 
-  /*
-   * restore marks from before undo/redo
-   */
-  for (i = 0; i < NMARKS; ++i)
-    if (curhead->uh_namedm[i].mark.lnum != 0) {
-      free_fmark(curbuf->b_namedm[i]);
-      curbuf->b_namedm[i] = curhead->uh_namedm[i];
-      curhead->uh_namedm[i] = namedm[i];
+      oldsize = bot - top - 1;        /* number of lines before undo */
+      newsize = uep->ue_size;         /* number of lines after undo */
+
+      if (top < newlnum) {
+        /* If the saved cursor is somewhere in this undo block, move it to
+         * the remembered position.  Makes "gwap" put the cursor back
+         * where it was. */
+        lnum = curhead->uh_cursor.lnum;
+        if (lnum >= top && lnum <= top + newsize + 1) {
+          curwin->w_cursor = curhead->uh_cursor;
+          newlnum = curwin->w_cursor.lnum - 1;
+        } else {
+          /* Use the first line that actually changed.  Avoids that
+           * undoing auto-formatting puts the cursor in the previous
+           * line. */
+          for (i = 0; i < newsize && i < oldsize; ++i)
+            if (STRCMP(uep->ue_array[i], ml_get(top + 1 + i)) != 0)
+              break;
+          if (i == newsize && newlnum == MAXLNUM && uep->ue_next == NULL) {
+            newlnum = top;
+            curwin->w_cursor.lnum = newlnum + 1;
+          } else if (i < newsize)   {
+            newlnum = top + i;
+            curwin->w_cursor.lnum = newlnum + 1;
+          }
+        }
+      }
+
+      empty_buffer = false;
+
+      /* delete the lines between top and bot and save them in newarray */
+      if (oldsize > 0) {
+        newarray = xmalloc(sizeof(char_u *) * (size_t)oldsize);
+        /* delete backwards, it goes faster in most cases */
+        for (lnum = bot - 1, i = oldsize; --i >= 0; --lnum) {
+          /* what can we do when we run out of memory? */
+          newarray[i] = u_save_line(lnum);
+          /* remember we deleted the last line in the buffer, and a
+           * dummy empty line will be inserted */
+          if (curbuf->b_ml.ml_line_count == 1)
+            empty_buffer = true;
+          ml_delete(lnum, false);
+        }
+      } else {
+        newarray = NULL;
+      }
+
+      /* insert the lines in u_array between top and bot */
+      if (newsize) {
+        for (lnum = top, i = 0; i < newsize; ++i, ++lnum) {
+          /*
+           * If the file is empty, there is an empty line 1 that we
+           * should get rid of, by replacing it with the new line
+           */
+          if (empty_buffer && lnum == 0)
+            ml_replace((linenr_T)1, uep->ue_array[i], true);
+          else
+            ml_append(lnum, uep->ue_array[i], (colnr_T)0, false);
+          xfree(uep->ue_array[i]);
+        }
+        xfree((char_u *)uep->ue_array);
+      }
+
+      /* adjust marks */
+      if (oldsize != newsize) {
+        mark_adjust(top + 1, top + oldsize, (long)MAXLNUM,
+            (long)newsize - (long)oldsize);
+        if (curbuf->b_op_start.lnum > top + oldsize)
+          curbuf->b_op_start.lnum += newsize - oldsize;
+        if (curbuf->b_op_end.lnum > top + oldsize)
+          curbuf->b_op_end.lnum += newsize - oldsize;
+      }
+
+      changed_lines(top + 1, 0, bot, newsize - oldsize);
+
+      /* set '[ and '] mark */
+      if (top + 1 < curbuf->b_op_start.lnum)
+        curbuf->b_op_start.lnum = top + 1;
+      if (newsize == 0 && top + 1 > curbuf->b_op_end.lnum)
+        curbuf->b_op_end.lnum = top + 1;
+      else if (top + newsize > curbuf->b_op_end.lnum)
+        curbuf->b_op_end.lnum = top + newsize;
+
+      u_newcount += newsize;
+      u_oldcount += oldsize;
+      uep->ue_size = oldsize;
+      uep->ue_array = newarray;
+      uep->ue_bot = top + newsize + 1;
+
+      /*
+       * insert this entry in front of the new entry list
+       */
+      nuep = uep->ue_next;
+      uep->ue_next = newlist;
+      newlist = uep;
     }
-  if (curhead->uh_visual.vi_start.lnum != 0) {
-    curbuf->b_visual = curhead->uh_visual;
-    curhead->uh_visual = visualinfo;
-  }
 
-  /*
-   * If the cursor is only off by one line, put it at the same position as
-   * before starting the change (for the "o" command).
-   * Otherwise the cursor should go to the first undone line.
-   */
-  if (curhead->uh_cursor.lnum + 1 == curwin->w_cursor.lnum
-      && curwin->w_cursor.lnum > 1)
-    --curwin->w_cursor.lnum;
-  if (curwin->w_cursor.lnum <= curbuf->b_ml.ml_line_count) {
-    if (curhead->uh_cursor.lnum == curwin->w_cursor.lnum) {
-      curwin->w_cursor.col = curhead->uh_cursor.col;
-      if (virtual_active() && curhead->uh_cursor_vcol >= 0)
-        coladvance((colnr_T)curhead->uh_cursor_vcol);
-      else
-        curwin->w_cursor.coladd = 0;
-    } else
-      beginline(BL_SOL | BL_FIX);
-  } else {
-    /* We get here with the current cursor line being past the end (eg
-     * after adding lines at the end of the file, and then undoing it).
-     * check_cursor() will move the cursor to the last line.  Move it to
-     * the first column here. */
-    curwin->w_cursor.col = 0;
-    curwin->w_cursor.coladd = 0;
-  }
-
-  /* Make sure the cursor is on an existing line and column. */
-  check_cursor();
-
-  /* Remember where we are for "g-" and ":earlier 10s". */
-  curbuf->b_u_seq_cur = curhead->uh_seq;
-  if (undo)
-    /* We are below the previous undo.  However, to make ":earlier 1s"
-     * work we compute this as being just above the just undone change. */
-    --curbuf->b_u_seq_cur;
-
-  /* Remember where we are for ":earlier 1f" and ":later 1f". */
-  if (curhead->uh_save_nr != 0) {
-    if (undo)
-      curbuf->b_u_save_nr_cur = curhead->uh_save_nr - 1;
+    curhead->uh_entry = newlist;
+    curhead->uh_flags = new_flags;
+    if ((old_flags & UH_EMPTYBUF) && bufempty())
+      curbuf->b_ml.ml_flags |= ML_EMPTY;
+    if (old_flags & UH_CHANGED)
+      changed();
     else
-      curbuf->b_u_save_nr_cur = curhead->uh_save_nr;
-  }
+      unchanged(curbuf, false);
 
-  /* The timestamp can be the same for multiple changes, just use the one of
-   * the undone/redone change. */
-  curbuf->b_u_time_cur = curhead->uh_time;
+    /*
+     * restore marks from before undo/redo
+     */
+    for (i = 0; i < NMARKS; ++i)
+      if (curhead->uh_namedm[i].mark.lnum != 0) {
+        free_fmark(curbuf->b_namedm[i]);
+        curbuf->b_namedm[i] = curhead->uh_namedm[i];
+        curhead->uh_namedm[i] = namedm[i];
+      }
+    if (curhead->uh_visual.vi_start.lnum != 0) {
+      curbuf->b_visual = curhead->uh_visual;
+      curhead->uh_visual = visualinfo;
+    }
 
-  unblock_autocmds();
+    /*
+     * If the cursor is only off by one line, put it at the same position as
+     * before starting the change (for the "o" command).
+     * Otherwise the cursor should go to the first undone line.
+     */
+    if (curhead->uh_cursor.lnum + 1 == curwin->w_cursor.lnum
+        && curwin->w_cursor.lnum > 1)
+      --curwin->w_cursor.lnum;
+    if (curwin->w_cursor.lnum <= curbuf->b_ml.ml_line_count) {
+      if (curhead->uh_cursor.lnum == curwin->w_cursor.lnum) {
+        curwin->w_cursor.col = curhead->uh_cursor.col;
+        if (virtual_active() && curhead->uh_cursor_vcol >= 0)
+          coladvance((colnr_T)curhead->uh_cursor_vcol);
+        else
+          curwin->w_cursor.coladd = 0;
+      } else {
+        beginline(BL_SOL | BL_FIX);
+      }
+    } else {
+      /* We get here with the current cursor line being past the end (eg
+       * after adding lines at the end of the file, and then undoing it).
+       * check_cursor() will move the cursor to the last line.  Move it to
+       * the first column here. */
+      curwin->w_cursor.col = 0;
+      curwin->w_cursor.coladd = 0;
+    }
+
+    /* Make sure the cursor is on an existing line and column. */
+    check_cursor();
+
+    /* Remember where we are for "g-" and ":earlier 10s". */
+    curbuf->b_u_seq_cur = curhead->uh_seq;
+    if (undo)
+      /* We are below the previous undo.  However, to make ":earlier 1s"
+       * work we compute this as being just above the just undone change. */
+      --curbuf->b_u_seq_cur;
+
+    /* Remember where we are for ":earlier 1f" and ":later 1f". */
+    if (curhead->uh_save_nr != 0) {
+      if (undo)
+        curbuf->b_u_save_nr_cur = curhead->uh_save_nr - 1;
+      else
+        curbuf->b_u_save_nr_cur = curhead->uh_save_nr;
+    }
+
+    /* The timestamp can be the same for multiple changes, just use the one of
+     * the undone/redone change. */
+    curbuf->b_u_time_cur = curhead->uh_time;
+  });
+
 #ifdef U_DEBUG
-  u_check(FALSE);
+  u_check(false);
 #endif
 }
 
