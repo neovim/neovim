3b7c4093e24fd413696e3f0aba9a7a1a470a8b4f
ZyX <kp-pav@yandex.ru>
2016-01-10 22:08:43 +0300

    shell: Unquote &shell* options before using them

--- test/unit/os/shell_spec.lua
+++ test/unit/os/shell_spec.lua
@@ -15,7 +15,8 @@ local shell = helpers.cimport(
   './src/nvim/os/shell.h',
   './src/nvim/option_defs.h',
   './src/nvim/main.h',
-  './src/nvim/misc1.h'
+  './src/nvim/misc1.h',
+  './src/nvim/memory.h'
 )
 local ffi, eq = helpers.ffi, helpers.eq
 local intern = helpers.internalize
@@ -33,6 +34,23 @@ describe('shell functions', function()
   teardown(function()
     shell.event_teardown()
   end)
+
+  local function shell_build_argv(cmd, extra_args)
+    local res = shell.shell_build_argv(
+        cmd and to_cstr(cmd),
+        extra_args and to_cstr(extra_args))
+    local argc = 0
+    local ret = {}
+    -- Explicitly free everything, so if it is not in allocated memory it will
+    -- crash.
+    while res[argc] ~= nil do
+      ret[#ret + 1] = ffi.string(res[argc])
+      shell.xfree(res[argc])
+      argc = argc + 1
+    end
+    shell.xfree(res)
+    return ret
+  end
 
   local function os_system(cmd, input)
     local input_or = input and to_cstr(input) or NULL
@@ -72,6 +90,46 @@ describe('shell functions', function()
     it ('returns non-zero exit code', function()
       local status = os_system('exit 2')
       eq(2, status)
+    end)
+  end)
+
+  describe('shell_build_argv', function()
+    local saved_opts = {}
+
+    setup(function()
+      saved_opts.p_sh = shell.p_sh
+      saved_opts.p_shcf = shell.p_shcf
+    end)
+
+    teardown(function()
+      shell.p_sh = saved_opts.p_sh
+      shell.p_shcf = saved_opts.p_shcf
+    end)
+
+    it('works with NULL arguments', function()
+      eq({'/bin/bash'}, shell_build_argv(nil, nil))
+    end)
+
+    it('works with cmd', function()
+      eq({'/bin/bash', '-c', 'abc  def'}, shell_build_argv('abc  def', nil))
+    end)
+
+    it('works with extra_args', function()
+      eq({'/bin/bash', 'ghi  jkl'}, shell_build_argv(nil, 'ghi  jkl'))
+    end)
+
+    it('works with cmd and extra_args', function()
+      eq({'/bin/bash', 'ghi  jkl', '-c', 'abc  def'}, shell_build_argv('abc  def', 'ghi  jkl'))
+    end)
+
+    it('splits and unquotes &shell and &shellcmdflag', function()
+      shell.p_sh = to_cstr('/Program" "Files/zsh -f')
+      shell.p_shcf = to_cstr('-x -o "sh word split" "-"c')
+      eq({'/Program Files/zsh', '-f',
+          'ghi  jkl',
+          '-x', '-o', 'sh word split',
+          '-c', 'abc  def'},
+         shell_build_argv('abc  def', 'ghi  jkl'))
     end)
   end)
 end)
