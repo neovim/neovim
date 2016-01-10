neovim/neovim

commit c6e481cba559d35891f20ef4ac9ca6a41bdcdbd3

portability: use portable format specifier

src/nvim/eval.c
@@ -7999,7 +7999,7 @@ static void prepare_assert_error(garray_T *gap)
     }
   }
   if (sourcing_lnum > 0) {
-    vim_snprintf(buf, ARRAY_SIZE(buf), "line %ld", (long)sourcing_lnum);
+    vim_snprintf(buf, ARRAY_SIZE(buf), "line %" PRId64, (long)sourcing_lnum);
     ga_concat(gap, (char_u *)buf);
   }
   if (sourcing_name != NULL || sourcing_lnum > 0) {

test/functional/legacy/100_lispwords_spec.lua
@@ -4,10 +4,10 @@ local helpers = require('test.functional.helpers')
 local source = helpers.source
 local clear, expect = helpers.clear, helpers.expect
 
-describe('undolevel', function()
+describe('lispwords', function()
   setup(clear)
 
-  it('is working', function()
+  it('global-local', function()
     source([[
       setglobal lispwords=foo,bar,baz
       setlocal lispwords-=foo 

commit a641b74140063ebe268fba241774e2e424e244f5

portability: fix cast.
PRId64 format expects int64_t.
Regression from c6e481cba559d35891f20ef4ac9ca6a41bdcdbd3.

src/nvim/eval.c
@@ -7999,7 +7999,7 @@ static void prepare_assert_error(garray_T *gap)
     }
   }
   if (sourcing_lnum > 0) {
-    vim_snprintf(buf, ARRAY_SIZE(buf), "line %" PRId64, (long)sourcing_lnum);
+    vim_snprintf(buf, ARRAY_SIZE(buf), "line %" PRId64, (int64_t)sourcing_lnum);
     ga_concat(gap, (char_u *)buf);
   }
   if (sourcing_name != NULL || sourcing_lnum > 0) {