5db511b6f1d1d5fc67fe46118d5e3aefc488bea4
Michael Reed <m.reed@mykolab.com>
2016-01-11 12:41:03 -0500

    Stop using <stropts.h>
    In Vim, at least the constant `I_PUSH` is used from <stropts.h>, but
    Neovim doesn't seem to use anything from said header.
    
    Besides that, POSIX.1-2008[1] marks this header as obsolescent, and
    there don't seem to be many platforms that even have it.
    
    [1]: http://pubs.opengroup.org/onlinepubs/9699919799/basedefs/stropts.h.html#tag_13_52_11

--- src/nvim/os_unix.c
+++ src/nvim/os_unix.c
@@ -44,10 +44,6 @@
 #include "nvim/os/signal.h"
 #include "nvim/msgpack_rpc/helpers.h"
 
-#ifdef HAVE_STROPTS_H
-# include <stropts.h>
-#endif
-
 #ifdef HAVE_SELINUX
 # include <selinux/selinux.h>
 static int selinux_enabled = -1;
