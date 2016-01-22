50c4c56967933afa30ff5fdfc689120cfd0f762e
Justin Gassner <justin.gassner@web.de>
2016-01-22 11:57:08 +0100

    vim-patch:dbcf19d
    Add test files for patch 7.4.680.
    
    https://github.com/vim/vim/commit/dbcf19dc498cb1561c9215a3f255e81cde0c0543

---
+++ test/functional/legacy/erasebackword_spec.lua
@@ -0,0 +1,24 @@
+-- Test for CTRL-W in Insert mode
+
+local helpers = require('test.functional.helpers')
+local clear, feed, expect = helpers.clear, helpers.feed, helpers.expect
+
+describe('CTRL-W in Insert mode', function()
+  setup(clear)
+
+  it('works for multi-byte characters', function()
+
+    for i = 1, 6 do
+      feed('o wwwこんにちわ世界ワールドvim ' .. string.rep('<C-w>', i) .. '<esc>')
+    end
+
+    expect([[
+      
+       wwwこんにちわ世界ワールド
+       wwwこんにちわ世界
+       wwwこんにちわ
+       www
+       
+      ]])
+  end)
+end)
