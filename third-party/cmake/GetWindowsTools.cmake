b0b9da1b1731b3f036907d1eaa405990c1a2be46
Rui Abreu Ferreira <raf-ep@gmx.com>
2016-02-24 23:08:08 +0000

    third-party: Download winpty 0.4 builds
    Winpty has x86/x64 binary builds, download them when building
    Neovim.

--- third-party/cmake/GetWindowsTools.cmake
+++ third-party/cmake/GetWindowsTools.cmake
@@ -48,3 +48,17 @@ GetTool(TARGET win32yank
              ${DEPS_BUILD_DIR}/src/win32yank/win32yank.exe
 	     ${DEPS_INSTALL_DIR}/bin)
 
+include(TargetArch)
+if("${ARCHITECTURE}" STREQUAL "X86_64")
+  set(ARCH x64)
+elseif(ARCHITECTURE STREQUAL "X86")
+  set(ARCH ia32)
+else()
+  message(FATAL_ERROR "Unsupported architecture(${ARCHITECTURE}) cannot download winpty")
+endif()
+GetTool(TARGET winpty
+  INSTALL_COMMAND ${CMAKE_COMMAND} -E make_directory ${DEPS_INSTALL_DIR}/bin
+    COMMAND ${CMAKE_COMMAND} -DFROM_GLOB=${DEPS_BUILD_DIR}/src/winpty/${ARCH}/bin/* -DTO=${DEPS_INSTALL_DIR}/bin/ -P ${CMAKE_CURRENT_SOURCE_DIR}/cmake/CopyFilesGlob.cmake
+    COMMAND ${CMAKE_COMMAND} -DFROM_GLOB=${DEPS_BUILD_DIR}/src/winpty/include/* -DTO=${DEPS_INSTALL_DIR}/include/ -P ${CMAKE_CURRENT_SOURCE_DIR}/cmake/CopyFilesGlob.cmake
+    COMMAND ${CMAKE_COMMAND} -DFROM_GLOB=${DEPS_BUILD_DIR}/src/winpty/${ARCH}/lib/* -DTO=${DEPS_INSTALL_DIR}/lib/ -P ${CMAKE_CURRENT_SOURCE_DIR}/cmake/CopyFilesGlob.cmake)
+
