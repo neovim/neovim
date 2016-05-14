d3a904309b8a685ebb0540c04f8825b2dda3e4ff
John Szakmeister <john@szakmeister.net>
2016-05-14 08:44:37 -0400

    Fix be64toh() detection on BSDs.
    This was noticed due to a user issue (#4750) when building Neovim 0.1.4
    via ports.  The crux of the issue is that we did not detect the
    be64toh() macro, because there is no endian.h on FreeBSD (along with
    several other BSDs).  So we were defaulting to our builtin version of
    be64toh().  However, it appears that sys/endian.h was being picked up by
    an include (likely msgpack.h) and so be64toh() was actually defined and
    corrupting our definition of it.
    
    So the answer here was to use the correct include file in our check, and
    export that information in the config.h.  Then we use that information
    to include the right header in shada.c.
    
    This fixes #4750.

--- src/nvim/shada.c
+++ src/nvim/shada.c
@@ -46,7 +46,7 @@
 #ifdef HAVE_BE64TOH
 # define _BSD_SOURCE 1
 # define _DEFAULT_SOURCE 1
-# include <endian.h>
+# include ENDIAN_INCLUDE_FILE
 #endif
 
 // Note: when using bufset hash pointers are intentionally casted to uintptr_t
