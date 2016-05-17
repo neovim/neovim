723497f030c76e07453b0cdd24ed404e6fb5a2a6
Yichao Zhou <broken.zhoug@gmail.com>
2016-05-17 16:26:03 -0700

    test: listchars

--- test/functional/ui/highlight_spec.lua
+++ test/functional/ui/highlight_spec.lua
@@ -369,6 +369,7 @@ describe("'cursorline' with 'listchars'", function()
     NonText = Screen.colors.Blue,
     Cursorline = Screen.colors.Grey90,
     SpecialKey = Screen.colors.Red,
+    Visual = Screen.colors.LightGrey,
   }
 
   before_each(function()
@@ -478,7 +479,7 @@ describe("'cursorline' with 'listchars'", function()
     execute('highlight SpecialKey guifg=#FF0000')
     execute('set cursorline')
     execute('set tabstop=8')
-    execute('set listchars=space:.,eol:¬,tab:>-,extends:],precedes:[,trail:* list')
+    execute('set listchars=space:.,eol:¬,tab:>-,extends:>,precedes:<,trail:* list')
     feed('i\t abcd <cr>\t abcd <cr><esc>k')
     screen:expect([[
       {5:>-------.}abcd{5:*}{4:¬}     |
@@ -502,6 +503,111 @@ describe("'cursorline' with 'listchars'", function()
       {4:¬}                   |
       {4:~                   }|
       :set nocursorline   |
+    ]])
+    execute('set nowrap')
+    feed('ALorem ipsum dolor sit amet<ESC>0')
+    screen:expect([[
+      {5:^>-------.}abcd{5:.}Lorem{4:>}|
+      {5:>-------.}abcd{5:*}{4:¬}     |
+      {4:¬}                   |
+      {4:~                   }|
+                          |
+    ]])
+    execute('set cursorline')
+    screen:expect([[
+      {2:^>-------.}{1:abcd}{2:.}{1:Lorem}{4:>}|
+      {5:>-------.}abcd{5:*}{4:¬}     |
+      {4:¬}                   |
+      {4:~                   }|
+      :set cursorline     |
+    ]])
+    feed('$')
+    screen:expect([[
+      {4:<}{1:r}{2:.}{1:sit}{2:.}{1:ame^t}{3:¬}{1:        }|
+      {4:<}                   |
+      {4:<}                   |
+      {4:~                   }|
+      :set cursorline     |
+    ]])
+    feed('G')
+    screen:expect([[
+      {5:>-------.}abcd{5:.}Lorem{4:>}|
+      {5:>-------.}abcd{5:*}{4:¬}     |
+      {3:^¬}{1:                   }|
+      {4:~                   }|
+      :set cursorline     |
+    ]])
+  end)
+
+  it("'listchar' in visual mode", function()
+    screen:set_default_attr_ids({
+      [1] = {background=hlgroup_colors.Cursorline},
+      [2] = {
+        foreground=hlgroup_colors.SpecialKey,
+        background=hlgroup_colors.Cursorline,
+      },
+      [3] = {
+        background=hlgroup_colors.Cursorline,
+        foreground=hlgroup_colors.NonText,
+        bold=true,
+      },
+      [4] = {
+        foreground=hlgroup_colors.NonText,
+        bold=true,
+      },
+      [5] = {
+        foreground=hlgroup_colors.SpecialKey,
+      },
+      [6] = {
+        background=hlgroup_colors.Visual,
+      },
+      [7] = {
+        background=hlgroup_colors.Visual,
+        foreground=hlgroup_colors.SpecialKey,
+      },
+      [8] = {
+        background=hlgroup_colors.Visual,
+        foreground=hlgroup_colors.NonText,
+        bold=true,
+      },
+    })
+    execute('highlight clear ModeMsg')
+    execute('highlight SpecialKey guifg=#FF0000')
+    execute('set cursorline')
+    execute('set tabstop=8')
+    execute('set nowrap')
+    execute('set listchars=space:.,eol:¬,tab:>-,extends:>,precedes:<,trail:* list')
+    feed('i\t abcd <cr>\t abcd Lorem ipsum dolor sit amet<cr><esc>kkk0')
+    screen:expect([[
+      {2:^>-------.}{1:abcd}{2:*}{3:¬}{1:     }|
+      {5:>-------.}abcd{5:.}Lorem{4:>}|
+      {4:¬}                   |
+      {4:~                   }|
+                          |
+    ]])
+    feed('lllvj')
+    screen:expect([[
+      {5:>-------.}a{6:bcd}{7:*}{8:¬}     |
+      {7:>-------.}{6:a}^bcd{5:.}Lorem{4:>}|
+      {4:¬}                   |
+      {4:~                   }|
+      -- VISUAL --        |
+    ]])
+    feed('<esc>V')
+    screen:expect([[
+      {5:>-------.}abcd{5:*}{4:¬}     |
+      {7:>-------.}{6:a}^b{6:cd}{7:.}{6:Lorem}{4:>}|
+      {4:¬}                   |
+      {4:~                   }|
+      -- VISUAL LINE --   |
+    ]])
+    feed('<esc>$')
+    screen:expect([[
+      {4:<}                   |
+      {4:<}{1:r}{2:.}{1:sit}{2:.}{1:ame^t}{3:¬}{1:        }|
+      {4:<}                   |
+      {4:~                   }|
+                          |
     ]])
   end)
 end)
