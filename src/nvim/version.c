78c6f5c06d0fc57b9456c3cf11a3c70743993c4d
Shota <shotat@users.noreply.github.com>
2016-06-05 17:00:58 +0900

    vim-patch:7.4.1056 (#4870)
    Problem:    Don't know why finding spell suggestions is slow.
    Solution:   Add some code to gather profiling information.
    
    https://github.com/vim/vim/commit/ca1fe985175385c609f8e06672a1014729aba05c

--- src/nvim/version.c
+++ src/nvim/version.c
@@ -637,7 +637,7 @@ static int included_patches[] = {
   1059,
   // 1058,
   1057,
-  // 1056,
+  1056,
   1055,
   1054,
   1053,
@@ -2020,3 +2020,4 @@ void ex_intro(exarg_T *eap)
   intro_message(TRUE);
   wait_return(TRUE);
 }
+
