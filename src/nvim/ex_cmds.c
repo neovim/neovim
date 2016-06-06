ec33778e7d40bcc83461ef09ac41e50fcc7393fc
DesbyP <hullot.philemon@gmail.com>
2016-06-06 10:01:43 +0200

    [disp result] width_long() replaced by log10()+1

--- src/nvim/ex_cmds.c
+++ src/nvim/ex_cmds.c
@@ -6013,7 +6013,7 @@ void ex_window_live_sub(char_u* sub, klist_t(matchedline_T) *lmatch)
   kl_iter(matchedline_T, lmatch, current)
     highest_num_line = (*current)->data.lnum;
 
-  int col_width = width_long(highest_num_line) + 4;
+  int col_width = log10(highest_num_line) + 1 + 4;
 
   // allocate a line sized for the window
   char *str = xmalloc((size_t )curwin->w_frame->fr_width);
@@ -6032,15 +6032,13 @@ void ex_window_live_sub(char_u* sub, klist_t(matchedline_T) *lmatch)
     sprintf(str, "%s%s", col, mat.line); //TODO : strcat
     ml_append(line++, (char_u *)str, (colnr_T)0, false);
 
-    int prefix_size = col_width;
-
 //    kl_iter(colnr_T, mat.start_col, col) {
 //      src_id_highlight = bufhl_add_hl(curbuf,
 //                                      src_id_highlight,
 //                                      2, // id of our highlight TODO : allow the user to change it
 //                                      line,                                     // line in curbuf
-//                                      (*col)->data + prefix_size + 1,           // beginning of word
-//                                      (*col)->data + prefix_size + sub_size); // end of word
+//                                      (*col)->data + col_width + 1,           // beginning of word
+//                                      (*col)->data + col_width + sub_size); // end of word
 //
 //    }
 //
