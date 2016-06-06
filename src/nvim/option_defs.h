10b283bc5712bbb872ee2e22d55628a5411be957
Clement <clement>
2016-06-06 15:46:07 +0200

    bug commented

--- src/nvim/option_defs.h
+++ src/nvim/option_defs.h
@@ -586,7 +586,7 @@ EXTERN int p_spr;               // 'splitright'
 EXTERN int p_sol;               // 'startofline'
 EXTERN char_u   *p_su;          // 'suffixes'
 EXTERN char_u   *p_swb;         // 'switchbuf'
-EXTERN int p_sub;               // 'live_sub'
+EXTERN bool p_sub;               // 'live_sub'
 EXTERN unsigned swb_flags;
 #ifdef IN_OPTION_C
 static char *(p_swb_values[]) =
