45da09ce0288148c366bee60c461dab12c362a06
aym7 <aym.collange@gmail.com>
2016-06-09 17:01:05 +0200

    added soutenance file

---
+++ demoSoutenance.c
@@ -0,0 +1,26 @@
+#include <string.h>
+#include <string.h>
+#include <string.h>
+#include <string.h>
+#include <string.h>
+#include <string.h>
+
+
+int main(int argc, char *argv[]) {
+  foo();
+
+  printf("hello TOTO and TITI are TATA of the TOTO");
+
+  bar();
+
+  printf("hello TOTO and TITI are 54687 of the TOTO");
+
+  printf("hello TITI and 1234 are TATA");
+  printf("hello TITI and TITI are TATA of the TOTO");
+  foo();
+}
+
+void foo(void) {
+  printf("sentence that will survive 8888 thanks to TOTO");
+  foo();
+}
