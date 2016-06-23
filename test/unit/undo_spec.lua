9a8ea5adaca6484e29be933ae3e63372f328ebb1
Christopher Waldon <christopher.waldon.dev@gmail.com>
2016-06-22 23:26:20 -0400

    test/unit/undo_spec.lua: Create first unit test for u_write_undo.

--- test/unit/undo_spec.lua
+++ test/unit/undo_spec.lua
@@ -1,38 +1,72 @@
 local lfs = require('lfs')
 local helpers = require('test.unit.helpers')
 
+local ffi = helpers.ffi
 local cimport = helpers.cimport
 local to_cstr = helpers.to_cstr
+local neq = helpers.neq
 
+cimport('./src/nvim/ex_cmds_defs.h')
+cimport('./src/nvim/buffer_defs.h')
+local options = cimport('./src/nvim/option_defs.h')
+local vim = cimport('./src/nvim/vim.h')
 local undo = cimport('./src/nvim/undo.h')
+local buffer = cimport('./src/nvim/buffer.h')
 
 describe('u_write_undo', function()
   setup(function()
-    -- TODO: check whether the global undodir needs to be configured here somehow
     lfs.mkdir('unit-test-directory')
+    lfs.chdir('unit-test-directory')
+    old_p_udir = options.p_udir  -- save the old value of p_udir (undodir)
+    options.p_udir = to_cstr(lfs.currentdir())  -- set p_udir to be the test dir
   end)
   
   teardown(function()
-    lfs.rmdir('unit-test-directory')
+    lfs.chdir('..')
+    success, err = lfs.rmdir('unit-test-directory')
+    if not success then
+      print(err)  -- inform tester if directory fails to delete
+    end
+    options.p_udir = old_p_udir  --restore old p_udir
   end)
 
   before_each(function()
     -- create a new buffer
-    -- TODO: mock creating a buffer
+    local c_file = to_cstr('../test/unit/undo_spec.lua')
+    buffer = buffer.buflist_new(c_file, c_file, 1, buffer.BLN_LISTED)
+    buffer.b_u_numhead = 1 -- Pretend that the buffer has been changed
+
+    -- TODO(christopher.waldon.dev@gmail.com): replace the 32 with UNDO_HASH_SIZE
+    -- requires refactor of UNDO_HASH_SIZE into constant/enum for ffi
+    --
+    -- compute a hash for this undofile
+    hash = ffi.new('char_u[32]')
+    undo.u_compute_hash(hash)
   end)
 
   -- Lua wrapper for u_write_undo
   local function u_write_undo(name, forceit, buf, hash)
-    name = to_cstr(name)
+    if name ~= nil then
+      name = to_cstr(name)
+    end
+  
     -- TODO: find out whether the other arguments need special handling
     return undo.u_write_undo(name, forceit, buf, hash)
   end
 
-  it('writes an undo file given the name of that undo file', function()
-    -- TODO: write test
+  it('writes an undo file to undodir given a buffer and hash', function()
+    u_write_undo(nil, false, buffer, hash)
+    correct_name = ffi.string(undo.u_get_undo_file_name(buffer.b_ffname, false))
+    undo_file = io.open(correct_name, "r")
+    
+    neq(undo_file, nil)
+    success, err = os.remove(correct_name)  -- delete the file now that we're done with it.
+    if not success then
+      print(err)  -- inform tester if undofile fails to delete
+    end
   end)
   
-  it('infers undo file name from buffer', function()
+  --[[it('writes a correctly-named undo file to undodir given a name, buffer, and hash', function()
     -- TODO: write test
   end)
   
@@ -54,5 +88,5 @@ describe('u_write_undo', function()
   
   it('does not write an undo file if there is no undo information for the buffer', function()
     -- TODO: write test
-  end)
+  end)]]--
 end)
