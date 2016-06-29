2714376afc62eafd55e82c5d29d226ded74a0f1e
Christopher Waldon <christopher.waldon.dev@gmail.com>
2016-06-28 20:24:50 -0400

    test/unit/undo_spec.lua: Test for correct file name.

--- test/unit/undo_spec.lua
+++ test/unit/undo_spec.lua
@@ -33,8 +33,8 @@ describe('u_write_undo', function()
   before_each(function()
     -- create a new buffer
     local c_file = to_cstr('../test/unit/undo_spec.lua')
-    buffer = buffer.buflist_new(c_file, c_file, 1, buffer.BLN_LISTED)
-    buffer.b_u_numhead = 1 -- Pretend that the buffer has been changed
+    file_buffer = buffer.buflist_new(c_file, c_file, 1, buffer.BLN_LISTED)
+    file_buffer.b_u_numhead = 1 -- Pretend that the buffer has been changed
 
     -- TODO(christopher.waldon.dev@gmail.com): replace the 32 with UNDO_HASH_SIZE
     -- requires refactor of UNDO_HASH_SIZE into constant/enum for ffi
@@ -50,13 +50,12 @@ describe('u_write_undo', function()
       name = to_cstr(name)
     end
   
-    -- TODO: find out whether the other arguments need special handling
     return undo.u_write_undo(name, forceit, buf, hash)
   end
 
   it('writes an undo file to undodir given a buffer and hash', function()
-    u_write_undo(nil, false, buffer, hash)
-    correct_name = ffi.string(undo.u_get_undo_file_name(buffer.b_ffname, false))
+    u_write_undo(nil, false, file_buffer, hash)
+    correct_name = ffi.string(undo.u_get_undo_file_name(file_buffer.b_ffname, false))
     undo_file = io.open(correct_name, "r")
     
     neq(undo_file, nil)
@@ -66,8 +65,16 @@ describe('u_write_undo', function()
     end
   end)
   
-  --[[it('writes a correctly-named undo file to undodir given a name, buffer, and hash', function()
-    -- TODO: write test
+  it('writes a correctly-named undo file to undodir given a name, buffer, and hash', function()
+    correct_name = "undofile.test"
+    u_write_undo(correct_name, false, file_buffer, hash)
+    undo_file = io.open(correct_name, "r")
+    
+    neq(undo_file, nil)
+    success, err = os.remove(correct_name)  -- delete the file now that we're done with it.
+    if not success then
+      print(err)  -- inform tester if undofile fails to delete
+    end
   end)
   
   it('forces writing undo file for :wundo! command', function()
@@ -88,5 +95,5 @@ describe('u_write_undo', function()
   
   it('does not write an undo file if there is no undo information for the buffer', function()
     -- TODO: write test
-  end)]]--
+  end)
 end)
