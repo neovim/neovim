41f5375474f9d7d75c440fc74a9c0e5c875278bc
oni-link <knil.ino@gmail.com>
2016-07-10 09:18:54 +0200

    Test: Problems with macOS rpc tests
    Is flush_stream() the problem?

--- src/nvim/event/process.c
+++ src/nvim/event/process.c
@@ -333,6 +333,7 @@ static void process_close(Process *proc)
   }
 }
 
+/*
 /// Flush output stream.
 ///
 /// @param proc     Process, for which an output stream should be flushed.
@@ -380,13 +381,14 @@ static void flush_stream(Process *proc, Stream *stream)
     }
   }
 }
+*/
 
 static void process_close_handles(void **argv)
 {
   Process *proc = argv[0];
 
-  flush_stream(proc, proc->out);
-  flush_stream(proc, proc->err);
+  //flush_stream(proc, proc->out);
+  //flush_stream(proc, proc->err);
 
   process_close_streams(proc);
   process_close(proc);
