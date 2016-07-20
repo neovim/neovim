67819b698f14ec182adfa575360ddbbfc13a5076
TJ DeVries <devries.timothyj@gmail.com>
2016-07-19 22:26:07 -0400

    Add tests

--- test/functional/plugin/health_spec.lua
+++ test/functional/plugin/health_spec.lua
@@ -1,21 +1,4 @@
 local helpers = require('test.functional.helpers')(after_each)
-local nvim_eval = helpers.eval
-local nvim_command = helpers.command
-
-local plugin_helpers = require('test.functional.plugin.helpers')
-local reset = plugin_helpers.reset
-
-describe('health#check()', function()
-  before_each(function()
-    reset()
-  end)
-
-  it('Should find the default nvim checker', function()
-      print(nvim_eval('echo "hello"'))
-  end)
-end)
-
-local helpers = require('test.functional.helpers')(after_each)
 local plugin_helpers = require('test.functional.plugin.helpers')
 
 describe('health.vim', function()
@@ -23,21 +6,46 @@ describe('health.vim', function()
     plugin_helpers.reset()
   end)
 
-  it('basic operation', function()
-    helpers.execute([[
-      call health#report_start('Foo')
-      call health#report_ok('Bar status')
-      call health#report_start('Baz')
-      call health#report_warn('Zub')
-      call health#report_warn('Zim', ['suggestion 1', 'suggestion 2']) ]])
+  it('should echo the results when using the basic functions', function()
+    helpers.execute("call health#report_start('Foo')")
+    local report = helpers.redir_exec([[call health#report_start('Check Bar')]])
+      .. helpers.redir_exec([[call health#report_ok('Bar status')]])
+      .. helpers.redir_exec([[call health#report_ok('Other Bar status')]])
+      .. helpers.redir_exec([[call health#report_warn('Zub')]])
+      .. helpers.redir_exec([[call health#report_start('Baz')]])
+      .. helpers.redir_exec([[call health#report_warn('Zim', ['suggestion 1', 'suggestion 2'])]])
 
-    helpers.expect([[
-      - Foo
-        - SUCCESS: Bar status
-      - Baz
-        - WARNING: Zub
-        - WARNING: Zim
-          - suggestion 1
-          - suggestion 2]])
+    local expected_contents = {
+      'Checking: Check Bar',
+      'SUCCESS: Bar status',
+      'WARNING: Zub',
+      'SUGGESTIONS:',
+      '- suggestion 1',
+      '- suggestion 2'
+    }
+
+    for _, content in ipairs(expected_contents) do
+      assert(string.find(report, content))
+    end
+  end)
+
+
+  describe('CheckHealth', function()
+    -- Run the health check and store important results
+    -- Run it here because it may take awhile to complete, depending on the system
+    helpers.execute([[CheckHealth!]])
+    local report = helpers.curbuf_contents()
+    local health_checkers = helpers.redir_exec("echo g:health_checkers")
+
+    it('should find the default checker upon execution', function()
+      -- helpers.execute([[CheckHealth!]])
+      assert(string.find(health_checkers, "'health#nvim#check': v:true"))
+    end)
+
+    it('should alert the user that health#nvim#check is running', function()
+      assert(string.find(report, '# Checking health'))
+      assert(string.find(report, 'Checker health#nvim#check says:'))
+      assert(string.find(report, 'Checking:'))
+    end)
   end)
 end)
