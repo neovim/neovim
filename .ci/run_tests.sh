75c39fbb273b73baf4b004cf8f195c5d96d0d5ae
Rui Abreu Ferreira <raf-ep@gmx.com>
2016-07-29 10:08:37 +0100

    Travis: Remove MinGW builds
    MinGW builds in Travis have been disabled for a while now, and some
    of the upcoming patches will break cross compilation even further.

--- .ci/run_tests.sh
+++ .ci/run_tests.sh
@@ -3,14 +3,6 @@
 set -e
 set -o pipefail
 
-# TODO: Stop here for MinGW builds,
-#       building Neovim doesn't work yet.
-if [[ "${BUILD_MINGW}" == ON ]]; then
-  echo "Neovim doesn't build on MinGW yet; stopping build."
-  touch "${SUCCESS_MARKER}"
-  exit
-fi
-
 CI_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "${CI_DIR}/common/build.sh"
 source "${CI_DIR}/common/test.sh"
