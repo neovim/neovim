9fa229473e44259949a925fb57d725f7388fb126
Rom Grk <romgrk.cc@gmail.com>
2016-08-09 06:58:37 -0400

    test: tidy up screen_advanced_spec, list additional specs

--- test/functional/ui/screen_advanced_spec.lua
+++ test/functional/ui/screen_advanced_spec.lua
@@ -6,24 +6,19 @@ local execute = helpers.execute
 
 describe('Screen rendering', function()
   local screen
-  local colors = Screen.colors
-  local hl_colors = {
-    NonText = colors.Blue,
-    Search = colors.Yellow,
-    Message = colors.Red,
-  }
 
   before_each(function()
     clear()
     screen = Screen.new(40, 7)
     screen:attach()
-    --ignore highligting of ~-lines
     screen:set_default_attr_ids( {
       [1] = {foreground = Screen.colors.Brown},
       [2] = {bold = true, foreground = Screen.colors.Brown},
-      [3] = {foreground = hl_colors.Message},
+      [3] = {foreground = Screen.colors.Message},
     })
-    screen:set_default_attr_ignore( {{bold=true, foreground=hl_colors.NonText}} )
+  end)
+
+  it('works with number and relative line number', function()
     insert("line 1\n")
     insert("line 2\n")
     insert("line 3\n")
@@ -31,49 +26,57 @@ describe('Screen rendering', function()
     insert("line 5\n")
     insert("line 6")
     feed("gg")
-  end)
-
-  it('works with line number', function()
     execute('set number')
     screen:expect([[
-      {1:  1 }^line 1                              |
-      {1:  2 }line 2                              |
-      {1:  3 }line 3                              |
-      {1:  4 }line 4                              |
-      {1:  5 }line 5                              |
-      {1:  6 }line 6                              |
-      :set number                             |
+      {1:  1 }{3:^line 1                              }|
+      {1:  2 }{3:line 2                              }|
+      {1:  3 }{3:line 3                              }|
+      {1:  4 }{3:line 4                              }|
+      {1:  5 }{3:line 5                              }|
+      {1:  6 }{3:line 6                              }|
+      {3::set number                             }|
     ]])
-  end)
 
-  it('works with relative line number', function()
+    execute('set nonumber')
     execute('set relativenumber')
     feed("4gg")
     screen:expect([[
-      {1:  3 }line 1                              |
-      {1:  2 }line 2                              |
-      {1:  1 }line 3                              |
-      {2:  0 }^line 4                              |
-      {1:  1 }line 5                              |
-      {1:  2 }line 6                              |
-      :set relativenumber                     |
+      {1:  3 }{3:line 1                              }|
+      {1:  2 }{3:line 2                              }|
+      {1:  1 }{3:line 3                              }|
+      {2:  0 }{3:^line 4                              }|
+      {1:  1 }{3:line 5                              }|
+      {1:  2 }{3:line 6                              }|
+      {3::set relativenumber                     }|
+    ]])
+
+    execute('set number')
+    execute('set relativenumber')
+    screen:expect([[
+      {1:  3 }{3:line 1                              }|
+      {1:  2 }{3:line 2                              }|
+      {1:  1 }{3:line 3                              }|
+      {2:4   }{3:^line 4                              }|
+      {1:  1 }{3:line 5                              }|
+      {1:  2 }{3:line 6                              }|
+      {3::set relativenumber                     }|
     ]])
   end)
 
-  it('works with number + relative line number', function()
-    execute('set number')
-    execute('set relativenumber')
-    feed("4gg")
-    screen:expect([[
-      {1:  3 }line 1                              |
-      {1:  2 }line 2                              |
-      {1:  1 }line 3                              |
-      {2:4   }^line 4                              |
-      {1:  1 }line 5                              |
-      {1:  2 }line 6                              |
-      :set relativenumber                     |
-    ]])
-  end)
+  it('works with line-wise visual-mode')
+
+  it('works with block-wise visual-mode')
+
+  it('works with character-wise visual-mode')
+
+  it('conceals :match matches')
+
+  -- TODO describe sign-column
+  -- TODO describe fold-column
+  -- TODO describe diff-rendering
+  -- TODO describe syntax-rendering
+  -- TODO describe spell-rendering
+  -- TODO describe utf8-rendering
 
 end)
 
