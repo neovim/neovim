eb2d547bd5a34c4c3355c671c417440b72b64ebf
Rui Abreu Ferreira <equalsraf@users.noreply.github.com>
2016-08-26 06:38:46 +0100

    build: Refactor appveyor/Windows scripts (#5244)
    - Join all msys build scripts into one
    - Clean up appveyor.yml and generate installer for tagged builds
    
    A side effect of the cosmetic changes is that appveyor artifact
    permalinks are easier to use, e.g. for the latest zip in the
    master branch
    
        https://ci.appveyor.com/api/projects/neovim/neovim/artifacts/build/Neovim.zip?branch=master&job=Configuration%3A%20MINGW_64

--- .ci/build.bat
+++ .ci/build.bat
@@ -2,7 +2,7 @@
 :: MSYS2, this allows using all the dependencies and tools available
 :: in MSYS2, but we cannot build inside the MSYS2 shell.
 echo on
-if "%TARGET%" == "MINGW_32" (
+if "%CONFIGURATION%" == "MINGW_32" (
 	set ARCH=i686
 	set BITS=32
 ) else (
@@ -12,7 +12,10 @@ if "%TARGET%" == "MINGW_32" (
 :: We cannot have sh.exe in the PATH (MinGW)
 set PATH=%PATH:C:\Program Files\Git\usr\bin;=%
 set PATH=C:\msys64\mingw%BITS%\bin;C:\Windows\System32;C:\Windows;%PATH%
+:: The default cpack in the PATH is not CMake
+set PATH=C:\Program Files (x86)\CMake\bin\cpack.exe;%PATH%
 
+:: Build third-party dependencies
 C:\msys64\usr\bin\bash -lc "pacman --verbose --noconfirm -Su" || goto :error
 C:\msys64\usr\bin\bash -lc "pacman --verbose --noconfirm --needed -S mingw-w64-%ARCH%-cmake mingw-w64-%ARCH%-perl mingw-w64-%ARCH%-python2 mingw-w64-%ARCH%-diffutils" || goto :error
 
@@ -21,6 +24,19 @@ cd .deps
 cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release ..\third-party\ || goto :error
 mingw32-make VERBOSE=1 || goto :error
 cd ..
+
+:: Build Neovim
+mkdir build
+cd build
+cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release .. || goto :error
+mingw32-make VERBOSE=1 || goto :error
+bin\nvim --version || goto :error
+cd ..
+
+:: Build artifacts
+cd build
+cpack -G ZIP -C Release
+if defined APPVEYOR_REPO_TAG_NAME cpack -G NSIS -C Release
 
 goto :EOF
 :error
