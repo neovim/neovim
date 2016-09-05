27554317f74bab53d69195d42bd8d3a5d6f4c3fb
Kalle Ranki <kalle.ranki@gmail.com>
2016-09-05 12:27:05 +0300

    test: Describe set_option_value mps

--- test/unit/option_spec.lua
+++ test/unit/option_spec.lua
@@ -1,7 +1,9 @@
+local ffi = require("ffi")
 local helpers = require("test.unit.helpers")
 
 local to_cstr = helpers.to_cstr
 local eq      = helpers.eq
+local NULL    = helpers.NULL
 
 local option = helpers.cimport("./src/nvim/option.h")
 local globals = helpers.cimport("./src/nvim/globals.h")
@@ -47,5 +49,36 @@ describe('get_sts_value', function()
     shiftwidth = 0
     globals.curbuf.b_p_sw = shiftwidth
     eq(tabstop, option.get_sts_value())
+  end)
+end)
+
+describe('set_option_value mps', function()
+  local get_matchpairs = function ()
+    local value = ffi.new("char *[1]")
+    option.get_option_value (to_cstr("mps"), NULL, value, 0)
+    return ffi.string(value[0])
+  end
+
+  local set_matchpairs = function(value)
+    option.set_option_value(to_cstr("mps"), 0, to_cstr(value), 0)
+  end
+
+  it('keeps old value given invalid matchpair', function()
+    local invalid_mps = { "(-)", "(:", "(-)-", "(:),(-)", "(:),(:" }
+    local default_mps = "(:),{:},[:]"
+    eq(default_mps, get_matchpairs())
+
+    for i=1, #invalid_mps do
+      set_matchpairs(invalid_mps[i])
+      eq(default_mps, get_matchpairs())
+    end
+  end)
+
+  it('accepts valid matchpairs', function()
+    local valid_mps = { "", "(:)", "(:]", "(:),", "(:),[:]"  }
+    for i=1, #valid_mps do
+      set_matchpairs(valid_mps[i])
+      eq(valid_mps[i], get_matchpairs())
+    end
   end)
 end)
