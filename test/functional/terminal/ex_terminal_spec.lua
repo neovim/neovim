ef4676ed5bd18fc2d5fb52b52304d59d94e9e806
Justin M. Keyes <justinkz@gmail.com>
2016-10-11 23:14:18 +0200

    test/terminal: Cover race when :term shell process exits.
    References #5445
    
    See https://github.com/neovim/neovim/pull/5445#issuecomment-252529766

--- test/functional/terminal/ex_terminal_spec.lua
+++ test/functional/terminal/ex_terminal_spec.lua
@@ -13,13 +13,19 @@ describe(':terminal', function()
     clear()
     screen = Screen.new(50, 4)
     screen:attach({rgb=false})
+    -- shell-test.c is a fake shell that prints its arguments and exits.
     nvim('set_option', 'shell', nvim_dir..'/shell-test')
     nvim('set_option', 'shellcmdflag', 'EXE')
-
   end)
 
+  -- Invokes `:terminal {cmd}` using a fake shell (shell-test.c) which prints
+  -- the {cmd} and exits immediately .
+  local function terminal_run_fake_shell_cmd(cmd)
+    execute("terminal "..(cmd and cmd or ""))
+  end
+
   it('with no argument, acts like termopen()', function()
-    execute('terminal')
+    terminal_run_fake_shell_cmd()
     wait()
     screen:expect([[
       ready $                                           |
@@ -30,7 +36,7 @@ describe(':terminal', function()
   end)
 
   it('executes a given command through the shell', function()
-    execute('terminal echo hi')
+    terminal_run_fake_shell_cmd('echo hi')
     wait()
     screen:expect([[
       ready $ echo hi                                   |
@@ -41,7 +47,7 @@ describe(':terminal', function()
   end)
 
   it('allows quotes and slashes', function()
-    execute([[terminal echo 'hello' \ "world"]])
+    terminal_run_fake_shell_cmd([[echo 'hello' \ "world"]])
     wait()
     screen:expect([[
       ready $ echo 'hello' \ "world"                    |
@@ -58,4 +64,16 @@ describe(':terminal', function()
       -- Verify that BufNew actually fired (else the test is invalid).
     eq('foo', eval('&shell'))
   end)
+
+  it('ignores writes if the backing stream closes', function()
+      terminal_run_fake_shell_cmd()
+      helpers.feed('iiXXXXXXX')
+      wait()
+      -- Race: Though the shell exited (and streams were closed by SIGCHLD
+      -- handler), :terminal cleanup is pending on the main-loop.
+      -- This write should be ignored (not crash, #5445).
+      helpers.feed('iiYYYYYYY')
+      wait()
+  end)
+
 end)
