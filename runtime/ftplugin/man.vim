neovim/neovim

commit 9bba8ba372298d7cfac543085df6546e18d2c256

[RFC] man.vim: remove <Plug> mappings (#5290)
- :Man with no arguments opens the manapage for the
  <cWORD> (man buffers) or <cword> (non-man buffers).
- remove now irrelevent comment about -P flag

runtime/ftplugin/man.vim
@@ -43,8 +43,8 @@ setlocal nolist
 setlocal nofoldenable
 
 if !exists('g:no_plugin_maps') && !exists('g:no_man_maps')
-  nmap     <silent> <buffer> <C-]>      <Plug>(man)
-  nmap     <silent> <buffer> K          <Plug>(man)
+  nmap     <silent> <buffer> <C-]>      :Man<CR>
+  nmap     <silent> <buffer> K          :Man<CR>
   nnoremap <silent> <buffer> <C-T>      :call man#pop_tag()<CR>
   if s:pager
     nnoremap <silent> <buffer> <nowait> q :q<CR>

runtime/autoload/man.vim
@@ -9,8 +9,7 @@ endif
 let s:man_find_arg = "-w"
 
 " TODO(nhooyr) Completion may work on SunOS; I'm not sure if `man -l` displays
-" the list of searched directories. I also do not think Solaris supports the
-" '-P' flag used above and uses only $PAGER.
+" the list of searched directories.
 try
   if !has('win32') && $OSTYPE !~? 'cygwin\|linux' && system('uname -s') =~? 'SunOS' && system('uname -r') =~# '^5'
     let s:man_find_arg = '-l'
@@ -23,11 +22,13 @@ function! man#open_page(count, count1, mods, ...) abort
   if a:0 > 2
     call s:error('too many arguments')
     return
-  elseif a:0 ==# 1
-    if empty(a:1)
+  elseif a:0 == 0
+    let ref = &filetype ==# 'man' ? expand('<cWORD>') : expand('<cword>')
+    if empty(ref)
       call s:error('no identifier under cursor')
       return
     endif
+  elseif a:0 ==# 1
     let ref = a:1
   else
     " Combine the name and sect into a manpage reference so that all

runtime/plugin/man.vim
@@ -5,15 +5,7 @@ if exists('g:loaded_man')
 endif
 let g:loaded_man = 1
 
-command! -range=0 -complete=customlist,man#complete -nargs=+ Man call man#open_page(v:count, v:count1, <q-mods>, <f-args>)
-
-function! s:cword() abort
-  return &filetype ==# 'man' ? expand('<cWORD>') : expand('<cword>')
-endfunction
-
-nnoremap <silent> <Plug>(man)        :<C-U>execute 'Man '         .<SID>cword()<CR>
-nnoremap <silent> <Plug>(man_vsplit) :<C-U>execute 'vertical Man '.<SID>cword()<CR>
-nnoremap <silent> <Plug>(man_tab)    :<C-U>execute 'tab Man '     .<SID>cword()<CR>
+command! -range=0 -complete=customlist,man#complete -nargs=* Man call man#open_page(v:count, v:count1, <q-mods>, <f-args>)
 
 augroup man
   autocmd!

commit 5ec20009f23d5a6ec88548d4f01195ec8be15fde

Man: use non-recursive mappings for :Man
This is a regression introduced by:

  https://github.com/neovim/neovim/pull/5290/files#diff-8691c83194ea5f1342ecc9f17b4c51d8R46

When the <plug> mappings were changed to using :Man, they should have changed to
use `nnoremap` as well.

Fixes #5776.

runtime/ftplugin/man.vim
@@ -31,8 +31,8 @@ setlocal nolist
 setlocal nofoldenable
 
 if !exists('g:no_plugin_maps') && !exists('g:no_man_maps')
-  nmap     <silent> <buffer> <C-]>      :Man<CR>
-  nmap     <silent> <buffer> K          :Man<CR>
+  nnoremap <silent> <buffer> <C-]>      :Man<CR>
+  nnoremap <silent> <buffer> K          :Man<CR>
   nnoremap <silent> <buffer> <C-T>      :call man#pop_tag()<CR>
   if s:pager
     nnoremap <silent> <buffer> <nowait> q :q<CR>