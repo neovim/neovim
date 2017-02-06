86242841e3c0f7bc87e1dbf9e44e2d1334d672b8
James McCoy <jamessan@jamessan.com>
2017-02-06 15:26:24 -0500

    lint

--- src/nvim/normal.c
+++ src/nvim/normal.c
@@ -2698,11 +2698,12 @@ do_mouse (
            && bt_quickfix(curbuf)) {
     if (State & INSERT)
       stuffcharReadbuff(Ctrl_O);
-    if (curwin->w_llist_ref == NULL)            /* quickfix window */
+    if (curwin->w_llist_ref == NULL) {          // quickfix window
       do_cmdline_cmd(".cc");
-    else                                        /* location list window */
+    } else {                                    // location list window
       do_cmdline_cmd(".ll");
-    got_click = false;                  /* ignore drag&release now */
+    }
+    got_click = false;                  // ignore drag&release now
   }
   /*
    * Ctrl-Mouse click (or double click in a help window) jumps to the tag
