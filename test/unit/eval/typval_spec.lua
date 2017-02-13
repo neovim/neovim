04ca33275dc39fe6e1a990e2f6dc39c541e75aef
Eiichi NISHINA <github@channel-247.net>
2017-02-13 23:41:24 +0900

    Remove typval_spec.lua that was just an example.

--- test/unit/eval/typval_spec.lua
+++
@@ -1,7 +0,0 @@
--- local helpers = require('test.unit.helpers')
--- local eval_helpers = require('test.unit.eval.helpers')
---
--- local eq = helpers.eq
--- local cimport = helpers.cimport
---
--- local typval = cimport('./src/nvim/eval/typval.h')
