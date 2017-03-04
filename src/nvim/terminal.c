7f92e13e16a4ecfaa082ee66d9fd5aee8a9fafb4
oni-link <knil.ino@gmail.com>
2017-03-04 14:18:29 +0100

    lint fix

--- src/nvim/terminal.c
+++ src/nvim/terminal.c
@@ -241,7 +241,7 @@ Terminal *terminal_open(TerminalOptions opts)
   buf_set_term_title(curbuf, (char *)curbuf->b_ffname);
   RESET_BINDING(curwin);
   // Reset cursor in current window.
-  curwin->w_cursor = (pos_T){.lnum = 1, .col = 0, .coladd = 0};
+  curwin->w_cursor = (pos_T){ .lnum = 1, .col = 0, .coladd = 0 };
 
   // Apply TermOpen autocmds _before_ configuring the scrollback buffer.
   apply_autocmds(EVENT_TERMOPEN, NULL, NULL, false, curbuf);
