4cd58fb794698dc247e5ad01bdecb28fdcd075f2
Matthew Malcomson <hardenedapple@gmail.com>
2017-03-26 14:54:20 +0100

    Use 'exiting' variable to mark while() loop extent
    This is used instead of having a condition on the 'c' variable.
    We do this so it's clear at what point the while () loop can exit, e.g.
    you can search for 'exiting = ' to find all places where it can happen.
    
    Moreover, in the future we may be able to separate concerns between 'c'
    as a variable to determine exiting the loop, as a temporary variable,
    and as the value to return.
    
    Remove vgetorpeek() unnecessary setting of c = -1
    
    Before it was used simply to signify continuing on in the loop, it's not
    needed any more.
    
    'c' is always reset on each iteration around the loop, any value
    leftover from the previous loop will be unused.

--- src/nvim/getchar.c
+++ src/nvim/getchar.c
@@ -1670,6 +1670,7 @@ static int handle_int(const int advance)
 /// K_SPECIAL and CSI may be escaped, need to get two more bytes then.
 static int vgetorpeek(const int advance)
 {
+  bool exiting = true;
   int c;
   int keylen;
   int mp_match_len = 0;
@@ -1733,6 +1734,7 @@ static int vgetorpeek(const int advance)
       if (typebuf.tb_no_abbr_cnt == 0)
         typebuf.tb_no_abbr_cnt = 1;             /* no abbreviations now */
     } else {
+      exiting = false;
       /*
        * Loop until we either find a matching mapped key, or we
        * are sure that it is not a mapped key.
@@ -1752,6 +1754,7 @@ static int vgetorpeek(const int advance)
         keylen = 0;
         if (got_int) {
           c = handle_int(advance);
+          exiting = true;
           break;
         } else if (typebuf.tb_len > 0) {
           /*
@@ -1967,6 +1970,7 @@ static int vgetorpeek(const int advance)
                 KeyNoremap = typebuf.tb_noremap[typebuf.tb_off];
                 del_typebuf(1, 0);
               }
+              exiting = !(c < 0 || (advance && c == NUL));
               break;  // got character, break for loop
             } else {
               keylen = mp_match_len;
@@ -2002,7 +2006,6 @@ static int vgetorpeek(const int advance)
                 setcursor();
               flush_buffers(FALSE);
               mapdepth = 0;                     /* for next one */
-              c = -1;
               break;
             }
 
@@ -2076,7 +2079,6 @@ static int vgetorpeek(const int advance)
             xfree(save_m_keys);
             xfree(save_m_str);
             if (i == FAIL) {
-              c = -1;
               break;
             }
             continue;
@@ -2212,6 +2214,7 @@ static int vgetorpeek(const int advance)
           else
             c = ESC;
           tc = c;
+          exiting = true;
           break;
         }
 
@@ -2300,8 +2303,10 @@ static int vgetorpeek(const int advance)
         if (c < 0)
           continue;                     /* end of input script reached */
         if (c == NUL) {                 /* no character available */
-          if (!advance)
+          if (!advance) {
+            exiting = true;
             break;
+          }
           if (wait_tb_len > 0) {                /* timed out */
             timedout = TRUE;
             continue;
@@ -2316,7 +2321,7 @@ static int vgetorpeek(const int advance)
     }           /* if (!character from stuffbuf) */
 
     /* if advance is FALSE don't loop on NULs */
-  } while (c < 0 || (advance && c == NUL));
+  } while (!exiting);
 
   do_insert_message(mode_deleted, c, advance);
 
