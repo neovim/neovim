24fd125893203c667789cb574b289fc1c9eaf6bc
ZyX <kp-pav@yandex.ru>
2017-03-31 11:49:18 +0300

    cmake: Allow failing to download small suppress files

--- cmake/Download.cmake
+++ cmake/Download.cmake
@@ -8,9 +8,11 @@ list(GET status 0 status_code)
 list(GET status 1 status_string)
 
 if(NOT status_code EQUAL 0)
-  message(FATAL_ERROR "error: downloading '${URL}' failed
-  status_code: ${status_code}
-  status_string: ${status_string}
-  log: ${log}
-")
+  if(NOT ALLOW_FAILURE)
+      message(FATAL_ERROR "error: downloading '${URL}' failed
+        status_code: ${status_code}
+        status_string: ${status_string}
+        log: ${log}
+      ")
+  endif()
 endif()
