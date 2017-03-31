8204eaea7f07c7fd2784bee2c2cfd495d49877df
ZyX <kp-pav@yandex.ru>
2017-03-31 11:39:31 +0300

    cmake: Make Download.cmake check for errors
    Copying from third-party/cmake/DownloadAndExtractFile.cmake.

--- cmake/Download.cmake
+++ cmake/Download.cmake
@@ -1,1 +1,16 @@
-file(DOWNLOAD "${URL}" "${FILE}")
+file(
+  DOWNLOAD "${URL}" "${FILE}"
+  STATUS status
+  LOG log
+)
+
+list(GET status 0 status_code)
+list(GET status 1 status_string)
+
+if(NOT status_code EQUAL 0)
+  message(FATAL_ERROR "error: downloading '${URL}' failed
+  status_code: ${status_code}
+  status_string: ${status_string}
+  log: ${log}
+")
+endif()
