e395423d9ff79411ed81bba63005bd16a825c088
dedmass <carlo.abelli@gmail.com>
2017-04-04 20:53:26 -0400

    refactor/single-include: digraph.h

--- src/nvim/digraph.h
+++ src/nvim/digraph.h
@@ -1,6 +1,9 @@
 #ifndef NVIM_DIGRAPH_H
 #define NVIM_DIGRAPH_H
 
+#include "nvim/types.h" // for char_u
+#include "nvim/ex_cmds_defs.h" // for exarg_T
+
 #ifdef INCLUDE_GENERATED_DECLARATIONS
 # include "digraph.h.generated.h"
 #endif
