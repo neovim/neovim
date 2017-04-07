16dbbb7411fc3e15bef838a801b84bb73e5cbb2d
dedmass <carlo.abelli@gmail.com>
2017-04-06 21:56:27 -0400

    refactor/single-include: move.h

--- src/nvim/move.h
+++ src/nvim/move.h
@@ -2,6 +2,8 @@
 #define NVIM_MOVE_H
 
 #include <stdbool.h>
+#include "nvim/buffer_defs.h" // for win_T
+#include "nvim/pos.h" // for linenr_T
 
 #ifdef INCLUDE_GENERATED_DECLARATIONS
 # include "move.h.generated.h"
