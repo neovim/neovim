0f4b4c7529d6699bcae5c943a4bc27827c9aa8d6
ZyX <kp-pav@yandex.ru>
2017-04-08 18:23:02 +0300

    headers: Remove useless HAVE_CONFIG_H macros
    We do not have non-cmake build options, cmake always does configure_file.

--- src/nvim/vim.h
+++ src/nvim/vim.h
@@ -13,18 +13,16 @@
 
 /* ============ the header file puzzle (ca. 50-100 pieces) ========= */
 
-#ifdef HAVE_CONFIG_H    /* GNU autoconf (or something else) was here */
-# include "auto/config.h"
-# define HAVE_PATHDEF
+#include "auto/config.h"
+#define HAVE_PATHDEF
 
 /*
  * Check if configure correctly managed to find sizeof(int).  If this failed,
  * it becomes zero.  This is likely a problem of not being able to run the
  * test program.  Other items from configure may also be wrong then!
  */
-# if (SIZEOF_INT == 0)
-Error: configure did not run properly.Check auto/config.log.
-# endif
+#if (SIZEOF_INT == 0)
+# error Configure did not run properly.
 #endif
 
 #include "nvim/os/os_defs.h"       /* bring lots of system header files */
