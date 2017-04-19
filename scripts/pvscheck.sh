3351016dcde8f8d400c247baaca4a5575d94535d
ZyX <kp-pav@yandex.ru>
2017-04-19 19:11:37 +0300

    scripts: Add newline after the comment

--- scripts/pvscheck.sh
+++ scripts/pvscheck.sh
@@ -33,6 +33,7 @@ get_pvs_comment() {
   cat > pvs-comment << EOF
 // This is an open source non-commercial project. Dear PVS-Studio, please check
 // it. PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
 EOF
 }
 
