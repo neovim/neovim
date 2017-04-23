ce82b5f78cd0ebda59d009cc3cb15796346fb423
Patrick Jackson <PatrickSJackson@gmail.com>
2017-04-22 17:01:21 -0700

    Remove unused Hash Map implementation.

--- src/nvim/map.c
+++ src/nvim/map.c
@@ -142,7 +142,6 @@ static inline bool String_eq(String a, String b)
 
 
 MAP_IMPL(int, int, DEFAULT_INITIALIZER)
-MAP_IMPL(cstr_t, uint64_t, DEFAULT_INITIALIZER)
 MAP_IMPL(cstr_t, ptr_t, DEFAULT_INITIALIZER)
 MAP_IMPL(ptr_t, ptr_t, DEFAULT_INITIALIZER)
 MAP_IMPL(uint64_t, ptr_t, DEFAULT_INITIALIZER)
