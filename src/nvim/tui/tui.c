0e9c72364be61dfc9425a44440fe339f3451ffc6
James McCoy <jamessan@jamessan.com>
2017-04-25 12:25:27 -0400

    tui: Only set cursor color if the highlight group is valid
    Closes #6584

--- src/nvim/tui/tui.c
+++ src/nvim/tui/tui.c
@@ -587,9 +587,11 @@ static void tui_set_mode(UI *ui, ModeShape mode)
 
   if (c.id != 0 && ui->rgb) {
     int attr = syn_id2attr(c.id);
-    attrentry_T *aep = syn_cterm_attr2entry(attr);
-    data->params[0].i = aep->rgb_bg_color;
-    unibi_out(ui, data->unibi_ext.set_cursor_color);
+    if (attr > 0) {
+      attrentry_T *aep = syn_cterm_attr2entry(attr);
+      data->params[0].i = aep->rgb_bg_color;
+      unibi_out(ui, data->unibi_ext.set_cursor_color);
+    }
   }
 }
 
