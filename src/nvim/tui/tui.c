neovim/neovim

commit af2e629be4d20dda334a7c6ca817f5599956e4ff

tui: check stty/termios for kbs
Requires libtermkey 0.19+

Closes #2048
Closes #5693

See https://github.com/neovim/libtermkey/compare/a9b61424aae9f7548162ff112393c5f706cf54f1%5E...c0eb4e4a05f49ad8fee0195c77f2c29d09cc36af
See https://bugzilla.redhat.com/show_bug.cgi?id=142659
See https://github.com/tmux/tmux/blob/fe4e9470bb504357d073320f5d305b22663ee3fd/tty-keys.c#L625-L632

src/nvim/tui/tui.c
@@ -7,9 +7,13 @@
 
 #include <uv.h>
 #include <unibilium.h>
+#if defined(HAVE_TERMIOS_H)
+# include <termios.h>
+#endif
 
 #include "nvim/lib/kvec.h"
 
+#include "nvim/ascii.h"
 #include "nvim/vim.h"
 #include "nvim/log.h"
 #include "nvim/ui.h"
@@ -195,6 +199,9 @@ static void tui_terminal_start(UI *ui)
   terminfo_start(ui);
   update_size(ui);
   signal_watcher_start(&data->winch_handle, sigwinch_cb, SIGWINCH);
+
+  data->input.tk_ti_hook_fn = tui_tk_ti_getstr;
+  term_input_init(&data->input, data->loop);
   term_input_start(&data->input);
 }
 
@@ -227,8 +234,6 @@ static void tui_main(UIBridgeData *bridge, UI *ui)
   signal_watcher_init(data->loop, &data->winch_handle, ui);
   signal_watcher_init(data->loop, &data->cont_handle, data);
   signal_watcher_start(&data->cont_handle, sigcont_cb, SIGCONT);
-  // initialize input reading structures
-  term_input_init(&data->input, &tui_loop);
   tui_terminal_start(ui);
   data->stop = false;
   // allow the main thread to continue, we are ready to start handling UI
@@ -957,3 +962,50 @@ static void flush_buf(UI *ui, bool toggle_cursor)
     unibi_out(ui, unibi_cursor_invisible);
   }
 }
+
+/// Try to get "kbs" code from stty because "the terminfo kbs entry is extremely
+/// unreliable." (Vim, Bash, and tmux also do this.)
+///
+/// @see tmux/tty-keys.c fe4e9470bb504357d073320f5d305b22663ee3fd
+/// @see https://bugzilla.redhat.com/show_bug.cgi?id=142659
+static const char *tui_get_stty_erase(void)
+{
+  static char stty_erase[2] = { 0 };
+#if defined(ECHOE) && defined(ICANON) \
+  && (defined(HAVE_TERMIO_H) || defined(HAVE_TERMIOS_H))
+  struct termios t;
+  if (tcgetattr(input_global_fd(), &t) != -1) {
+    stty_erase[0] = (char)t.c_cc[VERASE];
+    stty_erase[1] = '\0';
+    ILOG("stty/termios:erase=%s", stty_erase);
+  }
+#endif
+  return stty_erase;
+}
+
+/// libtermkey hook to override terminfo entries.
+/// @see TermInput.tk_ti_hook_fn
+static const char *tui_tk_ti_getstr(const char *name, const char *value,
+                                    void *data)
+{
+  static const char *stty_erase = NULL;
+  if (stty_erase == NULL) {
+    stty_erase = tui_get_stty_erase();
+  }
+
+  if (strcmp(name, "key_backspace") == 0) {
+    ILOG("libtermkey:kbs=%s", value);
+    if (stty_erase != NULL && stty_erase[0] != 0) {
+      return stty_erase;
+    }
+  } else if (strcmp(name, "key_dc") == 0) {
+    ILOG("libtermkey:kdch1=%s", value);
+    // Vim: "If <BS> and <DEL> are now the same, redefine <DEL>."
+    if (stty_erase != NULL && strcmp(stty_erase, value) == 0) {
+      return stty_erase[0] == DEL ? (char *)CTRL_H_STR : (char *)DEL_STR;
+    }
+  }
+
+  return value;
+}
+

config/CMakeLists.txt
@@ -26,6 +26,8 @@ if(NOT HAVE_SYS_WAIT_H AND UNIX)
   message(SEND_ERROR "header sys/wait.h is required for Unix")
 endif()
 check_include_files(sys/utsname.h HAVE_SYS_UTSNAME_H)
+check_include_files(termio.h HAVE_TERMIO_H)
+check_include_files(termios.h HAVE_TERMIOS_H)
 check_include_files(utime.h HAVE_UTIME_H)
 check_include_files(sys/uio.h HAVE_SYS_UIO_H)
 

config/config.h.in
@@ -43,6 +43,8 @@
 #cmakedefine HAVE_STRNCASECMP
 #cmakedefine HAVE_SYS_UTSNAME_H
 #cmakedefine HAVE_SYS_WAIT_H
+#cmakedefine HAVE_TERMIO_H
+#cmakedefine HAVE_TERMIOS_H
 #cmakedefine HAVE_UTIME
 #cmakedefine HAVE_UTIME_H
 #cmakedefine HAVE_UTIMES

src/nvim/os/input.c
@@ -39,8 +39,6 @@ static int events_enabled = 0;
 #ifdef INCLUDE_GENERATED_DECLARATIONS
 # include "os/input.c.generated.h"
 #endif
-// Helper function used to push bytes from the 'event' key sequence partially
-// between calls to os_inchar when maxlen < 3
 
 void input_init(void)
 {
@@ -389,6 +387,8 @@ static void process_interrupts(void)
   }
 }
 
+// Helper function used to push bytes from the 'event' key sequence partially
+// between calls to os_inchar when maxlen < 3
 static int push_event_key(uint8_t *buf, int maxlen)
 {
   static const uint8_t key[3] = { K_SPECIAL, KS_EXTRA, KE_EVENT };

src/nvim/tui/input.c
@@ -32,7 +32,14 @@ void term_input_init(TermInput *input, Loop *loop)
     term = "";  // termkey_new_abstract assumes non-null (#2745)
   }
 
+#if TERMKEY_VERSION_MAJOR > 0 || TERMKEY_VERSION_MINOR > 18
+  input->tk = termkey_new_abstract(term,
+                                   TERMKEY_FLAG_UTF8 | TERMKEY_FLAG_NOSTART);
+  termkey_hook_terminfo_getstr(input->tk, input->tk_ti_hook_fn, NULL);
+  termkey_start(input->tk);
+#else
   input->tk = termkey_new_abstract(term, TERMKEY_FLAG_UTF8);
+#endif
 
   int curflags = termkey_get_canonflags(input->tk);
   termkey_set_canonflags(input->tk, curflags | TERMKEY_CANON_DELBS);

src/nvim/tui/input.h
@@ -12,6 +12,7 @@ typedef struct term_input {
   bool paste_enabled;
   bool waiting;
   TermKey *tk;
+  TermKey_Terminfo_Getstr_Hook *tk_ti_hook_fn;  ///< libtermkey terminfo hook
   TimeWatcher timer_handle;
   Loop *loop;
   Stream read_stream;

third-party/CMakeLists.txt
@@ -105,8 +105,8 @@ set(LUAROCKS_SHA256 cae709111c5701235770047dfd7169f66b82ae1c7b9b79207f9df0afb722
 set(UNIBILIUM_URL https://github.com/mauke/unibilium/archive/v1.2.0.tar.gz)
 set(UNIBILIUM_SHA256 623af1099515e673abfd3cae5f2fa808a09ca55dda1c65a7b5c9424eb304ead8)
 
-set(LIBTERMKEY_URL http://www.leonerd.org.uk/code/libtermkey/libtermkey-0.18.tar.gz)
-set(LIBTERMKEY_SHA256 239746de41c845af52bb3c14055558f743292dd6c24ac26c2d6567a5a6093926)
+set(LIBTERMKEY_URL http://www.leonerd.org.uk/code/libtermkey/libtermkey-0.19.tar.gz)
+set(LIBTERMKEY_SHA256 c505aa4cb48c8fa59c526265576b97a19e6ebe7b7da20f4ecaae898b727b48b7)
 
 set(LIBVTERM_URL https://github.com/neovim/libvterm/archive/a9c7c6fd20fa35e0ad3e0e98901ca12dfca9c25c.tar.gz)
 set(LIBVTERM_SHA256 1a4272be91d9614dc183a503786df83b6584e4afaab7feaaa5409f841afbd796)

commit 3b62ac68bfbee232c886990fda16d2533e60d7dd

tui.c: Initialize TUI input component only once.
term_input_start should be called only once. This fixes a leak
introduced by af2e629be4d20dda334a7c6ca817f5599956e4ff.

Closes #6780

Steps to demonstrate memory leak:

    CC=clang CFLAGS=" -O0 -g -DEXITFREE " cmake .. -DMIN_LOG_LEVEL=0 -DCMAKE_BUILD_TYPE=Debug -DBUSTED_OUTPUT_TYPE=nvim -DCMAKE_INSTALL_PREFIX=$PWD/root -DCLANG_ASAN_UBSAN=ON -DPREFER_LUAJIT=false
    nvim -u NONE -i NONE --cmd $'function S()\nsuspend\nendfunction' --cmd 'inoremap <expr> X S()' --cmd 'call feedkeys("iX", "t")'
    fg<CR><Esc>:cq<CR>

```
=================================================================
==25050==ERROR: LeakSanitizer: detected memory leaks

Direct leak of 4159 byte(s) in 1 object(s) allocated from:
    #0 0x4f6a30 in calloc /var/tmp/portage/sys-devel/llvm-3.9.1-r1/work/llvm-3.9.1.src/projects/compiler-rt/lib/asan/asan_malloc_linux.cc:72
    #1 0xf8d222 in xcalloc /home/zyx/a.a/Proj/c/neovim/src/nvim/memory.c:147:15
    #2 0x1349d28 in rbuffer_new /home/zyx/a.a/Proj/c/neovim/src/nvim/rbuffer.c:24:17
    #3 0xa6867b in rstream_init /home/zyx/a.a/Proj/c/neovim/src/nvim/event/rstream.c:42:20
    #4 0xa68651 in rstream_init_fd /home/zyx/a.a/Proj/c/neovim/src/nvim/event/rstream.c:28:3
    #5 0x1866451 in term_input_init /home/zyx/a.a/Proj/c/neovim/src/nvim/tui/input.c:55:3
    #6 0x187f049 in tui_terminal_start /home/zyx/a.a/Proj/c/neovim/src/nvim/tui/tui.c:223:3
    #7 0x187b491 in tui_main /home/zyx/a.a/Proj/c/neovim/src/nvim/tui/tui.c:258:3
    #8 0x18b3171 in ui_thread_run /home/zyx/a.a/Proj/c/neovim/src/nvim/ui_bridge.c:124:3
    #9 0x7f54c2d5f39b  (/lib64/libpthread.so.0+0x739b)

Direct leak of 4159 byte(s) in 1 object(s) allocated from:
    #0 0x4f6a30 in calloc /var/tmp/portage/sys-devel/llvm-3.9.1-r1/work/llvm-3.9.1.src/projects/compiler-rt/lib/asan/asan_malloc_linux.cc:72
    #1 0xf8d222 in xcalloc /home/zyx/a.a/Proj/c/neovim/src/nvim/memory.c:147:15

src/nvim/tui/tui.c
@@ -216,11 +216,6 @@ static void tui_terminal_start(UI *ui)
   terminfo_start(ui);
   update_size(ui);
   signal_watcher_start(&data->winch_handle, sigwinch_cb, SIGWINCH);
-
-#if TERMKEY_VERSION_MAJOR > 0 || TERMKEY_VERSION_MINOR > 18
-  data->input.tk_ti_hook_fn = tui_tk_ti_getstr;
-#endif
-  term_input_init(&data->input, data->loop);
   term_input_start(&data->input);
 }
 
@@ -255,8 +250,14 @@ static void tui_main(UIBridgeData *bridge, UI *ui)
 #ifdef UNIX
   signal_watcher_start(&data->cont_handle, sigcont_cb, SIGCONT);
 #endif
+
+#if TERMKEY_VERSION_MAJOR > 0 || TERMKEY_VERSION_MINOR > 18
+  data->input.tk_ti_hook_fn = tui_tk_ti_getstr;
+#endif
+  term_input_init(&data->input, &tui_loop);
   tui_terminal_start(ui);
   data->stop = false;
+
   // allow the main thread to continue, we are ready to start handling UI
   // callbacks
   CONTINUE(bridge);