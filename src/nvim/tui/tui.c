1f3b5e1a826fbff8ce2fa84e0e50d082d65a08bd
Jonathan de Boyne Pollard <J.deBoynePollard-newsgroups@NTLWorld.com>
2017-05-27 19:04:06 +0100

    tui: Correct error in terminfo extension processing. Using the wrong unibilium query function just happened to work with the various terminfo records used in local testing.

--- src/nvim/tui/tui.c
+++ src/nvim/tui/tui.c
@@ -1133,7 +1133,7 @@ static void unibi_set_if_empty(unibi_term *ut, enum unibi_string str,
 
 static int unibi_find_ext_str(unibi_term *ut, const char *name)
 {
-  size_t max = unibi_count_ext_bool(ut);
+  size_t max = unibi_count_ext_str(ut);
   for (size_t i = 0; i < max; ++i) {
     const char * n = unibi_get_ext_str_name(ut, i);
     if (0 == strcmp(n, name)) {
