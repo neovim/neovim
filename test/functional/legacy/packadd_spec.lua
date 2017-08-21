536aecd20481ee0216b9362f9dc39a57d2fdefde
James McCoy <jamessan@jamessan.com>
2017-08-21 12:46:07 -0400

    vim-patch:8.0.0437
    Problem:    The packadd test does not create the symlink correctly and does
                not test the right thing.
    Solution:   Create the directory and symlink correctly.
    
    https://github.com/vim/vim/commit/644df41c44cbdfacdedbba55ef77a6c6031eccd8

--- test/functional/legacy/packadd_spec.lua
+++ test/functional/legacy/packadd_spec.lua
@@ -89,7 +89,8 @@ describe('packadd', function()
         endif
         let top2_dir = s:topdir . '/Xdir2'
         let real_dir = s:topdir . '/Xsym'
-        exec "silent! !ln -s" real_dir top2_dir
+        call mkdir(real_dir, 'p')
+        exec "silent! !ln -s Xsym" top2_dir
         let &rtp = top2_dir . ',' . top2_dir . '/after'
         let &packpath = &rtp
 
