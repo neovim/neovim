neovim/neovim

commit c484323dc67fbca10a2f3da3d6e65efdb3c678c3

terminal.c/redraw(): Remove cargo cult.

src/nvim/terminal.c
@@ -1126,14 +1126,6 @@ static void redraw(bool restore_cursor)
     update_screen(0);
   }
 
-  redraw_statuslines();
-
-  if (need_maketitle) {
-    maketitle();
-  }
-
-  showruler(false);
-
   if (term && is_focused(term)) {
     curwin->w_wrow = term->cursor.row;
     curwin->w_wcol = term->cursor.col + win_col_off(curwin);

commit 06f8ad5b2af59f61f053387d1347f6b1eada850e

terminal.c: need maketitle() in terminal-mode
normal_redraw() usually takes care of this, but that doesn't happen
during terminal-mode.

regression by c484323dc67f
steps to reproduce:
    nvim -u NORC --cmd 'execute("set titlestring=" . $NVIM_LISTEN_ADDRESS) | set title | startinsert | !sleep 1' term://sh

closes #7248

src/nvim/screen.c
@@ -5847,7 +5847,7 @@ static void screen_start_highlight(int attr)
   ui_start_highlight(attr);
 }
 
-void screen_stop_highlight(void)
+static void screen_stop_highlight(void)
 {
   ui_stop_highlight();
   screen_attr = 0;

src/nvim/terminal.c
@@ -1259,6 +1259,10 @@ static void redraw(bool restore_cursor)
     update_screen(0);
   }
 
+  if (need_maketitle) {  // Update title in terminal-mode. #7248
+    maketitle();
+  }
+
   if (restore_cursor) {
     ui_cursor_goto(save_row, save_col);
   } else if (term) {