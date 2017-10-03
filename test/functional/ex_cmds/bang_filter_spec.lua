91f5f81cb8fbd09a97b3a3326d018ec8687cbdce
Jan Edmund Lazo <janedmundlazo@hotmail.com>
2017-10-03 17:39:17 -0400

    win: enable backtick_expansion and bang_filter tests

--- test/functional/ex_cmds/bang_filter_spec.lua
+++ test/functional/ex_cmds/bang_filter_spec.lua
@@ -4,8 +4,6 @@ local helpers = require('test.functional.helpers')(after_each)
 local feed, command, clear = helpers.feed, helpers.command, helpers.clear
 local mkdir, write_file, rmdir = helpers.mkdir, helpers.write_file, helpers.rmdir
 
-if helpers.pending_win32(pending) then return end
-
 local Screen = require('test.functional.ui.screen')
 
 
@@ -28,7 +26,17 @@ describe('issues', function()
   end)
 
   it('#3269 Last line of shell output is not truncated', function()
-    command([[nnoremap <silent>\l :!ls bang_filter_spec<cr>]])
+    local map = [[nnoremap <silent>\l :!ls bang_filter_spec<cr>]]
+    local result = [[
+  :!ls bang_filter_spec                                |
+  ]]
+    if helpers.iswin() then
+      map = [[nnoremap <silent>\l :!dir /b bang_filter_spec<cr>]]
+      result = [[
+  :!dir /b bang_filter_spec                            |
+  ]]
+    end
+    command(map)
     feed([[\l]])
     screen:expect([[
       ~                                                    |
@@ -39,8 +47,9 @@ describe('issues', function()
       ~                                                    |
       ~                                                    |
       ~                                                    |
-      :!ls bang_filter_spec                                |
-                                                           |
+    ]]
+    .. result .. [[
+                                                         |
       f1                                                   |
       f2                                                   |
       f3                                                   |
