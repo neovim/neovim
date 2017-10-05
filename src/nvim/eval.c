ccabedf5f24c54cbf94e8a767a455d80921ad230
Andrew Ferreira <acferreir4@gmail.com>
2017-10-05 18:03:11 -0400

    eval.c: Fix lint issues

--- src/nvim/eval.c
+++ src/nvim/eval.c
@@ -6739,31 +6739,33 @@ static void prepare_assert_error(garray_T *gap)
  */
 static void ga_concat_esc(garray_T *gap, char_u *str)
 {
-    char_u *p;
-    char_u buf[NUMBUFLEN];
-
-    if (str == NULL) {
-	ga_concat(gap, (char_u *)"NULL");
-	return;
+  char_u *p;
+  char_u buf[NUMBUFLEN];
+  
+  if (str == NULL) {
+    ga_concat(gap, (char_u *)"NULL");
+    return;
+  }
+  
+  for (p = str; *p != NUL; ++p) {
+    switch (*p) {
+      case BS: ga_concat(gap, (char_u *)"\\b"); break;
+      case ESC: ga_concat(gap, (char_u *)"\\e"); break;
+      case FF: ga_concat(gap, (char_u *)"\\f"); break;
+      case NL: ga_concat(gap, (char_u *)"\\n"); break;
+      case TAB: ga_concat(gap, (char_u *)"\\t"); break;
+      case CAR: ga_concat(gap, (char_u *)"\\r"); break;
+      case '\\': ga_concat(gap, (char_u *)"\\\\"); break;
+      default:
+        if (*p < ' ') {
+          vim_snprintf((char *)buf, NUMBUFLEN, "\\x%02x", *p);
+          ga_concat(gap, buf);
+        } else {
+          ga_append(gap, *p);
+        }
+        break;
     }
-
-    for (p = str; *p != NUL; ++p)
-	switch (*p) {
-	    case BS: ga_concat(gap, (char_u *)"\\b"); break;
-	    case ESC: ga_concat(gap, (char_u *)"\\e"); break;
-	    case FF: ga_concat(gap, (char_u *)"\\f"); break;
-	    case NL: ga_concat(gap, (char_u *)"\\n"); break;
-	    case TAB: ga_concat(gap, (char_u *)"\\t"); break;
-	    case CAR: ga_concat(gap, (char_u *)"\\r"); break;
-	    case '\\': ga_concat(gap, (char_u *)"\\\\"); break;
-	    default:
-		if (*p < ' ') {
-		    vim_snprintf((char *)buf, NUMBUFLEN, "\\x%02x", *p);
-		    ga_concat(gap, buf);
-		} else
-		    ga_append(gap, *p);
-		break;
-	}
+  }
 }
 
 // Fill "gap" with information about an assert error.
