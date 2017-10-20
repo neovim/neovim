15efbb99c4ff856993cd90e88e6a9da32d720b98
Jan Edmund Lazo <janedmundlazo@hotmail.com>
2017-10-19 23:10:02 -0400

    win: enable options/autochdir_spec.lua

--- test/functional/options/autochdir_spec.lua
+++ test/functional/options/autochdir_spec.lua
@@ -3,8 +3,6 @@ local clear = helpers.clear
 local eq = helpers.eq
 local getcwd = helpers.funcs.getcwd
 
-if helpers.pending_win32(pending) then return end
-
 describe("'autochdir'", function()
   it('given on the shell gets processed properly', function()
     local targetdir = 'test/functional/fixtures'
@@ -12,9 +10,14 @@ describe("'autochdir'", function()
     -- By default 'autochdir' is off, thus getcwd() returns the repo root.
     clear(targetdir..'/tty-test.c')
     local rootdir = getcwd()
+    local expected = rootdir .. '/' .. targetdir
+
+    if helpers.iswin() then
+      expected = expected:gsub('/', '\\')
+    end
 
     -- With 'autochdir' on, we should get the directory of tty-test.c.
     clear('--cmd', 'set autochdir', targetdir..'/tty-test.c')
-    eq(rootdir..'/'..targetdir, getcwd())
+    eq(expected, getcwd())
   end)
 end)
