bcf266de46b054b564727d5d31e4421ba09f5704
Justin M. Keyes <justinkz@gmail.com>
2017-10-28 17:54:29 +0200

    test: :terminal + numberwidth=9

--- src/nvim/screen.c
+++ src/nvim/screen.c
@@ -698,12 +698,12 @@ static void win_update(win_T *wp)
                       (uint16_t)(MAX(0, curwin->w_width - win_col_off(curwin))),
                       (uint16_t)curwin->w_height);
     }
-  } else if (buf->b_mod_set && buf->b_mod_xlines != 0 && wp->w_redraw_top != 0) {
-    /*
-     * When there are both inserted/deleted lines and specific lines to be
-     * redrawn, w_redraw_top and w_redraw_bot may be invalid, just redraw
-     * everything (only happens when redrawing is off for while).
-     */
+  } else if (buf->b_mod_set
+             && buf->b_mod_xlines != 0
+             && wp->w_redraw_top != 0) {
+    // When there are both inserted/deleted lines and specific lines to be
+    // redrawn, w_redraw_top and w_redraw_bot may be invalid, just redraw
+    // everything (only happens when redrawing is off for while).
     type = NOT_VALID;
   } else {
     /*
