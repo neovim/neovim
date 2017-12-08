1f2b35860f755513a58c1d010f082d946c889b47
James McCoy <jamessan@jamessan.com>
2017-12-07 20:53:02 -0500

    mac: Set $LANG based on the system locale
    Unix's typical locale-related environment variables aren't always set
    appropriately on a Mac.  Instead of relying on them, query the locale
    information using Mac specific APIs and then set $LANG appropriately for
    the rest of nvim.
    
    Closes #5873

---
+++ src/nvim/os/lang.c
@@ -0,0 +1,40 @@
+#ifdef __APPLE__
+# define Boolean CFBoolean  // Avoid conflict with API's Boolean
+# include <CoreFoundation/CFLocale.h>
+# include <CoreFoundation/CFString.h>
+# undef Boolean
+#endif
+
+#ifdef HAVE_LOCALE_H
+# include <locale.h>
+#endif
+#include "nvim/os/os.h"
+
+void lang_init(void)
+{
+#ifdef __APPLE__
+  if (os_getenv("LANG") == NULL) {
+    CFLocaleRef cf_locale = CFLocaleCopyCurrent();
+    CFTypeRef cf_lang_region = CFLocaleGetValue(cf_locale,
+                                                kCFLocaleIdentifier);
+    CFRetain(cf_lang_region);
+    CFRelease(cf_locale);
+
+    const char *lang_region = CFStringGetCStringPtr(cf_lang_region,
+                                                    kCFStringEncodingUTF8);
+    if (lang_region) {
+      os_setenv("LANG", lang_region, true);
+    } else {
+      char buf[20] = { 0 };
+      if (CFStringGetCString(cf_lang_region, buf, 20,
+                             kCFStringEncodingUTF8)) {
+        os_setenv("LANG", lang_region, true);
+      }
+    }
+    CFRelease(cf_lang_region);
+# ifdef HAVE_LOCALE_H
+    setlocale(LC_ALL, "");
+# endif
+  }
+#endif
+}
