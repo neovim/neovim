9370a0e5d859f692f6fa24c27c76410b54595be4
Jan Edmund Lazo <jan.lazo@mail.utoronto.ca>
2018-01-07 18:37:31 -0500

    ci/travis: install neovim npm module on osx (#7825)
    Always get latest nvm on osx to fix lts aliases.

--- ci/before_install.sh
+++ ci/before_install.sh
@@ -38,14 +38,12 @@ else
   pip3 -q install --user --upgrade pip || true
 fi
 
-if [[ "${TRAVIS_OS_NAME}" == linux ]]; then
-  echo "Install node (LTS)"
+echo "Install node (LTS)"
 
-  if [ ! -f ~/.nvm/nvm.sh ]; then
-    curl -o ~/.nvm/nvm.sh https://raw.githubusercontent.com/creationix/nvm/master/nvm.sh
-  fi
-
-  source ~/.nvm/nvm.sh
-  nvm install --lts
-  nvm use --lts
+if [[ "${TRAVIS_OS_NAME}" == osx ]] || [ ! -f ~/.nvm/nvm.sh ]; then
+  curl -o ~/.nvm/nvm.sh https://raw.githubusercontent.com/creationix/nvm/master/nvm.sh
 fi
+
+source ~/.nvm/nvm.sh
+nvm install --lts
+nvm use --lts
