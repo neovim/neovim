5a39d2d00e398b404b4db7ec45b2f490d5bb845e
Jan Edmund Lazo <janedmundlazo@hotmail.com>
2018-01-13 08:25:11 -0500

    test: fnamemodify with :8 filename modifier

--- test/functional/eval/fnamemodify_spec.lua
+++ test/functional/eval/fnamemodify_spec.lua
@@ -4,9 +4,18 @@ local eq = helpers.eq
 local iswin = helpers.iswin
 local fnamemodify = helpers.funcs.fnamemodify
 local command = helpers.command
+local write_file = helpers.write_file
 
 describe('fnamemodify()', function()
+  setup(function()
+    write_file('Xtest-fnamemodify.txt', [[foobar]])
+  end)
+
   before_each(clear)
+
+  teardown(function()
+    os.remove('Xtest-fnamemodify.txt')
+  end)
 
   it('works', function()
     if iswin() then
@@ -28,5 +37,9 @@ describe('fnamemodify()', function()
       eq('/', fnamemodify([[/]], ':p:h'))
       eq('/', fnamemodify([[/]], ':p'))
     end
+  end)
+
+  it(':8 works', function()
+    eq('Xtest-fnamemodify.txt', fnamemodify([[Xtest-fnamemodify.txt]], ':8'))
   end)
 end)
