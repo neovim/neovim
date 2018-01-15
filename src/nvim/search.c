670164dde166c8e851b2249a152eefcd5c269a32
Daniel Hahler <git@thequod.de>
2018-01-15 19:46:12 +0100

    fixup! fixup! WIP: Fix passed timeout with backwards searchpair

--- src/nvim/search.c
+++ src/nvim/search.c
@@ -743,7 +743,7 @@ int searchit(
               }
               if (ptr[matchcol] == NUL
                   || (nmatched = vim_regexec_multi(
-                      &regmatch, hjwin, buf, lnum + matchpos.lnum, matchcol,
+                      &regmatch, win, buf, lnum + matchpos.lnum, matchcol,
                       tm)) == 0) {
                 if (tm != NULL && profile_passed_limit(*tm)) {
                   match_ok = false;
