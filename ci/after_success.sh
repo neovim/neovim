6e2bb564e15f69f88f0bd73c7f6eaa9b1dc2eff3
James McCoy <jamessan@jamessan.com>
2018-02-06 21:18:54 -0500

    ci: Remove coveralls coverage reporting

--- ci/after_success.sh
+++ ci/after_success.sh
@@ -4,6 +4,5 @@ set -e
 set -o pipefail
 
 if [[ -n "${GCOV}" ]]; then
-  coveralls --gcov "$(which "${GCOV}")" --encoding iso-8859-1 || echo 'coveralls upload failed.'
   bash <(curl -s https://codecov.io/bash) || echo 'codecov upload failed.'
 fi
