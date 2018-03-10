45e81e03f880f9ad6f23e57eed00d5178f47fd2f
Justin M. Keyes <justinkz@gmail.com>
2018-03-10 20:43:51 +0100

    ci/macOS: skip python2 on travis macOS
    macOS travis builds recently started failing (travis caches were cleared
    recently, maybe related). python2 is reasonably covered by linux CI. Not
    going to waste time on it for macOS CI.
    
        ==> Installing python@2
        ==> Downloading https://homebrew.bintray.com/bottles/python@2-2.7.14_3.el_capita
        ==> Pouring python@2-2.7.14_3.el_capitan.bottle.tar.gz
        Error: The `brew link` step did not complete successfully
        The formula built, but is not symlinked into /usr/local
        Could not symlink bin/2to3-2
        Target /usr/local/bin/2to3-2
        is a symlink belonging to python. You can unlink it:
          brew unlink python
        To force the link and overwrite all conflicting files:
          brew link --overwrite python@2
        To list all files that would be deleted:
          brew link --overwrite --dry-run python@2
        Possible conflicting files are:
        /usr/local/bin/2to3-2 -> /usr/local/Cellar/python/2.7.12_1/bin/2to3-2
        /usr/local/bin/2to3-2.7 -> /usr/local/Cellar/python/2.7.12_1/bin/2to3-2.7
        /usr/local/bin/idle -> /usr/local/Cellar/python/2.7.12_1/bin/idle
        ...

--- ci/install.sh
+++ ci/install.sh
@@ -13,16 +13,18 @@ if [[ "${TRAVIS_OS_NAME}" == osx ]]; then
   brew reinstall -s libtool
 fi
 
-# Use default CC to avoid compilation problems when installing Python modules.
-echo "Install neovim module for Python 2."
-CC=cc pip2.7 -q install --user --upgrade neovim
-
 echo "Install neovim module for Python 3."
 # Allow failure. pyenv pip3 on travis is broken:
 # https://github.com/travis-ci/travis-ci/issues/8363
 CC=cc pip3 -q install --user --upgrade neovim || true
 
 if ! [ "${TRAVIS_OS_NAME}" = osx ] ; then
+  # Update PATH for pip.
+  export PATH="$(python2.7 -c 'import site; print(site.getuserbase())')/bin:$PATH"
+  # Use default CC to avoid compilation problems when installing Python modules.
+  echo "Install neovim module for Python 2."
+  CC=cc pip2.7 -q install --user --upgrade neovim
+
   echo "Install neovim RubyGem."
   gem install --no-document --version ">= 0.2.0" neovim
 fi
