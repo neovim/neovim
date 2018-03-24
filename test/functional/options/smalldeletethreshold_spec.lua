667fc065cb720bc484a929446195ee8648891f9b
Himura Kazuto <glagol15@gmail.com>
2018-03-24 16:47:02 +0300

    Improve default test

--- test/functional/options/smalldeletethreshold_spec.lua
+++ test/functional/options/smalldeletethreshold_spec.lua
@@ -6,26 +6,28 @@ local eq = helpers.eq
 local getreg = helpers.funcs.getreg
 local reset, clear = shada_helpers.reset, shada_helpers.clear
 
+-- 'smalldeletethreshold' configures the number of characters 
+--  whose yank or deletion causes push into the "1.."9 registers stack.
+--  Possible values:
+--  * 0 - default behavior (deletions larger than 1 line are considered big)
+--  * 1 - any deletion (including x) appears in the "1.."9 registers
+--  * N - deletions of N or more characters are considered big
 describe("'smalldeletethreshold' option", function()
   before_each(reset)
   after_each(clear)
 
-  -- 'smalldeletethreshold' configures the number of characters 
-  --  whose yank or deletion causes push into the "1.."9 registers stack.
-  --  Possible values:
-  --  * 0 - default behavior (deletions larger than 1 line are considered big)
-  --  * 1 - any deletion (including x) appears in the "1.."9 registers
-  --  * N - deletions of N or more characters are considered big
   it("default behavior", function()
     -- Add some text
-    feed('iaaa bbb ccc<Esc>^')
-    -- Delete two words and then the whole line
-    feed('dwdwdd')
-    
+    feed('iaaa bbb ccc\nd<Esc>k')
+
+    -- Delete a word, a line and a char
+    feed('2dwddx')
+
     -- Check the registers
-    eq(getreg('"'), 'ccc\n')
+    eq(getreg('"'), 'd')
     eq(getreg('1'), 'ccc\n')
     eq(getreg('2'), '')
-    eq(getreg('-'), 'bbb ')
+
+    -- As you see, 'aaa bbb' is completely lost, which is not good...
   end)
 end)
