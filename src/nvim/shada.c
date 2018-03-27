3df11cfbca3dc037974eb6c61d8f9413b3b7ca9c
ZyX <kp-pav@yandex.ru>
2018-03-27 03:03:02 +0300

    Revert "shada: In place of ignoring cursor position with lnum 0 save with 1"
    This reverts commit aa728798b4bd89b59cee86103885c15d386f73ba.

--- src/nvim/shada.c
+++ src/nvim/shada.c
@@ -2796,7 +2796,7 @@ static ShaDaWriteResult shada_write(ShaDaWriteDef *const sd_writer,
 
   // Update numbered marks: '0' should be replaced with the current position,
   // '9' should be removed and all other marks shifted.
-  if (!ignore_buf(curbuf, &removable_bufs)) {
+  if (!ignore_buf(curbuf, &removable_bufs) && curwin->w_cursor.lnum != 0) {
     replace_numbered_mark(wms, 0, (PossiblyFreedShadaEntry) {
       .can_free_entry = false,
       .data = {
@@ -2804,9 +2804,7 @@ static ShaDaWriteResult shada_write(ShaDaWriteDef *const sd_writer,
         .timestamp = os_time(),
         .data = {
           .filemark = {
-            .mark = (curwin->w_cursor.lnum
-                     ? curwin->w_cursor
-                     : (pos_T) { 1, 0, 0 }),
+            .mark = curwin->w_cursor,
             .name = '0',
             .additional_data = NULL,
             .fname = (char *)curbuf->b_ffname,
