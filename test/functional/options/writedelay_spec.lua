442d869495b6f8cbbeb5013bbf107f65690a48ff
geekodour <hrishikeshbman@gmail.com>
2018-04-05 23:04:12 +0530

    wip

--- test/functional/options/writedelay_spec.lua
+++ test/functional/options/writedelay_spec.lua
@@ -1,10 +1,14 @@
 local helpers = require('test.functional.helpers')(after_each)
 local clear = helpers.clear
-local eq = helpers.eq
+local eq, feed = helpers.eq, helpers.feed
+local eval = helpers.eval
+local curbuf, curwin = helpers.curbuf, helpers.curwin
 local getcwd = helpers.funcs.getcwd
 
 describe("writedelay option changes", function()
   it('wd sets to 0 on <C-C><C-C>', function()
-
+    feed(':set writedelay=10<CR>')
+    feed('<c-c>')
+    eq(0, curbuf('get_option','writedelay'))
   end)
 end)
