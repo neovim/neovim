522443d6bfa155a71968e886cc7869c2d370cc47
Justin M. Keyes <justinkz@gmail.com>
2018-04-18 09:47:51 +0200

    test/util: retry(): also decorate non-string error

--- test/functional/helpers.lua
+++ test/functional/helpers.lua
@@ -306,10 +306,7 @@ local function retry(max, max_ms, fn)
     end
     luv.update_time()  -- Update cached value of luv.now() (libuv: uv_now()).
     if (max and tries >= max) or (luv.now() - start_time > timeout) then
-      if type(result) == "string" then
-        result = "\nretry() attempts: "..tostring(tries).."\n"..result
-      end
-      error(result)
+      error("\nretry() attempts: "..tostring(tries).."\n"..tostring(result))
     end
     tries = tries + 1
   end
