a02d22cca825f2c04381b40d50abfc7a15afec20
Justin M. Keyes <justinkz@gmail.com>
2018-04-21 00:28:53 +0200

    IO: shada should respect 'fsync' option
    shada_write_file() is called on exit (:quit and friends), this can be
    very slow.
    
    Note: AFAICT Vim (do_viminfo()) does not appear to fsync() viminfo.

--- src/nvim/event/process.c
+++ src/nvim/event/process.c
@@ -265,7 +265,7 @@ static void process_close_event(void **argv)
   if (proc->type == kProcessTypePty) {
     xfree(((PtyProcess *)proc)->term_name);
   }
-  if (proc->cb) {
+  if (proc->cb) {  // "on_exit" for jobstart(). See channel_job_start().
     proc->cb(proc, proc->status, proc->data);
   }
 }
