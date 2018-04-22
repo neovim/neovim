0eaecbaf47704b8b0b108dd44ad949f3cc9cf501
ZyX <kp-pav@yandex.ru>
2018-04-22 20:05:32 +0300

    strings: Fix PVS/V547: condition already checked by surrounding if

--- src/nvim/strings.c
+++ src/nvim/strings.c
@@ -1376,16 +1376,14 @@ int vim_vsnprintf(char *str, size_t str_m, const char *fmt, va_list ap,
         }
 
         // insert zero padding as requested by precision or min field width
-        if (number_of_zeros_to_pad > 0) {
-          size_t zn = number_of_zeros_to_pad;
-          if (str_avail) {
-            size_t avail = str_m - str_l;
-            memset(str + str_l, '0', MIN(zn, avail));
-            str_avail = zn < avail;
-          }
-          assert(zn <= SIZE_MAX - str_l);
-          str_l += zn;
+        size_t zn = number_of_zeros_to_pad;
+        if (str_avail) {
+          size_t avail = str_m - str_l;
+          memset(str + str_l, '0', MIN(zn, avail));
+          str_avail = zn < avail;
         }
+        assert(zn <= SIZE_MAX - str_l);
+        str_l += zn;
       }
 
       // insert formatted string
