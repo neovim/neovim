neovim/neovim

commit 98e71123900fbdf26a16a43297a1f58118cde41b

msg: do not scroll entire screen (#8088)

src/nvim/screen.c
@@ -300,13 +300,25 @@ void update_screen(int type)
    * if the screen was scrolled up when displaying a message, scroll it down
    */
   if (msg_scrolled) {
-    clear_cmdline = TRUE;
-    if (msg_scrolled > Rows - 5)            /* clearing is faster */
+    clear_cmdline = true;
+    if (dy_flags & DY_MSGSEP) {
+      FOR_ALL_WINDOWS_IN_TAB(wp, curtab) {
+        int valid = Rows - msg_scrollsize();
+        if (wp->w_winrow + wp->w_height > valid) {
+          wp->w_redr_type = NOT_VALID;
+          wp->w_lines_valid = 0;
+        }
+        if (wp->w_winrow + wp->w_height + wp->w_status_height > valid) {
+          wp->w_redr_status = true;
+        }
+      }
+    } else if (msg_scrolled > Rows - 5) {  // clearing is faster
       type = CLEAR;
-    else if (type != CLEAR) {
-      check_for_delay(FALSE);
-      if (screen_ins_lines(0, 0, msg_scrolled, (int)Rows, NULL) == FAIL)
+    } else if (type != CLEAR) {
+      check_for_delay(false);
+      if (screen_ins_lines(0, 0, msg_scrolled, (int)Rows, NULL) == FAIL) {
         type = CLEAR;
+      }
       FOR_ALL_WINDOWS_IN_TAB(wp, curtab) {
         if (wp->w_winrow < msg_scrolled) {
           if (wp->w_winrow + wp->w_height > msg_scrolled

test/functional/ui/screen_basic_spec.lua
@@ -355,7 +355,8 @@ describe('Screen', function()
       ]])
     end)
 
-    it('execute command with multi-line output', function()
+    it('execute command with multi-line output without msgsep', function()
+      command("set display-=msgsep")
       feed(':ls<cr>')
       screen:expect([[
         {0:~                                                    }|
@@ -369,6 +370,28 @@ describe('Screen', function()
         {0:~                                                    }|
         {0:~                                                    }|
         {0:~                                                    }|
+        :ls                                                  |
+          1 %a   "[No Name]"                    line 1       |
+        {7:Press ENTER or type command to continue}^              |
+      ]])
+      feed('<cr>') --  skip the "Press ENTER..." state or tests will hang
+    end)
+
+    it('execute command with multi-line output and with msgsep', function()
+      command("set display+=msgsep")
+      feed(':ls<cr>')
+      screen:expect([[
+                                                             |
+        {0:~                                                    }|
+        {0:~                                                    }|
+        {0:~                                                    }|
+        {0:~                                                    }|
+        {0:~                                                    }|
+        {0:~                                                    }|
+        {0:~                                                    }|
+        {0:~                                                    }|
+        {0:~                                                    }|
+        {1:                                                     }|
         :ls                                                  |
           1 %a   "[No Name]"                    line 1       |
         {7:Press ENTER or type command to continue}^              |
@@ -573,6 +596,7 @@ describe('Screen', function()
       command('nnoremap <F1> :echo "TEST"<CR>')
       feed(':ls<CR>')
       screen:expect([[
+                                                             |
         {0:~                                                    }|
         {0:~                                                    }|
         {0:~                                                    }|
@@ -582,8 +606,7 @@ describe('Screen', function()
         {0:~                                                    }|
         {0:~                                                    }|
         {0:~                                                    }|
-        {0:~                                                    }|
-        {0:~                                                    }|
+        {1:                                                     }|
         :ls                                                  |
           1 %a   "[No Name]"                    line 1       |
         {7:Press ENTER or type command to continue}^              |

src/nvim/globals.h
@@ -947,6 +947,7 @@ EXTERN int fill_stlnc INIT(= ' ');
 EXTERN int fill_vert INIT(= 9474);  // │
 EXTERN int fill_fold INIT(= 183);   // ·
 EXTERN int fill_diff INIT(= '-');
+EXTERN int fill_msgsep INIT(= ' ');
 
 /* Whether 'keymodel' contains "stopsel" and "startsel". */
 EXTERN int km_stopsel INIT(= FALSE);

src/nvim/highlight_defs.h
@@ -87,6 +87,7 @@ typedef enum {
   , HLF_QFL         // selected quickfix line
   , HLF_0           // Whitespace
   , HLF_INACTIVE    // NormalNC: Normal text in non-current windows
+  , HLF_MSGSEP      // message separator line
   , HLF_COUNT       // MUST be the last one
 } hlf_T;
 
@@ -137,7 +138,8 @@ EXTERN const char *hlf_names[] INIT(= {
   [HLF_MC] = "ColorColumn",
   [HLF_QFL] = "QuickFixLine",
   [HLF_0] = "Whitespace",
-  [HLF_INACTIVE] = "NormalNC"
+  [HLF_INACTIVE] = "NormalNC",
+  [HLF_MSGSEP] = "MsgSeparator",
 });
 
 

src/nvim/message.c
@@ -1875,13 +1875,29 @@ bool message_filtered(char_u *msg)
   return cmdmod.filter_force ? match : !match;
 }
 
+/// including horizontal separator
+int msg_scrollsize(void)
+{
+  return msg_scrolled + p_ch + 1;
+}
+
 /*
  * Scroll the screen up one line for displaying the next message line.
  */
 static void msg_scroll_up(void)
 {
-  /* scrolling up always works */
-  screen_del_lines(0, 0, 1, (int)Rows, NULL);
+  if (dy_flags & DY_MSGSEP) {
+    if (msg_scrolled == 0) {
+      screen_fill(Rows-p_ch-1, Rows-p_ch, 0, (int)Columns,
+                  fill_msgsep, fill_msgsep, hl_attr(HLF_MSGSEP));
+    }
+    int nscroll = MIN(msg_scrollsize()+1, Rows);
+    ui_call_set_scroll_region(Rows-nscroll, Rows-1, 0, Columns-1);
+    screen_del_lines(Rows-nscroll, 0, 1, nscroll, NULL);
+    ui_reset_scroll_region();
+  } else {
+    screen_del_lines(0, 0, 1, (int)Rows, NULL);
+  }
 }
 
 /*

src/nvim/option.c
@@ -3411,6 +3411,7 @@ static char_u *set_chars_option(char_u **varp)
     { &fill_vert,    "vert" , 9474 },  // │
     { &fill_fold,    "fold" , 183  },  // ·
     { &fill_diff,    "diff" , '-'  },
+    { &fill_msgsep,  "msgsep", ' ' },
   };
   static struct charstab lcstab[] = {
     { &lcs_eol,      "eol",      NUL },

src/nvim/option_defs.h
@@ -394,11 +394,13 @@ EXTERN char_u   *p_dir;         /* 'directory' */
 EXTERN char_u   *p_dy;          /* 'display' */
 EXTERN unsigned dy_flags;
 #ifdef IN_OPTION_C
-static char *(p_dy_values[]) = { "lastline", "truncate", "uhex", NULL };
+static char *(p_dy_values[]) = { "lastline", "truncate", "uhex", "msgsep",
+                                  NULL };
 #endif
 #define DY_LASTLINE             0x001
 #define DY_TRUNCATE             0x002
 #define DY_UHEX                 0x004
+#define DY_MSGSEP               0x008
 EXTERN int p_ed;                // 'edcompatible'
 EXTERN int p_emoji;             // 'emoji'
 EXTERN char_u   *p_ead;         // 'eadirection'

src/nvim/options.lua
@@ -642,7 +642,7 @@ return {
       vim=true,
       redraw={'all_windows'},
       varname='p_dy',
-      defaults={if_true={vi="", vim="lastline"}}
+      defaults={if_true={vi="", vim="lastline,msgsep"}}
     },
     {
       full_name='eadirection', abbreviation='ead',

commit 0d037ad978c01b0f57d0fc11da6554dda967b13d

messages: redraw tabline if it was overdrawn by messages
fixes #8354

Regression from #8088, where we try to avoid clearing the screen
if not absolutely necessary

src/nvim/screen.c
@@ -309,6 +309,9 @@ void update_screen(int type)
         if (wp->w_winrow + wp->w_height + wp->w_status_height > valid) {
           wp->w_redr_status = true;
         }
+        if (valid == 0) {
+          redraw_tabline = true;
+        }
       }
     } else if (msg_scrolled > Rows - 5) {  // clearing is faster
       type = CLEAR;

test/functional/ui/screen_basic_spec.lua
@@ -240,8 +240,8 @@ describe('Screen', function()
     end)
   end)
 
-  describe('tabnew', function()
-    it('creates a new buffer', function()
+  describe('tabs', function()
+    it('tabnew creates a new buffer', function()
       command('sp')
       command('vsp')
       command('vsp')
@@ -296,6 +296,62 @@ describe('Screen', function()
         {0:~                                                    }|
         {0:~                                                    }|
         {3:[No Name] [+]                                        }|
+                                                             |
+      ]])
+    end)
+
+    it('tabline is redrawn after messages', function()
+      command('tabnew')
+      screen:expect([[
+        {4: [No Name] }{2: [No Name] }{3:                              }{4:X}|
+        ^                                                     |
+        {0:~                                                    }|
+        {0:~                                                    }|
+        {0:~                                                    }|
+        {0:~                                                    }|
+        {0:~                                                    }|
+        {0:~                                                    }|
+        {0:~                                                    }|
+        {0:~                                                    }|
+        {0:~                                                    }|
+        {0:~                                                    }|
+        {0:~                                                    }|
+                                                             |
+      ]])
+
+      feed(':echo "'..string.rep('x\\n', 11)..'"<cr>')
+      screen:expect([[
+        {1:                                                     }|
+        x                                                    |
+        x                                                    |
+        x                                                    |
+        x                                                    |
+        x                                                    |
+        x                                                    |
+        x                                                    |
+        x                                                    |
+        x                                                    |
+        x                                                    |
+        x                                                    |
+                                                             |
+        {7:Press ENTER or type command to continue}^              |
+      ]])
+
+      feed('<cr>')
+      screen:expect([[
+        {4: [No Name] }{2: [No Name] }{3:                              }{4:X}|
+        ^                                                     |
+        {0:~                                                    }|
+        {0:~                                                    }|
+        {0:~                                                    }|
+        {0:~                                                    }|
+        {0:~                                                    }|
+        {0:~                                                    }|
+        {0:~                                                    }|
+        {0:~                                                    }|
+        {0:~                                                    }|
+        {0:~                                                    }|
+        {0:~                                                    }|
                                                              |
       ]])
     end)