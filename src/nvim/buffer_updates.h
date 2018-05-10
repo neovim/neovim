8ef3fb4e738c4021a1c779252a1dac53e09324fc
KillTheMule <KillTheMule@users.noreply.github.com>
2018-05-10 14:25:59 +0200

    Use autogenerated declarations

--- src/nvim/buffer_updates.h
+++ src/nvim/buffer_updates.h
@@ -3,14 +3,8 @@
 
 #include "nvim/buffer_defs.h"
 
-bool buf_updates_register(buf_T *buf, uint64_t channel_id, bool send_buffer);
-void buf_updates_unregister(buf_T *buf, uint64_t channel_id);
-void buf_updates_unregister_all(buf_T *buf);
-void buf_updates_send_changes(buf_T *buf,
-                              linenr_T firstline,
-                              int64_t num_added,
-                              int64_t num_removed,
-                              bool send_tick);
-void buf_updates_changedtick(buf_T *buf);
+#ifdef INCLUDE_GENERATED_DECLARATIONS
+# include "buffer_updates.h.generated.h"
+#endif
 
 #endif  // NVIM_BUFFER_UPDATES_H
