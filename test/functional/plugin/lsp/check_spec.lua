dd6b8a11e0cd38cdd58fa66db648ff8bd50f1ca5
TJ DeVries <devries.timothyj@gmail.com>
2018-05-19 11:03:40 -0500

    Update some docs and add todos

---
+++ test/functional/plugin/lsp/check_spec.lua
@@ -0,0 +1,5 @@
+
+-- TODO:
+--  Add some tests for disabling a comand completely
+--  Add some tests for disabling per filetype
+--  Add some tests for disabling based on random conditions, like last executed time
