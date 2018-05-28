3f8d3ad5f1c79c22311feeb22bc72a0193aaf2eb
Jan Edmund Lazo <janedmundlazo@hotmail.com>
2018-05-28 12:03:20 -0400

    ci/win: validate paths/vars and add fallbacks
    Build with MSVC if $env:CONFIGURATION is unset.
    Add codepath for Ninja generator on MinGW build.
    Check git, python, ruby paths from Appveyor to not break $env:PATH.
    Run codecov and oldtests if mingw is installed
    Stop the build if any cmdlet fails or a program returns a terminating error.
    Force all (script) local variables to be set before using them.

--- ci/build.ps1
+++ ci/build.ps1
@@ -1,9 +1,18 @@
-Set-PSDebug -Trace 1
+$ErrorActionPreference = 'stop'
+Set-PSDebug -Strict -Trace 1
 
-$env:CONFIGURATION -match '^(?<compiler>\w+)_(?<bits>32|64)(?:-(?<option>\w+))?$'
-$compiler = $Matches.compiler
-$compileOption = $Matches.option
-$bits = $Matches.bits
+$uploadToCodecov = $false
+if ($env:CONFIGURATION) {
+  $env:CONFIGURATION -match '^(?<compiler>\w+)_(?<bits>32|64)(?:-(?<option>\w+))?$'
+  $compiler = $Matches.compiler
+  $compileOption = $Matches.option
+  $bits = $Matches.bits
+} else {
+  $compiler = 'MSVC'
+  $compileOption = ''
+  $bits = if ($env:PROCESSOR_ARCHITECTURE -eq 'x86') {32} else {64}
+}
+
 $cmakeBuildType = 'RelWithDebInfo'
 $depsCmakeVars = @{
   CMAKE_BUILD_TYPE = $cmakeBuildType;
@@ -20,6 +29,7 @@ if ($env:APPVEYOR_PULL_REQUEST_HEAD_COMMIT -and $env:CONFIGURATION -match '^(MSV
 
 function exitIfFailed() {
   if ($LastExitCode -ne 0) {
+    Set-PSDebug -Off
     exit $LastExitCode
   }
 }
@@ -35,62 +45,93 @@ if ($compiler -eq 'MINGW') {
     $nvimCmakeVars['USE_GCOV'] = 'ON'
     $uploadToCodecov = $true
   }
+
   # These are native MinGW builds, but they use the toolchain inside
   # MSYS2, this allows using all the dependencies and tools available
   # in MSYS2, but we cannot build inside the MSYS2 shell.
   $cmakeGenerator = 'MinGW Makefiles'
-  $cmakeGeneratorArgs = 'VERBOSE=1'
 
-  # Add MinGW to the PATH
-  $env:PATH = "C:\msys64\mingw$bits\bin;$env:PATH"
+  if ($cmakeGenerator -eq 'Ninja') {
+    $cmakeGeneratorArgs = @('-v')
+  }
+  elseif ($cmakeGenerator -eq 'MinGW Makefiles') {
+    $env:CC = 'cc'
+    $cmakeGeneratorArgs = @('VERBOSE=1')
+    @{
+      CMAKE_SH = 'CMAKE_SH-NOTFOUND'
+      CMAKE_MAKE_PROGRAM = "C:/msys64/mingw$bits/bin/mingw32-make.exe"
+    }.GetEnumerator() | ForEach-Object {
+      $nvimCmakeVars[$_.Key] = $_.Value
+      $depsCmakeVars[$_.Key] = $_.Value
+      $cmakeGeneratorArgs += $_.Key + '=' + $_.Value
+    }
+  }
 
   # Build third-party dependencies
+  $mingwPackages = @('gcc', 'make', 'cmake', 'perl', 'diffutils', 'unibilium').ForEach({
+    @('mingw', 'w64', $arch, $_) -join '-'
+  })
   C:\msys64\usr\bin\bash -lc "pacman --verbose --noconfirm -Su" ; exitIfFailed
-  C:\msys64\usr\bin\bash -lc "pacman --verbose --noconfirm --needed -S mingw-w64-$arch-cmake mingw-w64-$arch-perl mingw-w64-$arch-diffutils mingw-w64-$arch-unibilium" ; exitIfFailed
+  C:\msys64\usr\bin\bash -lc "pacman --verbose --noconfirm --needed -S $mingwPackages"; exitIfFailed
 }
 elseif ($compiler -eq 'MSVC') {
   $cmakeGeneratorArgs = '/verbosity:normal'
-  if ($bits -eq 32) {
-    $cmakeGenerator = 'Visual Studio 15 2017'
-  }
-  elseif ($bits -eq 64) {
-    $cmakeGenerator = 'Visual Studio 15 2017 Win64'
+  $cmakeGenerator = 'Visual Studio 15 2017'
+  if ($bits -eq 64) {
+    $cmakeGenerator += ' Win64'
   }
 }
 
-# Remove Git Unix utilities from the PATH
-$env:PATH = $env:PATH.Replace('C:\Program Files\Git\usr\bin', '')
-
 # Setup python (use AppVeyor system python)
-C:\Python27\python.exe -m pip install neovim ; exitIfFailed
-C:\Python35\python.exe -m pip install neovim ; exitIfFailed
-# Disambiguate python3
-move c:\Python35\python.exe c:\Python35\python3.exe
-$env:PATH = "C:\Python35;C:\Python27;$env:PATH"
-# Sanity check
-python  -c "import neovim; print(str(neovim))" ; exitIfFailed
-python3 -c "import neovim; print(str(neovim))" ; exitIfFailed
+if (Test-Path -PathType Container C:\Python27) {
+  $env:PATH = "C:\Python27;$env:PATH"
+}
+if (Test-Path -PathType Container C:\Python35) {
+  # Disambiguate python3
+  if (Test-Path C:\Python35\python.exe) {
+    Move-Item C:\Python35\python.exe C:\Python35\python3.exe
+  }
+  $env:PATH = "C:\Python35;$env:PATH"
+}
+Get-Command -CommandType Application python.exe
+python.exe  -m pip install neovim ; exitIfFailed
+python.exe  -c "import neovim; print(str(neovim))" ; exitIfFailed
+Get-Command -CommandType Application python3.exe
+python3.exe -m pip install neovim ; exitIfFailed
+python3.exe -c "import neovim; print(str(neovim))" ; exitIfFailed
 
-$env:PATH = "C:\Ruby24\bin;$env:PATH"
-cmd /c gem.cmd install neovim ; exitIfFailed
-where.exe neovim-ruby-host.bat ; exitIfFailed
+if (Test-Path -PathType Container C:\Ruby24\bin) {
+  $env:PATH = "C:\Ruby24\bin;$env:PATH"
+}
+Get-Command -CommandType Application gem.cmd
+gem.cmd install neovim; exitIfFailed
+Get-Command -CommandType Application neovim-ruby-host.bat
 
-cmd /c npm.cmd install -g neovim ; exitIfFailed
-where.exe neovim-node-host.cmd ; exitIfFailed
-cmd /c npm link neovim
+Get-Command -CommandType Application npm.cmd
+npm.cmd install -g neovim; exitIfFailed
+Get-Command -CommandType Application neovim-node-host.cmd
+npm.cmd link neovim
 
 function convertToCmakeArgs($vars) {
   return $vars.GetEnumerator() | foreach { "-D$($_.Key)=$($_.Value)" }
 }
 
-mkdir .deps
+if ($compiler -eq 'MINGW') {
+  $env:PATH = "C:\msys64\mingw$bits\bin;$env:PATH"
+}
+
+if (-Not (Test-Path -PathType Container .deps)) {
+  mkdir .deps
+}
 cd .deps
 cmake -G $cmakeGenerator $(convertToCmakeArgs($depsCmakeVars)) ..\third-party\ ; exitIfFailed
 cmake --build . --config $cmakeBuildType -- $cmakeGeneratorArgs ; exitIfFailed
 cd ..
 
 # Build Neovim
-mkdir build
+if (-Not (Test-Path -PathType Container build)) {
+  mkdir build
+}
 cd build
 cmake -G $cmakeGenerator $(convertToCmakeArgs($nvimCmakeVars)) .. ; exitIfFailed
 cmake --build . --config $cmakeBuildType -- $cmakeGeneratorArgs ; exitIfFailed
@@ -98,28 +139,32 @@ bin\nvim --version ; exitIfFailed
 
 # Functional tests
 # The $LastExitCode from MSBuild can't be trusted
-$failed = $false
 # Temporarily turn off tracing to reduce log file output
 Set-PSDebug -Off
+$failed = $false
 cmake --build . --config $cmakeBuildType --target functionaltest -- $cmakeGeneratorArgs 2>&1 |
   foreach { $failed = $failed -or
     $_ -match 'Running functional tests failed with error'; $_ }
-Set-PSDebug -Trace 1
 if ($failed) {
   exit $LastExitCode
 }
+Set-PSDebug -Strict -Trace 1
 
 
-if ($uploadToCodecov) {
-  C:\msys64\usr\bin\bash -lc "cd /c/projects/neovim; bash <(curl -s https://codecov.io/bash) -c -F functionaltest || echo 'codecov upload failed.'"
-}
+if (Test-Path -PathType Container C:\msys64\usr\bin) {
+  if ($uploadToCodecov) {
+    C:\msys64\usr\bin\bash -lc "cd /c/projects/neovim; bash <(curl -s https://codecov.io/bash) -c -F functionaltest || echo 'codecov upload failed.'"
+  }
 
-# Old tests
-$env:PATH = "C:\msys64\usr\bin;$env:PATH"
-& "C:\msys64\mingw$bits\bin\mingw32-make.exe" -C $(Convert-Path ..\src\nvim\testdir) VERBOSE=1
+  # Old tests
+  if (Test-Path "C:\msys64\mingw$bits\bin\mingw32-make.exe") {
+    $env:PATH = "C:\msys64\usr\bin;$env:PATH"
+    & "C:\msys64\mingw$bits\bin\mingw32-make.exe" -C $(Convert-Path ..\src\nvim\testdir) VERBOSE=1
+  }
 
-if ($uploadToCodecov) {
-  C:\msys64\usr\bin\bash -lc "cd /c/projects/neovim; bash <(curl -s https://codecov.io/bash) -c -F oldtest || echo 'codecov upload failed.'"
+  if ($uploadToCodecov) {
+    C:\msys64\usr\bin\bash -lc "cd /c/projects/neovim; bash <(curl -s https://codecov.io/bash) -c -F oldtest || echo 'codecov upload failed.'"
+  }
 }
 
 # Build artifacts
@@ -127,3 +172,4 @@ cpack -G ZIP -C RelWithDebInfo
 if ($env:APPVEYOR_REPO_TAG_NAME -ne $null) {
   cpack -G NSIS -C RelWithDebInfo
 }
+Set-PSDebug -Off
