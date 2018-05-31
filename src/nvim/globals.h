637266212da95bc8043e6d8bd01b0d97a007b1df
ckelsel <ckelsel@hotmail.com>
2018-05-31 20:31:18 +0800

    rename EX -> Ex

--- src/nvim/globals.h
+++ src/nvim/globals.h
@@ -745,7 +745,7 @@ EXTERN int State INIT(= NORMAL);        /* This is the current state of the
 EXTERN bool finish_op INIT(= false);    // true while an operator is pending
 EXTERN long opcount INIT(= 0);          // count for pending operator
 
-// EX Mode (Q) state
+// Ex Mode (Q) state
 EXTERN int exmode_active INIT(= 0);     // zero, EXMODE_NORMAL or EXMODE_VIM
 EXTERN int ex_no_reprint INIT(= false);  // no need to print after z or p
 
