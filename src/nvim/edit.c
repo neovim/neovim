700b691e692330c169b3373f754d28547e0e578e
ckelsel <ckelsel@hotmail.com>
2018-06-12 08:55:30 +0800

    fix lint

--- src/nvim/edit.c
+++ src/nvim/edit.c
@@ -4292,12 +4292,12 @@ ins_compl_next (
       for (s = e = compl_shown_match->cp_fname; *e != NUL; mb_ptr_adv(e)) {
         space -= ptr2cells(e);
         while (space < 0) {
-            space += ptr2cells(s);
-            mb_ptr_adv(s);
+          space += ptr2cells(s);
+          mb_ptr_adv(s);
         }
       }
       vim_snprintf((char *)IObuff, IOSIZE, "%s %s%s", lead,
-              s > compl_shown_match->cp_fname ? "<" : "", s);
+                   s > compl_shown_match->cp_fname ? "<" : "", s);
       msg(IObuff);
       redraw_cmdline = false;     // don't overwrite!
     }
