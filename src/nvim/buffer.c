10083ec4cc6d8f03831445831a20c82fee87880b
Jan Edmund Lazo <janedmundlazo@hotmail.com>
2018-06-21 10:36:52 -0400

    vim-patch:8.0.0648: possible use of NULL pointer
    Problem:    Possible use of NULL pointer if buflist_new() returns NULL.
                (Coverity)
    Solution:   Check for NULL pointer in set_bufref().
    https://github.com/vim/vim/commit/fadacf01d0dbcc7a96ef5eee0ad57956eeab04d7

--- src/nvim/buffer.c
+++ src/nvim/buffer.c
@@ -343,7 +343,7 @@ open_buffer (
 void set_bufref(bufref_T *bufref, buf_T *buf)
 {
   bufref->br_buf = buf;
-  bufref->br_fnum = buf->b_fnum;
+  bufref->br_fnum = buf == NULL ? 0 : buf->b_fnum;
   bufref->br_buf_free_count = buf_free_count;
 }
 
