50396afca573139c5376547f940740a9b7633cdc
TJ DeVries <devries.timothyj@gmail.com>
2018-07-14 16:47:46 -0500

    Second step of configuring autocmds

--- runtime/lua/lsp/config/autocmds.lua
+++ runtime/lua/lsp/config/autocmds.lua
@@ -2,8 +2,9 @@ local autocmds = require('lsp.autocmds')
 
 local config = {}
 
-config.enable_event = function(request_name, autocmd_event)
-
+config.enable_event = function(request_name, autocmd_event, autocmd_pattern)
+  autocmds.get_autocmd_event_name(autocmd_event, autocmd_pattern)
+  autocmds.nvim_enable_autocmd(request_name, autocmd_event)
 end
 
 return config
