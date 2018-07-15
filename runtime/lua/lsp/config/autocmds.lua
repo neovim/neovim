ed02d9983f4d739e074aa42eec2461c279d48eec
TJ DeVries <devries.timothyj@gmail.com>
2018-07-14 19:45:56 -0500

    WIP: Autocmd cleanup and removing vimscript where possible

--- runtime/lua/lsp/config/autocmds.lua
+++ runtime/lua/lsp/config/autocmds.lua
@@ -7,4 +7,8 @@ config.enable_event = function(request_name, autocmd_event, autocmd_pattern)
   autocmds.nvim_enable_autocmd(request_name, autocmd_event)
 end
 
+config.disable_event = function(request_name, asdf)
+  print(request_name, asdf)
+end
+
 return config
