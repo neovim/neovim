3dffc842fec3365d70192cc7c8162412d9187f9d
Jan Edmund Lazo <janedmundlazo@hotmail.com>
2018-08-28 15:38:59 -0400

    vim-patch:8.0.0983: unnecessary check for NULL pointer
    Problem:    Unnecessary check for NULL pointer.
    Solution:   Remove the NULL check in dialog_changed(), it already happens in
                dialog_msg(). (Ken Takata)
    https://github.com/vim/vim/commit/3f9a1ff141412e9e85f7dff47d02946cb9be9228

--- src/nvim/ex_cmds2.c
+++ src/nvim/ex_cmds2.c
@@ -1285,9 +1285,8 @@ void dialog_changed(buf_T *buf, int checkall)
   int ret;
   exarg_T ea;
 
-  dialog_msg(buff, _("Save changes to \"%s\"?"),
-             (buf->b_fname != NULL) ?
-             buf->b_fname : (char_u *)_("Untitled"));
+  assert(buf->b_fname != NULL);
+  dialog_msg(buff, _("Save changes to \"%s\"?"), buf->b_fname);
   if (checkall) {
     ret = vim_dialog_yesnoallcancel(VIM_QUESTION, NULL, buff, 1);
   } else {
