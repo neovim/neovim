10e885bdfc42322dee3312b4373dc8f6653d2412
Jan Edmund Lazo <jan.lazo@mail.utoronto.ca>
2018-08-28 02:51:22 -0400

    vim-patch:8.0.1001: setting 'encoding' makes 'printheader' invalid (#8925)
    Problem:    Setting 'encoding' makes 'printheader' invalid.
    Solution:   Do not translate the default value of 'printheader'. (Yasuhiro
                Matsumoto, closes vim/vim#2026)
    https://github.com/vim/vim/commit/0903d56f5ca69bb1fa0bbb00ed2a3d9c4d06ddb4

--- src/nvim/options.lua
+++ src/nvim/options.lua
@@ -1774,7 +1774,7 @@ return {
       gettext=true,
       vi_def=true,
       varname='p_header',
-      defaults={if_true={vi=N_("%<%f%h%m%=Page %N")}}
+      defaults={if_true={vi="%<%f%h%m%=Page %N"}}
     },
     {
       full_name='printmbcharset', abbreviation='pmbcs',
