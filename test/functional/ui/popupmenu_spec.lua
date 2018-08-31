4897eff93950031147b2520afacb126cb8ca474d
rpigott <rpigott@berkeley.edu>
2018-08-30 21:00:04 -0700

    [WIP] popupmenu placement test

--- test/functional/ui/popupmenu_spec.lua
+++ test/functional/ui/popupmenu_spec.lua
@@ -12,6 +12,9 @@ describe('ui/ext_popupmenu', function()
     screen:set_default_attr_ids({
       [1] = {bold=true, foreground=Screen.colors.Blue},
       [2] = {bold = true},
+      [3] = {reverse = true},
+      [4] = {bold = true, reverse = true},
+      [5] = {bold = true, foreground = Screen.colors.SeaGreen}
     })
   end)
 
@@ -88,4 +91,47 @@ describe('ui/ext_popupmenu', function()
       {2:-- INSERT --}                                                |
     ]]}
   end)
+end)
+
+describe('popup placement', function()
+  local screen
+  before_each(function()
+    clear()
+    screen = Screen.new(25, 16)
+    screen:attach()
+    screen:set_default_attr_ids({
+      ['s'] = {background = Screen.colors.WebGray},
+      ['n'] = {background = Screen.colors.LightMagenta},
+      [1] = {bold = true, foreground = Screen.colors.Blue},
+      [2] = {bold = true},
+      [3] = {reverse = true},
+      [4] = {bold = true, reverse = true},
+      [5] = {bold = true, foreground = Screen.colors.SeaGreen}
+    })
+  end)
+
+  it('appears', function()
+    feed(':ped<cr>')
+    feed('ifoo<cr>bar<cr><c-x><c-n>')
+    screen:snapshot_util() -- why doesn't the popup show here?
+  end)
+
+  -- it('positions corectly', function()
+  --   feed(':ped<CR><c-w>+<c-w>r')
+  --   feed('iaaa<cr>bbb<cr>ccc<cr>ddd<esc>zto')
+  --   feed('<c-x><c-n>')
+  --   screen:snapshot_util()
+  --   screen:expect([[
+  --     ddd                                                         |
+  --     aaa^                                                         |
+  --     {3:[No Name] [+]                                               }|
+  --     aaa                                                         |
+  --     bbb                                                         |
+  --     ccc                                                         |
+  --     {2:[No Name] [Preview][+]                                      }|
+  --     {1:-- Keyword Local completion (^N^P) }{4:match 1 of 4}             |
+  --   ]]
+  -- )
+  -- end)
+
 end)
