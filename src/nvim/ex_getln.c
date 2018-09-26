28ac4a99f270fa866fa025a858ff6a503ef81a24
Jan Edmund Lazo <janedmundlazo@hotmail.com>
2018-09-25 22:57:45 -0400

    vim-patch:8.1.0433: mapping can obtain text from inputsecret()
    Problem:    Mapping can obtain text from inputsecret(). (Tommy Allen)
    Solution:   Disallow CTRL-R = and CTRL-\ e when using inputsecret().
    https://github.com/vim/vim/commit/31cbadf74bccc2a5cd8233bc31bbcfe466b00021

--- src/nvim/ex_getln.c
+++ src/nvim/ex_getln.c
@@ -793,9 +793,11 @@ static int command_line_execute(VimState *state, int key)
     no_mapping--;
     // CTRL-\ e doesn't work when obtaining an expression, unless it
     // is in a mapping.
-    if (s->c != Ctrl_N && s->c != Ctrl_G && (s->c != 'e'
-                                             || (ccline.cmdfirstc == '='
-                                                 && KeyTyped))) {
+    if (s->c != Ctrl_N
+        && s->c != Ctrl_G
+        && (s->c != 'e'
+            || (ccline.cmdfirstc == '=' && KeyTyped)
+            || cmdline_star)) {
       vungetc(s->c);
       s->c = Ctrl_BSL;
     } else if (s->c == 'e') {
@@ -1350,7 +1352,8 @@ static int command_line_handle_key(CommandLineState *s)
     // a new one...
     new_cmdpos = -1;
     if (s->c == '=') {
-      if (ccline.cmdfirstc == '=') {          // can't do this recursively
+      if (ccline.cmdfirstc == '='   // can't do this recursively
+          || cmdline_star) {        // or when typing a password
         beep_flush();
         s->c = ESC;
       } else {
