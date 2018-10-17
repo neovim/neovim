c558e888b0e5c66695183b115e996a56c426334c
TJ DeVries <devries.timothyj@gmail.com>
2018-10-16 21:17:58 -0500

    Start working on cleaning up API surface

--- runtime/autoload/lsp/request.vim
+++
@@ -1,9 +0,0 @@
-let g:nvim_lsp_client = -1
-
-function! s:get_client() abort
-   return luaeval("require('lsp.plugin').client.get()")
-endfunction
-
-function! lsp#request#textdocument_references()
-   " TODO
-endfunction
--- runtime/lua/neovim/meta.lua
+++ runtime/lua/neovim/meta.lua
@@ -1,6 +1,11 @@
 
 local meta = {}
 
+---
+-- Provide a "strongly typed" dictionary in Lua.
+--
+-- Does not allow insertion or deletion after creation.
+-- Only allows retrieval of created keys are allowed.
 meta.Enum = {
   new = function(self, map)
     return setmetatable(map, self)
@@ -15,6 +20,32 @@ meta.Enum = {
       string.format("attempt to update enum table with %s, %s, %s", t, k, v),
       2)
   end
+}
+
+---
+-- Provide a dictionary that will continue providing empty dictionaries upon access.
+--
+-- This allows you to do something like:
+--  local myEmpty =- meta.EmptyDictionary({a = 'b'})
+--  if myEmpty.b.a.c.d.e.f.g.i == nil then
+--      // Do some error stuff here
+--  end
+meta.EmptyDictionary = {
+  new = function(self, dictionary)
+    if dictionary == nil then
+      dictionary = {}
+    end
+
+    return setmetatable(dictionary, self)
+  end,
+
+  __index = function(self, key)
+    if rawget(self, key) ~= nil then
+      return rawget(self, key)
+    end
+
+    return setmetatable({}, self)
+  end,
 }
 
 return meta
