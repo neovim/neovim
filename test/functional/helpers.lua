81d771c89081ae08b3e287ec40bb5a539c783041
Justin M. Keyes <justinkz@gmail.com>
2018-11-23 11:33:01 +0100

    smurf

--- test/functional/helpers.lua
+++ test/functional/helpers.lua
@@ -34,7 +34,7 @@ local nvim_prog = (
   or Paths.test_build_dir .. '/bin/nvim'
 )
 -- Default settings for the test session.
-local nvim_set  = 'set shortmess+=I background=light noswapfile noautoindent'
+local nvim_set  = 'set shortmess+=I noswapfile noautoindent'
                   ..' laststatus=1 undodir=. directory=. viewdir=. backupdir=.'
                   ..' belloff= noshowcmd noruler nomore'
 local nvim_argv = {nvim_prog, '-u', 'NONE', '-i', 'NONE', '-N',
