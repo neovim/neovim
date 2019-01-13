9ed16750cc7959bc42674fd8d45b7304e6a5181d
Justin M. Keyes <justinkz@gmail.com>
2019-01-13 23:36:32 +0100

    pvscheck.sh: Fix download URL
    - old URL redirects to https://www.viva64.com/en/pvs-studio-download/
    - page now contains tgz files for macOS and Linux; the pattern must
      match only the Linux URL

--- scripts/pvscheck.sh
+++ scripts/pvscheck.sh
@@ -384,8 +384,8 @@ run_analysis() {(
 detect_url() {
   local url="${1:-detect}"
   if test "$url" = detect ; then
-    curl --silent -L 'https://www.viva64.com/en/pvs-studio-download-linux/' \
-    | grep -o 'https\{0,1\}://[^"<>]\{1,\}/pvs-studio[^/"<>]*\.tgz' \
+    curl --silent -L 'https://www.viva64.com/en/pvs-studio-download/' \
+    | grep -o 'https\{0,1\}://[^"<>]\{1,\}/pvs-studio[^/"<>]*-x86_64\.tgz' \
     || echo FAILED
   else
     printf '%s' "$url"
