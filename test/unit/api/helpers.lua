1b6d2d7e6bc5e3606b2bdb5dd3d522003669c1c8
Matěj Cepl <mcepl@cepl.eu>
2019-01-17 15:58:05 +0100

    When we have vim.compat Lua library, let us use it.
    Remove Lua 5.2 compatiblity construct.

--- test/unit/api/helpers.lua
+++ test/unit/api/helpers.lua
@@ -1,3 +1,4 @@
+require('vim.compat')
 local helpers = require('test.unit.helpers')(nil)
 local eval_helpers = require('test.unit.eval.helpers')
 
@@ -114,8 +115,7 @@ local lua2obj_type_tab = {
                      api.xmalloc(len * ffi.sizeof('KeyValuePair'))),
     }})
     for i = 1, len do
-      local table_unpack = table.unpack or unpack  -- luacheck: compat
-      local key, val = table_unpack(kvs[i])
+      local key, val = unpack(kvs[i])
       dct.data.dictionary.items[i - 1] = ffi.new(
           'KeyValuePair', {key=ffi.gc(lua2obj(key), nil).data.string,
                            value=ffi.gc(lua2obj(val), nil)})
