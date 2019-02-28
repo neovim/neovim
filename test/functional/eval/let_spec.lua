f6d47b2acb2d14bca6ae6812a24010ee0444446d
erw7 <erw7.github@gmail.com>
2019-02-28 07:28:49 +0100

    test/env: multibyte env var to child process

--- test/functional/eval/let_spec.lua
+++ test/functional/eval/let_spec.lua
@@ -5,6 +5,9 @@ local clear = helpers.clear
 local meths = helpers.meths
 local redir_exec = helpers.redir_exec
 local source = helpers.source
+local command = helpers.command
+local eval = helpers.eval
+local nvim_dir = helpers.nvim_dir
 
 before_each(clear)
 
@@ -41,5 +44,23 @@ describe(':let command', function()
       call garbagecollect(1)
       call feedkeys(":\e:echo l1 l3\n:echo 42\n:cq\n", "t")
     ]=])
+  end)
+
+  it("multibyte env var to child process #8398 #9267",  function()
+    local cmd_get_child_env = "let g:env_from_child = system(['"..nvim_dir.."/printenv-test', 'NVIM_TEST'])"
+    command("let $NVIM_TEST = 'AìaB'")
+    command(cmd_get_child_env)
+    eq(eval('$NVIM_TEST'), eval('g:env_from_child'))
+
+    command("let $NVIM_TEST = 'AaあB'")
+    command(cmd_get_child_env)
+    eq(eval('$NVIM_TEST'), eval('g:env_from_child'))
+
+    local mbyte = [[\p* .ม .ม .ม .ม่ .ม่ .ม่ ֹ ֹ ֹ .ֹ .ֹ .ֹ ֹֻ ֹֻ ֹֻ
+                    .ֹֻ .ֹֻ .ֹֻ ֹֻ ֹֻ ֹֻ .ֹֻ .ֹֻ .ֹֻ ֹ ֹ ֹ .ֹ .ֹ .ֹ ֹ ֹ ֹ .ֹ .ֹ .ֹ ֹֻ ֹֻ
+                    .ֹֻ .ֹֻ .ֹֻ a a a ca ca ca à à à]]
+    command("let $NVIM_TEST = '"..mbyte.."'")
+    command(cmd_get_child_env)
+    eq(eval('$NVIM_TEST'), eval('g:env_from_child'))
   end)
 end)
