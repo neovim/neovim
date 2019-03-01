6cd4ff2ab724583acf3a6e582ad55d0561c7bd28
Justin M. Keyes <justinkz@gmail.com>
2019-03-01 02:34:32 +0100

    os: remove uv_translate_sys_error impl #9652
    Since libuv minimum version is now v1.12, we can use libuv's impl.

--- src/nvim/os/fs_defs.h
+++ src/nvim/os/fs_defs.h
@@ -24,6 +24,9 @@ typedef struct {
 /// Converts libuv error (negative int) to error description string.
 #define os_strerror uv_strerror
 
+/// Converts system error code to libuv error code.
+#define os_translate_sys_error uv_translate_sys_error
+
 // Values returned by os_nodetype()
 #define NODE_NORMAL     0  // file or directory, check with os_isdir()
 #define NODE_WRITABLE   1  // something we can write to (character
