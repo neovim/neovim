6572c995fb5a3b7dc41f077e0434810e6679f96d
James McCoy <jamessan@jamessan.com>
2019-04-07 20:01:05 -0400

    Remove MSVC optimization workaround for SHM_ALL

--- src/nvim/option.c
+++ src/nvim/option.c
@@ -2495,11 +2495,6 @@ static bool valid_filetype(char_u *val)
   return true;
 }
 
-#ifdef _MSC_VER
-// MSVC optimizations are disabled for this function because it
-// incorrectly generates an empty string for SHM_ALL.
-#pragma optimize("", off)
-#endif
 /*
  * Handle string options that need some action to perform when changed.
  * Returns NULL for success, or an error message for an error.
@@ -3391,9 +3386,6 @@ ambw_end:
 
   return errmsg;
 }
-#ifdef _MSC_VER
-#pragma optimize("", on)
-#endif
 
 /*
  * Simple int comparison function for use with qsort()
