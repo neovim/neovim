d8d4f05564c32510f4ea7eb4f8ae5e3769f2f57d
Jan Edmund Lazo <jan.lazo@mail.utoronto.ca>
2019-04-10 20:34:48 -0400

    vim-patch:8.0.1411: reading invalid memory with CTRL-W :
    Problem:    Reading invalid memory with CTRL-W :.
    Solution:   Correct the command characters. (closes vim/vim#2469)
    https://github.com/vim/vim/commit/2efb323e875d2852f63e41c40641760d1d6b069f

--- src/nvim/normal.c
+++ src/nvim/normal.c
@@ -6528,6 +6528,8 @@ static void nv_window(cmdarg_T *cap)
 {
   if (cap->nchar == ':') {
     // "CTRL-W :" is the same as typing ":"; useful in a terminal window
+    cap->cmdchar = ':';
+    cap->nchar = NUL;
     nv_colon(cap);
   } else if (!checkclearop(cap->oap)) {
     do_window(cap->nchar, cap->count0, NUL);  // everything is in window.c
