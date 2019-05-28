4553fc5e6cb6c8c43f57c173d01b31a61e51d13f
Justin M. Keyes <justinkz@gmail.com>
2019-05-29 00:33:22 +0200

    vim-patch:8.1.1365: :source should check sandbox #10082
    Problem:    Source command doesn't check for the sandbox. (Armin Razmjou)
    Solution:   Check for the sandbox when sourcing a file.
    https://github.com/vim/vim/commit/53575521406739cf20bbe4e384d88e7dca11f040

--- src/nvim/getchar.c
+++ src/nvim/getchar.c
@@ -1244,6 +1244,13 @@ openscript (
     EMSG(_(e_nesting));
     return;
   }
+
+  // Disallow sourcing a file in the sandbox, the commands would be executed
+  // later, possibly outside of the sandbox.
+  if (check_secure()) {
+    return;
+  }
+
   if (ignore_script)
     /* Not reading from script, also don't open one.  Warning message? */
     return;
