afa15e509773fc3e30f34220c176457140e35b90
Daniel Hahler <git@thequod.de>
2019-06-12 22:22:00 +0200

    ci/common/submit_coverage.sh: cleanup

--- ci/common/submit_coverage.sh
+++ ci/common/submit_coverage.sh
@@ -1,24 +1,16 @@
 #!/bin/sh
 
-# Debug
 set -x
-echo "=== submit_coverage: $* ==="
-env
-pwd
-echo "$PATH"
+
+# Change to grandparent dir (POSIXly).
+CDPATH='' cd -P -- "$(dirname -- "$0")/../.." || exit
+
+echo "=== running submit_coverage in $PWD: $* ==="
+
+# Debug
 command -v gcov
 gcov --version
-
-# TODO
-cd /c/projects/neovim || exit
-
 find . -name '*.gcno' -o -name '*.gcna'
-
-codecov_sh="${TEMP:-/tmp}/codecov.bash"
-if ! [ -f "$codecov_sh" ]; then
-  curl --fail https://codecov.io/bash > "$codecov_sh"
-  chmod +x "$codecov_sh"
-fi
 
 # Run gcov manually.
 # Allows more control, and uses ';' instead of '+'
@@ -28,6 +20,14 @@ find build -type f -name '*.gcno' -execdir gcov -pb {} \;
 # Debug
 find . -name '*.gcov'
 
+# Download codecov-bash once.
+codecov_sh="${TEMP:-/tmp}/codecov.bash"
+if ! [ -f "$codecov_sh" ]; then
+  curl --fail https://codecov.io/bash > "$codecov_sh"
+  chmod +x "$codecov_sh"
+fi
+
+# Upload to codecov.
 # -X gcov: disable gcov, done manually above.
 # -Z: exit non-zero on failure
 # -c: clear discovered files
