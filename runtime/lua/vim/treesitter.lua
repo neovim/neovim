167a1cfdef0c4b3526830ad0356f06bf480df6af
Björn Linse <bjorn.linse@gmail.com>
2019-06-17 21:46:31 +0200

    tree-sitter: improve parser API (shared parser between plugins)

--- runtime/lua/vim/treesitter.lua
+++ runtime/lua/vim/treesitter.lua
@@ -3,8 +3,13 @@ local a = vim.api
 local Parser = {}
 Parser.__index = Parser
 
-function Parser:parse_tree(force)
-  if self.valid and not force then
+-- TODO(bfredl): currently we retain parsers for the lifetime of the buffer.
+-- Consider use weak references to release parser if all plugins are done with
+-- it.
+local parsers = {}
+
+function Parser:parse()
+  if self.valid then
     return self.tree
   end
   self.tree = self._parser:parse_buf(self.bufnr)
@@ -12,7 +17,7 @@ function Parser:parse_tree(force)
   return self.tree
 end
 
-local function change_cb(self, ev, bufnr, tick, start_row, oldstopline, stop_row)
+local function on_lines(self, bufnr, _, start_row, oldstopline, stop_row)
   local start_byte = a.nvim_buf_get_offset(bufnr,start_row)
   -- a bit messy, should we expose edited but not reparsed tree?
   -- are multiple edits safe in general?
@@ -21,41 +26,63 @@ local function change_cb(self, ev, bufnr, tick, start_row, oldstopline, stop_row
   local inode = root:descendant_for_point_range(oldstopline+9000,0, oldstopline,0)
   if inode == nil then
     local stop_byte = a.nvim_buf_get_offset(bufnr,stop_row)
-    self._parser:edit(start_byte,stop_byte,stop_byte,start_row,0,stop_row,0,stop_row,0)
+    self._parser:edit(start_byte,stop_byte,stop_byte,
+                      start_row,0,stop_row,0,stop_row,0)
   else
     local fakeoldstoprow, fakeoldstopcol, fakebyteoldstop = inode:start()
     local fake_rows = fakeoldstoprow-oldstopline
     local fakestop = stop_row+fake_rows
     local fakebytestop = a.nvim_buf_get_offset(bufnr,fakestop)+fakeoldstopcol
-    self._parser:edit(start_byte,fakebyteoldstop,fakebytestop,start_row,0,fakeoldstoprow,fakeoldstopcol,fakestop,fakeoldstopcol)
+    self._parser:edit(start_byte, fakebyteoldstop, fakebytestop,
+                      start_row, 0,
+                      fakeoldstoprow, fakeoldstopcol,
+                      fakestop, fakeoldstopcol)
   end
   self.valid = false
 end
 
-local function create_parser(bufnr, ft)
+local function create_parser(bufnr, ft, id)
   if bufnr == 0 then
+    bufnr = a.nvim_get_current_buf()
+  end
+  local self = setmetatable({bufnr=bufnr, valid=false}, Parser)
+  self._parser = vim._create_ts_parser(ft)
+  self:parse()
+    -- TODO: use weakref to self, so that the parser is free'd is no plugin is
+    -- using it.
+  local function lines_cb(ev, ...)
+    return on_lines(self, ...)
+  end
+  local detach_cb = nil
+  if id ~= nil then
+    detach_cb = function()
+      if parsers[id] == self then
+        parsers[id] = nil
+      end
+    end
+  end
+  a.nvim_buf_attach(self.bufnr, false, {on_lines=lines_cb, on_detach=detach_cb})
+  return self
+end
+
+local function get_parser(bufnr, ft)
+  if bufnr == nil or bufnr == 0 then
     bufnr = a.nvim_get_current_buf()
   end
   if ft == nil then
     ft = a.nvim_buf_get_option(bufnr, "filetype")
   end
-  local self = setmetatable({bufnr=bufnr, valid=false}, Parser)
-  self._parser = vim._create_ts_parser(ft)
-  self:parse_tree()
-  local function cb(ev, ...)
-    -- TODO: use weakref to self, so that the parser is free'd is no plugin is
-    -- using it.
-    return change_cb(self, ev, ...)
+  local id = tostring(bufnr)..'_'..ft
+
+  if parsers[id] == nil then
+    parsers[id] = create_parser(bufnr, ft, id)
   end
-  a.nvim_buf_attach(self.bufnr, false, {on_lines=cb})
-  return self
+  return parsers[id]
 end
 
--- TODO: weak table with reusable parser per buffer.
-
 return {
+  get_parser=get_parser,
   create_parser=create_parser,
   add_language=vim._ts_add_language,
   inspect_language=vim._ts_inspect_language,
 }
-
--- test/functional/lua/treesitter_spec.lua
+++ test/functional/lua/treesitter_spec.lua
@@ -8,6 +8,7 @@ local insert = helpers.insert
 local meth_pcall = helpers.meth_pcall
 local exec_lua = helpers.exec_lua
 local iswin = helpers.iswin
+local feed = helpers.feed
 
 before_each(clear)
 
@@ -46,12 +47,11 @@ describe('tree-sitter API', function()
         }]])
 
       exec_lua([[
-        parser = vim.treesitter.create_parser(0, "c")
-        tree = parser:parse_tree()
+        parser = vim.treesitter.get_parser(0, "c")
+        tree = parser:parse()
         root = tree:root()
       ]])
 
-      --eq("<parser>", exec_lua("return tostring(parser)"))
       eq("<tree>", exec_lua("return tostring(tree)"))
       eq("<node translation_unit>", exec_lua("return tostring(root)"))
       eq({0,0,3,0}, exec_lua("return {root:range()}"))
@@ -60,6 +60,27 @@ describe('tree-sitter API', function()
       exec_lua("child = root:child(0)")
       eq("<node function_definition>", exec_lua("return tostring(child)"))
       eq({0,0,2,1}, exec_lua("return {child:range()}"))
+
+      exec_lua("descendant = root:descendant_for_point_range(1,2,1,12)")
+      eq("<node declaration>", exec_lua("return tostring(descendant)"))
+      eq({1,2,1,12}, exec_lua("return {descendant:range()}"))
+      eq("(declaration (primitive_type) (init_declarator (identifier) (number_literal)))", exec_lua("return descendant:sexpr()"))
+
+      feed("2G7|ay")
+      exec_lua([[
+        tree2 = parser:parse()
+        root2 = tree2:root()
+        descendant2 = root2:descendant_for_point_range(1,2,1,13)
+      ]])
+      eq(false, exec_lua("return tree2 == tree1"))
+      eq("<node declaration>", exec_lua("return tostring(descendant2)"))
+      eq({1,2,1,13}, exec_lua("return {descendant2:range()}"))
+
+      -- orginal tree did not change
+      eq({1,2,1,12}, exec_lua("return {descendant:range()}"))
+
+      -- unchanged buffer: return the same tree
+      eq(true, exec_lua("return parser:parse() == tree2"))
     end)
 
   end)
