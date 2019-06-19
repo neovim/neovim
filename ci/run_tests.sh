44dc93f7ce33a454b6ed66e622cb614ec69fd907
Daniel Hahler <git@thequod.de>
2019-06-19 02:37:48 +0200

    fixup! debug

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -19,7 +19,8 @@ build_nvim
 # XXX: move to build_nvim?  Why is build_nvim in run_tests?!
 env | sort
 command -v python
-python -c 'print(__import__("vim"))'
+python --version
+python -c 'print(__import__("sys").path)'
 build/bin/nvim -u NONE -c 'exe !has("python")."cq"' || { echo "Python 2 is not available"; exit 1;}
 build/bin/nvim -u NONE -c 'exe !has("python3")."cq"' || { echo "Python 3 is not available"; exit 1;}
 
