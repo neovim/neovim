487eaf8d456de17449ade6826ca0d13a64db576d
Daniel Hahler <git@thequod.de>
2019-06-19 20:28:37 +0200

    CI: use -m to invoke pip #10275
    Also install "pynvim" instead of "neovim".
    
    Ref: https://github.com/neovim/neovim/pull/10228#discussion_r295266859

--- ci/install.sh
+++ ci/install.sh
@@ -13,9 +13,9 @@ fi
 
 # Use default CC to avoid compilation problems when installing Python modules.
 echo "Install neovim module for Python 3."
-CC=cc pip3 -q install --user --upgrade neovim
+CC=cc python3 -m pip -q install --user --upgrade pynvim
 echo "Install neovim module for Python 2."
-CC=cc pip2 -q install --user --upgrade neovim
+CC=cc python2 -m pip -q install --user --upgrade pynvim
 
 echo "Install neovim RubyGem."
 gem install --no-document --version ">= 0.8.0" neovim
