neovim/neovim

commit 204ec6337e91df9b79a3b3a36dda99baf95147ca

third-party: luarocks: Use Lua if LuaJIT is disabled

third-party/cmake/BuildLuarocks.cmake
@@ -56,13 +56,16 @@ if(UNIX OR (MINGW AND CMAKE_CROSSCOMPILING))
   if(USE_BUNDLED_LUAJIT)
     list(APPEND LUAROCKS_OPTS
       --with-lua=${HOSTDEPS_INSTALL_DIR}
-      --with-lua-include=${HOSTDEPS_INSTALL_DIR}/include/luajit-2.0)
+      --with-lua-include=${HOSTDEPS_INSTALL_DIR}/include/luajit-2.0
+      --lua-suffix=jit)
+  elseif(USE_BUNDLED_LUA)
+    list(APPEND LUAROCKS_OPTS
+      --with-lua=${HOSTDEPS_INSTALL_DIR})
   endif()
 
   BuildLuarocks(
     CONFIGURE_COMMAND ${DEPS_BUILD_DIR}/src/luarocks/configure
       --prefix=${HOSTDEPS_INSTALL_DIR} --force-config ${LUAROCKS_OPTS}
-      --lua-suffix=jit
     INSTALL_COMMAND ${MAKE_PRG} bootstrap)
 elseif(MSVC OR MINGW)
 
@@ -94,6 +97,8 @@ if(USE_BUNDLED_LUAJIT)
   if(MINGW AND CMAKE_CROSSCOMPILING)
     add_dependencies(luarocks luajit_host)
   endif()
+elseif(USE_BUNDLED_LUA)
+  add_dependencies(luarocks lua)
 endif()
 
 # DEPENDS on the previous module, because Luarocks breaks if parallel.

third-party/cmake/BuildLua.cmake
@@ -84,14 +84,13 @@ BuildLua(CONFIGURE_COMMAND ${LUA_CONFIGURE_COMMAND}
   BUILD_COMMAND ${LUA_BUILD_COMMAND}
   INSTALL_COMMAND ${LUA_INSTALL_COMMAND})
 list(APPEND THIRD_PARTY_DEPS lua)
-add_dependencies(lua busted)
 
 set(BUSTED ${DEPS_INSTALL_DIR}/bin/busted)
 set(BUSTED_LUA ${BUSTED}-lua)
 
 add_custom_command(OUTPUT ${BUSTED_LUA}
   COMMAND sed -e 's/^exec/exec $$LUA_DEBUGGER/' -e 's/jit//g' < ${BUSTED} > ${BUSTED_LUA} && chmod +x ${BUSTED_LUA}
-  DEPENDS lua)
+  DEPENDS lua busted)
 add_custom_target(busted-lua
   DEPENDS ${DEPS_INSTALL_DIR}/bin/busted-lua)
 

commit 86d76ed4274d8b164a52934a161a1bf71654a230

build: luarocks: fall back to luajit
This regressed in 204ec6337.

Currently it would detect/use lua5.3 from the system, but in general
luajit is / should be preferred.

Noticed this due to nvim-client failing to build with Lua 5.3
(https://github.com/neovim/lua-client/pull/43).

third-party/cmake/BuildLuarocks.cmake
@@ -61,6 +61,15 @@ if(UNIX OR (MINGW AND CMAKE_CROSSCOMPILING))
   elseif(USE_BUNDLED_LUA)
     list(APPEND LUAROCKS_OPTS
       --with-lua=${HOSTDEPS_INSTALL_DIR})
+  else()
+    list(APPEND CMAKE_MODULE_PATH "../cmake")
+    find_package(LuaJit)
+    if(LUAJIT_FOUND)
+      list(APPEND LUAROCKS_OPTS
+        --lua-version=5.1
+        --with-lua-include=${LUAJIT_INCLUDE_DIRS}
+        --lua-suffix=jit)
+    endif()
   endif()
 
   BuildLuarocks(