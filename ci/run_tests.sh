bda5011f93378dd2d5f693bb7da5c77782d4b8cb
Daniel Hahler <git@thequod.de>
2019-06-26 00:36:02 +0200

    travis: run cmake [skip appveyor]

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -13,23 +13,6 @@ enter_suite build
 check_core_dumps --delete quiet
 
 prepare_build
-build_nvim
-
-exit_suite --continue
-
-enter_suite tests
-
-if test "$CLANG_SANITIZER" != "TSAN" ; then
-  # Additional threads are only created when the builtin UI starts, which
-  # doesn't happen in the unit/functional tests
-  if test "${FUNCTIONALTEST}" != "functionaltest-lua"; then
-    run_test run_unittests
-  fi
-  run_test run_functionaltests
-fi
-run_test run_oldtests
-
-run_test install_nvim
 
 exit_suite --continue
 
