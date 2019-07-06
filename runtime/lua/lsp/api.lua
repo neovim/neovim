9234f0ea7d39a33936ce107dfba716aae41851e6
Hirokazu Hata <h.hata.ai.t@gmail.com>
2019-07-06 14:40:47 +0900

    runtime: refactor lsp api and lsp config modules

--- runtime/lua/lsp/api.lua
+++ runtime/lua/lsp/api.lua
@@ -2,17 +2,10 @@
 
 vim.lsp = {
   client = require('lsp.plugin').client,
-
   server = {
     add = require('lsp.server').add,
   },
-
-  config = {
-    autocmds  = require('lsp.config.autocmds'),
-    callbacks = require('lsp.config.callbacks'),
-    log       = require('lsp.config.log'),
-    request   = require('lsp.config.request'),
-  },
+  config = require('lsp.config'),
 }
 
 return vim.lsp
