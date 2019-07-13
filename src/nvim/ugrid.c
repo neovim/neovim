a502ce8220baf856689b2f60382bfb616bb2fcfb
Ihor Antonov <ngortheone@users.noreply.github.com>
2019-07-13 12:48:26 -0400

    pvs/V1028 ugrid.c:76

--- src/nvim/ugrid.c
+++ src/nvim/ugrid.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <limits.h>
 
+#include "nvim/assert.h"
 #include "nvim/vim.h"
 #include "nvim/ui.h"
 #include "nvim/ugrid.h"
@@ -72,8 +73,10 @@ void ugrid_scroll(UGrid *grid, int top, int bot, int left, int right, int count)
   for (i = start; i != stop; i += step) {
     UCell *target_row = grid->cells[i] + left;
     UCell *source_row = grid->cells[i + count] + left;
+    size_t bytes;
+    STRICT_ADD(right, -left + 1, &bytes, size_t);
     memcpy(target_row, source_row,
-           sizeof(UCell) * (size_t)(right - left + 1));
+           sizeof(UCell) * bytes);
   }
 }
 
