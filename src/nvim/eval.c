d4e93fedf13bce843aae4219fc9b5057394114ea
Jan Edmund Lazo <jan.lazo@mail.utoronto.ca>
2019-07-19 05:46:22 -0400

    vim-patch:8.1.0740: Tcl test fails (#10540)
    Problem:    Tcl test fails.
    Solution:   When the argument is empty don't give an error, instead rely on
                the error reporting higher up.
    https://github.com/vim/vim/commit/8309b0559da6e9a581a7816572594c90c2d7f942

--- src/nvim/eval.c
+++ src/nvim/eval.c
@@ -19250,8 +19250,11 @@ static int get_name_len(const char **const arg,
   }
 
   len += get_id_len(arg);
-  if (len == 0 && verbose)
+  // Only give an error when there is something, otherwise it will be
+  // reported at a higher level.
+  if (len == 0 && verbose && **arg != NUL) {
     EMSG2(_(e_invexpr2), *arg);
+  }
 
   return len;
 }
