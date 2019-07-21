465a0a3c09ece20c16c08e694ec9f47aeafb47fc
erw7 <erw7.github@gmail.com>
2019-07-21 16:15:25 +0900

    env: invalid pointer after os_setenv() #10558

--- src/nvim/tui/tui.c
+++ src/nvim/tui/tui.c
@@ -222,6 +222,8 @@ static void terminfo_start(UI *ui)
 #ifdef WIN32
   os_tty_guess_term(&term, data->out_fd);
   os_setenv("TERM", term, 1);
+  // Old os_getenv() pointer is invalid after os_setenv(), fetch it again.
+  term = os_getenv("TERM");
 #endif
 
   // Set up unibilium/terminfo.
