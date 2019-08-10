278c5d452c2cbc436a9cc317407ae6021a226c3a
Justin M. Keyes <justinkz@gmail.com>
2019-08-10 11:48:36 +0200

    win/os_env_exists(): workaround libuv bug #10734
    os_env_exists() fails on MSVC build:
        os_env_exists:104: uv_os_getenv(EMPTY_VAR) failed: -4094 UNKNOWN
    
    - Revert 396a3945c4eba733b3a99a7ded217af83a400791
    - HACK: Windows: return TRUE if uv_os_getenv() returns UV_UNKNOWN, until
      libuv bug is fixed: https://github.com/libuv/libuv/issues/2413
    
    ref https://github.com/neovim/neovim/commit/396a3945c4eba733b3a99a7ded217af83a400791#r34642361

--- test/functional/eval/environ_spec.lua
+++ test/functional/eval/environ_spec.lua
@@ -3,7 +3,6 @@ local clear = helpers.clear
 local eq = helpers.eq
 local environ = helpers.funcs.environ
 local exists = helpers.funcs.exists
-local iswin = helpers.iswin
 
 describe('environment variables', function()
   it('environ() handles empty env variable', function()
@@ -12,8 +11,8 @@ describe('environment variables', function()
     eq(nil, environ()['DOES_NOT_EXIST'])
   end)
   it('exists() handles empty env variable', function()
-    clear({env={EMPTY_VAR=""}})  -- Windows treats this as "undefined".
-    eq((iswin() and 0 or 1), exists('$EMPTY_VAR'))
+    clear({env={EMPTY_VAR=""}})
+    eq(1, exists('$EMPTY_VAR'))
     eq(0, exists('$DOES_NOT_EXIST'))
   end)
 end)
