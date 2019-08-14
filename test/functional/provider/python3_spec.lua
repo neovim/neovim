6afebe762b578d301ec52ca2897c3cb17c1a487b
Justin M. Keyes <justinkz@gmail.com>
2019-08-14 11:20:58 +0200

    fixup! provider: do not incorrectly prefer python2 when python3 is working

--- test/functional/provider/python3_spec.lua
+++ test/functional/provider/python3_spec.lua
@@ -90,6 +90,11 @@ describe('python3 provider', function()
     eq({1, 2, {['key'] = 'val'}}, eval([[py3eval('[1, 2, {"key": "val"}]')]]))
   end)
 
+  it('pyxeval #10758', function()
+    eq(3, eval([[pyxeval('sys.version_info[:3][0]')]]))
+    eq(3, eval([[&pyxversion]]))
+  end)
+
   it('RPC call to expand("<afile>") during BufDelete #5245 #5617', function()
     source([=[
       python3 << EOF
