neovim/neovim

commit 5cc45bb419892909f2ede9ec3f6fb12f9e0da82d

ci: Travis: check logs for TSan also  (#10775)
Uses `cat -A` with early "*San" check:
This prints terminal escape sequences as-is (for debugging), and does
not cause (display) issues with the Travis log.

Ref: https://github.com/neovim/neovim/pull/10591#issuecomment-521248233

ci/common/build.sh
@@ -84,12 +84,11 @@ build_nvim() {
   fi
 
   # Invoke nvim to trigger *San early.
-  if ! (bin/nvim --version && bin/nvim -u NONE -e -c ':qall') ; then
-    asan_check "${LOG_DIR}"
+  if ! (bin/nvim --version && bin/nvim -u NONE -e -cq | cat -A) ; then
+    check_sanitizer "${LOG_DIR}"
     exit 1
   fi
-  asan_check "${LOG_DIR}"
-
+  check_sanitizer "${LOG_DIR}"
 
   cd "${TRAVIS_BUILD_DIR}"
 }

ci/common/test.sh
@@ -80,8 +80,8 @@ valgrind_check() {
   check_logs "${1}" "valgrind-*"
 }
 
-asan_check() {
-  if test "${CLANG_SANITIZER}" = "ASAN_UBSAN" ; then
+check_sanitizer() {
+  if test -n "${CLANG_SANITIZER}"; then
     check_logs "${1}" "*san.*"
   fi
 }
@@ -104,7 +104,7 @@ run_functionaltests() {(
     fail 'functionaltests' F 'Functional tests failed'
   fi
   submit_coverage functionaltest
-  asan_check "${LOG_DIR}"
+  check_sanitizer "${LOG_DIR}"
   valgrind_check "${LOG_DIR}"
   check_core_dumps
   exit_suite
@@ -118,7 +118,7 @@ run_oldtests() {(
     fail 'oldtests' F 'Legacy tests failed'
   fi
   submit_coverage oldtest
-  asan_check "${LOG_DIR}"
+  check_sanitizer "${LOG_DIR}"
   valgrind_check "${LOG_DIR}"
   check_core_dumps
   exit_suite

src/nvim/testdir/runnvim.sh
@@ -64,7 +64,7 @@ main() {(
   fi
   valgrind_check .
   if test -n "$LOG_DIR" ; then
-    asan_check "$LOG_DIR"
+    check_sanitizer "$LOG_DIR"
   fi
   check_core_dumps
   if test "$FAILED" = 1 ; then

commit 7c0ae066649424d69c4d00918b6dd6aa0314106b

ci: Travis: build.sh: use cat "-vet" for osx
Fixes:

    cat: illegal option -- A
    usage: cat [-benstuv] [file ...]

Broken in 5cc45bb41.

squash! ci: Travis: build.sh: use cat "-vET" for osx  [skip appveyor]

cat -vet

ci/common/build.sh
@@ -84,7 +84,7 @@ build_nvim() {
   fi
 
   # Invoke nvim to trigger *San early.
-  if ! (bin/nvim --version && bin/nvim -u NONE -e -cq | cat -A) ; then
+  if ! (bin/nvim --version && bin/nvim -u NONE -e -cq | cat -vet) ; then
     check_sanitizer "${LOG_DIR}"
     exit 1
   fi