2c05830d7b8a38ecd0525cf5abec807ad93263af
Daniel Hahler <git@thequod.de>
2019-08-20 08:15:13 +0200

    test/functional/ui/mode_spec: improve "ui mode_change event"
    Set a shorter `&matchtime` (instead of asserting the default), and do
    not sleep - `screen:expect` will do that (wait for it).

--- test/functional/ui/mode_spec.lua
+++ test/functional/ui/mode_spec.lua
@@ -63,7 +63,7 @@ describe('ui mode_change event', function()
     ]], mode="normal"}
 
     command("set showmatch")
-    eq(eval('&matchtime'), 5) -- tenths of seconds
+    command("set matchtime=1") -- tenths of seconds
     feed('a(stuff')
     screen:expect{grid=[[
       word(stuff^               |
@@ -80,7 +80,6 @@ describe('ui mode_change event', function()
       {2:-- INSERT --}             |
     ]], mode="showmatch"}
 
-    screen:sleep(400)
     screen:expect{grid=[[
       word(stuff)^              |
       {0:~                        }|
