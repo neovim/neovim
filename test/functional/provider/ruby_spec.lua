a35d937c591d5a220c16902769897f602164e4a6
Daniel Hahler <git@thequod.de>
2019-08-20 07:55:52 +0200

    Revert "debug hanging ruby-provider test"
    This reverts commit cb036afa8b869eca61ba18b80cf10c07b8075d1c.

--- test/functional/provider/ruby_spec.lua
+++ test/functional/provider/ruby_spec.lua
@@ -105,9 +105,6 @@ describe('ruby provider', function()
     command(string.format([[set rtp+=%s/runtime]], helpers.test_build_dir))
 
     command([=[autocmd BufDelete * ruby VIM::evaluate('expand("<afile>")')]=])
-    command('enew')
-    command('set buftype=help')
-    eq('TEST', eval('tagfiles()'))
     feed_command('help help')
     eq(2, eval('1+1'))  -- Still alive?
   end)
