f79079947411e39ae529f98e83204ee9f7e05300
Daniel Hahler <git@thequod.de>
2019-08-26 10:21:08 +0200

    third-party: use neovim/unibilium (#10677)
    Ref: https://github.com/neovim/neovim/issues/10302
    Fixes https://github.com/neovim/neovim/issues/9071.

--- third-party/cmake/BuildUnibilium.cmake
+++ third-party/cmake/BuildUnibilium.cmake
@@ -11,9 +11,6 @@ if(WIN32)
       -DTARGET=unibilium
       -DUSE_EXISTING_SRC_DIR=${USE_EXISTING_SRC_DIR}
       -P ${CMAKE_CURRENT_SOURCE_DIR}/cmake/DownloadAndExtractFile.cmake
-    PATCH_COMMAND ${GIT_EXECUTABLE} -C ${DEPS_BUILD_DIR}/src/unibilium init
-      COMMAND ${GIT_EXECUTABLE} -C ${DEPS_BUILD_DIR}/src/unibilium apply --ignore-whitespace
-      ${CMAKE_CURRENT_SOURCE_DIR}/patches/unibilium-Relax-checks-for-extended-capability-to-support-new-.patch
     CONFIGURE_COMMAND ${CMAKE_COMMAND} -E copy
       ${CMAKE_CURRENT_SOURCE_DIR}/cmake/UnibiliumCMakeLists.txt
       ${DEPS_BUILD_DIR}/src/unibilium/CMakeLists.txt
@@ -41,8 +38,6 @@ else()
       -DTARGET=unibilium
       -DUSE_EXISTING_SRC_DIR=${USE_EXISTING_SRC_DIR}
       -P ${CMAKE_CURRENT_SOURCE_DIR}/cmake/DownloadAndExtractFile.cmake
-    PATCH_COMMAND patch -d ${DEPS_BUILD_DIR}/src/unibilium -Np1 --input
-      ${CMAKE_CURRENT_SOURCE_DIR}/patches/unibilium-Relax-checks-for-extended-capability-to-support-new-.patch
     CONFIGURE_COMMAND ""
     BUILD_IN_SOURCE 1
     BUILD_COMMAND ${MAKE_PRG} CC=${DEPS_C_COMPILER}
