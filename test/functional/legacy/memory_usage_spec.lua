31764a2bf486f891914853a67277acafba8e5a30
erw7 <erw7.github@gmail.com>
2019-09-06 00:28:58 +0900

    vim-patch:8.1.1033: memory usage test may still fail on some systems
    Problem:    Memory usage test may still fail on some systems. (Elimar
                Riesebieter)
    Solution:   Increase tolerance from 1% to 3%.
    https://github.com/vim/vim/commit/ba64ba093520e85d6bed2595960edb693bdb4c51

--- test/functional/legacy/memory_usage_spec.lua
+++ test/functional/legacy/memory_usage_spec.lua
@@ -146,9 +146,9 @@ describe('memory usage', function()
     end
     local last = monitor_memory_usage(pid)
     -- The usage may be a bit less than the last value, use 80%.
-    -- Allow for 1% tolerance at the upper limit.
+    -- Allow for 3% tolerance at the upper limit.
     local lower = before.last * 8 / 10
-    local upper = (after.max + (after.last - before.last)) * 101 / 100
+    local upper = (after.max + (after.last - before.last)) * 103 / 100
     check_result({before=before, after=after, last=last},
                  pcall(ok, lower < last.last))
     check_result({before=before, after=after, last=last},
