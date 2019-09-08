3eebd7b9173492c5e7bdd84d905f2e677e4dcbdd
Hirokazu Hata <h.hata.ai.t@gmail.com>
2019-09-08 14:49:45 +0900

    test: Move and fix lsp/setup_spec.lua to lsp/server_config_spec.lua

---
+++ test/functional/plugin/lsp/server_config_spec.lua
@@ -0,0 +1,30 @@
+local helpers = require('test.functional.helpers')(after_each)
+
+local clear = helpers.clear
+local eq = helpers.eq
+local luaeval = helpers.funcs.luaeval
+-- local neq = helpers.neq
+-- local dedent = helpers.dedent
+-- local source = helpers.source
+
+before_each(clear)
+
+describe('Setup and Configuration', function()
+  it('should allow you to add clients and commands', function()
+    luaeval([[
+      require('vim.lsp.server_config').add(
+        'rust',
+        { execute_path = 'rustup', args = { 'run', 'stable', 'rls' }},
+        { name = 'rls' }
+      )
+    ]])
+    local result_name = luaeval("require('vim.lsp.server_config').get_name('rust')")
+    eq('rls', result_name)
+  end)
+
+  it('should allow some extra configuration if you want', function()
+    luaeval([[require('vim.lsp.server_config').add('rust', { execute_path = 'rustup', args = { 'run', 'stable', 'rls' }})]])
+    local result_name = luaeval("require('vim.lsp.server_config').get_server_command('rust')").execute_path
+    eq('rustup', result_name)
+  end)
+end)
