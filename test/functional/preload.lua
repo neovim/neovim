ab3ba7227fadc77c2a3291bb7a82c9d272b7d573
Daniel Hahler <git@thequod.de>
2019-09-11 13:47:23 +0200

    test/functional/preload.lua: _set_fmode for Windows

--- test/functional/preload.lua
+++ test/functional/preload.lua
@@ -2,3 +2,13 @@
 -- Busted started doing this to help provide more isolation.  See issue #62
 -- for more information about this.
 local helpers = require('test.functional.helpers')(nil)
+local iswin = helpers.iswin
+
+if iswin() then
+  local ffi = require('ffi')
+  ffi.cdef[[
+  typedef int errno_t;
+  errno_t _set_fmode(int mode);
+  ]]
+  ffi.C._set_fmode(0x8000)
+end
