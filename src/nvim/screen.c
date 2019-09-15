18e5869f56aab8a52d84185e5bd043799c36ae2d
Zach Wegner <zwegner@gmail.com>
2019-09-15 14:16:44 -0500

    Fix "precedes" listchar behavior in wrap mode
    Previously, the "precedes" character would be rendered on every row
    when w_skipcol > 0 (i.e., when viewing a single line longer than the
    entire screen), instead of just on the first row. Make sure to only
    render it on the first row in this case.
    
    Add a test for this behavior.
    
    Fix documentation for the "precedes" character, which erroneously
    stated that it was only active when wrap mode was off.

--- src/nvim/screen.c
+++ src/nvim/screen.c
@@ -3763,7 +3763,7 @@ win_line (
      */
     if (lcs_prec_todo != NUL
         && wp->w_p_list
-        && (wp->w_p_wrap ? wp->w_skipcol > 0 : wp->w_leftcol > 0)
+        && (wp->w_p_wrap ? (wp->w_skipcol > 0 && row == 0) : wp->w_leftcol > 0)
         && filler_todo <= 0
         && draw_state > WL_NR
         && c != NUL) {
