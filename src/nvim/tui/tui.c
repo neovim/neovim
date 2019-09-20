0078829db8b5ca3b4ec7f56d8fe8b08ea9c535db
Daniel Hahler <git@thequod.de>
2019-09-20 23:16:19 +0200

    set waiting_for_bg_response before unibi_out_ext

--- src/nvim/tui/tui.c
+++ src/nvim/tui/tui.c
@@ -296,8 +296,8 @@ static void terminfo_start(UI *ui)
   unibi_out(ui, unibi_keypad_xmit);
   unibi_out(ui, unibi_clear_screen);
   // Ask the terminal to send us the background color.
-  unibi_out_ext(ui, data->unibi_ext.get_bg);
   data->input.waiting_for_bg_response = true;
+  unibi_out_ext(ui, data->unibi_ext.get_bg);
   // Enable bracketed paste
   unibi_out_ext(ui, data->unibi_ext.enable_bracketed_paste);
 
