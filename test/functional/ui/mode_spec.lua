neovim/neovim

commit 7ed21226224276627f43178ebfe6a3a0330006c8

test/functional/ui/mode_spec: improve "ui mode_change event" (#10816)
Set a shorter `&matchtime` (instead of asserting the default), and do
not sleep - `screen:expect` will do that (wait for it).

test/functional/ui/mode_spec.lua
@@ -2,8 +2,7 @@ local helpers = require('test.functional.helpers')(after_each)
 local Screen = require('test.functional.ui.screen')
 
 local clear, feed, insert = helpers.clear, helpers.feed, helpers.insert
-local command, eval = helpers.command, helpers.eval
-local eq = helpers.eq
+local command = helpers.command
 
 describe('ui mode_change event', function()
   local screen
@@ -63,7 +62,7 @@ describe('ui mode_change event', function()
     ]], mode="normal"}
 
     command("set showmatch")
-    eq(eval('&matchtime'), 5) -- tenths of seconds
+    command("set matchtime=1") -- tenths of seconds
     feed('a(stuff')
     screen:expect{grid=[[
       word(stuff^               |
@@ -80,7 +79,6 @@ describe('ui mode_change event', function()
       {2:-- INSERT --}             |
     ]], mode="showmatch"}
 
-    screen:sleep(400)
     screen:expect{grid=[[
       word(stuff)^              |
       {0:~                        }|

commit bce5453be2baf23ebc6edf1d0478caf1867b64c6

mode_spec: retry with increasing matchtime
`matchtime=2` might still be too low (with luacov on AppVeyor).

    [  ERROR   ] test/functional\ui\mode_spec.lua @ 47: ui mode_change event works in insert mode
    test\functional\ui\screen.lua:587: mode
    Expected objects to be the same.
    Passed in:
    (string) 'insert'
    Expected:
    (string) 'showmatch'
    Hint: full state of "mode":
      "insert"

Followup to fe60013fb.
ref #10941
Initially regressed in 7ed212262242

``

test/functional/ui/mode_spec.lua
@@ -3,6 +3,7 @@ local Screen = require('test.functional.ui.screen')
 
 local clear, feed, insert = helpers.clear, helpers.feed, helpers.insert
 local command = helpers.command
+local retry = helpers.retry
 
 describe('ui mode_change event', function()
   local screen
@@ -61,30 +62,36 @@ describe('ui mode_change event', function()
                                |
     ]], mode="normal"}
 
+    local matchtime = 0
     command("set showmatch")
-    command("set matchtime=2") -- tenths of seconds
-    feed('a(stuff')
-    screen:expect{grid=[[
-      word(stuff^               |
-      {0:~                        }|
-      {0:~                        }|
-      {2:-- INSERT --}             |
-    ]], mode="insert"}
+    retry(nil, nil, function()
+      matchtime = matchtime + 1
+      local screen_timeout = 1000 * matchtime  -- fail faster for retry.
 
-    feed(')')
-    screen:expect{grid=[[
-      word^(stuff)              |
-      {0:~                        }|
-      {0:~                        }|
-      {2:-- INSERT --}             |
-    ]], mode="showmatch"}
+      command("set matchtime=" .. matchtime) -- tenths of seconds
+      feed('a(stuff')
+      screen:expect{grid=[[
+        word(stuff^               |
+        {0:~                        }|
+        {0:~                        }|
+        {2:-- INSERT --}             |
+      ]], mode="insert", timeout=screen_timeout}
 
-    screen:expect{grid=[[
-      word(stuff)^              |
-      {0:~                        }|
-      {0:~                        }|
-      {2:-- INSERT --}             |
-    ]], mode="insert"}
+      feed(')')
+      screen:expect{grid=[[
+        word^(stuff)              |
+        {0:~                        }|
+        {0:~                        }|
+        {2:-- INSERT --}             |
+      ]], mode="showmatch", timeout=screen_timeout}
+
+      screen:expect{grid=[[
+        word(stuff)^              |
+        {0:~                        }|
+        {0:~                        }|
+        {2:-- INSERT --}             |
+      ]], mode="insert", timeout=screen_timeout}
+    end)
   end)
 
   it('works in replace mode', function()