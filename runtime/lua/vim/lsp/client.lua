fd074744af6e5c98c61f75b7e0a695d9a2094447
Hirokazu Hata <h.hata.ai.t@gmail.com>
2019-10-06 17:13:57 +0900

    runtime: Remove unnecessary code

--- runtime/lua/vim/lsp/client.lua
+++ runtime/lua/vim/lsp/client.lua
@@ -142,7 +142,6 @@ end
 
 client.initialize = function(self)
   local result = self:request_async('initialize', InitializeParams(self), function(_, data)
-    self:set_server_capabilities(data)
     self:notify('initialized', {})
     self:notify('textDocument/didOpen', DidOpenTextDocumentParams(self))
     self:set_server_capabilities(data.capabilities)
