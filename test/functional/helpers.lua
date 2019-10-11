fd410848583e51b053cac5d4662278a52d6288be
Daniel Hahler <git@thequod.de>
2019-10-11 14:19:29 +0200

    tests: fail with non-zero child exitcode/signal
    Ref: https://github.com/neovim/lua-client/pull/44

--- test/functional/helpers.lua
+++ test/functional/helpers.lua
@@ -827,6 +827,16 @@ return function(after_each)
             error(msg[3][2])
           end
         end
+
+        if not session.child_exit then
+          session:close()
+        end
+        if session.child_exit then  -- requires newer neovim-lua-client (WIP).
+          if session.child_exit ~= 0 or session.child_signal ~= 0 then
+            busted.fail(string.format("child exited non-zero (exitcode=%d, signal=%d)",
+              session.child_exit, session.child_signal))
+          end
+        end
       end
     end)
   end
