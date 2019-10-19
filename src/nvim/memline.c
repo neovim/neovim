79d74acbcf6bc52845b132381e2b7ebb2b64272c
Daniel Hahler <git@thequod.de>
2019-10-19 21:50:29 +0200

    vim-patch:8.1.2180: Error E303 is not useful when 'directory' is empty
    Problem:    Error E303 is not useful when 'directory' is empty.
    Solution:   Skip the error message. (Daniel Hahler, vim/vim#5067)
    https://github.com/vim/vim/commit/00e192becd50a38cb21a1bc3f86fcc7a21f8ee88

--- src/nvim/memline.c
+++ src/nvim/memline.c
@@ -523,9 +523,9 @@ void ml_open_file(buf_T *buf)
     }
   }
 
-  if (mfp->mf_fname == NULL) {          /* Failed! */
-    need_wait_return = TRUE;            /* call wait_return later */
-    ++no_wait_return;
+  if (*p_dir != NUL && mfp->mf_fname == NULL) {
+    need_wait_return = true;  // call wait_return later
+    no_wait_return++;
     (void)EMSG2(_(
             "E303: Unable to open swap file for \"%s\", recovery impossible"),
         buf_spname(buf) != NULL ? buf_spname(buf) : buf->b_fname);
