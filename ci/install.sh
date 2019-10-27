3bee2f16048b89d9906a0e38d029b9b4b727b6a3
Jan Edmund Lazo <jan.lazo@mail.utoronto.ca>
2019-10-26 20:26:56 -0400

    ci: use python3 for flake8
    'make pylint' fails if flake8 runs on python2.

--- ci/install.sh
+++ ci/install.sh
@@ -4,7 +4,7 @@ set -e
 set -o pipefail
 
 if [[ "${CI_TARGET}" == lint ]]; then
-  python -m pip -q install --user --upgrade flake8
+  python3 -m pip -q install --user --upgrade flake8
   exit
 fi
 
