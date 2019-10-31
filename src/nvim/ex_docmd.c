903cd73af1985bbf51255958c15e353913588cd3
Jan Edmund Lazo <jan.lazo@mail.utoronto.ca>
2019-10-30 22:22:23 -0400

    vim-patch:8.1.0573: cannot redefine user command without ! in same script
    Problem:    Cannot redefine user command without ! in same script
    Solution:   Allow redefining user command without ! in same script, like with
                functions.
    https://github.com/vim/vim/commit/55d46913084745a48749d7ac4f48930852e1d87e

--- src/nvim/ex_docmd.c
+++ src/nvim/ex_docmd.c
@@ -5026,8 +5026,13 @@ static int uc_add_command(char_u *name, size_t name_len, char_u *rep,
     }
 
     if (cmp == 0) {
-      if (!force) {
-        EMSG(_("E174: Command already exists: add ! to replace it"));
+      // Command can be replaced with "command!" and when sourcing the
+      // same script again, but only once.
+      if (!force
+          && (cmd->uc_script_ctx.sc_sid != current_sctx.sc_sid
+              || cmd->uc_script_ctx.sc_seq == current_sctx.sc_seq)) {
+        EMSG2(_("E174: Command already exists: add ! to replace it: %s"),
+              name);
         goto fail;
       }
 
