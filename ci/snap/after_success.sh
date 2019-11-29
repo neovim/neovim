c6d70d17860273c6de2c37c529b21c7b049210c5
Justin M. Keyes <justinkz@gmail.com>
2019-11-28 17:23:43 -0800

    snap: more yak-shaving
    - generate decryption step
    - skip job for PRs
    - remove transfer.sh support
    - deploy every (master) commit

--- ci/snap/after_success.sh
+++ ci/snap/after_success.sh
@@ -12,8 +12,3 @@ sudo snap install "$RESULT_SNAP" --dangerous --classic
 
 SHA256=$(sha256sum "$RESULT_SNAP")
 echo "SHA256: ${SHA256} ."
-
-timeout 240 /snap/bin/transfer "$RESULT_SNAP"
-
-travis_retry bash "${ROOT_PATH}/scripts/services/0x0.st.sh" "${RESULT_SNAP}"
-
