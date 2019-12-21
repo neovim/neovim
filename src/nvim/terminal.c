ffe71f24b834c909a7e8d78f8ce0452652e50ad9
Daniel Hahler <git@thequod.de>
2019-12-21 12:32:16 +0100

    lint

--- src/nvim/terminal.c
+++ src/nvim/terminal.c
@@ -526,11 +526,11 @@ void terminal_paste(long count, char_u **y_array, size_t y_size)
     for (size_t j = 0; j < y_size; j++) {
       if (j) {
         // terminate the previous line
-        #ifdef UNIX
+# ifdef UNIX
          terminal_send(curbuf->terminal, "\n", 1);
-        #else
+# else
          terminal_send(curbuf->terminal, "\r", 1);
-        #endif
+# endif
       }
       terminal_send(curbuf->terminal, (char *)y_array[j], STRLEN(y_array[j]));
     }
