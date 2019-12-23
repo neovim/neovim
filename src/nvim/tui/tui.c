96b52a0cfc050d3c5e2a77a2bfb24b554cab06ab
Daniel Hahler <git@thequod.de>
2019-12-23 14:18:19 +0100

    directly ask_for_terminal_bg

--- src/nvim/tui/tui.c
+++ src/nvim/tui/tui.c
@@ -420,14 +420,13 @@ static void tui_main(UIBridgeData *bridge, UI *ui)
 #endif
   tinput_init(&data->input, &tui_loop);
   tui_terminal_start(ui);
+  ask_for_terminal_bg(ui);
 
   // Allow main thread to continue, we are ready to handle UI callbacks.
   CONTINUE(bridge);
 
   loop_schedule_deferred(&main_loop,
                          event_create(show_termcap_event, 1, data->ut));
-  loop_schedule_deferred(&main_loop,
-                         event_create(ask_for_terminal_bg, 1, ui));
 
   // "Active" loop: first ~100 ms of startup.
   for (size_t ms = 0; ms < 100 && !tui_is_stopped(ui);) {
@@ -1230,19 +1229,13 @@ static void show_termcap_event(void **argv)
 }
 
 /// Ask the terminal to send us the background color.
-static void ask_for_terminal_bg(void **argv)
+static void ask_for_terminal_bg(UI *ui)
 {
-  if (!option_was_set("bg")) {
-    UI *const ui = argv[0];
-    if (!ui) {
-      abort();
-    }
-    if (!tui_is_stopped(ui)) {
-      TUIData *data = ui->data;
-      data->input.waiting_for_bg_response = 5;
-      unibi_out_ext(ui, data->unibi_ext.get_bg);
-      flush_buf(ui);
-    }
+  if (!option_was_set("bg") && !tui_is_stopped(ui)) {
+    TUIData *data = ui->data;
+    data->input.waiting_for_bg_response = 5;
+    unibi_out_ext(ui, data->unibi_ext.get_bg);
+    flush_buf(ui);
   }
 }
 
