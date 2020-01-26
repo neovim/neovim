3fa5946ab5e129bf47369d05ee96bd4b838e8764
Daniel Hahler <git@thequod.de>
2020-01-26 15:21:33 +0100

    scripts/vim-patch.sh: include --shortstat with -m

--- scripts/vim-patch.sh
+++ scripts/vim-patch.sh
@@ -587,7 +587,9 @@ list_missing_previous_vimpatches_for_patch() {
 
   local -a missing_unique
   while IFS= read -r line; do
-    missing_unique+=("$line")
+    local commit="${line%%:*}"
+    local stat="$(git -C "${VIM_SOURCE_DIR}" show --format= --shortstat "${commit}")"
+    missing_unique+=("$(printf '%s\n  %s' "$line" "$stat")")
   done < <(printf '%s\n' "${missing_list[@]}" | sort -u)
 
   msg_err "$(printf '%d missing previous Vim patches:' ${#missing_unique[@]})"
