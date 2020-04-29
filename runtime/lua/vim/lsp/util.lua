e4a1be779b9a6bb9a47700ebf92f8dd934bb1712
Blaž Hrastnik <blaz@mxxn.io>
2020-04-29 10:32:34 +0900

    lsp/completion: Expose completion_item under completed_items.user_data.
    By passing through completion_item it's now possible for snippet plugins
    to add LSP snippet support.

--- runtime/lua/vim/lsp/util.lua
+++ runtime/lua/vim/lsp/util.lua
@@ -240,6 +240,13 @@ function M.text_document_completion_list_to_complete_items(result, prefix)
       icase = 1,
       dup = 1,
       empty = 1,
+      user_data = {
+        nvim = {
+          lsp = {
+            completion_item = completion_item
+          }
+        }
+      },
     })
   end
 
