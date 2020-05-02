d00f0f940086ddb533144eff28002b9b52693c1c
Christian Clason <christian.clason@uni-due.de>
2020-05-02 13:53:21 +0200

    make lint happier

--- runtime/lua/vim/lsp/util.lua
+++ runtime/lua/vim/lsp/util.lua
@@ -445,18 +445,17 @@ function M.jump_to_location(location)
 end
 
 function M.peek_location(location)
-  local uri = location.uri or location.targetUri
+  -- location may be LocationLink or Location (more useful for the former)
+  local uri = location.targetUri or location.uri
   if uri == nil then return end
   local bufnr = vim.uri_to_bufnr(uri)
   if not api.nvim_buf_is_loaded(bufnr) then
     vim.fn.bufload(bufnr)
   end
-  -- location may be LocationLink or Location
-  local uri = location.targetUri or location.uri
   local range = location.targetRange or location.range
   local contents = api.nvim_buf_get_lines(bufnr, range.start.line, range["end"].line+1, false)
   local filetype = api.nvim_buf_get_option(bufnr, 'filetype')
-  local popup_bufnr, winnr = M.open_floating_preview(contents, filetype)
+  M.open_floating_preview(contents, filetype)
 end
 
 local function find_window_by_var(name, value)
