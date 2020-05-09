76cd9cda6c7aaef862bf94ac5d87a6644ecdd2e7
Christian Clason <christian.clason@uni-due.de>
2020-05-09 18:24:25 +0200

    use anonymous namespaces, avoid local api

--- runtime/lua/hl-yank.lua
+++ runtime/lua/hl-yank.lua
@@ -1,6 +1,3 @@
-local api = vim.api
-local yank_ns = api.nvim_create_namespace('hlyank')
-
 --- Highlight the yanked region
 --
 --- use from init.vim via
@@ -16,16 +13,17 @@ return function(higroup, timeout, event)
     local higroup = higroup or "IncSearch"
     local timeout = timeout or 500
 
-    local bufnr = api.nvim_get_current_buf()
-    api.nvim_buf_clear_namespace(bufnr, yank_ns, 0, -1)
+    local bufnr = vim.api.nvim_get_current_buf()
+    local yank_ns = vim.api.nvim_create_namespace('')
+    vim.api.nvim_buf_clear_namespace(bufnr, yank_ns, 0, -1)
 
     local region = vim.marks_to_region("'[", "']", event.regtype, event.inclusive)
     for linenr, cols in pairs(region) do
-        api.nvim_buf_add_highlight(bufnr, yank_ns, higroup, linenr, cols[1], cols[2])
+        vim.api.nvim_buf_add_highlight(bufnr, yank_ns, higroup, linenr, cols[1], cols[2])
     end
 
     vim.schedule_fn(
-        function() api.nvim_buf_clear_namespace(bufnr, yank_ns, 0, -1) end,
+        function() vim.api.nvim_buf_clear_namespace(bufnr, yank_ns, 0, -1) end,
         timeout
     )
 end
