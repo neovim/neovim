faae509bce42994541303e8abc9ae5d87d092e7c
pohzipohzi <poh.zihow@gmail.com>
2020-05-29 23:08:21 +0800

    fix prompt in vim-patch when bash version is too low

--- scripts/vim-patch.sh
+++ scripts/vim-patch.sh
@@ -7,7 +7,7 @@ set -p
 
 # Ensure that the user has a bash that supports -A
 if [[ "${BASH_VERSINFO[0]}" -lt 4  ]]; then
-  echo "This script requires bash version 3 or later (you have ${BASH_VERSION})." >&2
+  echo "This script requires bash version 4 or later (you have ${BASH_VERSION})." >&2
   exit 1
 fi
 
