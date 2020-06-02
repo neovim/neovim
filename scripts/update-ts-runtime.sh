662688817b2156edd47019f00f53774e5e00224a
Thomas Vigouroux <tomvig38@gmail.com>
2020-06-02 22:32:49 +0200

    treesitter: enhance script and add README
    The script now updates a `treesitter_commit_sha.txt` file to keep track
    of which commit/branch/tag we're at.

--- scripts/update-ts-runtime.sh
+++ scripts/update-ts-runtime.sh
@@ -3,14 +3,16 @@
 # This script will update the treesitter runtime to the provided commit.
 # Usage :
 #   $0 <tree-sitter commit sha>
+set -e
 
 ts_source_dir="/tmp/tree-sitter"
 ts_url="https://github.com/tree-sitter/tree-sitter.git"
 
 base_dir="$(cd "$(dirname $(dirname $0))" && pwd)"
 ts_dest_dir="$base_dir/src/tree_sitter/"
+ts_current_commit="$ts_dest_dir/treesitter_commit_hash.txt"
 
-echo "$ts_dest_dir"
+echo "Updating treesitter runtime from $(cat "$ts_current_commit") to $1..."
 
 if [ ! -d "$ts_source_dir" ]; then
   echo "Cloning treesitter..."
@@ -20,16 +22,17 @@ else
 fi
 
 echo "Checking out $1..."
-cd "$ts_source_dir"
 git -C "$ts_source_dir" checkout $1
 
 echo "Removing old files..."
-find "$ts_dest_dir" -not -name "LICENSE" -not -type d -delete
+find "$ts_dest_dir" -not -name "LICENSE" -not -name "README.md" -not -type d -delete
 
 echo "Copying files..."
 cp -t "$ts_dest_dir" -r "$ts_source_dir/lib/src"/*
 cp -t "$ts_dest_dir" "$ts_source_dir/lib/include/tree_sitter"/*
 
-cd "$base_dir"
+echo "$1" > "$ts_current_commit"
+
 make
 TEST_FILE="$base_dir/test/functional/lua/treesitter_spec.lua" make test
+
