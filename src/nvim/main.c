e4629f203fb563c3dcee807874d9c011ad54b4c5
James McCoy <jamessan@jamessan.com>
2020-06-23 06:15:32 -0400

    main.c: fix hang issue with recoverymode (#12496) (#12520)
    In the case of recoverymode, the headlessmode was true, causing the UI
    to hang without starting. Fix this problem by setting headlessmode to
    true for List swap files only.
    
    Co-authored-by: erw7 <erw7.github@gmail.com>

--- src/nvim/main.c
+++ src/nvim/main.c
@@ -285,6 +285,12 @@ int main(int argc, char **argv)
     fname = get_fname(&params, cwd);
   }
 
+  // Recovery mode without a file name: List swap files.
+  // In this case, no UI is needed.
+  if (recoverymode && fname == NULL) {
+    headless_mode = true;
+  }
+
   TIME_MSG("expanding arguments");
 
   if (params.diff_mode && params.window_count == -1)
@@ -958,7 +964,6 @@ static void command_line_scan(mparm_T *parmp)
         case 'r':    // "-r" recovery mode
         case 'L': {  // "-L" recovery mode
           recoverymode = 1;
-          headless_mode = true;
           break;
         }
         case 's': {
