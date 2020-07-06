8f21d7ad39a39950004ea415f42815a323eeb618
Cédric Barreteau <>
2020-07-06 22:44:53 +0200

    LSP: Jump to call location
    Jump to the call site instead of jumping to the definition of the
    caller/callee.

--- runtime/lua/vim/lsp/callbacks.lua
+++ runtime/lua/vim/lsp/callbacks.lua
@@ -222,12 +222,14 @@ local make_call_hierarchy_callback = function(direction)
     local items = {}
     for _, call_hierarchy_call in pairs(result) do
       local call_hierarchy_item = call_hierarchy_call[direction]
-      table.insert(items, {
-        filename = assert(vim.uri_to_fname(call_hierarchy_item.uri)),
-        text = call_hierarchy_item.name,
-        lnum = call_hierarchy_item.selectionRange.start.line + 1,
-        col = call_hierarchy_item.selectionRange.start.character + 1,
-      })
+      for _, range in pairs(call_hierarchy_call.fromRanges) do
+        table.insert(items, {
+          filename = assert(vim.uri_to_fname(call_hierarchy_item.uri)),
+          text = call_hierarchy_item.name,
+          lnum = range.start.line + 1,
+          col = range.start.character + 1,
+        })
+      end
     end
     util.set_qflist(items)
     api.nvim_command("copen")
