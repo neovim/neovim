neovim/neovim

commit 35362495c965554f45634bcde3c4ce6d5eca52aa

jumplist: avoid extra tail entry #9805
fixes #9775

src/nvim/mark.c
@@ -1188,9 +1188,23 @@ void cleanup_jumplist(void)
       xfree(curwin->w_jumplist[from].fname);
     }
   }
-  if (curwin->w_jumplistidx == curwin->w_jumplistlen)
+  if (curwin->w_jumplistidx == curwin->w_jumplistlen) {
     curwin->w_jumplistidx = to;
+  }
   curwin->w_jumplistlen = to;
+
+  // When pointer is below last jump, remove the jump if it matches the current
+  // line.  This avoids useless/phantom jumps. #9805
+  if (curwin->w_jumplistlen
+      && curwin->w_jumplistidx == curwin->w_jumplistlen) {
+    const xfmark_T *fm_last = &curwin->w_jumplist[curwin->w_jumplistlen - 1];
+    if (fm_last->fmark.fnum == curbuf->b_fnum
+        && fm_last->fmark.mark.lnum == curwin->w_cursor.lnum) {
+      xfree(fm_last->fname);
+      curwin->w_jumplistlen--;
+      curwin->w_jumplistidx--;
+    }
+  }
 }
 
 /*

test/functional/normal/jump_spec.lua
@@ -1,0 +1,48 @@
+local helpers = require('test.functional.helpers')(after_each)
+
+local clear = helpers.clear
+local command = helpers.command
+local eq = helpers.eq
+local funcs = helpers.funcs
+local feed = helpers.feed
+local write_file = helpers.write_file
+
+describe('jumplist', function()
+  local fname1 = 'Xtest-functional-normal-jump'
+  local fname2 = fname1..'2'
+  before_each(clear)
+  after_each(function()
+    os.remove(fname1)
+    os.remove(fname2)
+  end)
+
+  it('does not add a new entry on startup', function()
+    eq('\n jump line  col file/text\n>', funcs.execute('jumps'))
+  end)
+
+  it('does not require two <C-O> strokes to jump back', function()
+    write_file(fname1, 'first file contents')
+    write_file(fname2, 'second file contents')
+
+    command('args '..fname1..' '..fname2)
+    local buf1 = funcs.bufnr(fname1)
+    local buf2 = funcs.bufnr(fname2)
+
+    command('next')
+    feed('<C-O>')
+    eq(buf1, funcs.bufnr('%'))
+
+    command('first')
+    command('snext')
+    feed('<C-O>')
+    eq(buf1, funcs.bufnr('%'))
+    feed('<C-I>')
+    eq(buf2, funcs.bufnr('%'))
+    feed('<C-O>')
+    eq(buf1, funcs.bufnr('%'))
+
+    command('drop '..fname2)
+    feed('<C-O>')
+    eq(buf1, funcs.bufnr('%'))
+  end)
+end)

src/nvim/shada.c
@@ -2739,8 +2739,8 @@ static ShaDaWriteResult shada_write(ShaDaWriteDef *const sd_writer,
 
   // Initialize jump list
   const void *jump_iter = NULL;
-  setpcmark();
   cleanup_jumplist();
+  setpcmark();
   do {
     xfmark_T fm;
     jump_iter = mark_jumplist_iter(jump_iter, curwin, &fm);

test/functional/shada/marks_spec.lua
@@ -189,13 +189,13 @@ describe('ShaDa support code', function()
     eq(1, nvim_current_line())
     nvim_command('execute "normal! \\<C-o>"')
     eq(testfilename, funcs.bufname('%'))
-    eq(1, nvim_current_line())
-    nvim_command('execute "normal! \\<C-o>"')
-    eq(testfilename, funcs.bufname('%'))
     eq(2, nvim_current_line())
     nvim_command('execute "normal! \\<C-o>"')
     eq(testfilename_2, funcs.bufname('%'))
     eq(1, nvim_current_line())
+    nvim_command('execute "normal! \\<C-o>"')
+    eq(testfilename_2, funcs.bufname('%'))
+    eq(2, nvim_current_line())
     nvim_command('execute "normal! \\<C-o>"')
     eq(testfilename_2, funcs.bufname('%'))
     eq(2, nvim_current_line())

test/functional/shada/merging_spec.lua
@@ -912,12 +912,11 @@ describe('ShaDa jumps support code', function()
     eq('', curbufmeths.get_name())
     eq('\n'
        .. ' jump line  col file/text\n'
-       .. '   6     2    0 ' .. mock_file_path .. 'c\n'
-       .. '   5     2    0 ' .. mock_file_path .. 'd\n'
-       .. '   4     3    0 ' .. mock_file_path .. 'd\n'
-       .. '   3     2    0 ' .. mock_file_path .. 'e\n'
-       .. '   2     2    0 ' .. mock_file_path .. 'f\n'
-       .. '   1     1    0 \n'
+       .. '   5     2    0 ' .. mock_file_path .. 'c\n'
+       .. '   4     2    0 ' .. mock_file_path .. 'd\n'
+       .. '   3     3    0 ' .. mock_file_path .. 'd\n'
+       .. '   2     2    0 ' .. mock_file_path .. 'e\n'
+       .. '   1     2    0 ' .. mock_file_path .. 'f\n'
        .. '>', redir_exec('jumps'))
   end)
 

commit 92d4c0423aa280377733a2b98eeb82c1ac8b3c06

jumplist: alleviate *, # command regression
Commit 35362495c ("jumplist: avoid extra tail entry #9805", 2019-04-02)
introduced a regression where, when the * or # command leaves the cursor
in the same line, the jumplist entry is erroneously erased. In addition
to interactive use, this breaks mappings like

    nnoremap * *<C-O>

The cursor remains in the same line when

1. the cursor is outside of a word boundary;
2. the searched-for word occurs next time in the same line; or
3. the searched-for word is unique.

Only erase the jumplist entry when both the cursor line and column
position are unchanged. This still leaves case #3 confusingly broken but
hopefully it's enough to salvage interactive use -- why "jump back" when
the cursor doesn't move? -- even if it turns out to be insufficient for
aforementioned mapping.

The slightly more clumsy alternative mapping of

    nnoremap * *``

doesn't help: `` updates the jumplist, too, so unique keywords move the
cursor to the position before the * command instead of staying in place.
If that command is used on a unique keyword after being used on a
non-unique keyword, that position is the "next match of the searched-for
word" from the previous usage.

ref #9874

src/nvim/mark.c
@@ -1240,7 +1240,12 @@ void cleanup_jumplist(win_T *wp, bool checktail)
       && wp->w_jumplistidx == wp->w_jumplistlen) {
     const xfmark_T *fm_last = &wp->w_jumplist[wp->w_jumplistlen - 1];
     if (fm_last->fmark.fnum == curbuf->b_fnum
-        && fm_last->fmark.mark.lnum == wp->w_cursor.lnum) {
+        && fm_last->fmark.mark.lnum == wp->w_cursor.lnum
+        && fm_last->fmark.mark.col == wp->w_cursor.col) {
+        // If the * or # command doesn't move the cursor, this block
+        // erroneously erases the newly created jumplist entry. More generally,
+        // the answer to "did the cursor move?" is insufficient to determine
+        // whether erasing entries is really safe. neovim/neovim#9874
       xfree(fm_last->fname);
       wp->w_jumplistlen--;
       wp->w_jumplistidx--;

test/functional/normal/jump_spec.lua
@@ -2,11 +2,17 @@ local helpers = require('test.functional.helpers')(after_each)
 
 local clear = helpers.clear
 local command = helpers.command
+local curbuf_contents = helpers.curbuf_contents
 local eq = helpers.eq
 local funcs = helpers.funcs
 local feed = helpers.feed
 local redir_exec = helpers.redir_exec
 local write_file = helpers.write_file
+
+local ensure_empty_jumplist = function()
+  funcs.execute('clearjumps')
+  eq('\n jump line  col file/text\n>', funcs.execute('jumps'))
+end
 
 describe('jumplist', function()
   local fname1 = 'Xtest-functional-normal-jump'
@@ -45,6 +51,88 @@ describe('jumplist', function()
     command('drop '..fname2)
     feed('<C-O>')
     eq(buf1, funcs.bufnr('%'))
+  end)
+
+  it('adds an entry when * moves the cursor to "first keyword after" in-line #9874', function()
+    -- The cursor will start outside a word boundary so * will move the cursor
+    -- to "the first keyword after the cursor" and update the jumplist. The
+    -- keyword is unique but that doesn't matter here.
+
+    write_file(fname1, ' unique', true)
+    command('args '..fname1)
+
+    eq(' unique', curbuf_contents(), 'ensure leading whitespace')
+
+    -- Explicitly move to (0, 0) because of 'nostartofline'.
+    feed('0')
+
+    ensure_empty_jumplist()
+
+    feed('*')
+
+    eq(
+      '\n jump line  col file/text\n   1     1    0 unique\n>',
+      funcs.execute('jumps'))
+  end)
+
+  it('adds an entry when * moves the cursor to next match in-line #9874', function()
+    -- The cursor will start at the start of a word boundary. The keyword
+    -- occurs multiple times in the same line so the cursor will move to the
+    -- next occurrence and update the jumplist.
+
+    write_file(fname1, 'dup dup')
+    command('args '..fname1)
+
+    eq('dup dup', curbuf_contents(), 'ensure no leading whitespace')
+
+    ensure_empty_jumplist()
+
+    feed('*')
+
+    eq(
+      '\n jump line  col file/text\n   1     1    0 dup dup\n>',
+      funcs.execute('jumps'))
+  end)
+
+  it('adds an entry when * moves the cursor to next match in other line #9874', function()
+    -- The cursor will start at the start of a word boundary. The keyword
+    -- occurs multiple times in different lines so the cursor will move to the
+    -- next occurrence and update the jumplist.
+
+    write_file(fname1, 'dup\ndup')
+    command('args '..fname1)
+
+    eq('dup\ndup', curbuf_contents(), 'ensure no leading whitespace')
+
+    ensure_empty_jumplist()
+
+    feed('*')
+
+    eq(
+      '\n jump line  col file/text\n   1     1    0 dup\n>',
+      funcs.execute('jumps'))
+  end)
+
+  it('has regression for: adds an entry when * does not move the cursor #9874', function()
+    -- The cursor will start at the start of a word boundary. The keyword is
+    -- unique so the cursor stays in place and fails to update the jumplist.
+    -- (Actually the jumplist does update, the entry just gets erased).
+
+    write_file(fname1, 'unique')
+    command('args '..fname1)
+
+    eq('unique', curbuf_contents(), 'ensure no leading whitespace')
+
+    ensure_empty_jumplist()
+
+    feed('*')
+
+    local regression = function()
+      eq(
+        '\n jump line  col file/text\n   1     1    0 unique\n>',
+        funcs.execute('jumps'))
+    end
+    assert.has_error(regression)
   end)
 end)
 