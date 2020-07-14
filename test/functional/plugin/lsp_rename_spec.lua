707ebdac151b63d05417e5d7ac6131b7669a2375
francisco souza <fsouza@users.noreply.github.com>
2020-07-13 22:06:32 -0400

    some progress with test

--- test/functional/plugin/lsp_rename_spec.lua
+++ test/functional/plugin/lsp_rename_spec.lua
@@ -5,28 +5,49 @@ local exec_lua = helpers.exec_lua
 local eq = helpers.eq
 
 describe('Rename', function()
-  local bufnr, fake_uri
+  local builtin_input, bufnr, fake_uri
 
   before_each(function()
-    fake_uri = "file://fake/uri"
+    clear()
+    fake_uri = 'file://fake/uri'
 
     bufnr = exec_lua([[
       fake_uri = ...
       bufnr = vim.uri_to_bufnr(fake_uri)
-      local lines = {"line 1"; "line 2"; "what"; "else"; "do"; "you"; "want"}
+      local lines = {'line 1'; 'line 2'; 'what'; 'else'; 'do'; 'you'; 'want'}
       vim.fn.bufload(bufnr)
       vim.api.nvim_buf_set_lines(bufnr, 0, 1, false, lines)
+
+      -- place the cursor at the beginning of 'else'
+      vim.fn.cursor(4, 1)
       return bufnr
     ]], fake_uri)
   end)
 
   after_each(function()
-    clear()
+    if builtin_input ~= nil then
+      exec_lua([[
+        vim.fn.input = ...
+      ]], builtin_input)
+    end
+    exec_lua('vim.stubs = nil')
   end)
 
   describe('vim.lsp.buf.rename', function()
     it('should use <cword> in input if server doesnt support prepareRename', function()
+      builtin_input = exec_lua([[
+        local builtin_input = vim.fn.input
+        vim._stubs = {}
+        vim.fn.input = function(prompt)
+          vim._stubs.input_prompt = prompt
+          return prompt
+        end
+        return bultin_input
+      ]])
 
+      exec_lua('vim.lsp.buf.rename(...)', 'new_name')
+      local provided_prompt = exec_lua('return vim._stubs.input_prompt')
+      eq('else', provided_prompt)
     end)
   end)
 end)
