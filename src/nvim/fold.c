cc64f2cc1dc90c6e2bc24ea1b72c2947fe701549
Jan Edmund Lazo <jan.lazo@mail.utoronto.ca>
2020-09-01 21:02:54 -0400

    vim-patch:8.2.1561: using NULL pointers in fold code
    Problem:    Using NULL pointers in fold code.
    Solution:   Avoid using a NULL pointer. (Dominique Pellé, closes vim/vim#6831,
                closes vim/vim#6831)
    https://github.com/vim/vim/commit/81fcb67fb32a12414512b72e691a1bbbff9f8511

--- src/nvim/fold.c
+++ src/nvim/fold.c
@@ -2742,7 +2742,8 @@ static void truncate_fold(win_T *const wp, fold_T *fp, linenr_T end)
 }
 
 #define FOLD_END(fp) ((fp)->fd_top + (fp)->fd_len - 1)
-#define VALID_FOLD(fp, gap) ((fp) < ((fold_T *)(gap)->ga_data + (gap)->ga_len))
+#define VALID_FOLD(fp, gap) \
+  ((gap)->ga_len > 0 && (fp) < ((fold_T *)(gap)->ga_data + (gap)->ga_len))
 #define FOLD_INDEX(fp, gap) ((size_t)(fp - ((fold_T *)(gap)->ga_data)))
 void foldMoveRange(
     win_T *const wp, garray_T *gap,
