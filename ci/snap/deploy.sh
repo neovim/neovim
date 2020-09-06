1da67c186976ff6c723f862ad711e029660f3fc8
Carlos Hernandez <carlos@techbyte.ca>
2020-09-06 14:33:06 +0000

    snap: drop snapcraft push release

--- ci/snap/deploy.sh
+++ ci/snap/deploy.sh
@@ -11,11 +11,3 @@ mkdir -p .snapcraft
 openssl aes-256-cbc -K "$encrypted_ece1c4844832_key" -iv "$encrypted_ece1c4844832_iv" \
   -in ci/snap/travis_snapcraft.cfg -out .snapcraft/snapcraft.cfg -d
 
-SNAP=$(find ./ -name "*.snap")
-
-# TODO(justinmk): This always does `edge` until we enable tagged builds.
-if [[ "$SNAP" =~ "dirty" || "$SNAP" =~ "nightly" ]]; then
-  snapcraft push "$SNAP" --release edge
-else
-  snapcraft push "$SNAP" --release candidate
-fi
