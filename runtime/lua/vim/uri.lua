7111594fdc137bcfb91901b12149330c02a5fc5e
Raphael <glepnir@gopherhub.org>
2020-09-08 21:54:00 +0800

    fix duplicate variable name

--- runtime/lua/vim/uri.lua
+++ runtime/lua/vim/uri.lua
@@ -3,7 +3,7 @@
 -- https://tools.ietf.org/html/rfc2732
 -- https://tools.ietf.org/html/rfc2396
 
-local is_windows = vim.loop.os_uname().sysname:match("Windows")
+local in_windows = vim.loop.os_uname().sysname == "Windows"
 local uri_decode
 do
   local schar = string.char
@@ -79,7 +79,7 @@ local function uri_to_fname(uri)
   end
   uri = uri_decode(uri)
   -- TODO improve this.
-  if is_windows then
+  if in_windows then
     uri = uri:gsub('^file:///', '')
     uri = uri:gsub('/', '\\')
   else
