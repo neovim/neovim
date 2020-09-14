f34eeba2d85b4382d7504e1f3e12a7afb0788c1d
Raphael <glepnir@gopherhub.org>
2020-09-14 23:03:02 +0800

    lsp: fix failed switch window error (#12900)
    * fix failed switch window error
    
    * checks if  window already exists

--- runtime/lua/vim/lsp/util.lua
+++ runtime/lua/vim/lsp/util.lua
@@ -668,7 +668,7 @@ function M.focusable_float(unique_name, fn)
   local bufnr = api.nvim_get_current_buf()
   do
     local win = find_window_by_var(unique_name, bufnr)
-    if win then
+    if win and api.nvim_win_is_valid(win) and not vim.fn.pumvisible() then
       api.nvim_set_current_win(win)
       api.nvim_command("stopinsert")
       return
