1a49346319cafc5b8879cf6c7ce56c057467b4ee
G-flat <63449095+G-flat@users.noreply.github.com>
2020-10-05 14:16:51 +0800

    PVS/V1001: Remove assignment to unused variable

--- src/nvim/api/buffer.c
+++ src/nvim/api/buffer.c
@@ -1531,11 +1531,11 @@ Integer nvim_buf_add_highlight(Buffer buffer,
     end_line++;
   }
 
-  ns_id = extmark_set(buf, ns_id, 0,
-                      (int)line, (colnr_T)col_start,
-                      end_line, (colnr_T)col_end,
-                      decoration_hl(hl_id), kExtmarkUndo);
-  return (Integer)ns_id;
+  extmark_set(buf, ns_id, 0,
+              (int)line, (colnr_T)col_start,
+              end_line, (colnr_T)col_end,
+              decoration_hl(hl_id), kExtmarkUndo);
+  return src_id;
 }
 
 /// Clears namespaced objects (highlights, extmarks, virtual text) from
