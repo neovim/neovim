d4f40c29fbbe984c6044ee1d123608a2a1406a27
TJ DeVries <devries.timothyj@gmail.com>
2020-10-22 15:25:04 -0400

    [WIP]: Thu 22 Oct 2020 03:25:04 PM EDT

--- runtime/lua/vim/lsp.lua
+++ runtime/lua/vim/lsp.lua
@@ -1127,6 +1127,40 @@ function lsp.omnifunc(findstart, base)
   return -2
 end
 
+--- Implements an LSP `formatexpr`-compatible
+--@param start_line 1-indexed line
+--@param end_line 1-indexed line
+--@param timeout_ms optional
+function lsp.formatexpr(start_line, end_line, timeout_ms)
+  if not start_line or not end_line then
+    if vim.fn.mode() == 'i' or vim.fn.mode() == 'R' then
+      -- `formatexpr` is also called when exceding
+      -- `textwidth` in insert mode
+      -- fall back to internal formatting
+      return 1
+    end
+    start_line = vim.v.lnum
+    end_line = start_line + vim.v.count - 1
+  end
+  if start_line > 0 and end_line > 0 then
+    local params = {
+      textDocument = { uri = vim.uri_from_bufnr(0) };
+      range = {
+        start = { line = start_line - 1; character = 0; };
+        ["end"] = { line = end_line - 1; character = 0; };
+      };
+    };
+    local result = vim.lsp.buf_request_sync(0, "textDocument/rangeFormatting", params, timeout_ms)
+    if result then
+      result = result[1].result
+      vim.lsp.util.apply_text_edits(result)
+    end
+  end
+ -- do not run builtin formatter.
+  return 0
+end
+
+
 ---Checks whether a client is stopped.
 ---
 --@param client_id (Number)
--- runtime/lua/vim/lsp/util.lua
+++ runtime/lua/vim/lsp/util.lua
@@ -1558,39 +1558,6 @@ function M.character_offset(buf, row, col)
   return str_utfindex(line, col)
 end
 
--- for use with `formatexpr` if called without parms
--- @param start_line 1-indexed line
--- @param end_line 1-indexed line
--- @param timeout_ms optional
-function M.formatexpr(start_line, end_line, timeout_ms)
-  if not start_line or not end_line then
-    if vim.fn.mode() == 'i' or vim.fn.mode() == 'R' then
-      -- `formatexpr` is also called when exceding
-      -- `textwidth` in insert mode
-      -- fall back to internal formatting
-      return 1
-    end
-    start_line = vim.v.lnum
-    end_line = start_line + vim.v.count - 1
-  end
-  if start_line > 0 and end_line > 0 then
-    local params = {
-      textDocument = { uri = vim.uri_from_bufnr(0) };
-      range = {
-        start = { line = start_line - 1; character = 0; };
-        ["end"] = { line = end_line - 1; character = 0; };
-      };
-    };
-    local result = vim.lsp.buf_request_sync(0, "textDocument/rangeFormatting", params, timeout_ms)
-    if result then
-      result = result[1].result
-      vim.lsp.util.apply_text_edits(result)
-    end
-  end
- -- do not run builtin formatter.
-  return 0
-end
-
 M.buf_versions = {}
 
 return M
