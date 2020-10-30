d9a58573fd599d006518b403385c4b7333d8d1a0
Rom Grk <romgrk.cc@gmail.com>
2020-10-30 03:22:52 -0400

    tests: make scroll tests pass

--- test/functional/autocmd/winscrolled_spec.lua
+++ test/functional/autocmd/winscrolled_spec.lua
@@ -12,22 +12,22 @@ describe('WinScrolled', function()
   it('is triggered by scrolling in normal/visual mode', function()
     source([[
     let width = winwidth(0)
-    let lines = [repeat('*', range(width * 2))]
-    call nvim_buf_set_lines(g:buf, 0, -1, v:true, lines)
+    let lines = [repeat('*', width * 2), repeat('*', width * 2)]
+    call nvim_buf_set_lines(0, 0, -1, v:true, lines)
 
     let g:scrolled = 0
     autocmd WinScrolled * let g:scrolled += 1
     normal! zl
     normal! <C-e>
     ]])
-    eq(1, eval('g:scrolled'))
+    eq(2, eval('g:scrolled'))
   end)
 
   it('is triggered when the window scrolls in insert mode', function()
     source([[
     let height = winheight(0)
     let lines = map(range(height * 2), {_, i -> string(i)})
-    call nvim_buf_set_lines(g:buf, 0, -1, v:true, lines)
+    call nvim_buf_set_lines(0, 0, -1, v:true, lines)
 
     let g:scrolled = 0
     autocmd WinScrolled * let g:scrolled += 1
