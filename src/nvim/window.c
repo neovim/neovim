fb0ea5882d91aae5455dbd2302df0977e55992f9
Rom Grk <romgrk.cc@gmail.com>
2020-11-10 02:13:56 -0500

    fix: prevent use-after-free of window

--- src/nvim/window.c
+++ src/nvim/window.c
@@ -4873,13 +4873,13 @@ bool win_did_scroll(win_T *wp)
 /// Trigger WinScrolled autocmd
 void do_autocmd_winscrolled(win_T *wp)
 {
-  apply_autocmds(EVENT_WINSCROLLED, NULL, NULL, false, wp->w_buffer);
-
   wp->w_last_topline = wp->w_topline;
   wp->w_last_leftcol = wp->w_leftcol;
   wp->w_last_width = wp->w_width;
   wp->w_last_height = wp->w_height;
   wp->w_last_buffer = wp->w_buffer->handle;
+
+  apply_autocmds(EVENT_WINSCROLLED, NULL, NULL, false, wp->w_buffer);
 }
 
 /*
