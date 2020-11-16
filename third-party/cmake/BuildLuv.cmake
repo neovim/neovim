neovim/neovim

commit 78f1eccc8669687b50220961c0a920a4da85d7d0

deps: Fix luv-static build issues #11961
- Fix an issue where CMAKE_BUILD_TYPE was not reflected during
  construction.
- Remove the switch that uses the static library(-MTd, -MT) because it was
  specified in the CMakeLists.txt of luv when building on MSVC.

related: https://github.com/luvit/luv/pull/461

third-party/cmake/BuildLuv.cmake
@@ -65,6 +65,7 @@ set(LUV_PATCH_COMMAND
 set(LUV_CONFIGURE_COMMAND_COMMON
   ${CMAKE_COMMAND} ${LUV_SRC_DIR}
   -DCMAKE_GENERATOR=${CMAKE_GENERATOR}
+  -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
   -DCMAKE_INSTALL_PREFIX=${DEPS_INSTALL_DIR}
   -DLUA_BUILD_TYPE=System
   -DWITH_SHARED_LIBUV=ON
@@ -87,7 +88,8 @@ endif()
 if(USE_BUNDLED_LIBUV)
   set(LUV_CONFIGURE_COMMAND_COMMON
     ${LUV_CONFIGURE_COMMAND_COMMON}
-    -DCMAKE_PREFIX_PATH=${DEPS_INSTALL_DIR})
+    -DCMAKE_PREFIX_PATH=${DEPS_INSTALL_DIR}
+    -DLUA_COMPAT53_DIR=${DEPS_BUILD_DIR}/src/lua-compat-5.3)
 endif()
 
 if(MINGW AND CMAKE_CROSSCOMPILING)
@@ -107,27 +109,22 @@ elseif(MSVC)
     "-DCMAKE_C_FLAGS:STRING=${CMAKE_C_COMPILER_ARG1} ${LUV_INCLUDE_FLAGS}"
     # Make sure we use the same generator, otherwise we may
     # accidentally end up using different MSVC runtimes
-    -DCMAKE_GENERATOR=${CMAKE_GENERATOR}
-    # Use static runtime
-    -DCMAKE_C_FLAGS_DEBUG="-MTd"
-    -DCMAKE_C_FLAGS_RELEASE="-MT")
+    -DCMAKE_GENERATOR=${CMAKE_GENERATOR})
 else()
   set(LUV_CONFIGURE_COMMAND
     ${LUV_CONFIGURE_COMMAND_COMMON}
     -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
     "-DCMAKE_C_FLAGS:STRING=${CMAKE_C_COMPILER_ARG1} ${LUV_INCLUDE_FLAGS} -fPIC")
+  if(CMAKE_GENERATOR MATCHES "Unix Makefiles" AND
+      (CMAKE_SYSTEM_NAME MATCHES ".*BSD" OR CMAKE_SYSTEM_NAME MATCHES "DragonFly"))
+      set(LUV_CONFIGURE_COMMAND ${LUV_CONFIGURE_COMMAND} -DCMAKE_MAKE_PROGRAM=gmake)
+  endif()
 endif()
 
-if(CMAKE_GENERATOR MATCHES "Unix Makefiles" AND
-        (CMAKE_SYSTEM_NAME MATCHES ".*BSD" OR CMAKE_SYSTEM_NAME MATCHES "DragonFly"))
-        set(LUV_BUILD_COMMAND ${CMAKE_COMMAND}
-          "-DLUA_COMPAT53_DIR=${DEPS_BUILD_DIR}/src/lua-compat-5.3"
-          "-DCMAKE_MAKE_PROGRAM=gmake" --build .)
-else()
-  set(LUV_BUILD_COMMAND ${CMAKE_COMMAND}
-    "-DLUA_COMPAT53_DIR=${DEPS_BUILD_DIR}/src/lua-compat-5.3" --build .)
-endif()
-set(LUV_INSTALL_COMMAND ${CMAKE_COMMAND} --build . --target install)
+set(LUV_BUILD_COMMAND
+  ${CMAKE_COMMAND} --build . --config ${CMAKE_BUILD_TYPE})
+set(LUV_INSTALL_COMMAND
+  ${CMAKE_COMMAND} --build . --target install --config ${CMAKE_BUILD_TYPE})
 
 BuildLuv(PATCH_COMMAND ${LUV_PATCH_COMMAND}
   CONFIGURE_COMMAND ${LUV_CONFIGURE_COMMAND}

commit 4c6628a7eb3ec0711d8d118b044fb8c923400f03

deps: fix BuildLuv without bundled libuv
Fixes:

    % rm -rf .deps
    % cmake -DUSE_BUNDLED_LUV=ON -DUSE_BUNDLED=OFF -S third-party -B .deps
    % cmake --build .deps
    ...
    [ 50%] Building C object CMakeFiles/luv.dir/src/luv.c.o
    /home/daniel/Vcs/neovim/.deps/build/src/luv/src/luv.c:20:10: fatal error: c-api/compat-5.3.h: No such file or directory
       20 | #include "c-api/compat-5.3.h"
          |          ^~~~~~~~~~~~~~~~~~~~
    compilation terminated.

This was broken since 78f1eccc8, where apparently the `LUA_COMPAT53_DIR`
was defined in a wrong location.

third-party/cmake/BuildLuv.cmake
@@ -68,6 +68,7 @@ set(LUV_CONFIGURE_COMMAND_COMMON
   -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
   -DCMAKE_INSTALL_PREFIX=${DEPS_INSTALL_DIR}
   -DLUA_BUILD_TYPE=System
+  -DLUA_COMPAT53_DIR=${DEPS_BUILD_DIR}/src/lua-compat-5.3
   -DWITH_SHARED_LIBUV=ON
   -DBUILD_SHARED_LIBS=OFF
   -DBUILD_MODULE=OFF)
@@ -88,8 +89,7 @@ endif()
 if(USE_BUNDLED_LIBUV)
   set(LUV_CONFIGURE_COMMAND_COMMON
     ${LUV_CONFIGURE_COMMAND_COMMON}
-    -DCMAKE_PREFIX_PATH=${DEPS_INSTALL_DIR}
-    -DLUA_COMPAT53_DIR=${DEPS_BUILD_DIR}/src/lua-compat-5.3)
+    -DCMAKE_PREFIX_PATH=${DEPS_INSTALL_DIR})
 endif()
 
 if(MINGW AND CMAKE_CROSSCOMPILING)