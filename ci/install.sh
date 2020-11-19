80d1c4da5ffb652474e398f2d4ce6fd1d66a878e
James McCoy <jamessan@jamessan.com>
2020-11-18 22:44:00 -0500

    ci: Only try installing python-pynvim if Py2 pip is present

--- ci/install.sh
+++ ci/install.sh
@@ -15,8 +15,10 @@ fi
 # Use default CC to avoid compilation problems when installing Python modules.
 echo "Install neovim module for Python 3."
 CC=cc python3 -m pip -q install --user --upgrade pynvim
-echo "Install neovim module for Python 2."
-CC=cc python2 -m pip -q install --user --upgrade pynvim
+if python2 -m pip -c True 2>&1; then
+  echo "Install neovim module for Python 2."
+  CC=cc python2 -m pip -q install --user --upgrade pynvim
+fi
 
 echo "Install neovim RubyGem."
 gem install --no-document --bindir "$HOME/.local/bin" --user-install --pre neovim
