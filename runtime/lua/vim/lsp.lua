2fb08e0cb5480c630f85ec45bfc8a42fbca2991b
Matthieu Coudron <mcoudron@hotmail.com>
2020-11-24 20:03:57 +0100

    cleanup

--- runtime/lua/vim/lsp.lua
+++ runtime/lua/vim/lsp.lua
@@ -569,8 +569,6 @@ function lsp.start_client(config)
     -- TODO(remove-callbacks)
     callbacks = handlers;
     handlers = handlers;
-    -- for $/progress report
-    messages = { name = name, messages = {}, progress = {}, status = {} }
   }
 
   -- Store the uninitialized_clients for cleanup in case we exit before initialize finishes.
