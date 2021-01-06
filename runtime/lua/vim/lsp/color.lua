5143f72e900b7954f3e37ab5e6821850764d6429
Adam P. Regasz-Rethy <rethy.spud@gmail.com>
2021-01-06 14:53:21 -0500

    Add docs to lsp.txt and lua.txt

--- runtime/lua/vim/lsp/color.lua
+++ runtime/lua/vim/lsp/color.lua
@@ -3,81 +3,95 @@ local highlight = require 'vim.highlight'
 local validate = vim.validate
 local api = vim.api
 
-local document_color_ns = api.nvim_create_namespace("vim_lsp_documentColor")
+local client_ns = {}
 
 local M = {}
+
+--- Returns the namespace for the given client_id
+---
+--@param client_id number client id
+--@return number
+local function get_client_ns(client_id)
+  if client_ns[client_id] == nil then
+    client_ns[client_id] = api.nvim_create_namespace('vim_lsp_documentColor_'..client_id)
+  end
+  return client_ns[client_id]
+end
 
 --- Changes the guibg to @rgb for the text in @range. Also changes the guifg to
 --- either #ffffff or #000000 based on which makes the text easier to read for
 --- the given guibg
 ---
+--@param client_id number client id
 --@param bufnr (number) buffer handle
 --@param range (table) with the structure:
 --       {start={line=<number>,character=<number>}, end={line=<number>,character=<number>}}
 --@param rgb (table) with keys 'r', 'g', 'b' in [0,255]
-local function highlight_background(bufnr, range, rgb)
-    local hex = color.rgb_to_hex(rgb)
-    local fghex = color.perceived_lightness(rgb) < 50 and 'ffffff' or '000000'
+local function color_background(client_id, bufnr, range, rgb)
+  local hex = color.rgb_to_hex(rgb)
+  local fghex = color.perceived_lightness(rgb) < 50 and 'ffffff' or '000000'
 
-    local hlname = string.format('LspDocumentColorBackground%s', hex)
-    api.nvim_command(string.format('highlight %s guibg=#%s guifg=#%s', hlname, hex, fghex))
+  local hlname = string.format('LspDocumentColorBackground%s', hex)
+  api.nvim_command(string.format('highlight %s guibg=#%s guifg=#%s', hlname, hex, fghex))
 
-    local start_pos = {range["start"]["line"], range["start"]["character"]}
-    local end_pos = {range["end"]["line"], range["end"]["character"]}
-    highlight.range(bufnr, document_color_ns, hlname, start_pos, end_pos)
+  local start_pos = {range["start"]["line"], range["start"]["character"]}
+  local end_pos = {range["end"]["line"], range["end"]["character"]}
+  highlight.range(bufnr, get_client_ns(client_id), hlname, start_pos, end_pos)
 end
 
 --- Changes the guifg to @rgb for the text in @range.
 ---
+--@param client_id number client id
 --@param bufnr (number) buffer handle
 --@param range (table) with the structure:
 --       {start={line=<number>,character=<number>}, end={line=<number>,character=<number>}}
 --@param rgb (table) with keys 'r', 'g', 'b' in [0,255]
-local function highlight_foreground(bufnr, range, rgb)
-    local hex = color.rgb_to_hex(rgb)
+local function color_foreground(client_id, bufnr, range, rgb)
+  local hex = color.rgb_to_hex(rgb)
 
-    local hlname = string.format('LspDocumentColorForeground%s', hex)
-    api.nvim_command(string.format('highlight %s guifg=#%s', hlname, hex))
+  local hlname = string.format('LspDocumentColorForeground%s', hex)
+  api.nvim_command(string.format('highlight %s guifg=#%s', hlname, hex))
 
-    local start_pos = {range["start"]["line"], range["start"]["character"]}
-    local end_pos = {range["end"]["line"], range["end"]["character"]}
-    highlight.range(bufnr, document_color_ns, hlname, start_pos, end_pos)
+  local start_pos = {range["start"]["line"], range["start"]["character"]}
+  local end_pos = {range["end"]["line"], range["end"]["character"]}
+  highlight.range(bufnr, get_client_ns(client_id), hlname, start_pos, end_pos)
 end
 
 --- Adds virtual text with the color @rgb and the text @virtual_text_str on
 --- the last line of the @range.
 ---
+--@param client_id number client id
 --@param bufnr (number) buffer handle
 --@param range (table) with the structure:
 --       {start={line=<number>,character=<number>}, end={line=<number>,character=<number>}}
 --@param rgb (table) with keys 'r', 'g', 'b' in [0,255]
 --@param virtual_text_str (string) to display as virtual text and color
-local function highlight_virtual_text(bufnr, range, rgb, virtual_text_str)
-    local hex = color.rgb_to_hex(rgb)
+local function color_virtual_text(client_id, bufnr, range, rgb, virtual_text_str)
+  local hex = color.rgb_to_hex(rgb)
 
-    local hlname = string.format('LspDocumentColorVirtualText%s', hex)
-    api.nvim_command(string.format('highlight %s guifg=#%s', hlname, hex))
+  local hlname = string.format('LspDocumentColorVirtualText%s', hex)
+  api.nvim_command(string.format('highlight %s guifg=#%s', hlname, hex))
 
-    local line = range['end']['line']
-    api.nvim_buf_set_virtual_text(bufnr, document_color_ns, line, {{virtual_text_str, hlname}}, {})
+  local line = range['end']['line']
+  api.nvim_buf_set_virtual_text(bufnr, get_client_ns(client_id), line, {{virtual_text_str, hlname}}, {})
 end
 
 --- Clears the previous document colors and adds the new document colors from @result.
 --- Follows the same signature as :h lsp-handler
-function M.on_document_color(_, _, result, _, bufnr, config)
-  -- TODO debounce it and document
-  if not bufnr then return end
-  M.buf_clear_highlights(bufnr)
+function M.on_document_color(_, _, result, client_id, bufnr, config)
+  if not bufnr or not client_id then return end
+  M.buf_clear_color(client_id, bufnr)
   if not result then return end
-  M.buf_highlight(bufnr, result, config)
+  M.buf_color(client_id, bufnr, result, config)
 end
 
 --- Shows a list of document colors for a certain buffer.
 ---
+--@param client_id number client id
 --@param bufnr buffer id
 --@param color_infos Table of `ColorInformation` objects to highlight.
 --       See https://microsoft.github.io/language-server-protocol/specification#textDocument_documentColor
-function M.buf_highlight(bufnr, color_infos, config)
+function M.buf_color(client_id, bufnr, color_infos, config)
   validate {
     bufnr = {bufnr, 'n', false},
     color_infos = {color_infos, 't', false},
@@ -86,12 +100,12 @@ function M.buf_highlight(bufnr, color_infos, config)
   if not color_infos or not bufnr then return end
 
   config = vim.lsp._with_extend('vim.lsp.color.on_document_color', {
-    background = false,
-    foreground = false,
-    virtual_text = false,
-    virtual_text_str = '■',
-    background_color = vim.color.decode_24bit_rgb(vim.api.nvim_get_hl_by_name('Normal', true)['background']),
-  }, config)
+      background = false,
+      foreground = false,
+      virtual_text = false,
+      virtual_text_str = '■',
+      background_color = vim.color.decode_24bit_rgb(vim.api.nvim_get_hl_by_name('Normal', true)['background']),
+    }, config)
 
   for _, color_info in ipairs(color_infos) do
     local rgba, range = color_info.color, color_info.range
@@ -99,25 +113,29 @@ function M.buf_highlight(bufnr, color_infos, config)
     local rgb = color.rgba_to_rgb({r=r, g=g, b=b, a=a}, config.background_color)
 
     if config.background then
-      highlight_background(bufnr, range, rgb)
+      color_background(client_id, bufnr, range, rgb)
     end
 
     if config.foreground then
-      highlight_foreground(bufnr, range, rgb)
+      color_foreground(client_id, bufnr, range, rgb)
     end
 
     if config.virtual_text then
-      highlight_virtual_text(bufnr, range, rgb, config.virtual_text_str)
+      color_virtual_text(client_id, bufnr, range, rgb, config.virtual_text_str)
     end
   end
 end
 
 --- Removes document color highlights from a buffer.
 ---
+--@param client_id number client id
 --@param bufnr buffer id
-function M.buf_clear_highlights(bufnr)
-    validate { bufnr = {bufnr, 'n', true} }
-    api.nvim_buf_clear_namespace(bufnr, document_color_ns, 0, -1)
+function M.buf_clear_color(client_id, bufnr)
+  validate {
+    client_id = {client_id, 'n', true},
+    bufnr = {bufnr, 'n', true}
+  }
+  api.nvim_buf_clear_namespace(bufnr, get_client_ns(client_id), 0, -1)
 end
 
 return M
