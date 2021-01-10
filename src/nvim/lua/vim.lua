c29e5c0273d22d18ee31ac8a1cbe53f1f1c3414f
Adam P. Regasz-Rethy <rethy.spud@gmail.com>
2021-01-10 16:39:08 -0500

    Delete old lazy loading of vim.keymap module

--- src/nvim/lua/vim.lua
+++ src/nvim/lua/vim.lua
@@ -277,9 +277,6 @@ local function __index(t, key)
   elseif key == 'F' then
     t.F = require('vim.F')
     return t.F
-  elseif key == 'keymap' then
-    t.keymap = require('vim.keymap')
-    return t.keymap
   end
 end
 
