bcc6b9c0119655381812907088fb0bd4b3e4f963
Michael Lingelbach <m.j.lbach@gmail.com>
2021-02-02 08:55:32 -0800

    vim.ui: add notification widget

--- runtime/lua/vim/ui/_utils.lua
+++ runtime/lua/vim/ui/_utils.lua
@@ -16,4 +16,17 @@ function utils.tbl_apply_defaults(original, defaults)
   return original
 end
 
+function utils.tbl_longest_str(tbl)
+  local len = 0
+
+  for _,str in pairs(tbl) do
+    local str_len = #str
+    if str_len > len then
+      len = str_len
+    end
+  end
+
+  return len
+end
+
 return utils
