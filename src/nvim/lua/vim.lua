d60ddf70c95c532e0486e44c4b96bedc8c85b50b
Anirudh H M <anihm136@gmail.com>
2021-02-09 12:20:21 +0530

    vim.lua: Refactor vim.get_visual_selection()
    Separate functionality into vim.get_marked_region() and
    vim.get_visual_selection(). Use 'v' and '.' for getting visual selection region,
    and adjust positions based on visual mode

--- src/nvim/lua/vim.lua
+++ src/nvim/lua/vim.lua
@@ -457,62 +457,91 @@ function vim.region(bufnr, pos1, pos2, regtype, inclusive)
   return region
 end
 
---- Get the current/last visual selection as a string
+--- Get the region between two marks and the start and end positions for the region
 ---
---@return selection string containing the last visual selection
+--@param mark1 Name of mark starting the region
+--@param mark2 Name of mark ending the region
+--@param options Table containing the adjustment function, register type and selection mode
+--@return region region between the two marks, as returned by |vim.region|
+--@return start (row,col) tuple denoting the start of the region
+--@return finish (row,col) tuple denoting the end of the region
+function vim.get_marked_region(mark1, mark2, options)
+  local bufnr = 0
+  local adjust = options.adjust or function(pos1, pos2)
+    return pos1, pos2
+  end
+  local regtype = options.regtype or vim.fn.visualmode()
+  local selection = options.selection or (vim.o.selection ~= 'exclusive')
+
+  local pos1 = vim.fn.getpos(mark1)
+  local pos2 = vim.fn.getpos(mark2)
+  pos1, pos2 = adjust(pos1, pos2)
+
+  local start = { pos1[2] - 1, pos1[3] - 1 + pos1[4] }
+  local finish = { pos2[2] - 1, pos2[3] - 1 + pos2[4] }
+
+  -- Return if start or finish are invalid
+  if start[2] < 0 or finish[1] < start[1] then return end
+
+  local region = vim.region(bufnr, start, finish, regtype, selection)
+  return region, start, finish
+end
+
+--- Get the current visual selection as a string
+---
+--@return selection string containing the current visual selection
 function vim.get_visual_selection()
-	local visual_modes = {
-		v = true,
-		V = true,
-		-- [t'<C-v>'] = true, -- Visual block does not seem to be supported by vim.region
-	}
+  local visual_modes = {
+    v = true,
+    V = true,
+    -- [t'<C-v>'] = true, -- Visual block does not seem to be supported by vim.region
+  }
 
-	-- Return if not in visual mode
-	if visual_modes[vim.api.nvim_get_mode().mode] == nil then return end
+  -- Return if not in visual mode
+  if visual_modes[vim.api.nvim_get_mode().mode] == nil then return end
 
-	local bufnr = 0
+  local options = {}
+  options.adjust = function(pos1, pos2)
+    if vim.fn.visualmode() == "V" then
+      pos1[3] = 1
+      pos2[3] = 2^31 - 1
+    end
 
-	local pos1 = vim.fn.getpos("'<")
-	local pos2 = vim.fn.getpos("'>")
+    if pos1[2] > pos2[2] then
+      pos2[3], pos1[3] = pos1[3], pos2[3]
+      return pos2, pos1
+    elseif pos1[2]==pos2[2] and pos1[3] > pos2[3] then
+      return pos2, pos1
+    else
+      return pos1, pos2
+    end
+  end
 
-	local start = { pos1[2] - 1, pos1[3] - 1 + pos1[4] }
-	local finish = { pos2[2] - 1, pos2[3] - 1 + pos2[4] }
+  local region, start, finish = vim.get_marked_region('v', '.', options)
 
-	-- Return if start or finish are invalid
-	if start[2] < 0 or finish[1] < start[1] then return end
+  -- Compute the number of chars to get from the first line,
+  -- because vim.region returns -1 as the ending col if the
+  -- end of the line is included in the selection
+  local lines =
+    vim.api.nvim_buf_get_lines(bufnr, start[1], finish[1] + 1, false)
+  local line1_end
+  if region[start[1]][2] - region[start[1]][1] < 0 then
+    line1_end = #lines[1] - region[start[1]][1]
+  else
+    line1_end = region[start[1]][2] - region[start[1]][1]
+  end
 
-	local region =
-		vim.region(
-			bufnr,
-			start,
-			finish,
-			vim.fn.visualmode(),
-			(vim.o.selection ~= 'exclusive')
-		)
-	local lines =
-		vim.api.nvim_buf_get_lines(bufnr, start[1], finish[1] + 1, false)
-
-	-- Compute the number of chars to get from the first line,
-	-- because vim.region returns -1 as the ending col if the
-	-- end of the line is included in the selection
-	local line1_end
-	if region[start[1]][2] - region[start[1]][1] < 0 then
-		line1_end = #lines[1] - region[start[1]][1]
-	else
-		line1_end = region[start[1]][2] - region[start[1]][1]
-	end
-
-	lines[1] = vim.fn.strpart(lines[1], region[start[1]][1], line1_end, true)
-	if start[1] ~= finish[1] then
-		lines[#lines] =
-			vim.fn.strpart(
-				lines[#lines],
-				region[finish[1]][1],
-				region[finish[1]][2] - region[finish[1]][1],
-				true
-			)
-	end
-	return table.concat(lines)
+  lines[1] = vim.fn.strpart(lines[1], region[start[1]][1], line1_end, true)
+  if start[1] ~= finish[1] then
+    lines[#lines] =
+      vim.fn.strpart(
+        lines[#lines],
+        region[finish[1]][1],
+        region[finish[1]][2] - region[finish[1]][1],
+        false
+      )
+  end
+  return table.concat(lines)
 end
 
 --- Defers calling `fn` until `timeout` ms passes.
