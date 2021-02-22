a167ae6d5d1cfa1fded1252af8224d00c3d9ff16
Matthieu Coudron <mcoudron@hotmail.com>
2021-02-22 21:02:29 +0100

    fix(flake): can now generate vimdoc
    and removed warning about deprecated usage of stdenv.lib
    
    also can run `make pylint shlint`.

--- contrib/flake.nix
+++ contrib/flake.nix
@@ -42,12 +42,16 @@
             disallowedReferences = [];
           }));
 
-          # for neovim developers, very slow
+          # for neovim developers, builds a slow binary
+          # huge closure size but aims at covering all scripts
           # brings development tools as well
           neovim-developer =
             let
               lib = nixpkgs.lib;
-              pythonEnv = pkgs.python3;
+              pythonEnv = pkgs.python3.withPackages(ps: [
+                ps.msgpack
+                ps.flake8  # for 'make pylint'
+              ]);
               luacheck = pkgs.luaPackages.luacheck;
             in
             (neovim-debug.override ({ doCheck = pkgs.stdenv.isLinux; })).overrideAttrs (oa: {
@@ -56,7 +60,7 @@
                 "-DMIN_LOG_LEVEL=0"
                 "-DENABLE_LTO=OFF"
                 "-DUSE_BUNDLED=OFF"
-              ] ++ pkgs.stdenv.lib.optionals pkgs.stdenv.isLinux [
+              ] ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
                 # https://github.com/google/sanitizers/wiki/AddressSanitizerFlags
                 # https://clang.llvm.org/docs/AddressSanitizer.html#symbolizing-the-reports
                 "-DCLANG_ASAN_UBSAN=ON"
@@ -66,7 +70,8 @@
                 pythonEnv
                 include-what-you-use # for scripts/check-includes.py
                 jq # jq for scripts/vim-patch.sh -r
-                doxygen
+                shellcheck # for `make shlint`
+                doxygen    # for script/gen_vimdoc.py
               ]);
 
               shellHook = oa.shellHook + ''
@@ -102,6 +107,5 @@
         defaultApp = apps.nvim;
 
         devShell = pkgs.neovim-developer;
-      }
-    );
+    });
 }
