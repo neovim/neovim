da63b86c1a182c9ec83a288ca84fb9b129f8fb87
smolck <46855713+smolck@users.noreply.github.com>
2021-03-12 10:53:23 -0600

    initial workings for semantic tokens handler

--- runtime/lua/vim/lsp/semantic_tokens.lua
+++ runtime/lua/vim/lsp/semantic_tokens.lua
@@ -9,13 +9,41 @@ function M.on_semantic_tokens(...)
   prinspect(...)
 end
 
-function M.handle_semantic_tokens_full(client, bufnr, data)
+function M.handle_semantic_tokens_full(client, bufnr, response)
   local legend = client.server_capabilities.semanticTokensProvider.legend
   local token_types = legend.tokenTypes
   local token_modifiers = legend.tokenModifiers
+  local data = response.data
 
-  print('DATA')
-  prinspect(data)
+  local semantic_tokens = {}
+  local prev_line, prev_start = nil, 0
+  for i = 1, #data, 5 do
+    local delta_line = data[i]
+    prev_line = prev_line and prev_line + delta_line or delta_line
+    local delta_start = data[i + 1]
+    prev_start = delta_line == 0 and prev_start + delta_start or delta_start
+
+    -- data[i+3] +1 because Lua tables are 1-indexed
+    local token_type = token_types[data[i + 3] + 1]
+    print(token_type)
+
+    if delta_line == 0 and semantic_tokens[prev_line + 1] then
+      table.insert(semantic_tokens[prev_line + 1], #semantic_tokens, {
+        start_char = prev_start,
+        length = data[i + 2],
+        token_type = token_type,
+        -- token_modifiers = 
+      })
+    else
+      semantic_tokens[prev_line + 1] = {
+        {
+          start_char = prev_start,
+          length = data[i + 2],
+          token_type = token_type,
+        }
+      }
+    end
+  end
 end
 
 function M.request_tokens_full(client_id, bufnr)
@@ -24,8 +52,8 @@ function M.request_tokens_full(client_id, bufnr)
   local client = vim.lsp.get_client_by_id(client_id)
 
   -- TODO(smolck): Not sure what the other params to this are
-  local response = client.request('textDocument/semanticTokens/full', params, function(_, _method, data, _, _)
-    M.handle_semantic_tokens_full(client, bufnr, data)
+  local response = client.request('textDocument/semanticTokens/full', params, function(_, _method, response, _, _)
+    M.handle_semantic_tokens_full(client, bufnr, response)
   end)
 end
 
