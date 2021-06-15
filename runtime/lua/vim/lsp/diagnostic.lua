b8dfa2f112130c1dcb12409f881a3fd83aa5b56d
Jaehwang Jerry Jung <tomtomjhj@gmail.com>
2021-06-15 15:07:52 +0900

    fix(lsp): fix on_detach in lsp.diagnostic
    `lsp.diagnostic.get_all()` was returning diagnotics for `:bwipeout`-ed
    buffers because the diagnostic cache is not cleared. The first argument
    of on_detach callback is the string "detach", not the bufnr.

--- runtime/lua/vim/lsp/diagnostic.lua
+++ runtime/lua/vim/lsp/diagnostic.lua
@@ -321,9 +321,9 @@ function M.save(diagnostics, bufnr, client_id)
 
     -- Clean up our data when the buffer unloads.
     api.nvim_buf_attach(bufnr, false, {
-      on_detach = function(b)
+      on_detach = function(_, b)
         clear_diagnostic_cache(b, client_id)
-        _diagnostic_cleanup[bufnr][client_id] = nil
+        _diagnostic_cleanup[b][client_id] = nil
       end
     })
   end
