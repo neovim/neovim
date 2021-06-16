71f155963cd13546f47f9710d0fbdbeb4e924c39
Marc Jakobi <marc.jakobi@velasolaris.com>
2021-06-16 11:15:27 +0000

    Add vim.lsp.diagnositc.set_qflist() function.

--- runtime/lua/vim/lsp/diagnostic.lua
+++ runtime/lua/vim/lsp/diagnostic.lua
@@ -1190,6 +1190,38 @@ function M.reset(client_id, buffer_client_map)
   end)
 end
 
+--- Sets the quickfix list
+---@param opts table|nil Configuration table. Keys:
+---         - {open_qflist}: (boolean, default true)
+---             - Open quickfix list after set
+---         - {client_id}: (number)
+---             - If nil, will consider all clients attached to buffer.
+---         - {severity}: (DiagnosticSeverity)
+---             - Exclusive severity to consider. Overrides {severity_limit}
+---         - {severity_limit}: (DiagnosticSeverity)
+---             - Limit severity of diagnostics found. E.g. "Warning" means { "Error", "Warning" } will be valid.
+function M.set_qflist(opts)
+  opts = opts or {}
+  local open_qflist = if_nil(opts.onen_qflist, true)
+  local diags = M.get_all(opts.client_id) 
+  local predicate = function(d)
+    local severity = to_severity(opts.severity)
+    if severity then
+      return d.severity == severity
+    end
+    severity = to_severity(opts.severity_limit)
+    if severity then
+      return d.severity == severity
+    end
+    return true
+  end
+  local items = util.diagnostics_to_items(diags, predicate)
+  util.set_qflist(items)
+  if open_qflist then
+    vim.cmd [[copen]]
+  end
+end
+
 --- Sets the location list
 ---@param opts table|nil Configuration table. Keys:
 ---         - {open_loclist}: (boolean, default true)
