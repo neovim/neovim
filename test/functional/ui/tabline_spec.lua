26efdf0e0bb3c295ea9fa001db06fcf555b78228
John Gehrig <jdg.gehrig@gmail.com>
2021-06-27 14:38:55 -0400

    SQUASH

--- test/functional/ui/tabline_spec.lua
+++ test/functional/ui/tabline_spec.lua
@@ -4,14 +4,17 @@ local clear, command, eq = helpers.clear, helpers.command, helpers.eq
 
 describe('ui/ext_tabline', function()
   local screen
-  local event_tabs, event_curtab
+  local event_tabs, event_curtab, event_curbuf, event_buffers
 
   before_each(function()
     clear()
     screen = Screen.new(25, 5)
     screen:attach({rgb=true, ext_tabline=true})
-    function screen:_handle_tabline_update(curtab, tabs)
-      event_curtab, event_tabs = curtab, tabs
+    function screen:_handle_tabline_update(curtab, tabs, curbuf, buffers)
+      event_curtab = curtab
+      event_tabs = tabs
+      event_curbuf = curbuf
+      event_buffers = buffers
     end
   end)
 
@@ -43,6 +46,40 @@ describe('ui/ext_tabline', function()
     ]], condition=function()
       eq({ id = 1 }, event_curtab)
       eq(expected_tabs, event_tabs)
+    end}
+  end)
+
+  it('buffer UI events', function()
+    local expected_buffers_initial= {
+      {buffer = { id = 1 }, name = '[No Name]'},
+    }
+
+    screen:expect{grid=[[
+      ^                         |
+      ~                        |
+      ~                        |
+      ~                        |
+                               |
+    ]], condition=function()
+      eq({ id = 1}, event_curbuf)
+      eq(expected_buffers_initial, event_buffers)
+    end}
+
+    command("badd another-buffer")
+    command("bnext")
+
+    local expected_buffers = {
+      {buffer = { id = 2 }, name = 'another-buffer'},
+    }
+    screen:expect{grid=[[
+      ^                         |
+      ~                        |
+      ~                        |
+      ~                        |
+                               |
+    ]], condition=function()
+      eq({ id = 2 }, event_curbuf)
+      eq(expected_buffers, event_buffers)
     end}
   end)
 end)
