af4595a1db7688ff7a133dc16835e075f54d2bfd
Dundar Göc <gocdundar@gmail.com>
2021-07-09 00:54:31 +0200

    Create script that squashes all "typo" PR:s.

---
+++ scripts/collect-typos.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+pushd "$(git rev-parse --show-toplevel)" >/dev/null 
+
+git checkout -b catch-all-typos
+
+while read -r pr; do
+	patch -p1 <<< "$(gh pr diff "$pr")" 
+done <<< "$(gh pr list --label "typo" | awk '{print $1}')"
+
+git add -A
+git commit -m "Squash all PR:s with the typo label."
+git push --set-upstream origin catch-all-typos
+gh pr create --title "Squash of all typo pull-requests."
+
+git checkout -
+
+popd >/dev/null 
