ef0bb59099065fd0921cfba6b26a29b85bc690a7
Dundar Göc <gocdundar@gmail.com>
2021-08-02 10:18:09 +0200

    ci: disable false "footer-leading-blank" warning from commitlint

--- .github/workflows/commitlint.config_patch.js
+++ .github/workflows/commitlint.config_patch.js
@@ -5,7 +5,6 @@ module.exports = {
 	rules: {
 		'body-leading-blank': [1, 'always'],
 		'body-max-line-length': [2, 'always', 100],
-		'footer-leading-blank': [1, 'always'],
 		'footer-max-line-length': [2, 'always', 100],
 		'scope-case': [2, 'always', 'lower-case'],
 		'subject-case': [
