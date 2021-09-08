384a43fe7171e255fa212ca92387893fdd17185b
Michael Lingelbach <m.j.lbach@gmail.com>
2021-09-07 21:48:19 -0700

    chore(lsp): fix formatting in vim.lsp.log (#15596)

--- runtime/lua/vim/lsp/log.lua
+++ runtime/lua/vim/lsp/log.lua
@@ -33,15 +33,15 @@ do
   vim.fn.mkdir(vim.fn.stdpath('cache'), "p")
   local logfile = assert(io.open(logfilename, "a+"))
 
-	local log_info = vim.loop.fs_stat(logfilename)
-	if log_info and log_info.size > 1e9 then
-		local warn_msg = string.format(
-			"LSP client log is large (%d MB): %s",
-			log_info.size / (1000 * 1000),
-			logfilename
-		)
-		vim.notify(warn_msg)
-	end
+  local log_info = vim.loop.fs_stat(logfilename)
+  if log_info and log_info.size > 1e9 then
+    local warn_msg = string.format(
+      "LSP client log is large (%d MB): %s",
+      log_info.size / (1000 * 1000),
+      logfilename
+    )
+    vim.notify(warn_msg)
+  end
 
   -- Start message for logging
   logfile:write(string.format("[ START ] %s ] LSP logging initiated\n", os.date(log_date_format)))
