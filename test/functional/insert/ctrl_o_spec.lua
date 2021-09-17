5f144efefa66c9694123b679bab52992279a6015
zeertzjq <zeertzjq@outlook.com>
2021-09-17 21:17:51 +0800

    fix: prevent K_EVENT from stopping Select mode CTRL-O #15688
    When using Goneovim, Select mode `CTRL-O` returns back to Select mode
    immediately (even with `--clean`). Neovim TUI (with some plugins) also randomly
    returns to Select mode even if no keys are pressed when using `CTRL-O` in Select
    mode.

--- test/functional/insert/ctrl_o_spec.lua
+++ test/functional/insert/ctrl_o_spec.lua
@@ -1,5 +1,4 @@
 local helpers = require('test.functional.helpers')(after_each)
-local assert_alive = helpers.assert_alive
 local clear = helpers.clear
 local eq = helpers.eq
 local eval = helpers.eval
@@ -46,7 +45,7 @@ describe('insert-mode Ctrl-O', function()
   it("doesn't cancel Ctrl-O mode when processing event", function()
     feed('iHello World<c-o>')
     eq({mode='niI', blocking=false}, meths.get_mode()) -- fast event
-    assert_alive()  -- causes K_EVENT key
+    eq(2, eval('1+1'))  -- causes K_EVENT key
     eq({mode='niI', blocking=false}, meths.get_mode()) -- still in ctrl-o mode
     feed('dd')
     eq({mode='i', blocking=false}, meths.get_mode()) -- left ctrl-o mode
