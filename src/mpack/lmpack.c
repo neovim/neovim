620717e9a79ff652f6c03493fa30816e26f7c088
Dundar Göc <gocdundar@gmail.com>
2021-09-21 16:55:38 +0200

    chore: PVS/V009: eliminate warning about free usage of PVS-Studio

--- src/mpack/lmpack.c
+++ src/mpack/lmpack.c
@@ -1,3 +1,6 @@
+// This is an open source non-commercial project. Dear PVS-Studio, please check
+// it. PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
 /*
  * This module exports three classes, and each instance of those classes has its
  * own private registry for temporary reference storage(keeping state between
