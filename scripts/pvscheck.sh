508fcdadb7f56da9c556727bfa414b5411912255
Justin M. Keyes <justinkz@gmail.com>
2021-09-24 03:07:05 -0700

    fix(PVS): disable "typo" warnings V1051, V1074
    PVS is worried about typos. Now we need it to stop worrying...
    Disable these checks entirely, they are all false positives.
    
    tui.c:1873        V1074   Boundary between escape sequence and string is unclear. The escape sequence ends with a letter and the next character is also a letter. Check for typos.
    tui.c:1983        V1074   Boundary between escape sequence and string is unclear. The escape sequence ends with a letter and the next character is also a letter. Check for typos.
    
    regexp_nfa.c:6189 V1051   Consider checking for misprints. It's possible that the 'pim->result' should be checked here.
    screen.c:2928     V1051   Consider checking for misprints. It's possible that the 'vcol_sbr' should be checked here.
    screen.c:3187     V1051   Consider checking for misprints. It's possible that the 'line_attr' should be checked here.
    screen.c:3267     V1051   Consider checking for misprints. It's possible that the 'multi_attr' should be checked here.
    screen.c:4747     V1051   Consider checking for misprints. It's possible that the 'redraw_next' should be checked here.
    syntax.c:3448     V1051   Consider checking for misprints. It's possible that the 'arg_end' should be checked here.
    syntax.c:3625     V1051   Consider checking for misprints. It's possible that the 'arg_end' should be checked here.
    tui.c:1836        V1051   Consider checking for misprints. It's possible that the 'data->unibi_ext.set_cursor_style' should be checked here.
    tui.c:1863        V1051   Consider checking for misprints. It's possible that the 'data->unibi_ext.set_cursor_style' should be checked here.
    tui.c:1882        V1051   Consider checking for misprints. It's possible that the 'data->unibi_ext.set_cursor_style' should be checked here.

--- scripts/pvscheck.sh
+++ scripts/pvscheck.sh
@@ -379,7 +379,7 @@ run_analysis() {(
       --sourcetree-root . || true
 
   rm -rf PVS-studio.{xml,err,tsk,html.d}
-  local plog_args="PVS-studio.log --srcRoot . --excludedCodes V011,V1042"
+  local plog_args="PVS-studio.log --srcRoot . --excludedCodes V011,V1042,V1051,V1074"
   plog-converter $plog_args --renderTypes xml       --output PVS-studio.xml
   plog-converter $plog_args --renderTypes errorfile --output PVS-studio.err
   plog-converter $plog_args --renderTypes tasklist  --output PVS-studio.tsk
