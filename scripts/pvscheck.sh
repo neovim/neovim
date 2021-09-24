03ed72642ddfe6a603673efdc0998a154e581b88
Justin M. Keyes <justinkz@gmail.com>
2021-09-24 03:26:31 -0700

    fix(pvs): Exclude xdiff from analysis
    ref 088161a9459a
    ref fbe88ef8f5a4

--- scripts/pvscheck.sh
+++ scripts/pvscheck.sh
@@ -373,7 +373,7 @@ run_analysis() {(
     analyze \
       --lic-file PVS-Studio.lic \
       --threads "$(get_jobs_num)" \
-      --exclude-path src/nvim/xdiff \
+      --exclude-path src/xdiff \
       --output-file PVS-studio.log \
       --file build/compile_commands.json \
       --sourcetree-root . || true
