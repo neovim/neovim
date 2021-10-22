a75a0951d0a2d8d0d8975a98f2dbb5581a6d34ac
Dundar Göc <gocdundar@gmail.com>
2021-10-22 15:15:12 +0200

    revert: fix(build): export symbols on Windows
    This reverts commit aa644b7fd3ba195ed101b8afc4c599050160cc79.
    
    Revert commit that breaks CI for windows (MSVC_32).

--- src/nvim/generators/gen_declarations.lua
+++ src/nvim/generators/gen_declarations.lua
@@ -216,16 +216,7 @@ local footer = [[
 #include "nvim/func_attr.h"
 ]]
 
-local non_static = header .. [[
-#ifndef DLLEXPORT
-#  ifdef WIN32
-#    define DLLEXPORT __declspec(dllexport)
-#  else
-#    define DLLEXPORT
-#  endif
-#endif
-]]
-
+local non_static = header
 local static = header
 
 local filepattern = '^#%a* (%d+) "([^"]-)/?([^"/]+)"'
@@ -278,7 +269,6 @@ while init ~= nil do
       declaration = declaration:gsub(' $', '')
       declaration = declaration:gsub('^ ', '')
       declaration = declaration .. ';'
-
       if os.getenv('NVIM_GEN_DECLARATIONS_LINE_NUMBERS') == '1' then
         declaration = declaration .. ('  // %s/%s:%u'):format(
             curdir, curfile, declline)
@@ -287,7 +277,6 @@ while init ~= nil do
       if declaration:sub(1, 6) == 'static' then
         static = static .. declaration
       else
-        declaration = 'DLLEXPORT ' .. declaration
         non_static = non_static .. declaration
       end
       declendpos = e
