b537c8215e3cf96f2e7003e61870a8e9de8ae355
Michael Lingelbach <m.j.lbach@gmail.com>
2021-11-03 18:56:22 -0700

    is this real life, or is it just fantasy

--- runtime/lua/vim/lsp/util.lua
+++ runtime/lua/vim/lsp/util.lua
@@ -592,7 +592,10 @@ local function extract_text(lines, start_range, end_range)
 
     -- Add the fragment of the last line
     local line = lines[end_range.line_idx]
-    result = result .. string.sub(line, 1, end_range.byte_idx)
+    -- byte index 1 indicates rollover from previous line
+    if end_range.byte_idx > 1 then
+      result = result .. string.sub(line, 1, end_range.byte_idx)
+    end
 
     return result
   end
