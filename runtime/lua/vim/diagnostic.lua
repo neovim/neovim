neovim/neovim

commit 04bfd20bb8dfa2cfa0ca357e3a9cea79ed47ad25

fix(diagnostic): remove invalid buffers from cache (#16397)
Errors were being caused by invalid buffers being kept around in
diagnostic_cache, so add a metatable to diagnostic_cache which attaches
to new buffers in the cache, removing them after they are invalidated.

Closes #16391.

Co-authored-by: Gregory Anders <8965202+gpanders@users.noreply.github.com>

runtime/lua/vim/diagnostic.lua
@@ -43,9 +43,7 @@ local bufnr_and_namespace_cacher_mt = {
       bufnr = vim.api.nvim_get_current_buf()
     end
 
-    if rawget(t, bufnr) == nil then
-      rawset(t, bufnr, {})
-    end
+    rawset(t, bufnr, {})
 
     return rawget(t, bufnr)
   end,
@@ -59,8 +57,24 @@ local bufnr_and_namespace_cacher_mt = {
   end,
 }
 
-local diagnostic_cleanup = setmetatable({}, bufnr_and_namespace_cacher_mt)
-local diagnostic_cache = setmetatable({}, bufnr_and_namespace_cacher_mt)
+local diagnostic_cache = setmetatable({}, {
+  __index = function(t, bufnr)
+    if not bufnr or bufnr == 0 then
+      bufnr = vim.api.nvim_get_current_buf()
+    end
+
+    vim.api.nvim_buf_attach(bufnr, false, {
+      on_detach = function()
+        rawset(t, bufnr, nil) -- clear cache
+      end
+    })
+
+    rawset(t, bufnr, {})
+
+    return rawget(t, bufnr)
+  end,
+})
+
 local diagnostic_cache_extmarks = setmetatable({}, bufnr_and_namespace_cacher_mt)
 local diagnostic_attached_buffers = {}
 local diagnostic_disabled = {}
@@ -282,11 +296,6 @@ local function set_diagnostic_cache(namespace, bufnr, diagnostics)
     diagnostic.bufnr = bufnr
   end
   diagnostic_cache[bufnr][namespace] = diagnostics
-end
-
----@private
-local function clear_diagnostic_cache(namespace, bufnr)
-  diagnostic_cache[bufnr][namespace] = nil
 end
 
 ---@private
@@ -651,19 +660,8 @@ function M.set(namespace, bufnr, diagnostics, opts)
   }
 
   if vim.tbl_isempty(diagnostics) then
-    clear_diagnostic_cache(namespace, bufnr)
+    diagnostic_cache[bufnr][namespace] = nil
   else
-    if not diagnostic_cleanup[bufnr][namespace] then
-      diagnostic_cleanup[bufnr][namespace] = true
-
-      -- Clean up our data when the buffer unloads.
-      vim.api.nvim_buf_attach(bufnr, false, {
-        on_detach = function(_, b)
-          clear_diagnostic_cache(namespace, b)
-          diagnostic_cleanup[b][namespace] = nil
-        end
-      })
-    end
     set_diagnostic_cache(namespace, bufnr, diagnostics)
   end
 
@@ -1326,7 +1324,7 @@ function M.reset(namespace, bufnr)
   for _, iter_bufnr in ipairs(buffers) do
     local namespaces = namespace and {namespace} or vim.tbl_keys(diagnostic_cache[iter_bufnr])
     for _, iter_namespace in ipairs(namespaces) do
-      clear_diagnostic_cache(iter_namespace, iter_bufnr)
+      diagnostic_cache[iter_bufnr][iter_namespace] = nil
       M.hide(iter_namespace, iter_bufnr)
     end
   end

test/functional/lua/diagnostic_spec.lua
@@ -343,6 +343,16 @@ describe('vim.diagnostic', function()
       eq(0, result[5])
       eq(3, result[6])
     end)
+
+    it("doesn't error after bwipeout on buffer", function()
+      exec_lua [[
+        vim.diagnostic.set(diagnostic_ns, diagnostic_bufnr, { lnum = 0, end_lnum = 0, col = 0, end_col = 0 })
+        vim.cmd("bwipeout! " .. diagnostic_bufnr)
+
+        vim.diagnostic.show(diagnostic_ns)
+        vim.diagnostic.hide(diagnostic_ns)
+      ]]
+    end)
   end)
 
   describe('enable() and disable()', function()
@@ -628,6 +638,15 @@ describe('vim.diagnostic', function()
         return diagnostic_count
       ]])
 
+    end)
+
+    it("doesn't error after bwipeout called on buffer", function()
+      exec_lua [[
+        vim.diagnostic.set(diagnostic_ns, diagnostic_bufnr, { lnum = 0, end_lnum = 0, col = 0, end_col = 0 })
+        vim.cmd("bwipeout! " .. diagnostic_bufnr)
+
+        vim.diagnostic.reset(diagnostic_ns)
+      ]]
     end)
   end)
 

commit d1d7486c6f2a0c7d04ddadbc08ac4b634682dea3

fix(diagnostics): don't allow 0 bufnr for metatable index
04bfd20bb introduced a subtle bug where using 0 as the buffer number in
the diagnostic cache resets the cache for the current buffer. This
happens because we were not checking to see if the _resolved_ buffer
number already existed in the cache; rather, when the __index metamethod
was called we assumed the index did not exist so we set its value to an
empty table. The fix for this is to check `rawget()` for the resolved
buffer number to see if the index already exists.

However, the reason this bug was introduced in the first place was
because we are simply being too clever by allowing a 0 buffer number as
the index which is automatically resolved to a real buffer number.
In the interest of minimizing metatable magic, remove this "feature" by
requiring the buffer number index to always be a valid buffer. This
ensures that the __index metamethod is only ever called for non-existing
buffers (which is what we wanted originally) as well as reduces some of
the cognitive overhead for understanding how the diagnostic cache works.
The tradeoff is that all public API functions must now resolve 0 buffer
numbers to the current buffer number.

runtime/lua/vim/diagnostic.lua
@@ -39,39 +39,22 @@ M.handlers = setmetatable({}, {
 -- Metatable that automatically creates an empty table when assigning to a missing key
 local bufnr_and_namespace_cacher_mt = {
   __index = function(t, bufnr)
-    if not bufnr or bufnr == 0 then
-      bufnr = vim.api.nvim_get_current_buf()
-    end
-
-    rawset(t, bufnr, {})
-
-    return rawget(t, bufnr)
-  end,
-
-  __newindex = function(t, bufnr, v)
-    if not bufnr or bufnr == 0 then
-      bufnr = vim.api.nvim_get_current_buf()
-    end
-
-    rawset(t, bufnr, v)
+    assert(bufnr > 0, "Invalid buffer number")
+    t[bufnr] = {}
+    return t[bufnr]
   end,
 }
 
 local diagnostic_cache = setmetatable({}, {
   __index = function(t, bufnr)
-    if not bufnr or bufnr == 0 then
-      bufnr = vim.api.nvim_get_current_buf()
-    end
-
+    assert(bufnr > 0, "Invalid buffer number")
     vim.api.nvim_buf_attach(bufnr, false, {
       on_detach = function()
         rawset(t, bufnr, nil) -- clear cache
       end
     })
-
-    rawset(t, bufnr, {})
-
-    return rawget(t, bufnr)
+    t[bufnr] = {}
+    return t[bufnr]
   end,
 })
 
@@ -658,6 +641,8 @@ function M.set(namespace, bufnr, diagnostics, opts)
     diagnostics = {diagnostics, 't'},
     opts = {opts, 't', true},
   }
+
+  bufnr = get_bufnr(bufnr)
 
   if vim.tbl_isempty(diagnostics) then
     diagnostic_cache[bufnr][namespace] = nil
@@ -1320,7 +1305,7 @@ function M.reset(namespace, bufnr)
     bufnr = {bufnr, 'n', true},
   }
 
-  local buffers = bufnr and {bufnr} or vim.tbl_keys(diagnostic_cache)
+  local buffers = bufnr and {get_bufnr(bufnr)} or vim.tbl_keys(diagnostic_cache)
   for _, iter_bufnr in ipairs(buffers) do
     local namespaces = namespace and {namespace} or vim.tbl_keys(diagnostic_cache[iter_bufnr])
     for _, iter_namespace in ipairs(namespaces) do

test/functional/lua/diagnostic_spec.lua
@@ -111,7 +111,7 @@ describe('vim.diagnostic', function()
   it('retrieves diagnostics from all buffers and namespaces', function()
     local result = exec_lua [[
       local other_bufnr = vim.api.nvim_create_buf(true, false)
-      local lines = {"1st line of text", "2nd line of text", "wow", "cool", "more", "lines"}
+      local lines = vim.api.nvim_buf_get_lines(diagnostic_bufnr, 0, -1, true)
       vim.api.nvim_buf_set_lines(other_bufnr, 0, 1, false, lines)
 
       vim.diagnostic.set(diagnostic_ns, diagnostic_bufnr, {