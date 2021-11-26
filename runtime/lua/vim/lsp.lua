3e3454aa77f27eb2ea1490364450876e4e47ea06
Rishikesh Vaishnav <rishhvaishnav@gmail.com>
2021-11-26 00:34:54 +0000

    fix(lsp): handle offset encoding
    Co-authored-by: black-desk <clx814727823@gmail.com>
    Co-authored-by: Mathias Fußenegger <mfussenegger@users.noreply.github.com>
    (cherry picked from commit 4687d853a5b5475aa572e6004cb5c1a1ed45ce94)

--- runtime/lua/vim/lsp.lua
+++ runtime/lua/vim/lsp.lua
@@ -1542,11 +1542,7 @@ local function adjust_start_col(lnum, line, items, encoding)
     end
   end
   if min_start_char then
-    if encoding == 'utf-8' then
-      return min_start_char
-    else
-      return vim.str_byteindex(line, min_start_char, encoding == 'utf-16')
-    end
+    return util._str_byteindex_enc(line, min_start_char, encoding)
   else
     return nil
   end
