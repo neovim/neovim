6e30461cea4ff9e51088875f82b0c8c78d939fbd
Gregory Anders <greg@gpanders.com>
2021-11-27 12:47:03 -0700

    test(diagnostic): diagnostics passed to set() should be an array

--- test/functional/lua/diagnostic_spec.lua
+++ test/functional/lua/diagnostic_spec.lua
@@ -357,7 +357,7 @@ describe('vim.diagnostic', function()
 
     it("doesn't error after bwipeout on buffer", function()
       exec_lua [[
-        vim.diagnostic.set(diagnostic_ns, diagnostic_bufnr, { lnum = 0, end_lnum = 0, col = 0, end_col = 0 })
+        vim.diagnostic.set(diagnostic_ns, diagnostic_bufnr, {{ lnum = 0, end_lnum = 0, col = 0, end_col = 0 }})
         vim.cmd("bwipeout! " .. diagnostic_bufnr)
 
         vim.diagnostic.show(diagnostic_ns)
@@ -653,7 +653,7 @@ describe('vim.diagnostic', function()
 
     it("doesn't error after bwipeout called on buffer", function()
       exec_lua [[
-        vim.diagnostic.set(diagnostic_ns, diagnostic_bufnr, { lnum = 0, end_lnum = 0, col = 0, end_col = 0 })
+        vim.diagnostic.set(diagnostic_ns, diagnostic_bufnr, {{ lnum = 0, end_lnum = 0, col = 0, end_col = 0 }})
         vim.cmd("bwipeout! " .. diagnostic_bufnr)
 
         vim.diagnostic.reset(diagnostic_ns)
