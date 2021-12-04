c6b6d5766fc7619b745476ec951a6179b0572d3b
zeertzjq <zeertzjq@outlook.com>
2021-12-04 21:27:13 +0800

    docs(lsp): remove zero-width space

--- runtime/lua/vim/lsp/protocol.lua
+++ runtime/lua/vim/lsp/protocol.lua
@@ -788,7 +788,7 @@ export interface DocumentFilter {
   --- `*` to match one or more characters in a path segment
   --- `?` to match on one character in a path segment
   --- `**` to match any number of path segments, including none
-  --- `{}` to group conditions (e.g. `**​/*.{ts,js}` matches all TypeScript and JavaScript files)
+  --- `{}` to group conditions (e.g. `**/*.{ts,js}` matches all TypeScript and JavaScript files)
   --- `[]` to declare a range of characters to match in a path segment (e.g., `example.[0-9]` to match on `example.0`, `example.1`, …)
   --- `[!...]` to negate a range of characters to match in a path segment (e.g., `example.[!0-9]` to match on `example.a`, `example.b`, but not `example.0`)
   pattern?: string;
@@ -814,7 +814,7 @@ export interface DocumentFilter {
   --- `*` to match one or more characters in a path segment
   --- `?` to match on one character in a path segment
   --- `**` to match any number of path segments, including none
-  --- `{}` to group conditions (e.g. `**​/*.{ts,js}` matches all TypeScript and JavaScript files)
+  --- `{}` to group conditions (e.g. `**/*.{ts,js}` matches all TypeScript and JavaScript files)
   --- `[]` to declare a range of characters to match in a path segment (e.g., `example.[0-9]` to match on `example.0`, `example.1`, …)
   --- `[!...]` to negate a range of characters to match in a path segment (e.g., `example.[!0-9]` to match on `example.a`, `example.b`, but not `example.0`)
   pattern?: string;
