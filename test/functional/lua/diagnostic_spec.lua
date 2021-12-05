02c7156ab2fa095d55e8633acf830f4a609b8b65
Matthew Toohey <contact@mtoohey.com>
2021-12-05 14:03:29 -0500

    fix: remove wait in diagnostic autocmd test
    (cherry picked from commit 7ad910d5ccd19c9ac9bf3720b96436c3ad47519d)

--- test/functional/lua/diagnostic_spec.lua
+++ test/functional/lua/diagnostic_spec.lua
@@ -1938,11 +1938,9 @@ describe('vim.diagnostic', function()
         vim.g.diagnostic_autocmd_triggered = 0
         vim.cmd('autocmd DiagnosticChanged * let g:diagnostic_autocmd_triggered = 1')
         vim.api.nvim_buf_set_name(diagnostic_bufnr, "test | test")
-        vim.wait(100, function ()
-          vim.diagnostic.set(diagnostic_ns, diagnostic_bufnr, {
-            make_error('Diagnostic', 0, 0, 0, 0)
-          })
-        end)
+        vim.diagnostic.set(diagnostic_ns, diagnostic_bufnr, {
+          make_error('Diagnostic', 0, 0, 0, 0)
+        })
         return vim.g.diagnostic_autocmd_triggered
       ]])
       end)
@@ -1952,9 +1950,7 @@ describe('vim.diagnostic', function()
         vim.g.diagnostic_autocmd_triggered = 0
         vim.cmd('autocmd DiagnosticChanged * let g:diagnostic_autocmd_triggered = 1')
         vim.api.nvim_buf_set_name(diagnostic_bufnr, "test | test")
-        vim.wait(100, function ()
-          vim.diagnostic.reset(diagnostic_ns, diagnostic_bufnr)
-        end)
+        vim.diagnostic.reset(diagnostic_ns, diagnostic_bufnr)
         return vim.g.diagnostic_autocmd_triggered
       ]])
       end)
