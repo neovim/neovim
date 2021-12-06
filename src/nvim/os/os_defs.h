8fdf1b265d982b80bfa8e6d98374514ece20a03e
Claes Nästén <pekdon@gmail.com>
2021-12-06 07:44:44 +0100

    fix: define NAME_MAX from _XOPEN_NAME_MAX
    On SunOS NAME_MAX is not defined, _XOPEN_NAME_MAX is so fall back to
    defining NAME_MAX from _XOPEN_NAME_MAX.

--- src/nvim/os/os_defs.h
+++ src/nvim/os/os_defs.h
@@ -13,6 +13,10 @@
 # include "nvim/os/unix_defs.h"
 #endif
 
+#if !defined(NAME_MAX) && defined(_XOPEN_NAME_MAX)
+#define NAME_MAX _XOPEN_NAME_MAX
+#endif
+
 #define BASENAMELEN (NAME_MAX - 5)
 
 // Use the system path length if it makes sense.
