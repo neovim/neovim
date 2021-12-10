d5e1c56c04a78f76f851e4002b2aa2f5d2b48d8b
Dundar Göc <gocdundar@gmail.com>
2021-12-10 10:51:05 +0100

    refactor: replace deprecated lua functions with their new versions
    Calling vim.lsp.buf.definition() sometimes gives a deprecation warning.
    This will likely solve that.
    
    (cherry picked from commit 27cb2f6c2e08bae881763353a0ab872abf2a7916)

--- runtime/lua/vim/lsp/handlers.lua
+++ runtime/lua/vim/lsp/handlers.lua
@@ -1,6 +1,7 @@
 local log = require 'vim.lsp.log'
 local protocol = require 'vim.lsp.protocol'
 local util = require 'vim.lsp.util'
+local diag = require 'vim.diagnostic'
 local vim = vim
 local api = vim.api
 
@@ -285,7 +286,7 @@ local function location_handler(_, result, ctx, _)
     util.jump_to_location(result[1])
 
     if #result > 1 then
-      util.set_qflist(util.locations_to_items(result))
+      diag.setqflist(util.locations_to_items(result))
       api.nvim_command("copen")
     end
   else
@@ -379,7 +380,7 @@ local make_call_hierarchy_handler = function(direction)
         })
       end
     end
-    util.set_qflist(items)
+    diag.setqflist(items)
     api.nvim_command("copen")
   end
 end
