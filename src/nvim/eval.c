28929a2e19fb5458f4df4b779c497ef362c896e7
VVKot <volodymyr.kot.ua@gmail.com>
2021-12-28 17:51:58 +0000

    vim-patch:8.2.0061: the execute stack can grow big and never shrinks
    Problem:    The execute stack can grow big and never shrinks.
    Solution:   Reduce the size in gargage collect.
    https://github.com/vim/vim/commit/3fbcc128cbd2311819cc5a7bb89e45669860f008

--- src/nvim/eval.c
+++ src/nvim/eval.c
@@ -5261,6 +5261,24 @@ bool garbage_collect(bool testing)
     garbage_collect_at_exit = false;
   }
 
+  // The execution stack can grow big, limit the size.
+  if (exestack.ga_maxlen - exestack.ga_len > 500) {
+    size_t new_len;
+    // Keep 150% of the current size, with a minimum of the growth size.
+    int n = exestack.ga_len / 2;
+    if (n < exestack.ga_growsize) {
+      n = exestack.ga_growsize;
+    }
+
+    // Don't make it bigger though.
+    if (exestack.ga_len + n < exestack.ga_maxlen) {
+      new_len = exestack.ga_itemsize * (exestack.ga_len + n);
+      char_u *pp = xrealloc(exestack.ga_data, new_len);
+      exestack.ga_maxlen = exestack.ga_len + n;
+      exestack.ga_data = pp;
+    }
+  }
+
   // We advance by two (COPYID_INC) because we add one for items referenced
   // through previous_funccal.
   const int copyID = get_copyID();
