9b6c754b062b2b04b5007d2d7c7b94b742e1ac42
Dundar Göc <gocdundar@gmail.com>
2022-01-01 18:23:30 +0100

    fixup! ci: remove outdated travis-specific code

--- ci/script.sh
+++ ci/script.sh
@@ -3,9 +3,6 @@
 set -e
 set -o pipefail
 
-# This will pass the environment variables down to a bash process which runs
-# as $USER, while retaining the environment variables defined and belonging
-# to secondary groups given above in usermod.
 ci/run_${CI_TARGET}.sh
 
 if [[ -s "${GCOV_ERROR_FILE}" ]]; then
