8dcb447aff11389ffd7237bd3aa5195359d6c8ee
Lewis Russell <lewis6991@gmail.com>
2022-01-08 22:50:46 +0000

    fix(api): validate user_command name
    The name argument of nvim_add_user_command must begin with an uppercase
    character. Check that is does.

--- src/nvim/api/private/helpers.c
+++ src/nvim/api/private/helpers.c
@@ -1387,6 +1387,11 @@ void add_user_command(String name, Object command, Dict(user_command) *opts, int
   LuaRef luaref = LUA_NOREF;
   LuaRef compl_luaref = LUA_NOREF;
 
+  if (mb_islower(name.data[0])) {
+    api_set_error(err, kErrorTypeValidation, "'name' must begin with an uppercase letter");
+    goto err;
+  }
+
   if (HAS_KEY(opts->range) && HAS_KEY(opts->count)) {
     api_set_error(err, kErrorTypeValidation, "'range' and 'count' are mutually exclusive");
     goto err;
