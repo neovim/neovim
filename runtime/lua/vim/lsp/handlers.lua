a871984323ddcc186b1e8a2ce1bd3e7863f4f00e
Michael Lingelbach <m.j.lbach@gmail.com>
2022-01-15 15:21:10 -0800

    fix(lsp): fetch offset_encoding from client in references

--- runtime/lua/vim/lsp/handlers.lua
+++ runtime/lua/vim/lsp/handlers.lua
@@ -166,18 +166,19 @@ M['textDocument/references'] = function(_, result, ctx, config)
   if not result or vim.tbl_isempty(result) then
     vim.notify('No references found')
   else
+    local client = vim.lsp.get_client_by_id(ctx.client_id)
     config = config or {}
     if config.loclist then
       vim.fn.setloclist(0, {}, ' ', {
         title = 'References';
-        items = util.locations_to_items(result, ctx.offset_encoding);
+        items = util.locations_to_items(result, client.offset_encoding);
         context = ctx;
       })
       api.nvim_command("lopen")
     else
       vim.fn.setqflist({}, ' ', {
         title = 'References';
-        items = util.locations_to_items(result, ctx.offset_encoding);
+        items = util.locations_to_items(result, client.offset_encoding);
         context = ctx;
       })
       api.nvim_command("botright copen")
