abc62f0538b0a7b5c8941a882f10e9296f890927
Shougo Matsushita <Shougo.Matsu@gmail.com>
2022-01-31 14:35:12 +0900

    fix: remove failed test

--- test/functional/provider/provider_spec.lua
+++ test/functional/provider/provider_spec.lua
@@ -10,14 +10,6 @@ describe('providers', function()
     clear('--cmd', 'let &rtp = "test/functional/fixtures,".&rtp')
   end)
 
-  it('with #Call(), missing g:loaded_xx_provider', function()
-    command('set loadplugins')
-    -- Using test-fixture with broken impl:
-    -- test/functional/fixtures/autoload/provider/python.vim
-    eq('Vim:provider: python: missing required variable g:loaded_python_provider',
-      pcall_err(eval, "has('python')"))
-  end)
-
   it('with g:loaded_xx_provider, missing #Call()', function()
     -- Using test-fixture with broken impl:
     -- test/functional/fixtures/autoload/provider/ruby.vim
