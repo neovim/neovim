3a72431d89db89749566a15ccf6366ca9f05bae8
Shougo Matsushita <Shougo.Matsu@gmail.com>
2022-01-31 21:23:18 +0900

    fix: functional tests

--- test/functional/provider/provider_spec.lua
+++ test/functional/provider/provider_spec.lua
@@ -10,6 +10,14 @@ describe('providers', function()
     clear('--cmd', 'let &rtp = "test/functional/fixtures,".&rtp')
   end)
 
+  it('with #Call(), missing g:loaded_xx_provider', function()
+    command('set loadplugins')
+    -- Using test-fixture with broken impl:
+    -- test/functional/fixtures/autoload/provider/python.vim
+    eq('Vim:provider: python3: missing required variable g:loaded_python3_provider',
+      pcall_err(eval, "has('python3')"))
+  end)
+
   it('with g:loaded_xx_provider, missing #Call()', function()
     -- Using test-fixture with broken impl:
     -- test/functional/fixtures/autoload/provider/ruby.vim
