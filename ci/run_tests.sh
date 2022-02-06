f30ce7d273cb4792f8f0354e4ae80c888a019e9e
Dundar Göc <gocdundar@gmail.com>
2022-02-06 12:41:52 +0100

    ci: run all suites with run_suite function

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -3,34 +3,25 @@
 set -e
 set -o pipefail
 
+run_tests(){
+  if test "$CLANG_SANITIZER" != "TSAN"; then
+    # Additional threads are only created when the builtin UI starts, which
+    # doesn't happen in the unit/functional tests
+    if test "${FUNCTIONALTEST}" != "functionaltest-lua"; then
+      run_test run_unittests unittests
+    fi
+    run_test run_functionaltests functionaltests
+  fi
+  run_test run_oldtests oldtests
+  run_test install_nvim install_nvim
+}
+
 CI_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "${CI_DIR}/common/build.sh"
 source "${CI_DIR}/common/test.sh"
 source "${CI_DIR}/common/suite.sh"
 
-enter_suite build
-
-check_core_dumps --delete quiet
-
-prepare_build
-build_nvim
-
-exit_suite --continue
-
-enter_suite tests
-
-if test "$CLANG_SANITIZER" != "TSAN" ; then
-  # Additional threads are only created when the builtin UI starts, which
-  # doesn't happen in the unit/functional tests
-  if test "${FUNCTIONALTEST}" != "functionaltest-lua"; then
-    run_test run_unittests unittests
-  fi
-  run_test run_functionaltests functionaltests
-fi
-run_test run_oldtests oldtests
-
-run_test install_nvim install_nvim
-
-exit_suite --continue
+run_suite 'build_nvim' 'build'
+run_suite 'run_tests' 'tests'
 
 end_tests
