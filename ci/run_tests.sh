2f3fb53dbda20ac1af8028855c8cb81cfb31fd5f
Dundar Göc <gocdundar@gmail.com>
2022-02-07 14:00:08 +0100

    ci: remove meta-suite "tests"

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -3,25 +3,22 @@
 set -e
 set -o pipefail
 
-run_tests(){
-  if test "$CLANG_SANITIZER" != "TSAN"; then
-    # Additional threads are only created when the builtin UI starts, which
-    # doesn't happen in the unit/functional tests
-    if test "${FUNCTIONALTEST}" != "functionaltest-lua"; then
-      run_suite run_unittests unittests
-    fi
-    run_suite run_functionaltests functionaltests
-  fi
-  run_suite run_oldtests oldtests
-  run_suite install_nvim install_nvim
-}
-
 CI_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "${CI_DIR}/common/build.sh"
 source "${CI_DIR}/common/test.sh"
 source "${CI_DIR}/common/suite.sh"
 
 run_suite 'build_nvim' 'build'
-run_suite 'run_tests' 'tests'
+
+if test "$CLANG_SANITIZER" != "TSAN"; then
+  # Additional threads are only created when the builtin UI starts, which
+  # doesn't happen in the unit/functional tests
+  if test "${FUNCTIONALTEST}" != "functionaltest-lua"; then
+    run_suite run_unittests unittests
+  fi
+  run_suite run_functionaltests functionaltests
+fi
+run_suite run_oldtests oldtests
+run_suite install_nvim install_nvim
 
 end_tests
