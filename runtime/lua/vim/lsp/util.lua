be6794a7107fdbffb5baac587dc8bba519ba7812
Michael Lingelbach <m.j.lbach@gmail.com>
2022-02-19 08:07:56 -0800

    fix: lsp and diagnostic highlight priority
    Closes https://github.com/neovim/neovim/issues/17456
    
    * treesitter uses the default highlight priority of 50
    * diagnostic highlights have a priority of 150
    * highlight references have a priority of 200
    
    This ensures proper ordering.

--- runtime/lua/vim/lsp/util.lua
+++ runtime/lua/vim/lsp/util.lua
@@ -1551,9 +1551,9 @@ do --[[ References ]]
                       document_highlight_kind[kind],
                       { start_line, start_idx },
                       { end_line, end_idx },
-                      nil,
+                      'v',
                       false,
-                      40)
+                      200)
     end
   end
 end
