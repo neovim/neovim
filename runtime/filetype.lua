fdea15723fab6a3ee96218f13669d9f2e0a6d6d7
Gregory Anders <8965202+gpanders@users.noreply.github.com>
2022-02-24 10:02:17 -0700

    feat(filetype): support scripts.vim with filetype.lua (#17517)
    When filetype.vim is disabled, create the autocommand that runs
    scripts.vim in filetype.lua.

--- runtime/filetype.lua
+++ runtime/filetype.lua
@@ -7,6 +7,7 @@ if vim.g.do_filetype_lua ~= 1 then
   return
 end
 
+-- TODO: Remove vim.cmd once Lua autocommands land
 vim.cmd [[
 augroup filetypedetect
 au BufRead,BufNewFile * call v:lua.vim.filetype.match(expand('<afile>'))
@@ -17,6 +18,12 @@ runtime! ftdetect/*.lua
 
 " Set a marker so that the ftdetect scripts are not sourced a second time by filetype.vim
 let g:did_load_ftdetect = 1
+
+" If filetype.vim is disabled, set up the autocmd to use scripts.vim
+if exists('did_load_filetypes')
+  au BufRead,BufNewFile * if !did_filetype() && expand('<amatch>') !~ g:ft_ignore_pat | runtime! scripts.vim | endif
+  au StdinReadPost * if !did_filetype() | runtime! scripts.vim | endif
+endif
 
 augroup END
 ]]
