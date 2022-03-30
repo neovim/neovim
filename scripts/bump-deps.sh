06516e5b8ff1c26128e8000f7270b82b97fb0ee9
Abraham Francis <abfr049@gmail.com>
2022-03-30 21:02:58 +0530

    add bash wrapper

---
+++ scripts/bump-deps.sh
@@ -0,0 +1,88 @@
+#!/usr/bin/env bash
+set -e
+set -u
+# Use privileged mode, which e.g. skips using CDPATH.
+set -p
+
+# Ensure that the user has a bash that supports -A
+if [[ "${BASH_VERSINFO[0]}" -lt 4 ]]; then
+  echo >&2 "error: script requires bash 4+ (you have ${BASH_VERSION})."
+  exit 1
+fi
+
+readonly NVIM_SOURCE_DIR="${NVIM_SOURCE_DIR:-$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)}"
+readonly VIM_SOURCE_DIR_DEFAULT="${NVIM_SOURCE_DIR}/.vim-src"
+readonly VIM_SOURCE_DIR="${VIM_SOURCE_DIR:-${VIM_SOURCE_DIR_DEFAULT}}"
+BASENAME="$(basename "${0}")"
+readonly BASENAME
+
+usage() {
+  echo "Bump Neovim dependencies"
+  echo
+  echo "Usage:  ${BASENAME} <dependency> [-h | --version=<tag> | --commit=<hash> | --HEAD]"
+  echo
+  echo "Options:"
+  echo "    -h                 Show this message and exit."
+  echo "    --version=<tag>    Bump to a specific release or tag."
+  echo "    --commit=<hash>    Bump to a specific commit."
+  echo "    --HEAD             Bump to a current HEAD."
+  echo
+  echo "    <dependency> is one of:"
+  echo "    \"LuaJIT\", \"libuv\", \"Luv\", \"tree-sitter\""
+}
+
+# Checks if a program is in the user's PATH, and is executable.
+check_executable() {
+  test -x "$(command -v "${1}")"
+}
+
+require_executable() {
+  if ! check_executable "${1}"; then
+    echo >&2 "${BASENAME}: '${1}' not found in PATH or not executable."
+    exit 1
+  fi
+}
+
+require_executable "nvim"
+
+if [ $# -eq 0 ]; then
+  usage
+  exit 1
+fi
+
+DEPENDENCY=$1
+shift
+
+PARSED_ARGS=$(getopt -a -n "$BASENAME" -o h --long version:,commit:,HEAD -- "$@")
+
+eval set -- "$PARSED_ARGS"
+while :; do
+  case "$1" in
+  -h)
+    usage
+    exit 0
+    ;;
+  --version)
+    VERSION=$2
+    nvim -es +"lua require('scripts.bump_deps').version('$DEPENDENCY', '$VERSION')"
+    exit 0
+    ;;
+  --commit)
+    COMMIT=$2
+    nvim -es +"lua require('scripts.bump_deps').commit('$DEPENDENCY', '$COMMIT')"
+    exit 0
+    ;;
+  --HEAD)
+    nvim -es +"lua require('scripts.bump_deps').head('$DEPENDENCY')"
+    exit 0
+    ;;
+  *)
+    break
+    ;;
+  esac
+done
+
+usage
+exit 1
+
+# vim: et sw=2
