b7bc931f631febeeffee528cc3b5667cfbf60a90
zeertzjq <zeertzjq@outlook.com>
2022-04-08 07:57:50 +0800

    vim-patch:8.2.4700: buffer remains active if WinClosed event throws an exception
    Problem:    Buffer remains active if a WinClosed event throws an exception.
    Solution:   Ignore aborting() when closing the buffer. (closes vim/vim#10097)
    https://github.com/vim/vim/commit/c947b9ae419114ebfef9725814ea41a466fcf879

--- src/nvim/window.c
+++ src/nvim/window.c
@@ -2563,7 +2563,7 @@ static void win_close_buffer(win_T *win, bool free_buf, bool abort_if_last)
     bufref_T bufref;
     set_bufref(&bufref, curbuf);
     win->w_closing = true;
-    close_buffer(win, win->w_buffer, free_buf ? DOBUF_UNLOAD : 0, abort_if_last, false);
+    close_buffer(win, win->w_buffer, free_buf ? DOBUF_UNLOAD : 0, abort_if_last, true);
     if (win_valid_any_tab(win)) {
       win->w_closing = false;
     }
