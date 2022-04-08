cc411abdf6c019a05c20347fc91921b4bd138a96
Patrice Peterson <patrice.peterson@mailbox.org>
2022-04-08 13:58:57 +0200

    fix(lsp): unify progress message handling
    The LSP progress handler would put non-progress messages (such as from
    clangd or pyls; not part of the LSP spec) directly into
    `client.messages`, while `vim.lsp.util.get_progress_messages()` would
    try to fetch them from `client.messages.messages` instead (and come up
    empty everytime). This would result in these messages never being
    cleaned up by `get_progress_messages()`.
    
    This commit fixes that by treating those messages like show-once
    progress messages (by setting `done=true` immediately).
    
    (cherry picked from commit 2087960c76bd2a44fa06643b44ceddf69edd947e)

--- runtime/lua/vim/lsp/handlers.lua
+++ runtime/lua/vim/lsp/handlers.lua
@@ -33,7 +33,7 @@ local function progress_handler(_, result, ctx, _)
   local val = result.value    -- unspecified yet
   local token = result.token  -- string or number
 
-
+  if type(val) ~= 'table' then val = { content=val } end
   if val.kind then
     if val.kind == 'begin' then
       client.messages.progress[token] = {
@@ -53,7 +53,8 @@ local function progress_handler(_, result, ctx, _)
       end
     end
   else
-    table.insert(client.messages, {content = val, show_once = true, shown = 0})
+    client.messages.progress[token] = val
+    client.messages.progress[token].done = true
   end
 
   vim.api.nvim_command("doautocmd <nomodeline> User LspProgressUpdate")
