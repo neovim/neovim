def5b508941704eb480c7c2492231bca3d360799
Christian Clason <c.clason@uni-graz.at>
2022-04-11 18:52:50 +0200

    vim-patch:8.2.4737: // in JavaScript string recognized as comment
    Problem:    // in JavaScript string recognized as comment.
    Solution:   Only check for linecomment if 'cindent' is set. (closes vim/vim#10151)
    https://github.com/vim/vim/commit/1655619717ff109ea8bf1002883636d5af345e48

--- src/nvim/change.c
+++ src/nvim/change.c
@@ -1186,7 +1186,7 @@ int open_line(int dir, int flags, int second_line_indent, bool *did_do_comment)
   end_comment_pending = NUL;
   if (flags & OPENLINE_DO_COM) {
     lead_len = get_leader_len(saved_line, &lead_flags, dir == BACKWARD, true);
-    if (lead_len == 0 && do_cindent && dir == FORWARD) {
+    if (lead_len == 0 && curbuf->b_p_cin && do_cindent && dir == FORWARD) {
       // Check for a line comment after code.
       comment_start = check_linecomment(saved_line);
       if (comment_start != MAXCOL) {
