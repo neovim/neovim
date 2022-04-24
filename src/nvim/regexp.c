3e41e7d9c10d9159276f0258a6317175c9772955
zeertzjq <zeertzjq@outlook.com>
2022-04-24 21:47:31 +0800

    vim-patch:8.2.4810: missing changes in one file
    Problem:    Missing changes in one file.
    Solution:   Also change the struct initializers.
    https://github.com/vim/vim/commit/56dba60216a1bf72c1de299316f4d4ef19e50ad5

--- src/nvim/regexp.c
+++ src/nvim/regexp.c
@@ -2222,7 +2222,6 @@ static regengine_T bt_regengine =
   bt_regfree,
   bt_regexec_nl,
   bt_regexec_multi,
-  (char_u *)""
 };
 
 static regengine_T nfa_regengine =
@@ -2231,7 +2230,6 @@ static regengine_T nfa_regengine =
   nfa_regfree,
   nfa_regexec_nl,
   nfa_regexec_multi,
-  (char_u *)""
 };
 
 // Which regexp engine to use? Needed for vim_regcomp().
