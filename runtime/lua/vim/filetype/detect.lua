51972d03b3a86dc232dd253dbe30d2e476305c11
smjonas <jonas.strittmatter@gmx.de>
2022-04-25 20:23:49 +0200

    Rename getline to get_lines and use it instead of nvim API functions

--- runtime/lua/vim/filetype/detect.lua
+++ runtime/lua/vim/filetype/detect.lua
@@ -6,7 +6,7 @@ local function count_lines(bufnr)
 end
 
 ---@private
-local function getline(bufnr, start_lnum, end_lnum)
+local function get_lines(bufnr, start_lnum, end_lnum)
   end_lnum = end_lnum or start_lnum
   local lines = vim.api.nvim_buf_get_lines(bufnr, start_lnum - 1, end_lnum, false)
   return table.concat(lines) or ""
@@ -17,7 +17,7 @@ end
 -- In that case, it's probably an OpenFOAM file
 function M.foam(_, bufnr)
   local foam_file = false
-  for _, line in ipairs(vim.api.nvim_buf_get_lines(bufnr, 0, 15, true)) do
+  for _, line in ipairs(get_lines(bufnr, 1, 15)) do
     if line:find("^FoamFile") then
       foam_file = true
     elseif foam_file and line:find("^%s*object") then
@@ -30,7 +30,7 @@ end
 -- Determine if a *.tf file is TF mud client or terraform
 function M.tf(_, bufnr)
   local number_of_lines = count_lines(bufnr)
-  for _, line in ipairs(vim.api.nvim_buf_get_lines(bufnr, 0, number_of_lines, true)) do
+  for _, line in ipairs(get_lines(bufnr, 1, number_of_lines)) do
     if not line:find("^[;/ ]") then
       vim.bo[bufnr].filetype = "terraform"
       return
