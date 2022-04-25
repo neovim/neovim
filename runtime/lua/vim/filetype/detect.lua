8f4e92282f4927270ff9c85187286c9f1d8253d1
smjonas <jonas.strittmatter@gmx.de>
2022-04-25 23:27:00 +0200

    Add functions for nroff, redif; use get_lines correctly

--- runtime/lua/vim/filetype/detect.lua
+++ runtime/lua/vim/filetype/detect.lua
@@ -12,12 +12,27 @@ local function get_lines(bufnr, start_lnum, end_lnum)
   return table.concat(lines) or ""
 end
 
+-- This function checks if one of the first five lines start with a dot. In
+-- that case it is probably an nroff file.
+function M.nroff(_, bufnr)
+  if get_lines(bufnr, 1, 5):find("^%.") then
+    vim.bo[bufnr].filetype = "nroff"
+  end
+end
+
+function M.redif(_, bufnr)
+  local lines = get_lines(bufnr, 1, 5)
+  if lines:find("^[tT][eE][mM][pP][lL][aA][tT][eE]%-[tT][yY][pP][eE]:") then
+    vim.bo[bufnr].filetype = "redif"
+  end
+end
+
 -- This function checks the first 15 lines for appearance of 'FoamFile'
 -- and then 'object' in a following line.
 -- In that case, it's probably an OpenFOAM file
 function M.foam(_, bufnr)
   local foam_file = false
-  for _, line in ipairs(get_lines(bufnr, 1, 15)) do
+  for _, line in ipairs(vim.api.nvim_buf_get_lines(bufnr, 0, 15, false)) do
     if line:find("^FoamFile") then
       foam_file = true
     elseif foam_file and line:find("^%s*object") then
@@ -29,14 +44,12 @@ end
 
 -- Determine if a *.tf file is TF mud client or terraform
 function M.tf(_, bufnr)
-  local number_of_lines = count_lines(bufnr)
-  for _, line in ipairs(get_lines(bufnr, 1, number_of_lines)) do
-    if not line:find("^[;/ ]") then
-      vim.bo[bufnr].filetype = "terraform"
-      return
-    end
+  local lines = get_lines(bufnr, 1, count_lines(bufnr))
+  if not lines:find("^[;/ ]") then
+    vim.bo[bufnr].filetype = "terraform"
+  else
+    vim.bo[bufnr].filetype = "tf"
   end
-  vim.bo[bufnr].filetype = "tf"
 end
 
 return M
