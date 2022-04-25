dd7f9727c270b9f0199ce966a004c15f9f25ed4f
smjonas <jonas.strittmatter@gmx.de>
2022-04-25 19:46:53 +0200

    refactor(runtime): convert dist#ft functions to lua

---
+++ runtime/lua/vim/filetype/detect.lua
@@ -0,0 +1,42 @@
+local M = {}
+
+---@private
+local function count_lines(bufnr)
+  return vim.api.nvim_buf_line_count(bufnr)
+end
+
+---@private
+local function getline(bufnr, start_lnum, end_lnum)
+  end_lnum = end_lnum or start_lnum
+  local lines = vim.api.nvim_buf_get_lines(bufnr, start_lnum - 1, end_lnum, false)
+  return table.concat(lines) or ""
+end
+
+-- This function checks the first 15 lines for appearance of 'FoamFile'
+-- and then 'object' in a following line.
+-- In that case, it's probably an OpenFOAM file
+function M.foam(_, bufnr)
+  local foam_file = false
+  for _, line in ipairs(vim.api.nvim_buf_get_lines(bufnr, 0, 15, true)) do
+    if line:find("^FoamFile") then
+      foam_file = true
+    elseif foam_file and line:find("^%s*object") then
+      vim.bo[bufnr].filetype = "foam"
+      return
+    end
+  end
+end
+
+-- Determine if a *.tf file is TF mud client or terraform
+function M.tf(_, bufnr)
+  local number_of_lines = count_lines(bufnr)
+  for _, line in ipairs(vim.api.nvim_buf_get_lines(bufnr, 0, number_of_lines, true)) do
+    if not line:find("^[;/ ]") then
+      vim.bo[bufnr].filetype = "terraform"
+      return
+    end
+  end
+  vim.bo[bufnr].filetype = "tf"
+end
+
+return M
