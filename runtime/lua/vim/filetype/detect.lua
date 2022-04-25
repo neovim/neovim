a67ce41949a2bde385e4bffda3d19c6eb0c62360
Dundar Goc <gocdundar@gmail.com>
2022-04-25 16:24:44 +0200

    throw in some convenience functions

--- runtime/lua/vim/filetype/detect.lua
+++ runtime/lua/vim/filetype/detect.lua
@@ -1,8 +1,18 @@
 local M = {}
 
+---@private
+local function count_lines(bufnr)
+  return vim.api.nvim_buf_line_count(bufnr)
+end
+
+---@private
+local function getline(bufnr, lnum)
+  return vim.api.nvim_buf_get_lines(bufnr, lnum-1, lnum, false)[1] or ""
+end
+
 -- Determine if a *.tf file is TF mud client or terraform
 function M.tf(_, bufnr)
-  local number_of_lines = vim.api.nvim_buf_line_count(bufnr)
+  local number_of_lines = count_lines(bufnr)
   for _,line in ipairs(vim.api.nvim_buf_get_lines(bufnr, 0, number_of_lines, true)) do
     if not line:find("^[;/ ]") then
       return "terraform"
