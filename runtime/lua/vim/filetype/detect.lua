23b61c5eb0795180029b6ab586ca95d7a3848db4
smjonas <jonas.strittmatter@gmx.de>
2022-04-26 11:33:53 +0200

    Add functions for btm, ent, progress_cweb, sc, scd and y

--- runtime/lua/vim/filetype/detect.lua
+++ runtime/lua/vim/filetype/detect.lua
@@ -19,15 +19,41 @@ local function iter_lines(bufnr, start_lnum, end_lnum)
 end
 
 function M.inp_check(_, bufnr)
-  if get_lines(bufnr, 1):match("^%*") then
+  if get_lines(bufnr, 1):find("^%*") then
     vim.bo[bufnr].filetype = "abaqus"
   else
     for _, line in iter_lines(bufnr, 1, 500) do
-      if line:match("header surface data") then
+      if line:find("header surface data") then
         vim.bo[bufnr].filetype = "trasys"
+        return
       end
     end
   end
+end
+
+function M.btm(_, bufnr)
+  if vim.g.dosbatch_syntax_for_btm ~= nil and vim.g.dosbatch_syntax_for_btm ~= 0 then
+    vim.bo[bufnr].filetype = "dosbatch"
+  else
+    vim.bo[bufnr].filetype = "btm"
+  end
+end
+
+-- This function checks for valid cl syntax in the first five lines.
+-- Look for either an opening comment, '#', or a block start, '{'.
+-- If not found, assume SGML.
+function M.ent(_, bufnr)
+  for _, line in iter_lines(bufnr, 1, 5) do
+    if line:find("^%s*[#{]") then
+      vim.bo[bufnr].filetype = "cl"
+      return
+    elseif not line:find("^%s*$") then
+      -- Not a blank line, not a comment, and not a block start,
+      -- so doesn't look like valid cl code.
+      break
+    end
+  end
+  vim.bo[bufnr].filetype = "dtd"
 end
 
 function M.ex_check(_, bufnr)
@@ -36,7 +62,7 @@ function M.ex_check(_, bufnr)
   else
     for _, line in iter_lines(bufnr, 1, 100) do
     -- TODO: regex
-      if line:match("") then
+      if line:find("") then
         vim.bo[bufnr].filetype = "euphoria3"
       else
         vim.bo[bufnr].filetype = "elixir"
@@ -63,11 +89,49 @@ function M.nroff(_, bufnr)
   end
 end
 
+function M.progress_cweb(_, bufnr)
+  if vim.g.filetype_w ~= nil then
+    vim.bo[bufnr].filetype = vim.g.filetype_w
+  else
+    if get_lines(bufnr, 1) == "&ANALYZE" or get_lines(bufnr, 3) == "&GLOBAL-DEFINE" then
+      vim.bo[bufnr].filetype = "progress"
+    else
+      vim.bo[bufnr].filetype = "cweb"
+    end
+  end
+end
+
 function M.sql(_, bufnr)
   if vim.g.filetype_sql ~= nil then
     vim.bo[bufnr].filetype = vim.g.filetype_sql
   else
     vim.bo[bufnr].filetype = "sql"
+  end
+end
+
+-- This function checks the first 25 lines of file extension "sc" to resolve
+-- detection between scala and SuperCollider
+function M.sc(_, bufnr)
+  -- TODO: it still needs to be discussed if it's ok to use vim.regex in some cases
+  local regex = vim.regex([[[A-Za-z0-9]*\s:\s[A-Za-z0-9]\|var\s<\|classvar\s<\|\^this.*\||\w*|\|+\s\w*\s{\|\*ar\s]])
+  for _, line in iter_lines(bufnr, 1, 25) do
+    if regex:match_str(line) then
+      vim.bo[bufnr].filetype = "supercollider"
+      return
+    end
+  end
+  vim.bo[bufnr].filetype = "scala"
+end
+
+-- This function checks the first line of file extension "scd" to resolve
+-- detection between scdoc and SuperCollider
+function M.scd(_, bufnr)
+  -- TODO: it still needs to be discussed if it's ok to use vim.regex in some cases
+  local regex = vim.regex([[\%^\S\+(\d[0-9A-Za-z]*)\%(\s\+\"[^"]*\"\%(\s\+\"[^"]*\"\)\=\)\=$]])
+  if regex:match_str(get_lines(bufnr, 1)) then
+    vim.bo[bufnr].filetype = "scdoc"
+  else
+    vim.bo[bufnr].filetype = "supercollider"
   end
 end
 
@@ -93,6 +157,21 @@ function M.xml(_, bufnr)
     end
   end
   vim.bo[bufnr].filetype = "xml"
+end
+
+function M.y(_, bufnr)
+  for _, line in iter_lines(bufnr, 1, 100) do
+    if line:find("^%s*%%") then
+      vim.bo[bufnr].filetype = "yacc"
+      return
+    end
+    -- TODO: in the Vim regex, \> is used to match the end of the word after "class",
+    -- can this be omitted?
+    if line:find("^%s*#") or line:find("^%class") and not line:find("^%s*#%s*include") then
+      vim.bo[bufnr].filetype = "racc"
+    end
+  end
+  vim.bo[bufnr].filetype = "yacc"
 end
 
 function M.redif(_, bufnr)
