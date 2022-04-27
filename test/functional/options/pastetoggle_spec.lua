3090648584024075c741fe1bee5d7a92d0543f23
zeertzjq <zeertzjq@outlook.com>
2022-04-27 18:24:42 +0800

    test: revert test and doc changes from #6724, add a test for #6716
    Multi-char 'pastetoggle' now works without breaking character-find.

--- test/functional/options/pastetoggle_spec.lua
+++ test/functional/options/pastetoggle_spec.lua
@@ -4,16 +4,13 @@ local clear = helpers.clear
 local feed = helpers.feed
 local command = helpers.command
 local eq = helpers.eq
-local eval = helpers.eval
-local sleep = helpers.sleep
 local expect = helpers.expect
+local eval = helpers.eval
+local insert = helpers.insert
+local sleep = helpers.sleep
 
 describe("'pastetoggle' option", function()
-  before_each(function()
-    clear()
-    command('set nopaste')
-  end)
-
+  before_each(clear)
   it("toggles 'paste'", function()
     command('set pastetoggle=a')
     eq(0, eval('&paste'))
@@ -22,19 +19,25 @@ describe("'pastetoggle' option", function()
     feed('j')
     eq(1, eval('&paste'))
   end)
-
-
-  it('does not wait for timeout', function()
-    command('set pastetoggle=abc')
-    command('set ttimeoutlen=9999999')
+  it("multiple key 'pastetoggle' is waited for", function()
     eq(0, eval('&paste'))
-    -- n.b. need <esc> to return from vgetorpeek()
-    feed('abc<esc>')
+    local pastetoggle = 'lllll'
+    command('set pastetoggle=' .. pastetoggle)
+    command('set timeoutlen=1 ttimeoutlen=10000')
+    feed(pastetoggle:sub(0, 2))
+    -- sleep() for long enough that vgetorpeek() is gotten into, but short
+    -- enough that ttimeoutlen is not reached.
+    sleep(200)
+    feed(pastetoggle:sub(3, -1))
+    -- Need another key so that the vgetorpeek() function returns.
+    feed('j')
     eq(1, eval('&paste'))
-    feed('ab')
-    sleep(10)
-    feed('c<esc>')
-    expect('bc')
-    eq(1, eval('&paste'))
+  end)
+  it('does not interfere with character-find', function()
+    insert('foo,bar')
+    feed('0')
+    command('set pastetoggle=,sp')
+    feed('dt,')
+    expect(',bar')
   end)
 end)
