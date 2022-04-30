2ecf96cfbee049e343b684c8e2d3017ad2427443
ii14 <ii14@users.noreply.github.com>
2022-04-30 22:53:06 +0200

    docs(api): update api attributes generation
    * change fast attribute to link to `api-fast`
    * add remote only and lua only

--- scripts/gen_vimdoc.py
+++ scripts/gen_vimdoc.py
@@ -271,8 +271,10 @@ param_exclude = (
 
 # Annotations are displayed as line items after API function descriptions.
 annotation_map = {
-    'FUNC_API_FAST': '{fast}',
+    'FUNC_API_FAST': '|api-fast|',
     'FUNC_API_CHECK_TEXTLOCK': 'not allowed when |textlock| is active',
+    'FUNC_API_REMOTE_ONLY': '|RPC| only',
+    'FUNC_API_LUA_ONLY': '|vim.api| only',
 }
 
 
