0019886a84c7dfdaf452c8a715f26eb87c697b1b
zeertzjq <zeertzjq@outlook.com>
2022-05-11 19:07:31 +0800

    vim-patch:8.2.4901: NULL pointer access when using invalid pattern
    Problem:    NULL pointer access when using invalid pattern.
    Solution:   Check for failed regexp program.
    https://github.com/vim/vim/commit/8e4b76da1d7e987d43ca960dfbc372d1c617466f

--- src/nvim/buffer.c
+++ src/nvim/buffer.c
@@ -2387,7 +2387,7 @@ static char_u *fname_match(regmatch_T *rmp, char_u *name, bool ignore_case)
     rmp->rm_ic = p_fic || ignore_case;
     if (vim_regexec(rmp, name, (colnr_T)0)) {
       match = name;
-    } else {
+    } else if (rmp->regprog != NULL) {
       // Replace $(HOME) with '~' and try matching again.
       p = home_replace_save(NULL, name);
       if (vim_regexec(rmp, p, (colnr_T)0)) {
