1f1a65a9e4c602cd80458b213c28eadbdb5b1de1
dundargoc <33953936+dundargoc@users.noreply.github.com>
2022-05-21 17:27:54 +0200

    ci: fix lint errors caused by missing includes (#18673)
    This will fix the `check-single-includes` check which started failing
    after https://github.com/neovim/neovim/pull/18663/.

--- src/nvim/os/fs.h
+++ src/nvim/os/fs.h
@@ -1,6 +1,9 @@
 #ifndef NVIM_OS_FS_H
 #define NVIM_OS_FS_H
 
+#include "nvim/os/fs_defs.h"  // for uv_*
+#include "nvim/types.h"  // for char_u
+
 #ifdef INCLUDE_GENERATED_DECLARATIONS
 # include "os/fs.h.generated.h"
 #endif
