8e46cbf30c38e6976e6d33122cb78534a92db0ac
zeertzjq <zeertzjq@outlook.com>
2022-05-25 10:16:23 +0800

    refactor(vim-patch.sh): avoid using non-POSIX behavior
    With `sed --posix` "@" cannot be used as the separator for substitute,
    which means this behavior is non-POSIX, so use "/" instead.

--- scripts/vim-patch.sh
+++ scripts/vim-patch.sh
@@ -244,11 +244,11 @@ preprocess_patch() {
     "$file" > "$file".tmp && mv "$file".tmp "$file"
 
   # Rename test_urls.vim to check_urls.vim
-  LC_ALL=C sed -e 's@\( [ab]\)/runtime/doc/test\(_urls\.vim\)@\1/scripts/check\2@g' \
+  LC_ALL=C sed -e 's/\( [ab]\)\/runtime\/doc\/test\(_urls\.vim\)/\1\/scripts\/check\2/g' \
     "$file" > "$file".tmp && mv "$file".tmp "$file"
 
   # Rename path to check_colors.vim
-  LC_ALL=C sed -e 's@\( [ab]/runtime\)/colors/\(tools/check_colors\.vim\)@\1/\2@g' \
+  LC_ALL=C sed -e 's/\( [ab]\/runtime\)\/colors\/\(tools\/check_colors\.vim\)/\1\/\2/g' \
     "$file" > "$file".tmp && mv "$file".tmp "$file"
 }
 
