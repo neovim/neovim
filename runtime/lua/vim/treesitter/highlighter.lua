8780076a78fcb16d73c3debb244cbf85b01e795b
kevinhwang91 <kevin.hwang@live.com>
2022-05-26 23:29:31 +0800

    fix(treesitter): new iter if folded

--- runtime/lua/vim/treesitter/highlighter.lua
+++ runtime/lua/vim/treesitter/highlighter.lua
@@ -279,7 +279,7 @@ local function on_line_impl(self, buf, line)
       return
     end
 
-    if state.iter == nil then
+    if state.iter == nil or state.next_row < line then
       state.iter = highlighter_query:query():iter_captures(root_node, self.bufnr, line, root_end_row + 1)
     end
 
