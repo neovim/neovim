2c43421f3d0a6d38e28efd1937296513d11f5387
Elijah Conners <business@elijahpepe.com>
2022-06-07 16:02:11 -0700

    fix: replace if statement with assertion
    Signed-off-by: Elijah Conners <business@elijahpepe.com>

--- src/nvim/tui/input.c
+++ src/nvim/tui/input.c
@@ -213,9 +213,7 @@ static void forward_simple_utf8(TermInput *input, TermKeyKey *key)
   while (*ptr) {
     if (*ptr == '<') {
       int n = (size_t)snprintf(buf + len, sizeof(buf) - len, "<lt>");
-      if (n < 0 || n >= sizeof(buf) - len) {
-        break;
-      }
+      assert(n < 0 || n >= sizeof(buf) - len);
       len += n;
     } else {
       buf[len++] = *ptr;
