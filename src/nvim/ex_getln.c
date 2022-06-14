f69ea34c4435323bf9629d45f2220555ffb82617
Shougo Matsushita <Shougo.Matsu@gmail.com>
2022-06-14 13:28:56 +0900

    fix: remove input check

--- src/nvim/ex_getln.c
+++ src/nvim/ex_getln.c
@@ -688,7 +688,7 @@ static void finish_incsearch_highlighting(int gotesc, incsearch_state_T *s, bool
 /// @param init_ccline  clear ccline first
 static uint8_t *command_line_enter(int firstc, long count, int indent, bool init_ccline)
 {
-  bool cmdheight0 = p_ch < 1 && !ui_has(kUIMessages) && vpeekc() == NUL;
+  bool cmdheight0 = p_ch < 1 && !ui_has(kUIMessages);
 
   if (cmdheight0) {
     // If cmdheight is 0, cmdheight must be set to 1 when we enter command line.
