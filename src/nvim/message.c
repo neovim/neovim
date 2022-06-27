0a412917ba257f82c6857db17865bb1a23a7e99e
Shougo Matsushita <Shougo.Matsu@gmail.com>
2022-06-27 14:57:56 +0900

    fix: indentation

--- src/nvim/message.c
+++ src/nvim/message.c
@@ -3085,9 +3085,9 @@ void msg_clr_eos_force(void)
 
   if (p_ch > 0) {
     grid_fill(&msg_grid_adj, msg_row, msg_row + 1, msg_startcol, msg_endcol,
-        ' ', ' ', HL_ATTR(HLF_MSG));
+              ' ', ' ', HL_ATTR(HLF_MSG));
     grid_fill(&msg_grid_adj, msg_row + 1, Rows, 0, Columns,
-        ' ', ' ', HL_ATTR(HLF_MSG));
+              ' ', ' ', HL_ATTR(HLF_MSG));
   }
 
   redraw_cmdline = true;  // overwritten the command line
