2a3282e54d2a9707309e9cbacdd2f7cd918fdecc
L3MON4D3 <Simljk@outlook.de>
2022-07-05 23:34:56 +0200

    format: don't default to ""!
    Changes meaning, no if_text -> capture, empty if_text -> "".

--- runtime/lua/vim/lsp/snippet/ast.lua
+++ runtime/lua/vim/lsp/snippet/ast.lua
@@ -148,16 +148,16 @@ Format.__tostring = empty
 ---@param capture_transform (string | table | nil)
 ---         - (string): {capture_transform} is a modifier.
 ---         - (table): {capture_transform} can contain either of
----                    - {if_text} (string, default "") Inserted for nonempty
----                                                     capture.
----                    - {else_text} (string, default "") Inserted for empty or
----                                                       undefined capture.
+---                    - {if_text} (string) Inserted for nonempty
+---                                         capture.
+---                    - {else_text} (string) Inserted for empty or
+---                                           undefined capture.
 ---@return table |lsp-parser-format|
 function Format.new(capture_index, capture_transform)
   local if_text, else_text, modifier
   if type(capture_transform) == 'table' then
-    if_text = capture_transform.if_text or ''
-    else_text = capture_transform.else_text or ''
+    if_text = capture_transform.if_text
+    else_text = capture_transform.else_text
   elseif type(capture_transform) == 'string' then
     modifier = capture_transform
   end
