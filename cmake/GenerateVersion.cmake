e1912bd721f7851f0270adb7a4f7ff3685542b20
kylo252 <59826753+kylo252@users.noreply.github.com>
2022-07-20 12:48:14 +0200

    fix(build): non-git ("tarball") build fails
    Problem:
    Build fails without git or .git/.
    ref #19289
    
    Solution:
    Fix the version generation logic.
    
    Test cases:
    If `git` is missing:
        -- Using NVIM_VERSION_MEDIUM: v0.8.0-dev
    If `.git/` is missing:
        -- Git tag extraction failed:
           fatal: not a git repository (or any of the parent directories): .git
        -- Using NVIM_VERSION_MEDIUM: v0.8.0-dev
    If `git describe` fails
        -- Git tag extraction failed:
           fatal: ...
        -- Using NVIM_VERSION_MEDIUM: v0.8.0-dev
    
    Co-authored-by: Justin M. Keyes <justinkz@gmail.com>

--- cmake/GenerateVersion.cmake
+++ cmake/GenerateVersion.cmake
@@ -1,16 +1,10 @@
-# Handle generating version from Git.
-set(use_git_version 0)
 if(NVIM_VERSION_MEDIUM)
   message(STATUS "USING NVIM_VERSION_MEDIUM: ${NVIM_VERSION_MEDIUM}")
   return()
 endif()
 
-find_program(GIT_EXECUTABLE git)
-if(NOT GIT_EXECUTABLE)
-  message(AUTHOR_WARNING "Skipping version-string generation (cannot find git)")
-  file(WRITE "${OUTPUT}" "")
-  return()
-endif()
+set(NVIM_VERSION_MEDIUM
+    "v${NVIM_VERSION_MAJOR}.${NVIM_VERSION_MINOR}.${NVIM_VERSION_PATCH}${NVIM_VERSION_PRERELEASE}")
 
 execute_process(
   COMMAND git describe --first-parent --tags --always --dirty
@@ -19,30 +13,25 @@ execute_process(
   RESULT_VARIABLE RES
 )
 
-if("${RES}" EQUAL 1)
-  if(EXISTS ${OUTPUT})
-    message(STATUS "Unable to extract version-string from git: keeping the last known version")
-  else()
-    # this will only be executed once since the file will get generated afterwards
-    message(AUTHOR_WARNING "Git tag extraction failed with: " "${ERR}")
-    file(WRITE "${OUTPUT}" "")
-  endif()
+if(NOT RES EQUAL 0)
+  message(STATUS "Git tag extraction failed:\n"  "   ${GIT_TAG}${ERR}" )
+  # This will only be executed once since the file will get generated afterwards.
+  message(STATUS "Using NVIM_VERSION_MEDIUM: ${NVIM_VERSION_MEDIUM}")
+  file(WRITE "${OUTPUT}" "${NVIM_VERSION_STRING}")
   return()
 endif()
 
 string(STRIP "${GIT_TAG}" GIT_TAG)
 string(REGEX REPLACE "^v[0-9]+.[0-9]+.[0-9]+-" "" NVIM_VERSION_GIT "${GIT_TAG}")
-set(NVIM_VERSION_MEDIUM
-    "v${NVIM_VERSION_MAJOR}.${NVIM_VERSION_MINOR}.${NVIM_VERSION_PATCH}-dev-${NVIM_VERSION_GIT}"
-)
+set(NVIM_VERSION_MEDIUM "${NVIM_VERSION_MEDIUM}-${NVIM_VERSION_GIT}")
 set(NVIM_VERSION_STRING "#define NVIM_VERSION_MEDIUM \"${NVIM_VERSION_MEDIUM}\"\n")
-string(SHA1 CURRENT_VERSION_HASH "${NVIM_VERSION_STRING}")
 
+string(SHA1 CURRENT_VERSION_HASH "${NVIM_VERSION_STRING}")
 if(EXISTS ${OUTPUT})
   file(SHA1 "${OUTPUT}" NVIM_VERSION_HASH)
 endif()
 
 if(NOT "${NVIM_VERSION_HASH}" STREQUAL "${CURRENT_VERSION_HASH}")
-  message(STATUS "Updating NVIM_VERSION_MEDIUM: ${NVIM_VERSION_MEDIUM}")
+  message(STATUS "Using NVIM_VERSION_MEDIUM: ${NVIM_VERSION_MEDIUM}")
   file(WRITE "${OUTPUT}" "${NVIM_VERSION_STRING}")
 endif()
