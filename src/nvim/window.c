8e67af1b201adfca9f6c3589b4f7c59b323e9459
zeertzjq <zeertzjq@outlook.com>
2022-07-23 17:45:45 +0800

    vim-patch:9.0.0061: ml_get error with nested autocommand
    Problem:    ml_get error with nested autocommand.
    Solution:   Also check line numbers for a nested autocommand. (closes vim/vim#10761)
    https://github.com/vim/vim/commit/5fa9f23a63651a8abdb074b4fc2ec9b1adc6b089

--- src/nvim/window.c
+++ src/nvim/window.c
@@ -6855,17 +6855,16 @@ bool only_one_window(void) FUNC_ATTR_PURE FUNC_ATTR_WARN_UNUSED_RESULT
   return count <= 1;
 }
 
-/// Correct the cursor line number in other windows.  Used after changing the
-/// current buffer, and before applying autocommands.
-///
-/// @param do_curwin  when true, also check current window.
-void check_lnums(bool do_curwin)
+/// Implementation of check_lnums() and check_lnums_nested().
+static void check_lnums_both(bool do_curwin, bool nested)
 {
   FOR_ALL_TAB_WINDOWS(tp, wp) {
     if ((do_curwin || wp != curwin) && wp->w_buffer == curbuf) {
-      // save the original cursor position and topline
-      wp->w_save_cursor.w_cursor_save = wp->w_cursor;
-      wp->w_save_cursor.w_topline_save = wp->w_topline;
+      if (!nested) {
+        // save the original cursor position and topline
+        wp->w_save_cursor.w_cursor_save = wp->w_cursor;
+        wp->w_save_cursor.w_topline_save = wp->w_topline;
+      }
 
       if (wp->w_cursor.lnum > curbuf->b_ml.ml_line_count) {
         wp->w_cursor.lnum = curbuf->b_ml.ml_line_count;
@@ -6874,11 +6873,26 @@ void check_lnums(bool do_curwin)
         wp->w_topline = curbuf->b_ml.ml_line_count;
       }
 
-      // save the corrected cursor position and topline
+      // save the (corrected) cursor position and topline
       wp->w_save_cursor.w_cursor_corr = wp->w_cursor;
       wp->w_save_cursor.w_topline_corr = wp->w_topline;
     }
   }
+}
+
+/// Correct the cursor line number in other windows.  Used after changing the
+/// current buffer, and before applying autocommands.
+///
+/// @param do_curwin  when true, also check current window.
+void check_lnums(bool do_curwin)
+{
+  check_lnums_both(do_curwin, false);
+}
+
+/// Like check_lnums() but for when check_lnums() was already called.
+void check_lnums_nested(bool do_curwin)
+{
+  check_lnums_both(do_curwin, true);
 }
 
 /// Reset cursor and topline to its stored values from check_lnums().
