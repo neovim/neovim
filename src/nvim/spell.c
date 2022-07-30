3f327d0a1c44852bc17169eaa88c6aa7845e8819
zeertzjq <zeertzjq@outlook.com>
2022-07-30 18:42:48 +0800

    vim-patch:9.0.0109: writing over the end of a buffer on stack
    Problem:    Writing over the end of a buffer on stack when making list of
                spell suggestions.
    Solution:   Make sure suggested word is not too long. (closes vim/vim#10812)
    https://github.com/vim/vim/commit/1eead4cf1daf87ee41aeb4de3b3e38708417f9d5

--- src/nvim/spell.c
+++ src/nvim/spell.c
@@ -2965,12 +2965,11 @@ void spell_suggest(int count)
       stp = &SUG(sug.su_ga, i);
 
       // The suggested word may replace only part of the bad word, add
-      // the not replaced part.
+      // the not replaced part.  But only when it's not getting too long.
       STRLCPY(wcopy, stp->st_word, MAXWLEN + 1);
-      if (sug.su_badlen > stp->st_orglen) {
-        STRLCPY(wcopy + stp->st_wordlen,
-                sug.su_badptr + stp->st_orglen,
-                sug.su_badlen - stp->st_orglen + 1);
+      int el = sug.su_badlen - stp->st_orglen;
+      if (el > 0 && stp->st_wordlen + el <= MAXWLEN) {
+        STRLCPY(wcopy + stp->st_wordlen, sug.su_badptr + stp->st_orglen, el + 1);
       }
       vim_snprintf((char *)IObuff, IOSIZE, "%2d", i + 1);
       if (cmdmsg_rl) {
