402e0cabd0af3f632c0751ec5231e73100f95836
Raphael <glephunter@gmail.com>
2022-07-31 09:55:14 +0800

    fix: add test for clean diagnsotic cache

--- test/functional/lua/diagnostic_spec.lua
+++ test/functional/lua/diagnostic_spec.lua
@@ -128,6 +128,26 @@ describe('vim.diagnostic', function()
     eq('Diagnostic #1', result[1].message)
   end)
 
+  it('clean the diagnostic cache when buf delete', function()
+    local result = exec_lua [[
+      local other_bufnr = vim.api.nvim_create_buf(true,false)
+      local lines = vim.api.nvim_buf_get_lines(diagnostic_bufnr, 0, -1, true)
+      vim.api.nvim_buf_set_lines(other_bufnr, 0, 1, false, lines)
+
+      vim.diagnostic.set(diagnostic_ns, diagnostic_bufnr, {
+        make_error('Diagnostic #1', 1, 1, 1, 1),
+        make_error('Diagnostic #2', 2, 1, 2, 1),
+      })
+      vim.diagnostic.set(diagnostic_ns, other_bufnr, {
+        make_error('Diagnostic #3', 3, 1, 3, 1),
+      })
+      vim.api.nvim_set_current_buf(other_bufnr)
+      vim.cmd('bwipeout!')
+      return vim.diagnostic.get()
+    ]]
+    eq(2, #result)
+  end)
+
   it('resolves buffer number 0 to the current buffer', function()
     eq(2, exec_lua [[
       vim.api.nvim_set_current_buf(diagnostic_bufnr)
