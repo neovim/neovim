c77c7fff10421a2400750c05d57e97af9a9ca5f4
zeertzjq <zeertzjq@outlook.com>
2022-08-12 08:36:50 +0800

    test: make Ex mode exit test test what it wants to test

--- test/functional/core/exit_spec.lua
+++ test/functional/core/exit_spec.lua
@@ -3,6 +3,7 @@ local helpers = require('test.functional.helpers')(after_each)
 local assert_alive = helpers.assert_alive
 local command = helpers.command
 local feed_command = helpers.feed_command
+local feed = helpers.feed
 local eval = helpers.eval
 local eq = helpers.eq
 local run = helpers.run
@@ -36,11 +37,12 @@ describe('v:exiting', function()
     end
     run(on_request, nil, on_setup)
   end)
-  it('is 0 on exit from ex-mode involving try-catch', function()
+  it('is 0 on exit from Ex mode involving try-catch vim-patch:8.0.0184', function()
     local function on_setup()
       command('autocmd VimLeavePre * call rpcrequest('..cid..', "")')
       command('autocmd VimLeave    * call rpcrequest('..cid..', "")')
-      feed_command('call feedkey("Q")','try', 'call NoFunction()', 'catch', 'echo "bye"', 'endtry', 'quit')
+      feed('gQ')
+      feed_command('try', 'call NoFunction()', 'catch', 'echo "bye"', 'endtry', 'quit')
     end
     local function on_request()
       eq(0, eval('v:exiting'))
