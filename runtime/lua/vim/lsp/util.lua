3d0463185302fe599a778c32737f88c68a8fbdbe
shaunsingh <shaunsingh0207@gmail.com>
2022-08-18 12:07:29 -0400

    fix(lsp): create missing directory before creating file

--- runtime/lua/vim/lsp/util.lua
+++ runtime/lua/vim/lsp/util.lua
@@ -772,7 +772,14 @@ local function create_file(change)
   -- from spec: Overwrite wins over `ignoreIfExists`
   local fname = vim.uri_to_fname(change.uri)
   if not opts.ignoreIfExists or opts.overwrite then
-    local file = io.open(fname, 'w')
+    io.create_missing_dir_open = function(filename, mode)
+        local dir = vim.fs.dirname(filename)
+        if vim.fn.isdirectory(dir) == 0 then
+            vim.fn.mkdir(dir, "p")
+        end
+        return io.open(filename, mode).
+    end
+    local file = io.create_missing_dir_open(fname, 'w')
     file:close()
   end
   vim.fn.bufadd(fname)
