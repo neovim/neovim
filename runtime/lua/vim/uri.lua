db1841b6af1d79f6e93f65a1ac586ecb0cf20b1f
Raphael <glephunter@gmail.com>
2022-08-23 21:34:35 +0800

    fix: remove unused function

--- runtime/lua/vim/uri.lua
+++ runtime/lua/vim/uri.lua
@@ -54,13 +54,6 @@ do
   end
 end
 
----@deprecated
----@private
-local function is_windows_file_uri(uri)
-  vim.deprecate('is_windows_file_uri', nil, '0.9')
-  return uri:match('^file:/+[a-zA-Z]:') ~= nil
-end
-
 --- Get a URI from a file path.
 ---@param path string Path to file
 ---@return string URI
