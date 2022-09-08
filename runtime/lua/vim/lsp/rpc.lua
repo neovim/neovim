a943cf7169b121cc985104358d362713176fda6b
Mathias Fussenegger <f.mathias@zignar.net>
2022-09-08 17:19:43 +0200

    docs(lsp): update rpc.start stdio limitations

--- runtime/lua/vim/lsp/rpc.lua
+++ runtime/lua/vim/lsp/rpc.lua
@@ -635,7 +635,8 @@ local function connect(host, port)
 end
 
 --- Starts an LSP server process and create an LSP RPC client object to
---- interact with it. Communication with the server is currently limited to stdio.
+--- interact with it. Communication with the spawned process happens via stdio. For
+--- communication via TCP, spawn a process manually and use |vim.lsp.rpc.connect|
 ---
 ---@param cmd (string) Command to start the LSP server.
 ---@param cmd_args (table) List of additional string arguments to pass to {cmd}.
