e2c4717203012a3289f0177f3847829b14c31ec1
Raphael <glephunter@gmail.com>
2022-10-21 18:53:29 +0800

    fix: improve check

--- runtime/filetype.lua
+++ runtime/filetype.lua
@@ -10,7 +10,15 @@ vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile', 'StdinReadPost' }, {
   group = 'filetypedetect',
   callback = function(args)
     local ft, on_detect = vim.filetype.match({ filename = args.match, buf = args.buf })
-    if #ft == 0 then
+    local has_ft = false
+
+    if type(ft) == "nil" then
+      has_ft = not ft
+    elseif type(ft) == "string" then
+      has_ft = #ft == 0
+    end
+
+    if has_ft then
       -- Generic configuration file used as fallback
       ft = require('vim.filetype.detect').conf(args.file, args.buf)
       if ft then
