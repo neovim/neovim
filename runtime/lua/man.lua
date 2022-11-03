4282c8b41d158536ab5012dbcf9d840e03db7902
kevinhwang91 <kevin.hwang@live.com>
2022-11-03 08:45:36 +0800

    fix(man.lua): set modifiable before writing page

--- runtime/lua/man.lua
+++ runtime/lua/man.lua
@@ -457,7 +457,7 @@ local function get_page(path, silent)
 end
 
 local function put_page(page)
-  vim.bo.modified = true
+  vim.bo.modifiable = true
   vim.bo.readonly = false
   vim.bo.swapfile = false
 
