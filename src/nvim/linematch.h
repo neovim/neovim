04fbb1de4488852c3ba332898b17180500f8984e
Jonathon <32371757+jwhite510@users.noreply.github.com>
2022-11-04 05:07:22 -0400

    Enable new diff option linematch (#14537)
    Co-authored-by: Lewis Russell <me@lewisr.dev>

---
+++ src/nvim/linematch.h
@@ -0,0 +1,10 @@
+#ifndef NVIM_LINEMATCH_H
+#define NVIM_LINEMATCH_H
+
+#include <stddef.h>
+
+#ifdef INCLUDE_GENERATED_DECLARATIONS
+# include "linematch.h.generated.h"
+#endif
+
+#endif  // NVIM_LINEMATCH_H
