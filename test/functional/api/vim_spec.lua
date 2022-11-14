5c5187c6f809c50e2cfd0ba04961ae8e0d2dabd0
dundargoc <33953936+dundargoc@users.noreply.github.com>
2022-11-14 09:28:30 +0100

    test: remove skip for 32-bit MSVC (#21030)
    We don't support 32-bit windows anymore so it's not needed.

--- test/functional/api/vim_spec.lua
+++ test/functional/api/vim_spec.lua
@@ -2324,12 +2324,6 @@ describe('API', function()
       meths.set_option('isident', '')
     end)
 
-    local it_maybe_pending = it
-    if helpers.isCI() and os.getenv('CONFIGURATION') == 'MSVC_32' then
-      -- For "works with &opt" (flaky on MSVC_32), but not easy to skip alone.  #10241
-      it_maybe_pending = pending
-    end
-
     local function simplify_east_api_node(line, east_api_node)
       if east_api_node == NIL then
         return nil
@@ -2526,7 +2520,7 @@ describe('API', function()
       end
     end
     require('test.unit.viml.expressions.parser_tests')(
-        it_maybe_pending, _check_parsing, hl, fmtn)
+        it, _check_parsing, hl, fmtn)
   end)
 
   describe('nvim_list_uis', function()
