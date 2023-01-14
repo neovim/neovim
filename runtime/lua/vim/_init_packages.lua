e04acb1459760af829ed08276cb51ebd0ea6a95c
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-01-14 10:34:55 -0500

    feat(lua): `vim.autocmd` and `vim.augroup`
    test(lua): `vim.autocmd` and `vim.augroup`
    
    test: add describe blocks
    
    Organize tests with `describe` blocks.
    
    style: indentation

--- runtime/lua/vim/_init_packages.lua
+++ runtime/lua/vim/_init_packages.lua
@@ -59,6 +59,9 @@ setmetatable(vim, {
     elseif key == 'inspect_pos' or key == 'show_pos' then
       require('vim._inspector')
       return t[key]
+    elseif key == 'autocmd' or key == 'augroup' then
+      require('vim._auto')
+      return t[key]
     elseif vim.startswith(key, 'uri_') then
       local val = require('vim.uri')[key]
       if val ~= nil then
