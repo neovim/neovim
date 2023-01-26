bde5a7c0e454e9fc556cd6adeb11ad794a10c08c
Justin M. Keyes <justinkz@gmail.com>
2023-01-26 15:50:18 +0100

    refactor(api): VALIDATE macros
    Problem:
    - API validation involves too much boilerplate.
    - API validation errors are not consistently worded.
    
    Solution:
    Introduce some macros. Currently these are clumsy, but they at least
    help with consistency and avoid some nesting (at the cost of putting
    control-flow in a non-obvious place).

---
+++ src/nvim/api/private/validate.h
@@ -0,0 +1,44 @@
+#ifndef NVIM_API_PRIVATE_VALIDATE_H
+#define NVIM_API_PRIVATE_VALIDATE_H
+
+#include "nvim/api/private/defs.h"
+#include "nvim/api/private/helpers.h"
+
+#define VALIDATE_INT(cond, name, n_, code) \
+  do { \
+    if (cond) { \
+      api_set_error(err, kErrorTypeValidation, "Invalid " name ": %" PRId64, n_); \
+      code; \
+    } \
+  } while (0)
+
+#define VALIDATE_S(cond, name, str_, code) \
+  do { \
+    if (cond) { \
+      if (strequal(str_, "")) { \
+        api_set_error(err, kErrorTypeValidation, "Invalid " name); \
+      } else { \
+        api_set_error(err, kErrorTypeValidation, "Invalid " name ": '%s'", str_); \
+      } \
+      code; \
+    } \
+  } while (0)
+
+#define VALIDATE_T(name, expected_t, actual_t, code) \
+  do { \
+    if (expected_t != actual_t) { \
+      api_set_error(err, kErrorTypeValidation, "Invalid '" name "' type: expected %s, got %s", \
+                    api_typename(expected_t), api_typename(actual_t)); \
+      code; \
+    } \
+  } while (0)
+
+#define VALIDATE(cond, msg_, code) \
+  do { \
+    if (cond) { \
+      api_set_error(err, kErrorTypeValidation, "%s", msg_); \
+      code; \
+    } \
+  } while (0)
+
+#endif  // NVIM_API_PRIVATE_VALIDATE_H
