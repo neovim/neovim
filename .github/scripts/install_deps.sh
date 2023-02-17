8783dffe357627f05b011532516fe24d50cd73f6
dundargoc <gocdundar@gmail.com>
2023-02-17 19:05:23 +0100

    ci: remove former dependencies that are no longer needed
    libtool, autoconf, automake and perl are no longer dependencies of
    neovim and doesn't need to be installed in CI anymore. The dependencies
    and the commit that removed them as dependencies are the following:
    
    libtool: b05100a9eaad5980ea7652137bc4a1c2d15d752f
    perl: 20a932cb72cf077d54e3498ef93341ffe3d4cdbb
    autoconf+automake: e23c5fda0a3fe385af615372c474d4dad3b74464

--- .github/scripts/install_deps.sh
+++ .github/scripts/install_deps.sh
@@ -3,8 +3,8 @@
 os=$(uname -s)
 if [[ $os == Linux ]]; then
   sudo apt-get update
-  sudo apt-get install -y autoconf automake build-essential cmake curl gettext libtool-bin locales-all ninja-build pkg-config unzip "$@"
+  sudo apt-get install -y build-essential cmake curl gettext locales-all ninja-build pkg-config unzip "$@"
 elif [[ $os == Darwin ]]; then
   brew update --quiet
-  brew install automake ninja "$@"
+  brew install ninja "$@"
 fi
