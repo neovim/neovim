4ca34b809cbceae875c750ecaa60083786b75e53
kraftwerk28 <kefirchik3@gmail.com>
2023-02-26 20:21:51 +0200

    feat(man.lua): redraw manpage when window was resized

--- runtime/plugin/man.lua
+++ runtime/plugin/man.lua
@@ -32,3 +32,41 @@ vim.api.nvim_create_autocmd('BufReadCmd', {
     require('man').read_page(vim.fn.matchstr(params.match, 'man://\\zs.*'))
   end,
 })
+
+if (vim.g.man_redraw_resized or 1) == 1 then
+  local resize_timer, resized_winids
+
+  local function redraw_manpages()
+    local winid = vim.api.nvim_get_current_win()
+    if
+      resized_winids == nil
+      or vim.b.pager
+      or not vim.api.nvim_win_is_valid(winid)
+      or not vim.tbl_contains(resized_winids, winid)
+    then
+      return
+    end
+    local ref = vim.fn.matchstr(vim.fn.bufname(), 'man://\\zs.*')
+    if ref == '' then
+      return
+    end
+    local winview = vim.fn.winsaveview()
+    pcall(require('man').read_page, ref)
+    vim.fn.winrestview(winview)
+    resize_timer, resized_winids = nil, nil
+  end
+
+  vim.api.nvim_create_autocmd('WinResized', {
+    group = augroup,
+    callback = function()
+      if vim.bo.filetype ~= 'man' then
+        return
+      end
+      if resize_timer ~= nil then
+        resize_timer:stop()
+      end
+      resized_winids = vim.v.event.windows
+      resize_timer = vim.defer_fn(redraw_manpages, vim.g.man_redraw_debounce_ms or 1000)
+    end,
+  })
+end
