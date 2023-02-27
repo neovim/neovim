2898ffaebce153eeacd869880d6243431eb59911
Mike <Mike325@users.noreply.github.com>
2023-02-27 15:36:55 +0100

    docs(fs.lua): update find description
    Co-authored-by: Gregory Anders <8965202+gpanders@users.noreply.github.com>

--- runtime/lua/vim/fs.lua
+++ runtime/lua/vim/fs.lua
@@ -168,7 +168,7 @@ end
 ---@param names (string|table|fun(name: string, path: string): boolean) Names of the files
 ---             and directories to find.
 ---             Must be base names, paths and globs are not supported when {names} is a string or a table.
----             If {names} is a function, it will be called for each file and directory
+---             If {names} is a function, it will be called once for each file or directory
 ---             within the traversed directory with the base name of the
 ---             file and directory and the path of the directory being traversed.
 ---             The function should return `true` if the given file or directory is considered a match.
