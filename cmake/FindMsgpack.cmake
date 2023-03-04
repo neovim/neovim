b1d5857f799d6f8e9e979ac8926cf6352fc56579
Christian Clason <c.clason@uni-graz.at>
2023-03-04 15:20:50 +0100

    build(deps): bump msgpack-c to v6.0.0

--- cmake/FindMsgpack.cmake
+++ cmake/FindMsgpack.cmake
@@ -10,14 +10,7 @@ else()
   set(MSGPACK_VERSION_STRING)
 endif()
 
-if(MSVC)
-  # The import library for the msgpack DLL has a different name
-  list(APPEND MSGPACK_NAMES msgpackc_import)
-else()
-  list(APPEND MSGPACK_NAMES msgpackc msgpack)
-endif()
-
-find_library(MSGPACK_LIBRARY NAMES ${MSGPACK_NAMES}
+find_library(MSGPACK_LIBRARY NAMES msgpackc msgpack msgpackc_import msgpack-c
   NAMES_PER_DIR)
 
 mark_as_advanced(MSGPACK_INCLUDE_DIR MSGPACK_LIBRARY)
