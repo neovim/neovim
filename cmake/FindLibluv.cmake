da0c66bcddbe4e6ebc72357c9f6c5de75e176744
dundargoc <gocdundar@gmail.com>
2023-03-05 17:06:13 +0100

    build: remove workaround for incorrectly packaged libluv
    This removes a workaround for incorrectly packaged libluv in
    90e44ecf1144cb32195da00e24d23afb111ea680 as it should not be needed
    anymore.

--- cmake/FindLibluv.cmake
+++ cmake/FindLibluv.cmake
@@ -1,14 +1,5 @@
 find_path(LIBLUV_INCLUDE_DIR luv/luv.h)
-
-# Explicitly look for luv.so. #10407
-list(APPEND LIBLUV_NAMES luv_a luv libluv_a luv${CMAKE_SHARED_LIBRARY_SUFFIX})
-
-find_library(LIBLUV_LIBRARY NAMES ${LIBLUV_NAMES})
-
-set(LIBLUV_LIBRARIES ${LIBLUV_LIBRARY})
-set(LIBLUV_INCLUDE_DIRS ${LIBLUV_INCLUDE_DIR})
-
+find_library(LIBLUV_LIBRARY NAMES luv_a luv libluv_a luv.so)
 find_package_handle_standard_args(Libluv DEFAULT_MSG
   LIBLUV_LIBRARY LIBLUV_INCLUDE_DIR)
-
 mark_as_advanced(LIBLUV_INCLUDE_DIR LIBLUV_LIBRARY)
