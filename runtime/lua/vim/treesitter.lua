e2322bbd3efa20cb1d79b396d95c95e9981a6160
zeertzjq <zeertzjq@outlook.com>
2023-03-06 23:25:10 +0800

    Revert "refactor(treesitter): simplify some range functions"
    This reverts commit f9a46391ab5961fe6c6b7d1efdc96befdd495c11.

--- runtime/lua/vim/treesitter.lua
+++ runtime/lua/vim/treesitter.lua
@@ -2,7 +2,6 @@ local a = vim.api
 local query = require('vim.treesitter.query')
 local language = require('vim.treesitter.language')
 local LanguageTree = require('vim.treesitter.languagetree')
-local Range = require('vim.treesitter._range')
 
 ---@type table<integer,LanguageTree>
 local parsers = setmetatable({}, { __mode = 'v' })
@@ -191,7 +190,20 @@ end
 ---
 ---@return boolean True if the position is in node range
 function M.is_in_node_range(node, line, col)
-  return M.node_contains(node, { line, col, line, col })
+  local start_line, start_col, end_line, end_col = M.get_node_range(node)
+  if line >= start_line and line <= end_line then
+    if line == start_line and line == end_line then
+      return col >= start_col and col < end_col
+    elseif line == start_line then
+      return col >= start_col
+    elseif line == end_line then
+      return col < end_col
+    else
+      return true
+    end
+  else
+    return false
+  end
 end
 
 --- Determines if a node contains a range
@@ -201,11 +213,11 @@ end
 ---
 ---@return boolean True if the {node} contains the {range}
 function M.node_contains(node, range)
-  vim.validate({
-    node = { node, 'userdata' },
-    range = { range, Range.validate, 'integer list with 4 or 6 elements' },
-  })
-  return Range.contains({ node:range() }, range)
+  local start_row, start_col, end_row, end_col = node:range()
+  local start_fits = start_row < range[1] or (start_row == range[1] and start_col <= range[2])
+  local end_fits = end_row > range[3] or (end_row == range[3] and end_col >= range[4])
+
+  return start_fits and end_fits
 end
 
 --- Returns a list of highlight captures at the given position
