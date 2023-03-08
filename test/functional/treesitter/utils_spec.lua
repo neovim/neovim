2bcc1cbfc574124c9310cac76915880602a67093
Lewis Russell <lewis6991@gmail.com>
2023-03-08 22:50:23 +0000

    fix(treesitter): is_in_node_range
    TS ranges are end column exclusive, so fix is_in_node_range
    to account for that.

--- test/functional/treesitter/utils_spec.lua
+++ test/functional/treesitter/utils_spec.lua
@@ -28,4 +28,21 @@ describe('treesitter utils', function()
     eq(true, exec_lua('return vim.treesitter.is_ancestor(ancestor, child)'))
     eq(false, exec_lua('return vim.treesitter.is_ancestor(child, ancestor)'))
   end)
+
+  it('can detect if a position is contained in a node', function()
+    exec_lua([[
+      node = {
+        range = function()
+          return 0, 4, 0, 8
+        end,
+      }
+    ]])
+
+    eq(false, exec_lua('return vim.treesitter.is_in_node_range(node, 0, 3)'))
+    for i = 4, 7 do
+      eq(true, exec_lua('return vim.treesitter.is_in_node_range(node, 0, ...)', i))
+    end
+    -- End column exclusive
+    eq(false, exec_lua('return vim.treesitter.is_in_node_range(node, 0, 8)'))
+  end)
 end)
