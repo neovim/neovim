c2f7f8d61c6436e1de13d921f13760fbdfe1d624
nullchilly <nullchilly@gmail.com>
2023-03-11 17:23:50 +0700

    refactor(highlight)!: remove deprecated functions vim.highlight.create/link

--- runtime/lua/vim/highlight.lua
+++ runtime/lua/vim/highlight.lua
@@ -10,30 +10,6 @@ M.priorities = {
   user = 200,
 }
 
----@private
-function M.create(higroup, hi_info, default)
-  vim.deprecate('vim.highlight.create', 'vim.api.nvim_set_hl', '0.9')
-  local options = {}
-  -- TODO: Add validation
-  for k, v in pairs(hi_info) do
-    table.insert(options, string.format('%s=%s', k, v))
-  end
-  vim.cmd(
-    string.format(
-      [[highlight %s %s %s]],
-      default and 'default' or '',
-      higroup,
-      table.concat(options, ' ')
-    )
-  )
-end
-
----@private
-function M.link(higroup, link_to, force)
-  vim.deprecate('vim.highlight.link', 'vim.api.nvim_set_hl', '0.9')
-  vim.cmd(string.format([[highlight%s link %s %s]], force and '!' or ' default', higroup, link_to))
-end
-
 --- Highlight range between two positions
 ---
 ---@param bufnr integer Buffer number to apply highlighting to
