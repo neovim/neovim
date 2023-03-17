2720d2c793b22e6c0c8719e5592922619fffdf7a
Yochem van Rosmalen <yochem+git@icloud.com>
2023-03-17 12:41:57 +0100

    fix(treesitter): InspectTree does not respect 'splitright' #22692
    Problem:
    vim.treesitter.inspect_tree() and :InspectTree does not respect 'splitright'.
    
    Solution:
    - Change the default `command` from `topleft 60vnew` to `60vnew`.
    - Change :InspectTree to respect command mods (`:vertical`, count, etc.).
    
    Closes #22656

--- runtime/plugin/nvim.lua
+++ runtime/plugin/nvim.lua
@@ -6,6 +6,15 @@ vim.api.nvim_create_user_command('Inspect', function(cmd)
   end
 end, { desc = 'Inspect highlights and extmarks at the cursor', bang = true })
 
-vim.api.nvim_create_user_command('InspectTree', function()
-  vim.treesitter.inspect_tree()
-end, { desc = 'Inspect treesitter language tree for buffer' })
+vim.api.nvim_create_user_command('InspectTree', function(cmd)
+  if cmd.mods ~= '' or cmd.count ~= 0 then
+    local count = cmd.count ~= 0 and cmd.count or ''
+    local new = cmd.mods ~= '' and 'new' or 'vnew'
+
+    vim.treesitter.inspect_tree({
+      command = ('%s %s%s'):format(cmd.mods, count, new),
+    })
+  else
+    vim.treesitter.inspect_tree()
+  end
+end, { desc = 'Inspect treesitter language tree for buffer', count = true })
