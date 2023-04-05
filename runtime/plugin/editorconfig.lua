94ab67eb2bc583210470a40d6ee5b8d2ac6ab5fa
Gregory Anders <greg@gpanders.com>
2023-04-05 12:53:00 -0600

    feat(lua): allow vim.F.if_nil to take multiple arguments
    The first argument which is non-nil is returned. This is useful when
    using nested default values (e.g. in the EditorConfig plugin).
    
    Before:
    
      local enable = vim.F.if_nil(vim.b.editorconfig, vim.F.if_nil(vim.g.editorconfig, true))
    
    After:
    
      local enable = vim.F.if_nil(vim.b.editorconfig, vim.g.editorconfig, true)

--- runtime/plugin/editorconfig.lua
+++ runtime/plugin/editorconfig.lua
@@ -3,7 +3,7 @@ vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead', 'BufFilePost' }, {
   group = group,
   callback = function(args)
     -- Buffer-local enable has higher priority
-    local enable = vim.F.if_nil(vim.b.editorconfig, vim.F.if_nil(vim.g.editorconfig, true))
+    local enable = vim.F.if_nil(vim.b.editorconfig, vim.g.editorconfig, true)
     if not enable then
       return
     end
