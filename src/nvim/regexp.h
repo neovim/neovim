97e10c1635adc0c21ba79a174b78885acc1a525b
bfredl <bjorn.linse@gmail.com>
2023-04-17 13:08:53 +0200

    fix(runtime): do not allow breakcheck inside runtime path calculation
    problem: breakcheck might run arbitrary lua code, which might require
    modules and thus invoke runtime path calculation recursively.
    solution: Block the use of breakcheck when expanding glob patterns
    inside 'runtimepath'
    
    fixes #23012
    
    (cherry picked from commit aee6f08ce12a62e9104892702a658a8d3daee4df)

--- src/nvim/regexp.h
+++ src/nvim/regexp.h
@@ -10,6 +10,7 @@
 #define RE_STRING       2       ///< match in string instead of buffer text
 #define RE_STRICT       4       ///< don't allow [abc] without ]
 #define RE_AUTO         8       ///< automatic engine selection
+#define RE_NOBREAK      16      ///< don't use breakcheck functions
 
 // values for reg_do_extmatch
 #define REX_SET        1       ///< to allow \z\(...\),
