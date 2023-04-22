669030ec08042e41d57f74adb4abb836c387930a
dundargoc <33953936+dundargoc@users.noreply.github.com>
2023-04-22 13:50:27 +0200

    ci: remove team reviewers
    Team reviewers is a nice feature that comes with a severe drawback: it
    makes testing the workflows incredibly difficult as they won't work
    without a similar token by the tester.

--- .github/scripts/remove-reviewers.js
+++ .github/scripts/remove-reviewers.js
@@ -1,18 +1,16 @@
-module.exports = async ({github, context}) => {
+module.exports = async ({ github, context }) => {
   const requestedReviewers = await github.rest.pulls.listRequestedReviewers({
     owner: context.repo.owner,
     repo: context.repo.repo,
-    pull_number: context.issue.number
+    pull_number: context.issue.number,
   });
 
-  const reviewers = requestedReviewers.data.users.map(e => e.login)
-  const team_reviewers = requestedReviewers.data.teams.map(e => e.name);
+  const reviewers = requestedReviewers.data.users.map((e) => e.login);
 
   github.rest.pulls.removeRequestedReviewers({
     owner: context.repo.owner,
     repo: context.repo.repo,
     pull_number: context.issue.number,
     reviewers: reviewers,
-    team_reviewers: team_reviewers
   });
-}
+};
