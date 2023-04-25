a7578a873caad127384add14c790ec2af632c453
zeertzjq <zeertzjq@outlook.com>
2023-04-25 14:04:39 +0800

    revert: "fix(ui_client): check return code of dup()"
    This reverts commit c0845343b68bc7b58e8241cb012c78d41d8a33d8.

--- src/nvim/ui_client.c
+++ src/nvim/ui_client.c
@@ -65,11 +65,7 @@ uint64_t ui_client_start_server(int argc, char **argv)
 #ifdef MSWIN
     os_open_conin_fd();
 #else
-    int fd = dup(stderr_isatty ? STDERR_FILENO : STDOUT_FILENO);
-    if (fd < 0) {
-      return 0;
-    }
-    // FIXME: resource leak of fd
+    dup(stderr_isatty ? STDERR_FILENO : STDOUT_FILENO); 
 #endif
   }
 
