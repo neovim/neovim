685a2b6f50e7d08caa9327bdeceffbfa536ad1bf
marshmallow <marshycity@gmail.com>
2023-05-04 11:38:22 +1000

    fix(vim.net): format LuaNet file, whoops

--- runtime/plugin/net.lua
+++ runtime/plugin/net.lua
@@ -1,64 +1,64 @@
 local id = vim.api.nvim_create_augroup('LuaNetwork', {
-        clear = true,
-    })
+  clear = true,
+})
 
 local namespace = vim.api.nvim_create_namespace('LuaNetwork')
 
 vim.api.nvim_create_autocmd({ 'BufReadCmd' }, {
-    pattern = { 'https://*', 'http://*' },
-    group = id,
-    desc = 'Lua Network File Handler',
-    callback = function(ev)
-      local view = vim.fn.winsaveview()
-      local buf = ev.buf
-      local file = ev.file
+  pattern = { 'https://*', 'http://*' },
+  group = id,
+  desc = 'Lua Network File Handler',
+  callback = function(ev)
+    local view = vim.fn.winsaveview()
+    local buf = ev.buf
+    local file = ev.file
 
-      local complete = false
-      local err
+    local complete = false
+    local err
 
-      local mark = vim.api.nvim_buf_set_extmark(buf, namespace, 0, 0, {
-              virt_text = {
-                  { 'Loading ' .. file .. '...', 'Comment' },
-              },
-          })
+    local mark = vim.api.nvim_buf_set_extmark(buf, namespace, 0, 0, {
+      virt_text = {
+        { 'Loading ' .. file .. '...', 'Comment' },
+      },
+    })
 
-      vim.net.fetch(file, {
-          on_complete = function(result)
-            local text = vim.split(result.text(), '\n')
+    vim.net.fetch(file, {
+      on_complete = function(result)
+        local text = vim.split(result.text(), '\n')
 
-            vim.api.nvim_buf_set_lines(buf, -2, -1, false, text)
+        vim.api.nvim_buf_set_lines(buf, -2, -1, false, text)
 
-            vim.fn.winrestview(view)
+        vim.fn.winrestview(view)
 
-            local ft = vim.filetype.match({
-                    filename = file,
-                    contents = vim.g.net_ft_full and text or nil
-                })
+        local ft = vim.filetype.match({
+          filename = file,
+          contents = vim.g.net_ft_full and text or nil,
+        })
 
-            if ft then
-              vim.cmd(':set ft=' .. ft)
-            end
+        if ft then
+          vim.cmd(':set ft=' .. ft)
+        end
 
-            complete = true
-          end,
-          on_err = function(data)
-            err = data
-          end,
-      })
+        complete = true
+      end,
+      on_err = function(data)
+        err = data
+      end,
+    })
 
-      -- block until complete
-      local block, code = vim.wait(10000, function()
-            return complete
-          end)
+    -- block until complete
+    local block, code = vim.wait(10000, function()
+      return complete
+    end)
 
-      -- Block timed out
-      if block == false and code == -1 then
-        vim.notify(
-            'Failed to fetch ' .. file .. ': ' .. table.concat(err, '\n'),
-            vim.log.levels.ERROR
-        )
-      end
+    -- Block timed out
+    if block == false and code == -1 then
+      vim.notify(
+        'Failed to fetch ' .. file .. ': ' .. table.concat(err, '\n'),
+        vim.log.levels.ERROR
+      )
+    end
 
-      vim.api.nvim_buf_del_extmark(buf, namespace, mark)
-    end,
+    vim.api.nvim_buf_del_extmark(buf, namespace, mark)
+  end,
 })
