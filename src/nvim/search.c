f3c71824821a73a4724ea0861d452b7e6cab2938
zeertzjq <zeertzjq@outlook.com>
2023-05-15 07:36:48 +0800

    vim-patch:9.0.1555: setcharsearch() does not clear last searched char properly
    Problem:    setcharsearch() does not clear last searched char properly.
    Solution:   Do not accept lastc_bytelen smaller than one. (closes vim/vim#12398)
    
    https://github.com/vim/vim/commit/e5d91ba1de83949eb9357c0fb8cbd91e7e69be6f
    (cherry picked from commit 8a751ca3609a4bb985657f761c7f91987d633943)

--- src/nvim/search.c
+++ src/nvim/search.c
@@ -1513,7 +1513,7 @@ int searchc(cmdarg_T *cap, int t_cmd)
       }
     }
   } else {            // repeat previous search
-    if (*lastc == NUL && lastc_bytelen == 1) {
+    if (*lastc == NUL && lastc_bytelen <= 1) {
       return FAIL;
     }
     if (dir) {        // repeat in opposite direction
@@ -1556,7 +1556,7 @@ int searchc(cmdarg_T *cap, int t_cmd)
         }
         col -= utf_head_off(p, p + col - 1) + 1;
       }
-      if (lastc_bytelen == 1) {
+      if (lastc_bytelen <= 1) {
         if (p[col] == c && stop) {
           break;
         }
