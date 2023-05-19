bda88a68bc91708109de42a4b8e5fe3fb5f670c3
C.D. MacEachern <craig.daniel.maceachern@gmail.com>
2023-05-19 15:54:00 -0400

    fix: bail on shada global vars off and no shada start

--- runtime/plugin/versioncheck.lua
+++ runtime/plugin/versioncheck.lua
@@ -1,4 +1,4 @@
-if not vim.g.versioncheck then
+if vim.g.versioncheck == false then
   return
 end
 
@@ -7,9 +7,21 @@ if vim.tbl_contains(vim.v.argv, '-l') then
   return
 end
 
+-- startup with -i NONE means no shada file read or written, so bail
+if vim.tbl_contains(vim.v.argv, '-i NONE') then
+  return
+end
+
+-- missing '!' in shada option means no storing/reading of global vars
+if not vim.o.shada:match('!') then
+  return
+end
+
 if vim.version().prerelease ~= true then
   return
 end
+
+vim.g.versioncheck = {}
 
 local augroup = vim.api.nvim_create_augroup('versioncheck', {})
 vim.api.nvim_create_autocmd('CursorHold', {
