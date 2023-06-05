65033cd2ca699c106c8ed696bfedb4bed796d33c
ChrisjStone <chrisjstone79@gmail.com>
2023-06-05 10:12:55 -0600

    build(cmake): apply fix based on #10407
    	use path_prefixes to locate header and lib files.
    	add add_library and set_property in CmakeLists.txt based on
    	pull request #10661

--- cmake/FindLuv.cmake
+++ cmake/FindLuv.cmake
@@ -9,13 +9,13 @@ function(ubuntu)
 endfunction()
 ubuntu()
 
-# Fedora-specific workaround to find system paths
-function(fedora)
-  set(CMAKE_FIND_LIBRARY_PREFIXES "")
+# Fedora workaround to find luv.so
+if (NOT ${LUV_INCLUDE_DIR})
+set(USE_LUV TRUE CACHE BOOL "")
   find_path(LUV_INCLUDE_DIR luv/luv.h PATH_SUFFIXES luajit-2.1)
+# Fedora defaults to lua/5.1 for luv.so location regardless of PATH_SUFFIXES
   find_library(LUV_LIBRARY NAMES luv PATH_SUFFIXES luajit/2.1)
-endfunction()
-fedora()
+endif()
 
 find_package_handle_standard_args(Luv DEFAULT_MSG
   LUV_LIBRARY LUV_INCLUDE_DIR)
