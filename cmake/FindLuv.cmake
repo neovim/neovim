02d5445365394e572b7ee57a6d4747a9fd8bf800
ChrisjStone <chrisjstone79@gmail.com>
2023-06-04 18:33:06 -0600

    build(cmake): update FindLuv.cmake
     Add fedora specefic funtion so cmake can find luv header and library

--- cmake/FindLuv.cmake
+++ cmake/FindLuv.cmake
@@ -9,6 +9,14 @@ function(ubuntu)
 endfunction()
 ubuntu()
 
+# Fedora-specific workaround to find system paths
+function(fedora)
+  set(CMAKE_FIND_LIBRARY_PREFIXES "")
+  find_path(LUV_INCLUDE_DIR luv/luv.h PATH_SUFFIXES luajit-2.1)
+  find_library(LUV_LIBRARY NAMES luv PATH_SUFFIXES luajit/2.1)
+endfunction()
+fedora()
+
 find_package_handle_standard_args(Luv DEFAULT_MSG
   LUV_LIBRARY LUV_INCLUDE_DIR)
 mark_as_advanced(LUV_INCLUDE_DIR LUV_LIBRARY)
