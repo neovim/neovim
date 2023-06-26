54294140d716d3fa5dac9e6c284ca3a3c503c6b4
zeertzjq <zeertzjq@outlook.com>
2023-06-26 11:04:09 +0800

    fix(charset): fix wrong display of 0xffff
    (cherry picked from commit fe3493679d189f9ef64bcab74dd33031a8453f53)

--- src/nvim/charset.c
+++ src/nvim/charset.c
@@ -645,8 +645,8 @@ size_t transchar_hex(char *const buf, const int c)
   size_t i = 0;
 
   buf[i++] = '<';
-  if (c > 255) {
-    if (c > 255 * 256) {
+  if (c > 0xFF) {
+    if (c > 0xFFFF) {
       buf[i++] = (char)nr2hex((unsigned)c >> 20);
       buf[i++] = (char)nr2hex((unsigned)c >> 16);
     }
