1fe4d77531e1c0b6fa6b0fd67572065815d837ec
Raphael <glephunter@gmail.com>
2023-06-29 16:27:44 +0800

    Update src/nvim/autocmd.c
    Co-authored-by: Justin M. Keyes <justinkz@gmail.com>

--- src/nvim/autocmd.c
+++ src/nvim/autocmd.c
@@ -1973,7 +1973,7 @@ static bool call_autocmd_callback(const AutoCmd *ac, const AutoPatCmd *apc)
     PUT(data, "match", CSTR_TO_OBJ(autocmd_match));
     PUT(data, "file", CSTR_TO_OBJ(autocmd_fname));
     PUT(data, "buf", INTEGER_OBJ(autocmd_bufnr));
-    PUT(data, "winid", INTEGER_OBJ(apc->arg_winid));
+    PUT(data, "win", INTEGER_OBJ(apc->arg_winid));
 
     if (apc->data) {
       PUT(data, "data", copy_object(*apc->data, NULL));
