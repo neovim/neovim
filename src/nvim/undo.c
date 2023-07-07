0639fbee7626f549f507c5f04162a0c9d8d037f8
Alexandre Teoi <alexandre.teoi@gmail.com>
2023-07-07 19:33:48 -0300

    fix(inccommand): restrict cmdpreview undo calls
    Problem:
    The cmdpreview saved undo nodes on `cmdpreview_prepare` from `ex_getln.c` may
    become invalid (free) if the preview function makes undo operations, causing
    heap-use-after-free errors.
    
    Solution:
    Save the buffer undo list on `cmdpreview_prepare` and start a new empty one. On
    `cmdpreview_restore_state`, undo all the entries in the new undo list and
    restore the original one. With this approach, the preview function will be
    allowed to undo only its own changes.
    
    (cherry picked from commit aba0d5ef4a3d97f14d572d04b88369c81501c9c5)

--- src/nvim/undo.c
+++ src/nvim/undo.c
@@ -1794,7 +1794,6 @@ bool u_undo_and_forget(int count, bool do_buf_event)
 {
   if (curbuf->b_u_synced == false) {
     u_sync(true);
-    count = 1;
   }
   undo_undoes = true;
   u_doit(count, true, do_buf_event);
