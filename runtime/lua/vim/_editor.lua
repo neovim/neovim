neovim/neovim

commit 6318edadc32acce3ed41a6995a5faa5395b5f562

fix(defaults): visual hash (#) on text with "?"
Problem:
The default "#" mapping fails on the following example after v$h# with
cursor at start of the first line:

    aa?/\bb
    aa
    aa?/\bb

Solution:
Also escape "?".

runtime/lua/vim/_editor.lua
@@ -1029,7 +1029,7 @@ function vim._init_default_mappings()
     local esc_chunks = vim
       .iter(chunks)
       :map(function(v)
-        return vim.fn.escape(v, [[/\]])
+        return vim.fn.escape(v, [[?/\]])
       end)
       :totable()
     local esc_pat = table.concat(esc_chunks, [[\n]])

commit b9a0e762f1d79d17631b7d17cf25b6e25006d8c2

fix(defaults): visual star (*) on text with "?"
regression from 6318edadc32acce3ed41a6995a5faa5395b5f562

runtime/lua/vim/_editor.lua
@@ -1029,7 +1029,7 @@ function vim._init_default_mappings()
     local esc_chunks = vim
       .iter(chunks)
       :map(function(v)
-        return vim.fn.escape(v, [[?/\]])
+        return vim.fn.escape(v, cmd == '/' and [[/\]] or [[?\]])
       end)
       :totable()
     local esc_pat = table.concat(esc_chunks, [[\n]])