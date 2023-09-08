a1c05ab58a3d3c5d9c0f439f063b331c06b9f3aa
dundargoc <gocdundar@gmail.com>
2023-09-08 17:29:08 +0200

    ci: remove container solution for the linux runner
    This will fix the failing release job.
    
    Ubuntu 18.04 is incompatible with checkout action version 4, which
    requires glibc 2.28+. This will bump the minimum glibc version required
    to use the release versions to 2.31. People requring the older releases
    can find them at https://github.com/neovim/neovim-releases.

--- .github/scripts/install_deps.sh
+++ .github/scripts/install_deps.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-SUDO="sudo"
-
 while (($# > 0)); do
   case $1 in
   --test) # install test dependencies
     TEST=1
-    shift
-    ;;
-  --container) # don't use sudo
-    SUDO=""
     shift
     ;;
   esac
@@ -17,10 +11,10 @@ done
 
 os=$(uname -s)
 if [[ $os == Linux ]]; then
-  $SUDO apt-get update
-  $SUDO apt-get install -y build-essential cmake curl gettext ninja-build unzip
+  sudo apt-get update
+  sudo apt-get install -y build-essential cmake curl gettext ninja-build unzip
   if [[ -n $TEST ]]; then
-    $SUDO apt-get install -y locales-all cpanminus
+    sudo apt-get install -y locales-all cpanminus
   fi
 elif [[ $os == Darwin ]]; then
   brew update --quiet
