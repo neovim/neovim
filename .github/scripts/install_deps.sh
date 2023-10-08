715d48e2df0471acae9013716f75368b5e0d7660
dundargoc <gocdundar@gmail.com>
2023-10-08 23:05:23 +0200

    ci: use clang 17 as the default clang version is too old

--- .github/scripts/install_deps.sh
+++ .github/scripts/install_deps.sh
@@ -13,6 +13,22 @@ os=$(uname -s)
 if [[ $os == Linux ]]; then
   sudo apt-get update
   sudo apt-get install -y attr build-essential cmake curl gettext libattr1-dev ninja-build unzip
+
+  if [[ $CC == clang ]]; then
+    DEFAULT_CLANG_VERSION=$(echo |  clang -dM -E - | grep __clang_major | awk '{print $3}')
+    CLANG_VERSION=17
+    if ((DEFAULT_CLANG_VERSION >= CLANG_VERSION)); then
+      echo "Default clang version is $DEFAULT_CLANG_VERSION, which equal or larger than wanted version $CLANG_VERSION. Aborting!"
+      exit 1
+    fi
+
+    wget https://apt.llvm.org/llvm.sh
+    chmod +x llvm.sh
+    sudo ./llvm.sh $CLANG_VERSION
+    sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-$CLANG_VERSION 100
+    sudo update-alternatives --set clang /usr/bin/clang-$CLANG_VERSION
+  fi
+
   if [[ -n $TEST ]]; then
     sudo apt-get install -y locales-all cpanminus
   fi
