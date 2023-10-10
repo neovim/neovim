8ca318f6be5f2c3c90b5dea75685ebbcb705c0b5
James Tirta Halim <tirtajames45@gmail.com>
2023-10-10 15:02:19 +0700

    ops.c: fix typo

--- src/nvim/ops.c
+++ src/nvim/ops.c
@@ -987,7 +987,7 @@ static int stuff_yank(int regname, char *p)
     const size_t pl = strlen(p);
     char *lp = xmalloc(ppl + pl + 1);
     memcpy(lp, *pp, ppl);
-    *((char *)memcpy(lp + ppl, p, pl) + ppl + pl) = NUL;
+    *((char *)memcpy(lp + ppl, p, pl) + pl) = NUL;
     xfree(p);
     xfree(*pp);
     *pp = lp;
