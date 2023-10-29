2e4f0b6ba83bac915209421ec6b304005d3f6a5f
Maria José Solano <majosolano99@gmail.com>
2023-10-29 15:58:13 -0700

    feat(keymaps): use vim.ui.select for z=

--- runtime/lua/vim/_editor.lua
+++ runtime/lua/vim/_editor.lua
@@ -1112,6 +1112,26 @@ function vim._init_default_mappings()
     do_open(get_visual_selection())
   end, { desc = gx_desc })
 
+  -- z=
+  local spell_on_choice = vim.schedule_wrap(function(_, idx)
+    if type(idx) == 'number' then
+      vim.cmd.normal({ idx .. 'z=', bang = true })
+    end
+  end)
+  local spell_select = function()
+    if vim.v.count > 0 then
+      spell_on_choice(nil, vim.v.count)
+      return
+    end
+    local cword = vim.fn.expand('<cword>')
+    vim.ui.select(
+      vim.fn.spellsuggest(cword, vim.o.lines),
+      { prompt = 'Change ' .. cword .. ' to:' },
+      spell_on_choice
+    )
+  end
+  vim.keymap.set('n', 'z=', spell_select, { desc = 'Shows spelling suggestions' })
+
   -- menus
 
   -- TODO VimScript, no l10n
