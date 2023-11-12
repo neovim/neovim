6d14f3ddab33144966e46487b6039440ac7c43f2
zeertzjq <zeertzjq@outlook.com>
2023-11-13 06:33:34 +0800

    vim-patch:9.0.2103: recursive callback may cause issues on some archs (#26013)
    Problem:  recursive callback may cause issues on some archs
    Solution: Decrease the limit drastically to 20
    
    Recursive callback limit causes problems on some architectures
    
    Since commit 47510f3d6598a1218958c03ed11337a43b73f48d we have a test
    that causes a recursive popup callback function to be executed. However
    it seems the current limit of 'maxfuncdepth' option value is still too
    recursive for some 32bit architectures (e.g. 32bit ARM).
    
    So instead of allowing a default limit of 100 (default value for
    'maxfuncdepth'), let's reduce this limit to 20. I don't think there is a
    use case where one would need such a high recursive callback limit and a
    limit of 20 seems reasonable (although it is currently hard-coded).
    
    closes: vim/vim#13495
    closes: vim/vim#13502
    
    https://github.com/vim/vim/commit/2076463e383901cef44685aaf4b63e4306444f9e
    
    Co-authored-by: Christian Brabandt <cb@256bit.org>

--- runtime/lua/vim/_meta/options.lua
+++ runtime/lua/vim/_meta/options.lua
@@ -4037,7 +4037,6 @@ vim.go.mat = vim.go.matchtime
 --- Increasing this limit above 200 also changes the maximum for Ex
 --- command recursion, see `E169`.
 --- See also `:function`.
---- Also used for maximum depth of callback functions.
 ---
 --- @type integer
 vim.o.maxfuncdepth = 100
