f6dab3ce045ef06a5c460d20cf09333cc56107da
Gregory Anders <greg@gpanders.com>
2023-11-16 13:06:17 -0600

    fix(osc52): enable OSC 52 by default in tmux sessions
    tmux has a set-clipboard option which, when set to 'on', allows
    applications to set the system clipboard using the usual OSC 52 escape
    sequence.

--- runtime/plugin/osc52.lua
+++ runtime/plugin/osc52.lua
@@ -2,18 +2,17 @@ local tty = vim.iter(vim.api.nvim_list_uis()):any(function(ui)
   return ui.chan == 1 and ui.stdout_tty
 end)
 
-if
-  not tty
-  or vim.g.clipboard ~= nil
-  or vim.o.clipboard ~= ''
-  or not os.getenv('SSH_TTY')
-  or os.getenv('TMUX')
-then
+if not tty or vim.g.clipboard ~= nil or vim.o.clipboard ~= '' or not os.getenv('SSH_TTY') then
   return
 end
 
 require('vim.termcap').query('Ms', function(cap, seq)
   assert(cap == 'Ms')
+
+  -- Check 'clipboard' and g:clipboard again to avoid a race condition
+  if vim.o.clipboard ~= '' or vim.g.clipboard ~= nil then
+    return
+  end
 
   -- If the terminal reports a sequence other than OSC 52 for the Ms capability
   -- then ignore it. We only support OSC 52 (for now)
