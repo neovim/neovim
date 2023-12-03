988b472d902e6116353fc2ae605bed32a37e6391
Christian Clason <c.clason@uni-graz.at>
2023-12-03 15:58:27 +0100

    feat(treesitter): highlight help files by default (#26347)

--- runtime/ftplugin/help.lua
+++ runtime/ftplugin/help.lua
@@ -1,3 +1,7 @@
+-- use treesitter over syntax (for highlighted code blocks)
+vim.treesitter.start()
+
+-- add custom highlights for list in `:h highlight-groups`
 if vim.endswith(vim.fs.normalize(vim.api.nvim_buf_get_name(0)), '/doc/syntax.txt') then
   require('vim.vimhelp').highlight_groups()
 end
