de6ad619a31ea7b7a4a0dddf02d71359d942f284
dundargoc <gocdundar@gmail.com>
2023-12-07 13:29:50 +0100

    ci: remove mac arm support

--- .github/scripts/build_universal_macos.sh
+++ .github/scripts/build_universal_macos.sh
@@ -4,18 +4,10 @@ MACOSX_DEPLOYMENT_TARGET="$(sw_vers -productVersion | cut -f1 -d.)"
 export MACOSX_DEPLOYMENT_TARGET
 cmake -S cmake.deps -B .deps -G Ninja \
   -D CMAKE_BUILD_TYPE=${NVIM_BUILD_TYPE} \
-  -D CMAKE_OSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET} \
-  -D CMAKE_OSX_ARCHITECTURES=arm64\;x86_64 \
   -D CMAKE_FIND_FRAMEWORK=NEVER
 cmake --build .deps
 cmake -B build -G Ninja \
   -D CMAKE_BUILD_TYPE=${NVIM_BUILD_TYPE} \
-  -D CMAKE_OSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET} \
-  -D CMAKE_OSX_ARCHITECTURES=arm64\;x86_64 \
   -D CMAKE_FIND_FRAMEWORK=NEVER
 cmake --build build
-# Make sure we build everything for M1 as well
-for macho in build/bin/* build/lib/nvim/parser/*.so; do
-  lipo -info "$macho" | grep -q arm64 || exit 1
-done
 cpack --config build/CPackConfig.cmake
