fbd0f6658f960a069f8fef09f5f91c4b11259977
dundargoc <gocdundar@gmail.com>
2023-12-12 16:42:02 +0100

    docs: add installation and build guides from wiki to repo

--- runtime/lua/nvim/health.lua
+++ runtime/lua/nvim/health.lua
@@ -17,7 +17,7 @@ local shell_error = function()
   return vim.v.shell_error ~= 0
 end
 
-local suggest_faq = 'https://github.com/neovim/neovim/wiki/Building-Neovim#optimized-builds'
+local suggest_faq = 'https://github.com/neovim/neovim/blob/docs/install/BUILD.md#building'
 
 local function check_runtime()
   health.start('Runtime')
