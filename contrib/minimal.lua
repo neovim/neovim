26ef3b54192b9780f8eaf38c1a8ea4cb4a1ac9ac
dundargoc <gocdundar@gmail.com>
2024-01-01 13:08:56 +0100

    build: various build improvements
    - remove "ran-" prefix from touch files as it's redundant since the
      they're already in the directory named `touches`.
    - Include `contrib` when formatting with `make formatlua`.
    - Use TARGET_FILE generator expression instead of assuming the
      executable location.
    - reuse logic that determines whether to use lua or luajit.
    - add translations to the `nvim` target.
    
    Makefile improvements:
    - rename variable `CMAKE_PRG` to `CMAKE` to make it more consistent with
      the builtin `MAKE` variable.
    - stop propagating flags to generator. Users should use cmake for
      non-standard use cases.
    - remove `+` prefix from targets. If the user for whatever reason wants
      to dry-run a target then they should be able to.

--- contrib/minimal.lua
+++ contrib/minimal.lua
@@ -1,10 +1,10 @@
 -- Run this file as `nvim --clean -u minimal.lua`
 
-for name, url in pairs{
+for name, url in pairs {
   -- ADD PLUGINS _NECESSARY_ TO REPRODUCE THE ISSUE, e.g:
   -- some_plugin = 'https://github.com/author/plugin.nvim'
 } do
-  local install_path = vim.fn.fnamemodify('nvim_issue/'..name, ':p')
+  local install_path = vim.fn.fnamemodify('nvim_issue/' .. name, ':p')
   if vim.fn.isdirectory(install_path) == 0 then
     vim.fn.system { 'git', 'clone', '--depth=1', url, install_path }
   end
