dfc8c1fa38807924380bc5c3a7573ee9912cb79b
altermo <>
2024-01-14 13:24:53 +0100

    fix: make i_ctrl-] not activate InsertCharPre

---
+++ test/functional/autocmd/insertcharpre_spec.lua
@@ -0,0 +1,16 @@
+local helpers = require('test.functional.helpers')(after_each)
+
+local clear = helpers.clear
+local feed = helpers.feed
+local command = helpers.command
+local expect = helpers.expect
+
+describe('autocmd InsertCharPre', function()
+  before_each(clear)
+
+  it('Ctrl-] should not activate InsertCharPre', function()
+    command('autocmd InsertCharPre * let v:char="a"')
+    feed('ifoo<C-]>')
+    expect('aaa')
+  end)
+end)
