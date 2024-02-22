bb15fa035610bb9765ca16900703804a88faa3bb
再生花 <hoangtun0810@gmail.com>
2024-02-23 05:58:59 +0900

    feat(treesitter): add folding for `InspectTree` (#27518)
    As the InspectTree buffer is now a valid tree-sitter query tree, we can
    use the bundled fold queries to have folding for the tree.

--- runtime/lua/vim/treesitter.lua
+++ runtime/lua/vim/treesitter.lua
@@ -461,7 +461,8 @@ end
 ---
 --- While in the window, press "a" to toggle display of anonymous nodes, "I" to toggle the
 --- display of the source language of each node, "o" to toggle the query editor, and press
---- <Enter> to jump to the node under the cursor in the source buffer.
+--- <Enter> to jump to the node under the cursor in the source buffer. Folding also works
+--- (try |zo|, |zc|, etc.).
 ---
 --- Can also be shown with `:InspectTree`. *:InspectTree*
 ---
