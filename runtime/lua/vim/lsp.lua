2e1f5055acdef650c27efc4afdf8606037ec021b
Gregory Anders <8965202+gpanders@users.noreply.github.com>
2024-02-24 19:21:57 -0600

    fix(lsp): add assertion for explicit bufnr in apply_text_edits (#27614)
    Assert that the buffer number passed to apply_text_edits is fully
    resolved (not 0 or null). Pass the known buffer number to
    apply_text_edits from lsp.formatexpr().

--- runtime/lua/vim/lsp.lua
+++ runtime/lua/vim/lsp.lua
@@ -1280,7 +1280,7 @@ function lsp.formatexpr(opts)
       local response =
         client.request_sync(ms.textDocument_rangeFormatting, params, timeout_ms, bufnr)
       if response and response.result then
-        lsp.util.apply_text_edits(response.result, 0, client.offset_encoding)
+        lsp.util.apply_text_edits(response.result, bufnr, client.offset_encoding)
         return 0
       end
     end
