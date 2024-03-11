99d8ad01ec184767221b19542ceecd64c5f75466
Tomas Slusny <slusnucky@gmail.com>
2024-03-11 02:02:52 +0100

    docs: adjust fswatch overflow message to mention docs with info
    - Add :h fswatch-limitations that notifies user about default inotify
      limitations on linux and how to adjust them
    - Check for Event queue overflow message from fswatch and refer user to
      new documentation
    
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- runtime/lua/vim/_watch.lua
+++ runtime/lua/vim/_watch.lua
@@ -289,6 +289,9 @@ function M.fswatch(path, opts, callback)
       end
 
       if data and #vim.trim(data) > 0 then
+        if vim.fn.has('linux') == 1 and vim.startswith(data, 'Event queue overflow') then
+          data = 'inotify(7) limit reached, see :h fswatch-limitations for more info.'
+        end
         vim.schedule(function()
           vim.notify('fswatch: ' .. data, vim.log.levels.ERROR)
         end)
