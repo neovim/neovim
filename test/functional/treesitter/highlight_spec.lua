e24809e45d608ed28839049cb1a6c3b5664f34ba
Christian Clason <c.clason@uni-graz.at>
2024-03-17 13:13:12 +0100

    feat(treesitter): update Vimdoc parser and queries to v2.4.0

--- test/functional/treesitter/highlight_spec.lua
+++ test/functional/treesitter/highlight_spec.lua
@@ -832,7 +832,7 @@ describe('treesitter highlighting (help)', function()
 
     screen:expect {
       grid = [[
-      {1:>ruby}                                   |
+      {1:>}{3:ruby}                                   |
       {1:  -- comment}                            |
       {1:  local this_is = 'actually_lua'}        |
       {1:<}                                       |
@@ -845,7 +845,7 @@ describe('treesitter highlighting (help)', function()
 
     screen:expect {
       grid = [[
-      {1:>lua}                                    |
+      {1:>}{3:lua}                                    |
       {1:  -- comment}                            |
       {1:  }{3:local}{1: }{4:this_is}{1: }{3:=}{1: }{5:'actually_lua'}        |
       {1:<}                                       |
@@ -858,7 +858,7 @@ describe('treesitter highlighting (help)', function()
 
     screen:expect {
       grid = [[
-      {1:>ruby}                                   |
+      {1:>}{3:ruby}                                   |
       {1:  -- comment}                            |
       {1:  local this_is = 'actually_lua'}        |
       {1:<}                                       |
