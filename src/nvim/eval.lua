971cb9bcf6d9a94167b8df5dc2911cb33ed8638f
zeertzjq <zeertzjq@outlook.com>
2024-03-23 16:10:15 +0800

    vim-patch:9.1.0199: Not enough tests for the slice() function
    Problem:  Not enough tests for the slice() function.
    Solution: Test with multibyte chars, and in both Legacy and Vim9 script.
              Update docs to be clearer about how it treats composing chars.
              (zeertzjq)
    
    closes: vim/vim#14275
    
    https://github.com/vim/vim/commit/ad38769030b5fa86aa0e8f1f0b4266690dfad4c9

--- src/nvim/eval.lua
+++ src/nvim/eval.lua
@@ -10485,7 +10485,8 @@ M.funcs = {
       Similar to using a |slice| "expr[start : end]", but "end" is
       used exclusive.  And for a string the indexes are used as
       character indexes instead of byte indexes.
-      Also, composing characters are not counted.
+      Also, composing characters are treated as a part of the
+      preceding base character.
       When {end} is omitted the slice continues to the last item.
       When {end} is -1 the last item is omitted.
       Returns an empty value if {start} or {end} are invalid.
@@ -10957,8 +10958,8 @@ M.funcs = {
       of byte index and length.
       When {skipcc} is omitted or zero, composing characters are
       counted separately.
-      When {skipcc} set to 1, Composing characters are ignored,
-      similar to  |slice()|.
+      When {skipcc} set to 1, composing characters are treated as a
+      part of the preceding base character, similar to |slice()|.
       When a character index is used where a character does not
       exist it is omitted and counted as one character.  For
       example: >vim
@@ -10981,7 +10982,7 @@ M.funcs = {
       in String {string}.
       When {skipcc} is omitted or zero, composing characters are
       counted separately.
-      When {skipcc} set to 1, Composing characters are ignored.
+      When {skipcc} set to 1, composing characters are ignored.
       |strcharlen()| always does this.
 
       Returns zero on error.
