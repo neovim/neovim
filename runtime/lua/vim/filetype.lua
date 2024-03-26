d6f406db4527deb693f0aef90316c25d846f5195
zeertzjq <zeertzjq@outlook.com>
2024-03-26 13:31:37 +0800

    fix(filetype): don't use fnamemodify() with :e for extension (#27976)
    Use pattern matching instead, as fnamemodify() with :e produces an empty
    string when the file name only has an extension, leading to differences
    in behavior from Vim.
    
    Related #16955 #27972

--- runtime/lua/vim/filetype.lua
+++ runtime/lua/vim/filetype.lua
@@ -1472,7 +1472,6 @@ local filename = {
   ['bash.bashrc'] = detect.bash,
   bashrc = detect.bash,
   ['.bashrc'] = detect.bash,
-  ['.env'] = detect.sh,
   ['.kshrc'] = detect.ksh,
   ['.profile'] = detect.sh,
   ['/etc/profile'] = detect.sh,
@@ -2387,7 +2386,9 @@ function M.match(args)
     end
 
     -- Next, check file extension
-    local ext = fn.fnamemodify(name, ':e')
+    -- Don't use fnamemodify() with :e modifier here,
+    -- as that's empty when there is only an extension.
+    local ext = name:match('%.([^.]-)$') or ''
     ft, on_detect = dispatch(extension[ext], path, bufnr)
     if ft then
       return ft, on_detect
