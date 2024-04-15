747602e8c35255b035f8d280998f35060f3ef933
Joey Gouly <joey.gouly@gmail.com>
2024-04-15 09:29:57 +0100

    fix(highlight): combine line attributes before column attributes
    Fixes: #28118
    
    TODO: add test
        nvim --clean -u NONE -c 'color vim' -c 'norm Ia' -c 'norm ob'  -c 'vnew|norm Ia' -c 'norm oc' -c 'windo diffthis|set cc=8'

--- src/nvim/drawline.c
+++ src/nvim/drawline.c
@@ -2606,6 +2606,8 @@ int win_line(win_T *wp, linenr_T lnum, int startrow, int endrow, int col_rows, s
 
           int col_attr = base_attr;
 
+          col_attr = hl_combine_attr(col_attr, wlv.line_attr);
+
           if (wp->w_p_cuc && vcol_hlc(wlv) == wp->w_virtcol
               && lnum != wp->w_cursor.lnum) {
             col_attr = hl_combine_attr(col_attr, cuc_attr);
@@ -2616,8 +2618,6 @@ int win_line(win_T *wp, linenr_T lnum, int startrow, int endrow, int col_rows, s
           if (wp->w_buffer->terminal && wlv.vcol < TERM_ATTRS_MAX) {
             col_attr = hl_combine_attr(col_attr, term_attrs[wlv.vcol]);
           }
-
-          col_attr = hl_combine_attr(col_attr, wlv.line_attr);
 
           linebuf_attr[wlv.off] = col_attr;
           // linebuf_vcol[] already filled by the for loop above
