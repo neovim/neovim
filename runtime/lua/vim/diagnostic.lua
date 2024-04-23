d3f387ceb7fb52af8dcb0657644fbc68c827a2b5
Guilherme Soares <guilhermesoares1970@gmail.com>
2024-04-23 18:01:38 +0200

    fix(type): allow nil severity field in diagnostic.GotoOpts

--- runtime/lua/vim/diagnostic.lua
+++ runtime/lua/vim/diagnostic.lua
@@ -1159,7 +1159,7 @@ end
 --- @field wrap? boolean
 ---
 --- See |diagnostic-severity|.
---- @field severity vim.diagnostic.Severity
+--- @field severity? vim.diagnostic.Severity
 ---
 --- If `true`, call |vim.diagnostic.open_float()| after moving.
 --- If a table, pass the table as the {opts} parameter to |vim.diagnostic.open_float()|.
