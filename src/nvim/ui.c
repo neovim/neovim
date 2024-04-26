7626f431d84fc3a6eb82d0d23d436e3e31e991ce
Luuk van Baal <luukvbaal@gmail.com>
2024-04-26 16:21:56 +0200

    fix(ui): update ext_ui widgets when attaching non-remote UI
    Problem:  Updating internalized UI capabilities is postponed until a
              remote UI attaches.
    Solution: Always update active UI widgets in ui_refresh().

--- src/nvim/ui.c
+++ src/nvim/ui.c
@@ -194,15 +194,6 @@ void ui_refresh(void)
     abort();
   }
 
-  if (!ui_active()) {
-    return;
-  }
-
-  if (updating_screen) {
-    ui_schedule_refresh();
-    return;
-  }
-
   int width = INT_MAX;
   int height = INT_MAX;
   bool ext_widgets[kUIExtCount];
@@ -234,9 +225,17 @@ void ui_refresh(void)
     }
     ui_ext[i] = ext_widgets[i];
     if (i < kUIGlobalCount) {
-      ui_call_option_set(cstr_as_string(ui_ext_names[i]),
-                         BOOLEAN_OBJ(ext_widgets[i]));
+      ui_call_option_set(cstr_as_string(ui_ext_names[i]), BOOLEAN_OBJ(ext_widgets[i]));
     }
+  }
+
+  if (!ui_active()) {
+    return;
+  }
+
+  if (updating_screen) {
+    ui_schedule_refresh();
+    return;
   }
 
   ui_default_colors_set();
