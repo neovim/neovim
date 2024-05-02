ec3039a87dea6715579a2a56b7fa14c13aff1754
Gregory Anders <greg@gpanders.com>
2024-05-02 08:27:20 -0500

    docs: fix filetype.lua example for matching on contents
    When using a table, the first value is the filetype string or function,
    and the second value is the options table. The current example "works"
    because the function still has index 1, but the priority field is
    ignored.

--- runtime/lua/vim/filetype.lua
+++ runtime/lua/vim/filetype.lua
@@ -2308,7 +2308,6 @@ end
 --- vim.filetype.add {
 ---   pattern = {
 ---     ['.*'] = {
----       priority = -math.huge,
 ---       function(path, bufnr)
 ---         local content = vim.api.nvim_buf_get_lines(bufnr, 0, 1, false)[1] or ''
 ---         if vim.regex([[^#!.*\\<mine\\>]]):match_str(content) ~= nil then
@@ -2317,6 +2316,7 @@ end
 ---           return 'drawing'
 ---         end
 ---       end,
+---       { priority = -math.huge },
 ---     },
 ---   },
 --- }
