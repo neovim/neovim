4cff4185647c17b1a397c4c26ff7d8b0738f6396
Gregory Anders <greg@gpanders.com>
2024-05-30 19:57:47 -0500

    fix(vim.text): remove assert from vim.text.hexdecode
    Instead, return nil plus an error message if the input is invalid.

--- runtime/lua/vim/text.lua
+++ runtime/lua/vim/text.lua
@@ -18,15 +18,19 @@ end
 --- Hex decode a string.
 ---
 --- @param enc string String to decode
---- @return string : Decoded string
+--- @return string? : Decoded string
+--- @return string? : Error message, if any
 function M.hexdecode(enc)
-  assert(#enc % 2 == 0, 'string must have an even number of hex characters')
+  if #enc % 2 ~= 0 then
+    return nil, 'string must have an even number of hex characters'
+  end
+
   local str = {} ---@type string[]
   for i = 1, #enc, 2 do
     local n = assert(tonumber(enc:sub(i, i + 1), 16))
     str[#str + 1] = string.char(n)
   end
-  return table.concat(str)
+  return table.concat(str), nil
 end
 
 return M
