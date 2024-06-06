df169176ddc139b044a47fe1500b4c1750c0dbcf
James Tirta Halim <tirtajames45@gmail.com>
2024-06-06 12:32:59 +0700

    refactor: inline short functions

--- src/nvim/memory.h
+++ src/nvim/memory.h
@@ -5,6 +5,8 @@
 #include <time.h>  // IWYU pragma: keep
 
 #include "auto/config.h"
+#include "nvim/ascii_defs.h"
+#include "nvim/func_attr.h"
 #include "nvim/macros_defs.h"
 #include "nvim/memory_defs.h"  // IWYU pragma: keep
 
@@ -66,6 +68,15 @@ EXTERN size_t arena_alloc_count INIT( = 0);
 
 #ifndef HAVE_STRNLEN
 # define strnlen xstrnlen  // Older versions of SunOS may not have strnlen
+REAL_FATTR_NONNULL_ALL REAL_FATTR_PURE
+static inline size_t xstrnlen(const char *s, size_t n)
+{
+  const char *end = memchr(s, NUL, n);
+  if (end == NULL) {
+    return n;
+  }
+  return (size_t)(end - s);
+}
 #endif
 
 #define STRCPY(d, s)        strcpy((char *)(d), (char *)(s))  // NOLINT(runtime/printf)
@@ -74,3 +85,46 @@ EXTERN size_t arena_alloc_count INIT( = 0);
 #define STRMOVE(d, s)       memmove((d), (s), strlen(s) + 1)
 
 #define STRCAT(d, s)        strcat((char *)(d), (char *)(s))  // NOLINT(runtime/printf)
+
+/// Copies `len` bytes of `src` to `dst` and zero terminates it.
+///
+/// @see {xstrlcpy}
+/// @param[out]  dst  Buffer to store the result.
+/// @param[in]  src  Buffer to be copied.
+/// @param[in]  len  Number of bytes to be copied.
+REAL_FATTR_NONNULL_ALL REAL_FATTR_NONNULL_RET
+static inline void *xmemcpyz(void *dst, const void *src, size_t len)
+{
+  memcpy(dst, src, len);
+  ((char *)dst)[len] = NUL;
+  return dst;
+}
+
+/// A version of memchr() that returns a pointer one past the end
+/// if it doesn't find `c`.
+///
+/// @param addr The address of the memory object.
+/// @param c    The char to look for.
+/// @param size The size of the memory object.
+/// @returns a pointer to the first instance of `c`, or one past the end if not
+///          found.
+REAL_FATTR_NONNULL_RET REAL_FATTR_NONNULL_ALL REAL_FATTR_PURE
+static inline void *xmemscan(const void *addr, char c, size_t size)
+{
+  char *p = memchr(addr, c, size);
+  return p ? p : (char *)addr + size;
+}
+
+/// A version of strchr() that returns a pointer to the terminating NUL if it
+/// doesn't find `c`.
+///
+/// @param str The string to search.
+/// @param c   The char to look for.
+/// @returns a pointer to the first instance of `c`, or to the NUL terminator
+///          if not found.
+REAL_FATTR_NONNULL_RET REAL_FATTR_NONNULL_ALL REAL_FATTR_PURE
+static inline char *xstrchrnul(const char *str, char c)
+{
+  char *p = strchr(str, c);
+  return p ? p : (char *)(str + strlen(str));
+}
