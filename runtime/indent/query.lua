12c9791e0fef7ee0d6cf6d3b828caa488d6347ea
zeertzjq <zeertzjq@outlook.com>
2024-07-03 15:24:12 +0800

    fix(runtime): stop treesitter highlight in b:undo_ftplugin (#29533)
    It seems that nvim-treesitter stops treesitter highlight when changing
    filetype, so it makes sense for builtin ftplugins to do this as well.
    
    Use :call and v:lua here to allow separation with '|'.

--- runtime/indent/query.lua
+++ runtime/indent/query.lua
@@ -1,6 +1,6 @@
 -- Neovim indent file
 -- Language:	Treesitter query
--- Last Change:	2022 Mar 29
+-- Last Change:	2024 Jul 03
 
 -- it's a lisp!
-vim.cmd([[ runtime! indent/lisp.vim ]])
+vim.cmd([[runtime! indent/lisp.vim]])
