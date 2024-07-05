75d95e997ebe82ae22d2a6f963e6ea86d7d17e6b
zeertzjq <zeertzjq@outlook.com>
2024-07-05 15:59:09 +0800

    vim-patch:9.1.0518: initialize the random buffer can be improved
    Problem:  initialize the random buffer can be improved
    Solution: refactor init_srand() function, move machine-specific parts to
              os_mswin and os_unix, implement a fallback for Windows 10 and
              later (LemonBoy)
    
    closes: vim/vim#15125
    
    https://github.com/vim/vim/commit/9987fe8ca05e5d367fc54eb94a5a583138d2e1f8
    
    Co-authored-by: LemonBoy <thatlemon@gmail.com>

---
+++ src/nvim/os/random.c
@@ -0,0 +1,65 @@
+#include <stddef.h>
+#include <stdint.h>
+
+#include "nvim/os/fs.h"
+#include "nvim/os/os_defs.h"
+#include "nvim/os/random.h"
+#include "nvim/vim_defs.h"
+
+#ifdef MSWIN
+/// Fill the buffer "buf" with "len" random bytes.
+/// Returns FAIL if the OS PRNG is not available or something went wrong.
+int os_get_random(uint8_t *buf, size_t len)
+{
+  static int initialized = NOTDONE;
+  static HINSTANCE hInstLib;
+  static BOOL(WINAPI *pProcessPrng)(PBYTE, SIZE_T);
+
+  if (initialized == NOTDONE) {
+    hInstLib = LoadLibrary("bcryptprimitives.dll");
+    if (hInstLib != NULL) {
+      pProcessPrng = (void *)GetProcAddress(hInstLib, "ProcessPrng");
+    }
+    if (hInstLib == NULL || pProcessPrng == NULL) {
+      FreeLibrary(hInstLib);
+      initialized = FAIL;
+    } else {
+      initialized = OK;
+    }
+  }
+
+  if (initialized == FAIL) {
+    return FAIL;
+  }
+
+  // According to the documentation this call cannot fail.
+  pProcessPrng(buf, len);
+
+  return OK;
+}
+#else
+/// Fill the buffer "buf" with "len" random bytes.
+/// Returns FAIL if the OS PRNG is not available or something went wrong.
+int os_get_random(uint8_t *buf, size_t len)
+{
+  static int dev_urandom_state = NOTDONE;  // FAIL or OK once tried
+
+  if (dev_urandom_state == FAIL) {
+    return FAIL;
+  }
+
+  const int fd = os_open("/dev/urandom", O_RDONLY, 0);
+
+  // Attempt reading /dev/urandom.
+  if (fd < 0) {
+    dev_urandom_state = FAIL;
+  } else if (read(fd, buf, len) == (ssize_t)len) {
+    dev_urandom_state = OK;
+  } else {
+    dev_urandom_state = FAIL;
+    os_close(fd);
+  }
+
+  return dev_urandom_state;
+}
+#endif
---
+++ src/nvim/os/random.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <stddef.h>  // IWYU pragma: keep
+#include <stdint.h>  // IWYU pragma: keep
+
+#ifdef INCLUDE_GENERATED_DECLARATIONS
+# include "os/random.h.generated.h"
+#endif
