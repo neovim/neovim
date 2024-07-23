fa07a4bc67e663bfcd33922046b06c7dd9748206
Evgeni Chasnovski <evgeni.chasnovski@gmail.com>
2024-07-23 18:21:49 +0300

    docs(filetype): use correct pattern in `add()` example

--- runtime/lua/vim/filetype.lua
+++ runtime/lua/vim/filetype.lua
@@ -2431,7 +2431,7 @@ end
 ---     ['.*/etc/foo/.*%.conf'] = { 'dosini', { priority = 10 } },
 ---     -- A pattern containing an environment variable
 ---     ['${XDG_CONFIG_HOME}/foo/git'] = 'git',
----     ['README.(%a+)$'] = function(path, bufnr, ext)
+---     ['.*README.(%a+)'] = function(path, bufnr, ext)
 ---       if ext == 'md' then
 ---         return 'markdown'
 ---       elseif ext == 'rst' then
