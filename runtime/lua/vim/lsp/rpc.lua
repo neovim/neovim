4f1510f8221cb763e080cfd8185340d97c0cb3b1
Justin M. Keyes <justinkz@gmail.com>
2024-09-01 15:41:41 -0700

    Update runtime/lua/vim/lsp/rpc.lua

--- runtime/lua/vim/lsp/rpc.lua
+++ runtime/lua/vim/lsp/rpc.lua
@@ -632,7 +632,7 @@ end
 --- Return a function that can be passed to the `cmd` field for
 --- |vim.lsp.start_client()| or |vim.lsp.start()|.
 ---
----@param host_or_path string host (IP Address) to connect to or path to a pipe/domain socket
+---@param host_or_path string host (IP address) to connect to or path to a pipe/domain socket
 ---@param port integer? TCP port to connect to. If absent the first argument must be a pipe
 ---@return fun(dispatchers: vim.lsp.rpc.Dispatchers): vim.lsp.rpc.PublicClient
 function M.connect(host_or_path, port)
