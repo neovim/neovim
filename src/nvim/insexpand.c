28fa71d74312f9e2282aef4f6bf21930bd423c85
zeertzjq <zeertzjq@outlook.com>
2024-09-16 09:10:51 +0800

    vim-patch:9.1.0733: keyword completion does not work with fuzzy
    Problem:  keyword completion does not work with fuzzy
              (egesip)
    Solution: handle ctrl_x_mode_normal() specifically
              (glepnir)
    
    fixes: vim/vim#15412
    closes: vim/vim#15424
    
    https://github.com/vim/vim/commit/7cfe693f9bfa74690867e4d96c25f2205d0d13e4
    
    Co-authored-by: glepnir <glephunter@gmail.com>

--- src/nvim/insexpand.c
+++ src/nvim/insexpand.c
@@ -4602,6 +4602,7 @@ static int ins_compl_start(void)
     line = ml_get(curwin->w_cursor.lnum);
   }
 
+  bool in_fuzzy = get_cot_flags() & kOptCotFlagFuzzy;
   if (compl_status_adding()) {
     edit_submode_pre = _(" Adding");
     if (ctrl_x_mode_line_or_eval()) {
@@ -4615,6 +4616,9 @@ static int ins_compl_start(void)
       curbuf->b_p_com = old;
       compl_length = 0;
       compl_col = curwin->w_cursor.col;
+    } else if (ctrl_x_mode_normal() && in_fuzzy) {
+      compl_startpos = curwin->w_cursor;
+      compl_cont_status &= CONT_S_IPOS;
     }
   } else {
     edit_submode_pre = NULL;
