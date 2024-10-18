a19d598e8e8254260d6696c75e3193fd568c15b0
Yi Ming <ofseed@foxmail.com>
2024-10-18 17:04:30 +0800

    fix(lsp): avoid calling API functions on unsupported buffers

--- runtime/lua/vim/lsp/document_highlight.lua
+++ runtime/lua/vim/lsp/document_highlight.lua
@@ -274,10 +274,15 @@ function M.is_enabled(filter)
   end
 
   bufnr = bufnr == 0 and api.nvim_get_current_buf() or bufnr
-  if bufstates[bufnr].enabled == nil then
+  local bufstate = bufstates[bufnr]
+  if not bufstate then
+    return false
+  end
+
+  if bufstate.enabled == nil then
     return globalstate.enabled
   else
-    return bufstates[bufnr].enabled
+    return bufstate.enabled
   end
 end
 
@@ -305,10 +310,15 @@ function M.enable(enable, filter)
     end
   else
     bufnr = bufnr == 0 and api.nvim_get_current_buf() or bufnr
+    local bufstate = bufstates[bufnr]
+    if not bufstate then
+      return
+    end
+
     if enable == globalstate.enabled then
-      bufstates[bufnr].enabled = nil
+      bufstate.enabled = nil
     else
-      bufstates[bufnr].enabled = enable
+      bufstate.enabled = enable
     end
     refresh(bufnr)
   end
