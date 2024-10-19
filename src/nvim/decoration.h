3abd7ed4a4cdc5128c5e7e0d02c313585bd71745
vanaigr <vanaigranov@gmail.com>
2024-10-19 16:28:47 -0500

    perf: consider only active decorations when drawing lines

--- src/nvim/decoration.h
+++ src/nvim/decoration.h
@@ -34,6 +34,10 @@ typedef struct {
   int start_col;
   int end_row;
   int end_col;
+  int ordering;
+  DecorPriority priority;
+  bool owned;  ///< ephemeral decoration, free memory immediately
+  DecorRangeKind kind: 8;
   // next pointers MUST NOT be used, these are separate ranges
   // vt->next could be pointing to freelist memory at this point
   union {
@@ -46,9 +50,6 @@ typedef struct {
     } ui;
   } data;
   int attr_id;  ///< cached lookup of inl.hl_id if it was a highlight
-  bool owned;   ///< ephemeral decoration, free memory immediately
-  DecorPriority priority;
-  DecorRangeKind kind;
   /// Screen column to draw the virtual text.
   /// When -1, it should be drawn on the current screen line after deciding where.
   /// When -3, it may be drawn at a position yet to be assigned.
@@ -57,9 +58,23 @@ typedef struct {
   int draw_col;
 } DecorRange;
 
+typedef union {
+  DecorRange range;
+  int next_free_i;
+} DecorRangeSlot;
+
 typedef struct {
   MarkTreeIter itr[1];
-  kvec_t(DecorRange) active;
+  kvec_t(DecorRangeSlot) slots;
+  kvec_t(int) ranges_i;
+  /// Indices in [0; current_end) range of `ranges_i` point to ranges that start before the
+  /// current position and are sorted by priority and order of insertion.
+  int current_end;
+  /// Indices in [future_begin, kv_size(ranges_i)) range of `ranges_i` point to ranges that
+  /// start after the current position and are sorted by starting position.
+  int future_begin;
+  int free_slot_i;  ///< Index of the last freed slot.
+  int new_range_ordering;  ///< Index for keeping track of range insertion order.
   win_T *win;
   int top_row;
   int row;
@@ -76,11 +91,21 @@ typedef struct {
   bool running_decor_provider;
 } DecorState;
 
-EXTERN DecorState decor_state INIT( = { 0 });
+EXTERN DecorState decor_state INIT( = { .free_slot_i = -1 });
 // TODO(bfredl): These should maybe be per-buffer, so that all resources
 // associated with a buffer can be freed when the buffer is unloaded.
 EXTERN kvec_t(DecorSignHighlight) decor_items INIT( = KV_INITIAL_VALUE);
 
 #ifdef INCLUDE_GENERATED_DECLARATIONS
 # include "decoration.h.generated.h"
+# include "decoration.h.inline.generated.h"
 #endif
+
+static inline int decor_redraw_col(win_T *wp, int col, int win_col, bool hidden, DecorState *state)
+  FUNC_ATTR_ALWAYS_INLINE
+{
+  if (col <= state->col_until) {
+    return state->current;
+  }
+  return decor_redraw_col_impl(wp, col, win_col, hidden, state);
+}
