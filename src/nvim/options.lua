4c7f5032af6baca61764cde5122765b2ec684e4a
Kai Moschcau <kai.moschcau@blecon.de>
2024-10-29 15:36:58 +0100

    docs(options): shell-powershell #30969
    `-NonInteractive` at least somewhat hints to pwsh/powershell, that shell
    sessions created from :! are not interactive, though even that is not
    foolproof, because powershell is weird.
    `$PSStyle.OutputRendering='plaintext'` causes pwsh/powershell
    to omit ANSI escape sequences in its output.

--- src/nvim/options.lua
+++ src/nvim/options.lua
@@ -7111,7 +7111,7 @@ return {
         						*shell-powershell*
         To use PowerShell: >vim
         	let &shell = executable('pwsh') ? 'pwsh' : 'powershell'
-        	let &shellcmdflag = '-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues[''Out-File:Encoding'']=''utf8'';Remove-Alias -Force -ErrorAction SilentlyContinue tee;'
+        	let &shellcmdflag = '-NoLogo -NonInteractive -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues[''Out-File:Encoding'']=''utf8'';$PSStyle.OutputRendering=''plaintext'';Remove-Alias -Force -ErrorAction SilentlyContinue tee;'
         	let &shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
         	let &shellpipe  = '2>&1 | %%{ "$_" } | tee %s; exit $LastExitCode'
         	set shellquote= shellxquote=
