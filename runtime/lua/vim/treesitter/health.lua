a4c441e234725d4743d1cfb30182a724b931ca3e
Christian Clason <c.clason@uni-graz.at>
2024-11-02 10:40:44 +0100

    fix(health): better layout of vim.treesitter health check
    Problem: Long lists of available parsers make it hard to see WASM
    status.
    
    Solution: Add separate headings for "treesitter features" (ABI, WASM)
    and "treesitter parsers". Also add minimum supported ABI version.

--- runtime/lua/vim/treesitter/health.lua
+++ runtime/lua/vim/treesitter/health.lua
@@ -4,10 +4,21 @@ local health = vim.health
 
 --- Performs a healthcheck for treesitter integration
 function M.check()
+  health.start('Treesitter features')
+
+  health.info(
+    string.format(
+      'Treesitter ABI support: min %d, max %d',
+      vim.treesitter.minimum_language_version,
+      ts.language_version
+    )
+  )
+
+  local can_wasm = vim._ts_add_language_from_wasm ~= nil
+  health.info(string.format('WASM parser support: %s', tostring(can_wasm)))
+
+  health.start('Treesitter parsers')
   local parsers = vim.api.nvim_get_runtime_file('parser/*', true)
-
-  health.info(string.format('Nvim runtime ABI version: %d', ts.language_version))
-
   for _, parser in pairs(parsers) do
     local parsername = vim.fn.fnamemodify(parser, ':t:r')
     local is_loadable, err_or_nil = pcall(ts.language.add, parsername)
@@ -28,9 +39,6 @@ function M.check()
       )
     end
   end
-
-  local can_wasm = vim._ts_add_language_from_wasm ~= nil
-  health.info(string.format('Can load WASM parsers: %s', tostring(can_wasm)))
 end
 
 return M
