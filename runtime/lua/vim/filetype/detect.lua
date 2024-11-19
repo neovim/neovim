23ead4f2cb19b05d83f1c515b35bd5896ee77365
Christian Clason <c.clason@uni-graz.at>
2024-11-19 22:33:30 +0100

    vim-patch:9.1.0876: filetype: openCL files are not recognized
    Problem:  filetype: openCL files are not recognized
    Solution: detect '*.cl' files as opencl or lisp filetype,
              include a opencl syntax and filetype plugin (Wu, Zhenyu)
    
    closes: vim/vim#15825
    
    https://github.com/vim/vim/commit/e2c27ca8eff7cc8ec852b531d5a7f328a343a761
    
    Co-authored-by: Wu, Zhenyu <wuzhenyu@ustc.edu>

--- runtime/lua/vim/filetype/detect.lua
+++ runtime/lua/vim/filetype/detect.lua
@@ -181,6 +181,16 @@ function M.changelog(_, bufnr)
 end
 
 --- @type vim.filetype.mapfn
+function M.cl(_, bufnr)
+  local lines = table.concat(getlines(bufnr, 1, 4))
+  if lines:match('/%*') then
+    return 'opencl'
+  else
+    return 'lisp'
+  end
+end
+
+--- @type vim.filetype.mapfn
 function M.class(_, bufnr)
   -- Check if not a Java class (starts with '\xca\xfe\xba\xbe')
   if not getline(bufnr, 1):find('^\202\254\186\190') then
