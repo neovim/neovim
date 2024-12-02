d1982e9de6e6ac2449173c18868073c708ee654d
luukvbaal <luukvbaal@gmail.com>
2024-12-02 15:11:38 +0100

    fix(extmark): builtin completion can still affect nearby extmarks #31387
    Problem:
    Built-in completion can still affect nearby extmarks. #31384
    
    Solution:
    Restore extmarks when completion leader changes.
    
    (cherry picked from commit c7ec010ade0832e43c7a319ea69fae642771479d)

--- src/nvim/insexpand.c
+++ src/nvim/insexpand.c
@@ -1735,6 +1735,7 @@ static void ins_compl_new_leader(void)
   ins_compl_del_pum();
   ins_compl_delete();
   ins_bytes(compl_leader + get_compl_len());
+  restore_orig_extmarks();
   compl_used_match = false;
 
   if (compl_started) {
