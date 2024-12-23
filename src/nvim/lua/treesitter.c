neovim/neovim

commit 2a7d0ed6145bf3f8b139c2694563f460f829813a

refactor: iwyu #31637
Result of `make iwyu` (after some "fixups").

src/nvim/lua/treesitter.c
@@ -19,8 +19,8 @@
 # include <wasm.h>
 #endif
 
-#include "klib/kvec.h"
 #include "nvim/api/private/helpers.h"
+#include "nvim/ascii_defs.h"
 #include "nvim/buffer_defs.h"
 #include "nvim/globals.h"
 #include "nvim/lua/treesitter.h"
@@ -28,7 +28,6 @@
 #include "nvim/map_defs.h"
 #include "nvim/memline.h"
 #include "nvim/memory.h"
-#include "nvim/os/fs.h"
 #include "nvim/pos_defs.h"
 #include "nvim/strings.h"
 #include "nvim/types_defs.h"

commit 76e6fc6c0aedbaaff20f21e05313168bb91e7468

fix: fix broken wasmtime build
Regression from 2a7d0ed6145bf3f8b139c2694563f460f829813a, which removed
header that is only needed if wasmtime support is enabled. Prevent this
from happening again by wrapping the include in a `HAVE_WASMTIME` check.

src/nvim/lua/treesitter.c
@@ -17,6 +17,8 @@
 
 #ifdef HAVE_WASMTIME
 # include <wasm.h>
+
+# include "nvim/os/fs.h"
 #endif
 
 #include "nvim/api/private/helpers.h"