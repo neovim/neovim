548f19ccc3018aa563d25cf99a9ce70a56b115fe
Maria José Solano <majosolano99@gmail.com>
2025-01-05 09:51:51 -0800

    feat(health): close checkhealth buffers with q #31870

--- runtime/lua/vim/health.lua
+++ runtime/lua/vim/health.lua
@@ -408,6 +408,15 @@ function M._check(mods, plugin_names)
   vim.cmd.redraw()
   vim.print('')
 
+  -- Quit with 'q' inside healthcheck buffers.
+  vim.api.nvim_buf_set_keymap(
+    bufnr,
+    'n',
+    'q',
+    '<c-w>q',
+    { silent = true, noremap = true, nowait = true }
+  )
+
   -- Once we're done writing checks, set nomodifiable.
   vim.bo[bufnr].modifiable = false
 end
