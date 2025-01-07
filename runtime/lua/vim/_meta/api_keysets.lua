5dfbfb4c50a1c9d97b4083e1aed6bb6e743c6b6e
Will Hopkins <willothyh@gmail.com>
2025-01-07 10:28:15 -0800

    docs: make doc

--- runtime/lua/vim/_meta/api_keysets.lua
+++ runtime/lua/vim/_meta/api_keysets.lua
@@ -440,6 +440,11 @@ error('Cannot require a meta file')
 --- @class vim.api.keyset.tabpage_config
 --- @field after? integer
 
+--- @class vim.api.keyset.tabpage_config
+--- @field layout? any[]
+
+--- @class vim.api.keyset.tabpage_get
+
 --- @class vim.api.keyset.user_command
 --- @field addr? any
 --- @field bang? boolean
