d14ed675e615125eb156fe0f9eb290d4f3756d44
dundargoc <gocdundar@gmail.com>
2025-01-12 20:02:23 +0100

    fixup: quick update, squash later [skip ci]

--- scripts/bump_deps.lua
+++ scripts/bump_deps.lua
@@ -7,6 +7,28 @@ local _trace = false
 local required_branch_prefix = 'bump-'
 local commit_prefix = 'build(deps): '
 
+local repos = {
+  'LuaJIT/LuaJIT',
+  'libuv/libuv',
+  'luvit/luv',
+  'neovim/unibilium',
+  'JuliaStrings/utf8proc',
+  'tree-sitter/tree-sitter',
+  'tree-sitter/tree-sitter-c',
+  'tree-sitter-grammars/tree-sitter-lua',
+  'tree-sitter-grammars/tree-sitter-vim',
+  'neovim/tree-sitter-vimdoc',
+  'tree-sitter-grammars/tree-sitter-query',
+  'tree-sitter-grammars/tree-sitter-markdown',
+  'bytecodealliance/wasmtime',
+  'uncrustify/uncrustify',
+}
+
+local dependency_table = {} --- @type table<string, string>
+for _, repo in pairs(repos) do
+  dependency_table[vim.fs.basename(repo)] = repo
+end
+
 -- Print message
 local function p(s)
   vim.cmd('set verbose=1')
@@ -56,72 +78,14 @@ local deps_file = nvim_src_dir .. '/' .. 'cmake.deps/deps.txt'
 local temp_dir = nvim_src_dir .. '/tmp'
 run({ 'mkdir', '-p', temp_dir })
 
-local function get_dependency(dependency_name)
-  local dependency_table = {
-    ['luajit'] = {
-      repo = 'LuaJIT/LuaJIT',
-      symbol = 'LUAJIT',
-    },
-    ['libuv'] = {
-      repo = 'libuv/libuv',
-      symbol = 'LIBUV',
-    },
-    ['luv'] = {
-      repo = 'luvit/luv',
-      symbol = 'LUV',
-    },
-    ['unibilium'] = {
-      repo = 'neovim/unibilium',
-      symbol = 'UNIBILIUM',
-    },
-    ['utf8proc'] = {
-      repo = 'JuliaStrings/utf8proc',
-      symbol = 'UTF8PROC',
-    },
-    ['tree-sitter'] = {
-      repo = 'tree-sitter/tree-sitter',
-      symbol = 'TREESITTER',
-    },
-    ['tree-sitter-c'] = {
-      repo = 'tree-sitter/tree-sitter-c',
-      symbol = 'TREESITTER_C',
-    },
-    ['tree-sitter-lua'] = {
-      repo = 'tree-sitter-grammars/tree-sitter-lua',
-      symbol = 'TREESITTER_LUA',
-    },
-    ['tree-sitter-vim'] = {
-      repo = 'tree-sitter-grammars/tree-sitter-vim',
-      symbol = 'TREESITTER_VIM',
-    },
-    ['tree-sitter-vimdoc'] = {
-      repo = 'neovim/tree-sitter-vimdoc',
-      symbol = 'TREESITTER_VIMDOC',
-    },
-    ['tree-sitter-query'] = {
-      repo = 'tree-sitter-grammars/tree-sitter-query',
-      symbol = 'TREESITTER_QUERY',
-    },
-    ['tree-sitter-markdown'] = {
-      repo = 'tree-sitter-grammars/tree-sitter-markdown',
-      symbol = 'TREESITTER_MARKDOWN',
-    },
-    ['wasmtime'] = {
-      repo = 'bytecodealliance/wasmtime',
-      symbol = 'WASMTIME',
-    },
-    ['uncrustify'] = {
-      repo = 'uncrustify/uncrustify',
-      symbol = 'UNCRUSTIFY',
-    },
-  }
-  local dependency = dependency_table[dependency_name]
-  if dependency == nil then
-    p('Not a dependency: ' .. dependency_name)
-    die()
-  end
-  dependency.name = dependency_name
-  return dependency
+local function get_dependency(name_preformat)
+  -- Allow all variations of tree-sitter, treesitter, tree_sitter_c, tree_sitter-c etc
+  local name = string.gsub(name_preformat, 'treesitter', 'tree-sitter'):gsub('_', '-')
+  local repo = dependency_table[name]
+
+  local symbol = name:gsub('tree%-sitter', 'treesitter'):gsub('%-', '_'):upper()
+
+  return name, repo, symbol
 end
 
 local function get_gh_commit_sha(repo, ref)
@@ -143,7 +107,7 @@ local function get_archive_info(repo, ref)
   local archive_path = temp_dir .. '/' .. archive_name
   local archive_url = 'https://github.com/' .. repo .. '/archive/' .. archive_name
 
-  vim.fs.rm(archive_path, {force = true})
+  vim.fs.rm(archive_path, { force = true })
   run_die(
     { 'curl', '-sL', archive_url, '-o', archive_path },
     'Failed to download archive from GitHub'
@@ -184,18 +148,18 @@ local function verify_branch(branch_suffix)
   end
 end
 
-local function update_deps_file_wrapper(dependency, archive, comment)
-  verify_branch(dependency.name)
+local function update_deps_file_wrapper(name, symbol, archive, comment)
+  verify_branch(name)
 
-  p('Updating ' .. dependency.name .. ' to ' .. archive.url .. '\n')
-  update_deps_file(dependency.symbol, 'URL', archive.url:gsub('/', '\\/'))
-  update_deps_file(dependency.symbol, 'SHA256', archive.sha)
+  p('Updating ' .. name .. ' to ' .. archive.url .. '\n')
+  update_deps_file(symbol, 'URL', archive.url:gsub('/', '\\/'))
+  update_deps_file(symbol, 'SHA256', archive.sha)
   run_die({
     'git',
     'commit',
     deps_file,
     '-m',
-    commit_prefix .. 'bump ' .. dependency.name .. ' to ' .. comment,
+    commit_prefix .. 'bump ' .. name .. ' to ' .. comment,
   }, 'git failed to commit')
 end
 
@@ -205,14 +169,14 @@ local function short_commit(commit)
 end
 
 local function ref(dependency_name, _ref)
-  local dependency = assert(get_dependency(dependency_name))
+  local name, repo, symbol = get_dependency(dependency_name)
   run_die(
     { 'git', 'diff', '--quiet', 'HEAD', '--', deps_file },
     deps_file .. ' has uncommitted changes'
   )
-  local commit_sha = get_gh_commit_sha(dependency.repo, _ref)
-  local archive = get_archive_info(dependency.repo, commit_sha)
-  update_deps_file_wrapper(dependency, archive, short_commit(_ref))
+  local commit_sha = get_gh_commit_sha(repo, _ref)
+  local archive = get_archive_info(repo, commit_sha)
+  update_deps_file_wrapper(name, symbol, archive, short_commit(_ref))
 end
 
 local function usage()
@@ -235,7 +199,7 @@ local function usage()
         --head                bump to a current head.
 
         <dependency> is one of:
-        "luajit", "libuv", "luv", "tree-sitter"
+        "luajit", "libuv", "luv", "treesitter"
   ]=]):format(this_script, this_script, this_script, this_script, this_script, this_script))
 end
 
