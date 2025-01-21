5470d60f51c0af10b56fbdc02a769896e21899be
Gregory Anders <greg@gpanders.com>
2025-01-21 09:50:44 -0600

    fix(tui): ensure all pending escape sequences are processed before exiting
    Neovim disables a number of terminal modes when it exits, some of which
    cause the terminal to send asynchronous events to Neovim. It's possible
    that Neovim exits before the terminal has received and processed all of
    the sequences to disable these modes, causing the terminal to emit one
    of these asynchronous sequences after Neovim has already exited. If this
    happens, then the sequence is received by the user's shell (or some
    other program that is not Neovim).
    
    When Neovim exits, it now emits a Device Attributes request (DA1)
    after disabling all of the different modes. When the terminal responds
    to this request we know that it has already received all of our other
    sequences disabling the other modes. At that point, it should not be
    emitting any further asynchronous sequences. This means the process of
    exiting Neovim is now asynchronous as well since it depends on receiving
    the DA1 response from the terminal.

--- src/nvim/tui/input.c
+++ src/nvim/tui/input.c
@@ -643,24 +643,17 @@ static void handle_unknown_csi(TermInput *input, const TermKeyKey *key)
     switch (initial) {
     case '?':
       // Kitty keyboard protocol query response.
-      if (input->waiting_for_kkp_response) {
-        input->waiting_for_kkp_response = false;
-        input->key_encoding = kKeyEncodingKitty;
-        tui_set_key_encoding(input->tui_data);
-      }
-
+      input->key_encoding = kKeyEncodingKitty;
       break;
     }
     break;
   case 'c':
     switch (initial) {
     case '?':
-      // Primary Device Attributes response
-      if (input->waiting_for_kkp_response) {
-        input->waiting_for_kkp_response = false;
-
-        // Enable the fallback key encoding (if any)
-        tui_set_key_encoding(input->tui_data);
+      // Primary Device Attributes (DA1) response
+      if (input->callbacks.primary_device_attr) {
+        input->callbacks.primary_device_attr(input->tui_data);
+        input->callbacks.primary_device_attr = NULL;
       }
 
       break;
