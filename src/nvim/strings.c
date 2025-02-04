1deb580977f4144ff3e4704765d13684e84405c5
zeertzjq <zeertzjq@outlook.com>
2025-02-05 07:06:33 +0800

    vim-patch:9.1.1076: vim_strnchr() is strange and unnecessary (#32327)
    Problem:  vim_strnchr() is strange and unnecessary (after v9.1.1009)
    Solution: Remove vim_strnchr() and use memchr() instead.  Also remove a
              comment referencing an #if that is no longer present.
    
    vim_strnchr() is strange in several ways:
    - It's named like vim_strchr(), but unlike vim_strchr() it doesn't
      support finding a multibyte char.
    - Its logic is similar to vim_strbyte(), but unlike vim_strbyte() it
      uses char instead of char_u.
    - It takes a pointer as its size argument, which isn't convenient for
      all its callers.
    - It allows embedded NULs, unlike other "strn*" functions which stop
      when encountering a NUL byte.
    
    In comparison, memchr() also allows embedded NULs, and it converts bytes
    in the string to (unsigned char).
    
    closes: vim/vim#16579
    
    https://github.com/vim/vim/commit/34e1e8de91ff4a8922d454e3147ea425784aa0a0

--- src/nvim/strings.c
+++ src/nvim/strings.c
@@ -498,20 +498,6 @@ char *vim_strchr(const char *const string, const int c)
   }
 }
 
-// Sized version of strchr that can handle embedded NULs.
-// Adjusts n to the new size.
-char *strnchr(const char *p, size_t *n, int c)
-{
-  while (*n > 0) {
-    if (*p == c) {
-      return (char *)p;
-    }
-    p++;
-    (*n)--;
-  }
-  return NULL;
-}
-
 // Sort an array of strings.
 
 static int sort_compare(const void *s1, const void *s2)
