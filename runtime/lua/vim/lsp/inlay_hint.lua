6b1da37ff942600a2bd346ea99d4d23416f48ea9
Yi Ming <ofseed@foxmail.com>
2025-02-14 22:52:56 +0800

    fix(lsp): reset the applied hints when processing the `refresh` request

--- runtime/lua/vim/lsp/inlay_hint.lua
+++ runtime/lua/vim/lsp/inlay_hint.lua
@@ -94,6 +94,10 @@ function M.on_refresh(err, _, ctx)
   for _, bufnr in ipairs(vim.lsp.get_buffers_by_client_id(ctx.client_id)) do
     for _, winid in ipairs(api.nvim_list_wins()) do
       if api.nvim_win_get_buf(winid) == bufnr then
+        local bufstate = bufstates[bufnr]
+        if bufstate then
+          bufstates[bufnr].applied = {}
+        end
         util._refresh(ms.textDocument_inlayHint, { bufnr = bufnr })
       end
     end
