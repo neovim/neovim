e76643ca98a19ab2e11e5cdd554616e174866c77
zeertzjq <zeertzjq@outlook.com>
2025-02-16 10:22:54 +0800

    typo:

--- src/nvim/api/extmark.c
+++ src/nvim/api/extmark.c
@@ -445,7 +445,7 @@ Array nvim_buf_get_extmarks(Buffer buffer, Integer ns_id, Object start, Object e
 ///                   placed below the buffer line containing the mark.
 ///
 ///               - virt_lines_above: place virtual lines above instead.
-///               - virt_lines_leftcol: Place extmarks in the leftmost
+///               - virt_lines_leftcol: Place virtual lines in the leftmost
 ///                                     column of the window, bypassing
 ///                                     sign and number columns.
 ///
