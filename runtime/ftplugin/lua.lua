273a537aec8df0c9bc86c4eb82048db84eb40ba4
brianhuster <phambinhanctb2004@gmail.com>
2025-02-17 09:36:46 +0700

    feat(ftplugin): set 'omnifunc' of Lua to 'v:lua.vim.lua_omnifunc'
    Problem:
    - Many other ftplugin have defined 'omnifunc', but the Lua one doesn't
      define one, even though there is `vim.lua_omnifunc()`
    - Users may want "stupid" completion to fix Lua config with
      `nvim --clean` in case they breaks it
    
    Solution:
    Set 'omnifunc' to 'v:lua.vim.lua_omnifunc' in ftplugin/lua.lua

--- runtime/ftplugin/lua.lua
+++ runtime/ftplugin/lua.lua
@@ -1,4 +1,7 @@
 -- use treesitter over syntax
 vim.treesitter.start()
 
-vim.b.undo_ftplugin = (vim.b.undo_ftplugin or '') .. '\n call v:lua.vim.treesitter.stop()'
+vim.bo.omnifunc = 'v:lua.vim.lua_omnifunc'
+
+vim.b.undo_ftplugin = (vim.b.undo_ftplugin or '')
+  .. '\n call v:lua.vim.treesitter.stop() \n setl omnifunc<'
