71f3bcf9ec7bcad9f1d728965a7066d6ee605c70
Justin M. Keyes <justinkz@gmail.com>
2025-02-23 08:04:06 -0800

    Revert "feat(ftplugin): set Lua 'omnifunc' to vim.lua_omnifunc #32491"
    This reverts commit f398e3a61abbf802b49867d2f533be1b0725c0d7.

--- runtime/ftplugin/lua.lua
+++ runtime/ftplugin/lua.lua
@@ -1,7 +1,4 @@
 -- use treesitter over syntax
 vim.treesitter.start()
 
-vim.bo.omnifunc = 'v:lua.vim.lua_omnifunc'
-
-vim.b.undo_ftplugin = (vim.b.undo_ftplugin or '')
-  .. '\n call v:lua.vim.treesitter.stop() \n setl omnifunc<'
+vim.b.undo_ftplugin = (vim.b.undo_ftplugin or '') .. '\n call v:lua.vim.treesitter.stop()'
