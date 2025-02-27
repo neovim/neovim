0300cef44e0250547168d4df6642f34f6410ca7a
Lewis Russell <lewis6991@gmail.com>
2025-02-27 12:05:23 +0000

    feat: add more information to _ts_inspect_language()

--- test/functional/treesitter/language_spec.lua
+++ test/functional/treesitter/language_spec.lua
@@ -63,7 +63,14 @@ describe('treesitter language API', function()
       return { keys, lang.fields, lang.symbols }
     end))
 
-    eq({ fields = true, symbols = true, _abi_version = true, _wasm = false }, keys)
+    eq({
+      fields = true,
+      symbols = true,
+      state_count = true,
+      supertypes = true,
+      _abi_version = true,
+      _wasm = false,
+    }, keys)
 
     local fset = {}
     for _, f in pairs(fields) do
