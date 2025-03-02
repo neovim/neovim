8e19f8993ff663706889401b50f0c085058629ad
brianhuster <phambinhanctb2004@gmail.com>
2025-03-02 18:33:47 +0700

    feat(ftplugin): set 'omnifunc' and 'foldexpr' of Lua
    Problem:
    - Many other ftplugin have defined 'omnifunc', but the Lua one doesn't
      define one, even though there is `vim.lua_omnifunc()`
    - Users may want "stupid" completion to fix Lua config with
      `nvim --clean` in case they breaks it
    - Nvim doesn't port Lua foldexpr from Vim
    
    Solution:
    - Set 'omnifunc' to 'v:lua.vim.lua_omnifunc' in ftplugin/lua.lua
    - Set 'foldexpr' to use treesitter

--- runtime/ftplugin/lua.lua
+++ runtime/ftplugin/lua.lua
@@ -1,4 +1,9 @@
 -- use treesitter over syntax
 vim.treesitter.start()
 
-vim.b.undo_ftplugin = (vim.b.undo_ftplugin or '') .. '\n call v:lua.vim.treesitter.stop()'
+vim.bo.omnifunc = 'v:lua.vim.lua_omnifunc'
+vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+
+vim.b.undo_ftplugin = (vim.b.undo_ftplugin or '')
+  .. '\n call v:lua.vim.treesitter.stop()'
+  .. '\n setl omnifunc< foldexpr<'
