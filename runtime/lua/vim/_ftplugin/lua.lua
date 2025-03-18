1369d86812a55d2a1a575299e05e75ea4a6a8461
Justin M. Keyes <justinkz@gmail.com>
2025-03-17 17:13:04 -0700

    test: cleanup ftplugin_spec.lua #32948
    Problem:
    - cannot run ftplugin_spec.lua by itself
    - test leaves foo/ dir
    
    Solution:
    - fix setup and teardown

--- runtime/lua/vim/_ftplugin/lua.lua
+++ runtime/lua/vim/_ftplugin/lua.lua
@@ -3,18 +3,12 @@ local M = {}
 --- @param module string
 ---@return string
 function M.includeexpr(module)
-  ---@param fname string
-  ---@return boolean
-  local function filereadable(fname)
-    return vim.fn.filereadable(fname) == 1
-  end
-
   local fname = module:gsub('%.', '/')
 
   local root = vim.fs.root(vim.api.nvim_buf_get_name(0), 'lua') or vim.fn.getcwd()
   for _, suf in ipairs { '.lua', '/init.lua' } do
     local path = vim.fs.joinpath(root, 'lua', fname .. suf)
-    if filereadable(path) then
+    if vim.uv.fs_stat(path) then
       return path
     end
   end
