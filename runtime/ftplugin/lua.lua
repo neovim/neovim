eb14aaf68abbc9490ab9e2f5a41d8891e9ae4fa5
phanium <91544758+phanen@users.noreply.github.com>
2025-03-18 08:59:48 +0800

    fix(runtime): E15: Invalid expression in lua file when `gf`
    Problem:
    after https://github.com/neovim/neovim/pull/32719, `gf` error in lua:
    ```
    E15: Invalid expression: "v:lua.require"vim._ftplugin.lua".includeexpr()"
    E447: Can't find file "vim._ftplugin.lua" in path
    ```
    
    Solution:
    * use single quote (no idea why there's two pair double quote in
      expression).
    * add missing `v:fname`.

--- runtime/ftplugin/lua.lua
+++ runtime/ftplugin/lua.lua
@@ -1,7 +1,7 @@
 -- use treesitter over syntax
 vim.treesitter.start()
 
-vim.bo.includeexpr = 'v:lua.require"vim._ftplugin.lua".includeexpr()'
+vim.bo.includeexpr = [[v:lua.require'vim._ftplugin.lua'.includeexpr(v:fname)]]
 vim.bo.omnifunc = 'v:lua.vim.lua_omnifunc'
 vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
 
