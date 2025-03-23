9516997eb0ad20146ddddb48ba48c905d512998c
Au. <acehinnnqru@gmail.com>
2025-03-24 07:10:42 +0800

    fix(paste): wrong '[ mark after pasting a big string (streamed chunks) #33025
    Problem
    Pasting a big string ("streamed paste" with multiple chunks) sets the '[
    mark to the edit from the last chunk, instead of the start of the paste.
    
    Solution:
    Set the '[ mark where the paste started, not where the last chunk was
    inserted.
    
    Note: `startpos == nil` is not equal to `phase == 1` because there may
    be some empty chunks pasted which won't arrive here (returned at code
    before).

--- runtime/lua/vim/_editor.lua
+++ runtime/lua/vim/_editor.lua
@@ -213,7 +213,7 @@ end
 vim.inspect = vim.inspect
 
 do
-  local tdots, tick, got_line1, undo_started, trailing_nl = 0, 0, false, false, false
+  local startpos, tdots, tick, got_line1, undo_started, trailing_nl = nil, 0, 0, false, false, false
 
   --- Paste handler, invoked by |nvim_paste()|.
   ---
@@ -328,7 +328,13 @@ do
       -- message when there are zero dots.
       vim.api.nvim_command(('echo "%s"'):format(dots))
     end
+    if startpos == nil then
+      startpos = vim.fn.getpos("'[")
+    else
+      vim.fn.setpos("'[", startpos)
+    end
     if is_last_chunk then
+      startpos = nil
       vim.api.nvim_command('redraw' .. (tick > 1 and '|echo ""' or ''))
     end
     return true -- Paste will not continue if not returning `true`.
