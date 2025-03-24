bf5b35042ab6ea5b75f41cb74da71b0d750e4eca
shadmansaleh <13149513+shadmansaleh@users.noreply.github.com>
2025-03-25 02:24:28 +0600

    fix: lint errors

--- test/functional/legacy/autocmd_option_spec.lua
+++ test/functional/legacy/autocmd_option_spec.lua
@@ -355,10 +355,19 @@ describe('au OptionSet', function()
 
     it('with string global-local (to window) option', function()
       local oldval = eval('&statusline')
-      local default_statusline = "%<%f %{%nvim_eval_statusline('%h%w%m%r', {'maxwidth': 30}).width > 0 ? '%h%w%m%r ' : ''%}%=%{% &showcmdloc == 'statusline' ? '%-10.S ' : '' %}%{% exists('b:keymap_name') ? '<'..b:keymap_name..'> ' : '' %}%{% &ruler ? ( &rulerformat == '' ? '%-14.(%l,%c%V%) %P' : &rulerformat ) : '' %}"
+      local default_statusline =
+        "%<%f %{%nvim_eval_statusline('%h%w%m%r', {'maxwidth': 30}).width > 0 ? '%h%w%m%r ' : ''%}%=%{% &showcmdloc == 'statusline' ? '%-10.S ' : '' %}%{% exists('b:keymap_name') ? '<'..b:keymap_name..'> ' : '' %}%{% &ruler ? ( &rulerformat == '' ? '%-14.(%l,%c%V%) %P' : &rulerformat ) : '' %}"
 
       command('set statusline=foo')
-      expected_combination({ 'statusline', oldval, oldval, default_statusline, 'foo', 'global', 'set' })
+      expected_combination({
+        'statusline',
+        oldval,
+        oldval,
+        default_statusline,
+        'foo',
+        'global',
+        'set',
+      })
 
       -- Note: v:option_old is the old global value for global-local options.
       -- but the old local value for all other kinds of options.
