44e9dfd34987e15485d40d2e8d4cdfff972d8733
SkohTV <qlpth+github@proton.me>
2025-03-25 11:25:02 +0100

    test(autowriteall): add test to check autowriteall behavior on deadly signals

--- test/functional/autocmd/signal_spec.lua
+++ test/functional/autocmd/signal_spec.lua
@@ -8,6 +8,8 @@ local fn = n.fn
 local next_msg = n.next_msg
 local is_os = t.is_os
 local skip = t.skip
+local read_file = t.read_file
+local feed = n.feed
 
 if skip(is_os('win'), 'Only applies to POSIX systems') then
   return
@@ -43,4 +45,71 @@ describe('autocmd Signal', function()
     posix_kill('USR1', fn.getpid())
     eq(nil, next_msg(500))
   end)
+
+end)
+
+
+
+describe('autowriteall on kill', function()
+  before_each(clear)
+
+  local function test_deadly_sig(signame, awa, should_write)
+
+    local testfile = 'Xtest_SIG' .. signame .. (awa and '_awa' or '_noawa')
+    local teststr = 'Testaaaaaaa'
+
+    if (awa) then
+      command('set awa')
+    end
+
+    command('edit ' .. testfile)
+    feed('i' .. teststr)
+    posix_kill(signame, fn.getpid())
+
+    eq(
+      (should_write and (teststr .. '\n') or nil),
+      read_file(testfile)
+    )
+  end
+
+  it('write if SIGHUP & awa on', function()
+    test_deadly_sig('HUP', true, true)
+  end)
+
+  it('write if SIGQUIT & awa on', function()
+    test_deadly_sig('QUIT', true, true)
+  end)
+
+  it('write if SIGTSTP & awa on', function()
+    test_deadly_sig('TSTP', true, true)
+  end)
+
+  it('dont write if SIGTERM & awa on', function()
+    test_deadly_sig('TERM', true, false)
+  end)
+
+  it('dont write if SIGKILL & awa on', function()
+    test_deadly_sig('KILL', true, false)
+  end)
+
+  it('dont write if SIGHUP & awa off', function()
+    test_deadly_sig('HUP', false, false)
+  end)
+
+  it('dont write if SIGQUIT & awa off', function()
+    test_deadly_sig('HUP', false, false)
+  end)
+
+  it('dont write if SIGTSTP & awa off', function()
+    test_deadly_sig('TSTP', false, false)
+  end)
+
+  it('dont write if SIGTERM & awa off', function()
+    test_deadly_sig('TERM', false, false)
+  end)
+
+  it('dont write if SIGKILL & awa off', function()
+    test_deadly_sig('KILL', false, false)
+  end)
+
 end)
