de33cb19b86f23c631076a0eac7bac419305ddc3
Alexej Kowalew <616b2f@gmail.com>
2025-03-25 21:53:29 +0100

    fix: and another one

--- test/functional/testnvim.lua
+++ test/functional/testnvim.lua
@@ -666,7 +666,7 @@ function M.set_shell_powershell(fake)
   M.exec([[
     let &shell = ']] .. shell .. [['
     set shellquote= shellxquote=
-    let &shellcmdflag = '-NoLogo -NoProfile -NoNewWindow -Wait -NonInteractive -ExecutionPolicy RemoteSigned -Command '
+    let &shellcmdflag = '-NoLogo -NoProfile -Wait -NonInteractive -ExecutionPolicy RemoteSigned -Command '
     let &shellcmdflag .= '[Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();'
     let &shellcmdflag .= '$PSDefaultParameterValues[''*:Encoding'']=''utf8'';'
     let &shellcmdflag .= ']] .. cmd .. [['
