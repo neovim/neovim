b207942fea2bfb78f5d4886ddcf0d9cd1e37c787
Justin M. Keyes <justinkz@gmail.com>
2025-03-30 12:15:49 -0700

    Update runtime/lua/vim/_defaults.lua

--- runtime/lua/vim/_defaults.lua
+++ runtime/lua/vim/_defaults.lua
@@ -625,7 +625,7 @@ do
       vim.keymap.set({ 'n', 'x' }, '[[', function()
         jump_to_prompt(nvim_terminal_prompt_ns, 0, args.buf, -vim.v.count1)
       end, { buffer = args.buf, desc = 'Jump [count] shell prompts backward' })
-      vim.keymap.set({ 'n', 'v' }, ']]', function()
+      vim.keymap.set({ 'n', 'x' }, ']]', function()
         jump_to_prompt(nvim_terminal_prompt_ns, 0, args.buf, vim.v.count1)
       end, { buffer = args.buf, desc = 'Jump [count] shell prompts forward' })
     end,
