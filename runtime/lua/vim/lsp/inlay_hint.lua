c4335b08208d12ee1d573d1444ab653d2401a12c
Yi Ming <ofseed@foxmail.com>
2025-04-11 18:45:57 +0800

    fix(lsp): completely clear all hints after being requested

--- runtime/lua/vim/lsp/inlay_hint.lua
+++ runtime/lua/vim/lsp/inlay_hint.lua
@@ -94,8 +94,12 @@ function M.on_refresh(err, _, ctx)
   for _, bufnr in ipairs(vim.lsp.get_buffers_by_client_id(ctx.client_id)) do
     for _, winid in ipairs(api.nvim_list_wins()) do
       if api.nvim_win_get_buf(winid) == bufnr then
-        if bufstates[bufnr] and bufstates[bufnr].enabled then
-          bufstates[bufnr].applied = {}
+        local bufstate = bufstates[bufnr]
+        if bufstate and bufstate.enabled then
+          bufstate.applied = {}
+          if bufstate.client_hints then
+            bufstate.client_hints[ctx.client_id] = {}
+          end
           util._refresh(ms.textDocument_inlayHint, { bufnr = bufnr })
         end
       end
