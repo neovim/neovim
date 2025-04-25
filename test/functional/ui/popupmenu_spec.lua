d9353bd44285a9a3abbe97410730fbf9a252aee3
zeertzjq <zeertzjq@outlook.com>
2025-04-25 22:51:25 +0800

    test(pum): fix passing incorrect grid (#33635)

--- test/functional/ui/popupmenu_spec.lua
+++ test/functional/ui/popupmenu_spec.lua
@@ -7243,7 +7243,7 @@ describe('builtin popupmenu', function()
         ## grid 8
           ^popup menu test                 |
         ]],
-          float_pos = { [4] = { -1, 'NW', 1, 1, 14, false, 250, 2, 1, 14 } },
+          float_pos = { [4] = { -1, 'NW', 2, 1, 14, false, 250, 2, 3, 19 } },
         }
       else
         no_sel_screen = {
@@ -7273,7 +7273,7 @@ describe('builtin popupmenu', function()
       if multigrid then
         for i = 1, 7 do
           local _, row, col = unpack(pos[i])
-          pos[i] = { 1, row - 2, col - 5 }
+          pos[i] = { 2, row - 2, col - 5 }
         end
       end
 
@@ -7393,7 +7393,7 @@ describe('builtin popupmenu', function()
         ## grid 8
                            tset unem pupo^p|
         ]],
-          float_pos = { [4] = { -1, 'NW', 1, 1, 12, false, 250, 2, 1, 12 } },
+          float_pos = { [4] = { -1, 'NW', 2, 1, 12, false, 250, 2, 3, 17 } },
         }
       else
         no_sel_screen = {
@@ -7423,7 +7423,7 @@ describe('builtin popupmenu', function()
       if multigrid then
         for i = 1, 7 do
           local _, row, col = unpack(pos[i])
-          pos[i] = { 1, row - 2, col - 5 }
+          pos[i] = { 2, row - 2, col - 5 }
         end
       end
 
