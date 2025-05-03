968947b3c340b904b7211f55d0395d81663f2043
PRIZ ;] <voxelprismatic@pm.me>
2025-05-03 13:34:25 -0500

    docs(lua): typing for vim.fn.winlayout #33817
    Problem:
    `any[]` means nothing, and the return value is not the same as what's
    documented in the comment (eg, Lua returns `{ "row", { { "leaf", 1000 },
    { "leaf", 1001 } } }`, not `{ "row", { "leaf", 1000, "leaf", 1001 } }`)
    
    Solution:
    Create two classes (vim.fn.winlayout.leaf and vim.fn.winlayout.branch)
    and one alias that links the two together.
    
    Also: Due to LuaLS limitations, there is an empty class,
    vim.fn.winlayout.empty
    
    Signed-Off-By: VoxelPrismatic <voxelprismatic@pm.me>
    (cherry picked from commit 902b689c4d57094cc8955f8274773a308b2e8f08)

--- src/nvim/eval.lua
+++ src/nvim/eval.lua
@@ -13239,7 +13239,7 @@ M.funcs = {
     ]=],
     name = 'winlayout',
     params = { { 'tabnr', 'integer' } },
-    returns = 'any[]',
+    returns = 'vim.fn.winlayout.ret',
     signature = 'winlayout([{tabnr}])',
   },
   winline = {
