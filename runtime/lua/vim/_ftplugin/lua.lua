824b16ba9c2a06d8ececa62cce8772d4da3ea366
brianhuster <phambinhanctb2004@gmail.com>
2025-05-06 14:47:30 +0700

    fix(ft-lua): make includeexpr work with non-Nvim-style Lua modules
    Closes #33862

--- runtime/lua/vim/_ftplugin/lua.lua
+++ runtime/lua/vim/_ftplugin/lua.lua
@@ -3,13 +3,15 @@ local M = {}
 --- @param module string
 ---@return string
 function M.includeexpr(module)
-  local fname = module:gsub('%.', '/')
+  module = module:gsub('%.', '/')
 
   local root = vim.fs.root(vim.api.nvim_buf_get_name(0), 'lua') or vim.fn.getcwd()
-  for _, suf in ipairs { '.lua', '/init.lua' } do
-    local path = vim.fs.joinpath(root, 'lua', fname .. suf)
-    if vim.uv.fs_stat(path) then
-      return path
+  for _, fname in ipairs { module, vim.fs.joinpath(root, 'lua', module) } do
+    for _, suf in ipairs { '.lua', '/init.lua' } do
+      local path = fname .. suf
+      if vim.uv.fs_stat(path) then
+        return path
+      end
     end
   end
 
