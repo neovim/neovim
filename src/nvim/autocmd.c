bbfed0da7dc8d2a4e4319fc9105d2be0591217a6
Sean Dewar <6256228+seandewar@users.noreply.github.com>
2025-05-09 23:17:10 +0100

    fix(api): nvim_get_option_value FileType autocmd handling
    Problem: nvim_get_option_value with "filetype" set silently returns incorrect
    defaults if autocommands are blocked, like when they're already running.
    
    Solution: Allow its FileType autocommands to nest. Also error if executing them
    fails, rather than silently return wrong defaults.
    
    Endless nesting from misbehaving scripts should be prevented by the recursion
    limit in apply_autocmds_group, which is 10.

--- src/nvim/autocmd.c
+++ src/nvim/autocmd.c
@@ -2729,12 +2729,13 @@ void do_autocmd_focusgained(bool gained)
   recursive = false;
 }
 
-void do_filetype_autocmd(buf_T *buf, bool force)
+/// @return Whether any FileType autocommands were executed.
+bool do_filetype_autocmd(buf_T *buf, bool force)
 {
   static int ft_recursive = 0;
 
   if (ft_recursive > 0 && !force) {
-    return;  // disallow recursion
+    return false;  // disallow recursion
   }
 
   int secure_save = secure;
@@ -2747,8 +2748,10 @@ void do_filetype_autocmd(buf_T *buf, bool force)
   buf->b_did_filetype = true;
   // Only pass true for "force" when it is true or
   // used recursively, to avoid endless recurrence.
-  apply_autocmds(EVENT_FILETYPE, buf->b_p_ft, buf->b_fname, force || ft_recursive == 1, buf);
+  bool ret
+    = apply_autocmds(EVENT_FILETYPE, buf->b_p_ft, buf->b_fname, force || ft_recursive == 1, buf);
   ft_recursive--;
 
   secure = secure_save;
+  return ret;
 }
