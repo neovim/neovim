d43da2be1c89016121bded843637c9be2f8212f1
Evan Hahn <me@evanhahn.com>
2025-05-11 16:41:47 +0000

    fix(lsp): fix error with `InsertReplaceEdit`s
    **Problem:**
    
    Some LSPs cause the following completion error (reformatted slightly):
    
        Error executing vim.schedule lua callback:
        .../runtime/lua/vim/lsp/completion.lua:373
        attempt to index field 'range' (a nil value)
    
    This is because an internal function assumes edits are either missing
    or of type `TextEdit`, but there's a third [possibility][0] that's not
    handled: the `InsertReplaceEdit`.
    
    This was previously reported in at least two issues:
    
    - <https://github.com/neovim/neovim/issues/33142>
    - <https://github.com/neovim/neovim/issues/33224>
    
    **Solution:**
    
    Don't assume the edit is a `TextEdit`. This implicitly handles
    `InsertReplaceEdit`s.
    
    Also, add a test case for this, which previously caused an error.
    
    (I recommend reviewing this with whitespace changes disabled.)
    
    [0]: https://github.com/neovim/neovim/blob/2c07428966a74c76003e00e2a37bf98eb8802c93/runtime/lua/vim/lsp/_meta/protocol.lua#L1099

--- runtime/lua/vim/lsp/completion.lua
+++ runtime/lua/vim/lsp/completion.lua
@@ -370,7 +370,7 @@ end
 local function adjust_start_col(lnum, line, items, encoding)
   local min_start_char = nil
   for _, item in pairs(items) do
-    if item.textEdit and item.textEdit.range.start.line == lnum then
+    if item.textEdit and item.textEdit.range and item.textEdit.range.start.line == lnum then
       if min_start_char and min_start_char ~= item.textEdit.range.start.character then
         return nil
       end
