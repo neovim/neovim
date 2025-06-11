7bacc1853adf54b4efb1a156df010cf5b97db19c
Dheeraj Devaraj <dheeraj020602@gmail.com>
2025-06-11 17:42:33 +0530

    Updated documentation to better show support for invocation methods for functions in nvim_set_client_info()

--- src/nvim/api/vim.c
+++ src/nvim/api/vim.c
@@ -1536,8 +1536,9 @@ Array nvim_get_api_info(uint64_t channel_id, Arena *arena)
 ///     versions of Nvim, thus unknown keys are ignored. Clients
 ///     must only use keys defined in this or later versions of
 ///     Nvim):
-///     - "async"  if true, send as a notification. If false or unspecified,
-///                use a blocking request
+///     - "async" Indicates the preffered invocation mode. If true, send as 
+///               an async notification. If false or unspecified, use a 
+///               blocking request.
 ///     - "nargs" Number of arguments. Could be a single integer or an array
 ///                of two integers, minimum and maximum inclusive.
 ///
