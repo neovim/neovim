64b82005a4b0df543a98429a438f573bc03e577c
Thibaulltt <>
2025-06-18 16:02:06 +0200

    feat(diagnostics): add as_inlay to virtual_text
    This is a new parameter that allows the virtual_text to be set at the
    correct location in a line, and displayed as an inlay hint (visually).
    
    Previously, all diagnostics for a line would be grouped together at the
    user-defined position (based on the `virt_text_pos` parameter) and this
    would result in all diagnostics being bunched up on the start/end of the
    line, which is (in my opinion) not desired. If I enable virtual_text, I
    expect my issues to be reported where they happen.
    
    This parameter can be true, false or nil (default).
    
    If false/nil, the previously implemented behaviour is enabled: all
    diagnostics for a line are bunched up at the 0-th column and displayed
    using an extmark placed at that column.
    
    If true, it groups the diagnostics on each line based on the column
    where they appear, and later produces text chunks based on the
    diagnostic messages. These chunks are produced using the same logic as
    before (the `M._get_virt_text_chunks()` function).
    
    There are still tests to write :)

--- runtime/lua/vim/diagnostic.lua
+++ runtime/lua/vim/diagnostic.lua
@@ -205,6 +205,13 @@ end
 --- (default `nil`)
 --- @field current_line? boolean
 ---
+--- Show diagnostics at the location they report, as an inlay hint. If enabled, this means all
+--- diagnostics that span multiple lines will be reported at the end of their region. If `false` or
+--- `nil` (the default), diagnostics will be reported per-line, and follow your prefered position based
+--- on the `virt_text_pos` option.
+--- (default `nil`)
+--- @field as_inlay? boolean
+---
 --- Include the diagnostic source in virtual text. Use `'if_many'` to only
 --- show sources if there is more than one diagnostic source in the buffer.
 --- Otherwise, any truthy value means to always show the diagnostic source.
@@ -643,18 +650,25 @@ local floating_highlight_map = make_highlight_map('Floating')
 local sign_highlight_map = make_highlight_map('Sign')
 
 --- @param diagnostics vim.Diagnostic[]
+--- @param use_end_lnum? boolean
 --- @return table<integer,vim.Diagnostic[]>
-local function diagnostic_lines(diagnostics)
+local function diagnostic_lines(diagnostics, use_end_lnum)
   if not diagnostics then
     return {}
   end
 
   local diagnostics_by_line = {} --- @type table<integer,vim.Diagnostic[]>
   for _, diagnostic in ipairs(diagnostics) do
-    local line_diagnostics = diagnostics_by_line[diagnostic.lnum]
+    local line = diagnostic.lnum
+
+    if use_end_lnum and diagnostic.end_lnum ~= nil then
+      line = diagnostic.end_lnum
+    end
+
+    local line_diagnostics = diagnostics_by_line[line]
     if not line_diagnostics then
       line_diagnostics = {}
-      diagnostics_by_line[diagnostic.lnum] = line_diagnostics
+      diagnostics_by_line[line] = line_diagnostics
     end
     table.insert(line_diagnostics, diagnostic)
   end
@@ -1643,6 +1657,42 @@ M.handlers.underline = {
   end,
 }
 
+--- Returns the text to show for the given diagnostics, grouped by columns.
+---
+--- If `opts.as_inlay` is false or nil (default) then all diagnostics are
+--- grouped on the 0-th column, as was previously done.
+---
+--- @param line_diagnostics vim.Diagnostic[]
+--- @param opts vim.diagnostic.Opts.VirtualText
+--- @return table<integer, string[]> chunks_per_column
+local function get_virtual_text_chunks_per_column(line_diagnostics, opts)
+  local group_diags = {} --- @type table<integer, vim.Diagnostic[]>
+
+  if opts.as_inlay then
+    --- Gather diagnostics per-column:
+    for _, diag in ipairs(line_diagnostics) do
+      local col = diag.end_col or diag.col
+      local col_diags = group_diags[col]
+      if col_diags == nil then
+        col_diags = {}
+        group_diags[col] = col_diags
+      end
+      table.insert(col_diags, diag)
+    end
+  else
+    group_diags = { [0] = line_diagnostics }
+  end
+
+  local chunks = {} ---@type table<integer, string[]>
+
+  --- Convert them to chunks:
+  for col, diags in pairs(group_diags) do
+    chunks[col] = M._get_virt_text_chunks(diags, opts)
+  end
+
+  return chunks
+end
+
 --- @param namespace integer
 --- @param bufnr integer
 --- @param diagnostics table<integer, vim.Diagnostic[]>
@@ -1666,15 +1716,17 @@ local function render_virtual_text(namespace, bufnr, diagnostics, opts)
 
   for line, line_diagnostics in pairs(diagnostics) do
     if should_render(line) then
-      local virt_texts = M._get_virt_text_chunks(line_diagnostics, opts)
-      if virt_texts then
-        api.nvim_buf_set_extmark(bufnr, namespace, line, 0, {
-          hl_mode = opts.hl_mode or 'combine',
-          virt_text = virt_texts,
-          virt_text_pos = opts.virt_text_pos,
-          virt_text_hide = opts.virt_text_hide,
-          virt_text_win_col = opts.virt_text_win_col,
-        })
+      local virt_texts = get_virtual_text_chunks_per_column(line_diagnostics, opts)
+      if not vim.tbl_isempty(virt_texts) then
+        for column, text_chunks in pairs(virt_texts) do
+          api.nvim_buf_set_extmark(bufnr, namespace, line, column, {
+            hl_mode = opts.hl_mode or 'combine',
+            virt_text = text_chunks,
+            virt_text_pos = opts.virt_text_pos,
+            virt_text_hide = opts.virt_text_hide,
+            virt_text_win_col = opts.virt_text_win_col,
+          })
+        end
       end
     end
   end
@@ -1720,7 +1772,7 @@ M.handlers.virtual_text = {
 
     api.nvim_clear_autocmds({ group = ns.user_data.virt_text_augroup, buffer = bufnr })
 
-    local line_diagnostics = diagnostic_lines(diagnostics)
+    local line_diagnostics = diagnostic_lines(diagnostics, opts.virtual_text.as_inlay)
 
     if opts.virtual_text.current_line ~= nil then
       api.nvim_create_autocmd('CursorMoved', {
