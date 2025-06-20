dd0f8047bd5994877603eb88b182a0b2c34dd2a8
siddhantdev <agarwalsiddhant53@gmail.com>
2025-06-20 22:23:46 +0530

    feat(ui): :connect command
    Add the `:connect <address>` command which connects the currently
    running TUI to the server running at the given address.

--- src/nvim/ui_client.c
+++ src/nvim/ui_client.c
@@ -283,6 +283,62 @@ void ui_client_event_raw_line(GridLineEvent *g)
                (const schar_T *)grid_line_buf_char, grid_line_buf_attr);
 }
 
+/// When a "connect" UI event is received, its arguments are saved here when
+/// waiting for the server to exit.
+static Array connect_args;
+static bool connect_pending = false;
+static bool connect_successful = false;
+
+void ui_client_event_connect(Array args)
+{
+  api_free_array(connect_args);
+  connect_args = copy_array(args, NULL);
+  connect_pending = true;
+}
+
+/// Called when the current server has exited.
+bool ui_client_may_connect(void)
+{
+  if (!connect_pending) {
+    return false;
+  }
+  if (connect_successful) {
+    return true;
+  }
+  connect_pending = false;
+  if (connect_args.size < 1 || connect_args.items[0].type != kObjectTypeString) {
+    ELOG("Error handling UI event 'connect'");
+    api_free_array(connect_args);
+    connect_args = (Array)ARRAY_DICT_INIT;
+    return false;
+  }
+
+  char *server_address = connect_args.items[0].data.string.data;
+  // connect a channel to server at server_address
+  CallbackReader on_data = CALLBACK_READER_INIT;
+  const char *error = NULL;
+  bool is_tcp = strrchr(server_address, ':') ? true : false;
+  uint64_t chan = channel_connect(is_tcp, server_address, true, on_data, 50, &error);
+  if (error) {
+    api_free_array(connect_args);
+    connect_args = (Array)ARRAY_DICT_INIT;
+    ELOG("Error handling UI event 'connect': %s", error);
+    return false;
+  }
+
+  // attach the UI to the new channel
+  ui_client_channel_id = chan;
+  ui_client_is_remote = true;
+  ui_client_attach(tui_width, tui_height, tui_term, tui_rgb);
+
+  api_free_array(connect_args);
+  connect_args = (Array)ARRAY_DICT_INIT;
+
+  ILOG("Connected to channel %lu", ui_client_channel_id);
+  connect_successful = true;
+  return true;
+}
+
 /// When a "restart" UI event is received, its arguments are saved here when
 /// waiting for the server to exit.
 static Array restart_args = ARRAY_DICT_INIT;
