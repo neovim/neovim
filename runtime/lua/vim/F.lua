95a4956aeaed487221a730c1c5e600dfd405e30e
Justin M. Keyes <justinkz@gmail.com>
2025-06-24 15:37:35 +0200

    refactor(lua)!: deprecate vim.F
    Problem:
    `vim.F` has these problems:
    - does not follow naming conventions (should be lowercase)
    - has some questionable functions which are not used in this codebase,
      or which proliferate questionable patterns.
    
    Solution:
    - rename `if_nil` to `vim.f.nonnil`. or just drop it?
      - NB: `(a or b or …)` respects short-circuting evalulation, unlike `if_nil`.
    - drop `nil_wrap`, `ok_or_nil`
    - move `pack_len`/`unpack_len` to `table.pack`, `table.unpack` directly (like lua 5.2)
      ```
      table.pack = table.pack or function(...) return { n = select("#", ...), ... } end
      ```

--- runtime/lua/vim/F.lua
+++ runtime/lua/vim/F.lua
@@ -18,6 +18,7 @@ local F = {}
 ---@param ... T
 ---@return T
 function F.if_nil(...)
+  vim.deprecate('vim.F.if_nil', '(a or b or …)', '0.13')
   local nargs = select('#', ...)
   for i = 1, nargs do
     local v = select(i, ...)
@@ -28,8 +29,9 @@ function F.if_nil(...)
   return nil
 end
 
--- Use in combination with pcall
+---@deprecated
 function F.ok_or_nil(status, ...)
+  vim.deprecate('vim.F.ok_or_nil', 'actual error handling', '0.13')
   if not status then
     return
   end
@@ -45,8 +47,9 @@ function F.npcall(fn, ...)
   return F.ok_or_nil(pcall(fn, ...))
 end
 
---- Wrap a function to return nil if it fails, otherwise the value
+---@deprecated
 function F.nil_wrap(fn)
+  vim.deprecate('vim.F.nil_wrap', 'actual error handling', '0.13')
   return function(...)
     return F.npcall(fn, ...)
   end
