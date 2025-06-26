3eaa6c5a665999a9e374338137841937541ec33e
Maria José Solano <majosolano99@gmail.com>
2025-06-26 02:23:31 -0700

    fix(lsp): add `RequestFailed` error code constant #34645
    Also remove `serverErrorStart/End` as [the spec](https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#errorCodes)
    says that they're deprecated and don't even represent a real error code.

--- runtime/lua/vim/lsp/protocol.lua
+++ runtime/lua/vim/lsp/protocol.lua
@@ -162,14 +162,13 @@ local constants = {
     MethodNotFound = -32601,
     InvalidParams = -32602,
     InternalError = -32603,
-    serverErrorStart = -32099,
-    serverErrorEnd = -32000,
     ServerNotInitialized = -32002,
     UnknownErrorCode = -32001,
     -- Defined by the protocol.
     RequestCancelled = -32800,
     ContentModified = -32801,
     ServerCancelled = -32802,
+    RequestFailed = -32803,
   },
 
   -- Describes the content type that a client supports in various
