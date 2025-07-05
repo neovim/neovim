febe721caae39b36f9e4d5e1cfe6dd2e50fe2cd5
shadmansaleh <13149513+shadmansaleh@users.noreply.github.com>
2025-07-05 11:13:25 +0600

    try fixing test?

--- src/nvim/change.c
+++ src/nvim/change.c
@@ -156,8 +156,8 @@ void changed_internal(buf_T *buf)
 {
   if (!bt_prompt(buf)) {
     buf->b_changed = true;
-    buf->b_changed_invalid = true;
   }
+  buf->b_changed_invalid = true;
   ml_setflags(buf);
   redraw_buf_status_later(buf);
   redraw_tabline = true;
