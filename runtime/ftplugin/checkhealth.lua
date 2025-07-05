9f0fb57182bdd78281aad2077a122dcc6af168d5
glepnir <glephunter@gmail.com>
2025-07-05 13:58:07 +0800

    fix(health): prevent floating window from closing when opening TOC
    Problem: Health check floating window gets closed when pressing 'gO' to show TOC because LSP floating preview system auto-closes on BufEnter events triggered by :lopen.
    
    Solution: Temporarily disable BufEnter event for the current window during TOC operations and adjust window layout to prevent overlap.

--- runtime/ftplugin/checkhealth.lua
+++ runtime/ftplugin/checkhealth.lua
@@ -1,5 +1,22 @@
 vim.keymap.set('n', 'gO', function()
+  local win = vim.api.nvim_get_current_win()
+  vim.wo[win].eventignorewin = 'BufEnter'
   require('vim.treesitter._headings').show_toc()
+  vim.schedule(function()
+    if not vim.api.nvim_win_is_valid(win) then
+      return
+    end
+    local curwin = vim.api.nvim_get_current_win()
+    if curwin ~= win then
+      local cfg = vim.api.nvim_win_get_config(win)
+      local qfheight = vim.api.nvim_win_get_height(curwin)
+      if vim.o.lines - cfg.row - cfg.height < qfheight then
+        cfg.height = cfg.height - vim.api.nvim_win_get_height(curwin)
+        vim.api.nvim_win_set_config(win, cfg)
+      end
+    end
+    vim.wo[win].eventignorewin = ''
+  end)
 end, { buffer = 0, silent = true, desc = 'Show an Outline of the current buffer' })
 
 vim.keymap.set('n', ']]', function()
