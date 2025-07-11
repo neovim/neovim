c677cfe10e381cb841f93e6d5200ac330c92c9d7
brianhuster <phambinhanctb2004@gmail.com>
2025-07-12 00:56:20 +0700

    feat(env): remove VIM_VERSION_NODOT macro
    Problem:
    - The VIM_VERSION_NODOT macro maintained support for legacy Vim
      version-specific runtime directories (e.g., "vim82") which I believe
      have never been relevant for Neovim
    
    Solution:
    - Remove it
    - Rename `vim_version_dir()` to `vim_runtime_dir()`

--- src/nvim/os/env.c
+++ src/nvim/os/env.c
@@ -781,20 +781,15 @@ size_t expand_env_esc(const char *restrict srcp, char *restrict dst, int dstlen,
   return (size_t)(dst - dst_start);
 }
 
-/// Check if the directory "vimdir/<version>" or "vimdir/runtime" exists.
+/// Check if the directory "vimdir/runtime" exists.
 /// Return NULL if not, return its name in allocated memory otherwise.
 /// @param vimdir directory to test
-static char *vim_version_dir(const char *vimdir)
+static char *vim_runtime_dir(const char *vimdir)
 {
   if (vimdir == NULL || *vimdir == NUL) {
     return NULL;
   }
-  char *p = concat_fnames(vimdir, VIM_VERSION_NODOT, true);
-  if (os_isdir(p)) {
-    return p;
-  }
-  xfree(p);
-  p = concat_fnames(vimdir, RUNTIME_DIRNAME, true);
+  char *p = concat_fnames(vimdir, RUNTIME_DIRNAME, true);
   if (os_isdir(p)) {
     return p;
   }
@@ -946,7 +941,7 @@ char *vim_getenv(const char *name)
       && *default_vimruntime_dir == NUL) {
     kos_env_path = os_getenv("VIM");    // kos_env_path was NULL.
     if (kos_env_path != NULL) {
-      vim_path = vim_version_dir(kos_env_path);
+      vim_path = vim_runtime_dir(kos_env_path);
       if (vim_path == NULL) {
         vim_path = kos_env_path;
       } else {
@@ -981,10 +976,9 @@ char *vim_getenv(const char *name)
         vim_path_end = remove_tail(vim_path, vim_path_end, "doc");
       }
 
-      // for $VIM, remove "runtime/" or "vim54/", if present
+      // for $VIM, remove "runtime/", if present
       if (!vimruntime) {
         vim_path_end = remove_tail(vim_path, vim_path_end, RUNTIME_DIRNAME);
-        vim_path_end = remove_tail(vim_path, vim_path_end, VIM_VERSION_NODOT);
       }
 
       // remove trailing path separator
@@ -1012,7 +1006,7 @@ char *vim_getenv(const char *name)
       vim_path = xstrdup(default_vimruntime_dir);
     } else if (*default_vim_dir != NUL) {
       if (vimruntime
-          && (vim_path = vim_version_dir(default_vim_dir)) == NULL) {
+          && (vim_path = vim_runtime_dir(default_vim_dir)) == NULL) {
         vim_path = xstrdup(default_vim_dir);
       }
     }
