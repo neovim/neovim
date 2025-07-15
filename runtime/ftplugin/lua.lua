a610297e890c14ac4f4707ed64011cb7e9335dfd
brianhuster <phambinhanctb2004@gmail.com>
2025-07-15 23:24:45 +0700

    Revert "fix(runtime): set 'foldmethod' for Lua ftplugin #34929"
    This reverts commit 12276832ab67b16f795d78c72386ab28c6ee26c7.

--- runtime/ftplugin/lua.lua
+++ runtime/ftplugin/lua.lua
@@ -4,8 +4,7 @@ vim.treesitter.start()
 vim.bo.includeexpr = [[v:lua.require'vim._ftplugin.lua'.includeexpr(v:fname)]]
 vim.bo.omnifunc = 'v:lua.vim.lua_omnifunc'
 vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-vim.wo[0][0].foldmethod = 'expr'
 
 vim.b.undo_ftplugin = (vim.b.undo_ftplugin or '')
   .. '\n call v:lua.vim.treesitter.stop()'
-  .. '\n setl omnifunc< foldexpr< foldmethod< includeexpr<'
+  .. '\n setl omnifunc< foldexpr< includeexpr<'
