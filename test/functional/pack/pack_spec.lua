a7f5597695d8a31cbe898253f214b90dec84ca3a
Evgeni Chasnovski <evgeni.chasnovski@gmail.com>
2025-07-25 13:11:11 +0300

    test(pack): inline 'pack/testutil.lua' inside tested file

--- test/functional/pack/pack_spec.lua
+++ test/functional/pack/pack_spec.lua
@@ -1,32 +1,147 @@
 local t = require('test.testutil')
 local n = require('test.functional.testnvim')()
-local t_pack = require('test.functional.pack.testutil')
+
+local fn = n.fn
 
 local eq = t.eq
 local exec_lua = n.exec_lua
 
-local get_test_src = t_pack.get_test_src
-local get_pack_dir = t_pack.get_pack_dir
+-- Helpers ====================================================================
+-- Installed plugins ----------------------------------------------------------
+
+local function pack_get_dir()
+  return vim.fs.joinpath(fn.stdpath('data'), 'site', 'pack', 'core', 'opt')
+end
+
+local function pack_get_plug_path(plug_name)
+  return vim.fs.joinpath(pack_get_dir(), plug_name)
+end
+
+-- Test repos (to be installed) -----------------------------------------------
+
+local repos_dir = vim.fs.abspath('test/functional/lua/pack-test-repos')
+
+--- Map from repo name to its proper `src` used in plugin spec
+--- @type table<string,string>
+local repos_src = {}
+
+local function repo_get_path(repo_name)
+  return vim.fs.joinpath(repos_dir, repo_name)
+end
+
+local function repo_write_file(repo_name, rel_path, text, no_dedent, append)
+  local path = vim.fs.joinpath(repo_get_path(repo_name), rel_path)
+  fn.mkdir(vim.fs.dirname(path), 'p')
+  t.write_file(path, text, no_dedent, append)
+end
+
+--- @return vim.SystemCompleted
+local function system_sync(cmd, opts)
+  return n.exec_lua(function()
+    local obj = vim.system(cmd, opts)
+
+    if opts and opts.timeout then
+      -- Minor delay before calling wait() so the timeout uv timer can have a headstart over the
+      -- internal call to vim.wait() in wait().
+      vim.wait(10)
+    end
+
+    local res = obj:wait()
+
+    -- Check the process is no longer running
+    assert(not vim.api.nvim_get_proc(obj.pid), 'process still exists')
+
+    return res
+  end)
+end
+
+local function git_cmd(cmd, repo_name)
+  local git_cmd_prefix = {
+    'git',
+    '-c',
+    'gc.auto=0',
+    '-c',
+    'user.name=Marvim',
+    '-c',
+    'user.email=marvim@neovim.io',
+    '-c',
+    'init.defaultBranch=main',
+  }
+
+  cmd = vim.list_extend(git_cmd_prefix, cmd)
+  local cwd = repo_get_path(repo_name)
+  local sys_opts = { cwd = cwd, text = true, clear_env = true }
+  local out = system_sync(cmd, sys_opts)
+  if out.code ~= 0 then
+    error(out.stderr)
+  end
+  return (out.stdout:gsub('\n+$', ''))
+end
+
+local function init_test_repo(repo_name)
+  local path = repo_get_path(repo_name)
+  fn.mkdir(path, 'p')
+  repos_src[repo_name] = 'file://' .. path
+
+  git_cmd({ 'init' }, repo_name)
+end
+
+local function git_add_commit(msg, repo_name)
+  git_cmd({ 'add', '*' }, repo_name)
+  git_cmd({ 'commit', '-m', msg }, repo_name)
+end
+
+-- Common test repos ----------------------------------------------------------
+local function setup_repo_basic()
+  init_test_repo('basic')
+
+  local text = 'return { get = function() return "basic main" end }'
+  repo_write_file('basic', 'lua/basic.lua', text)
+  git_add_commit('Initial commit for "basic"', 'basic')
+
+  git_cmd({ 'checkout', '-b', 'feat-branch' }, 'basic')
+
+  text = 'return { get = function() return "basic feat-branch" end }'
+  repo_write_file('basic', 'lua/basic.lua', text)
+  git_add_commit('Add important feature', 'basic')
+end
+
+local function setup_repo_dummy()
+  init_test_repo('dummy')
+
+  repo_write_file('dummy', 'README', '# Dummy plugin')
+  git_add_commit('Initial commit for "dummy"', 'dummy')
+end
+
+-- Tests ======================================================================
 
 describe('vim.pack', function()
+  setup(function()
+    n.clear()
+    setup_repo_basic()
+    setup_repo_dummy()
+  end)
+
+  before_each(function()
+    n.clear()
+  end)
+
+  after_each(function()
+    vim.fs.rm(pack_get_dir(), { force = true, recursive = true })
+  end)
+
+  teardown(function()
+    vim.fs.rm(repos_dir, { force = true, recursive = true })
+  end)
+
   describe('add()', function()
-    before_each(function()
-      n.clear()
-      t_pack.setup_plug_basic()
-    end)
-
-    after_each(function()
-      t_pack.clean_repos()
-    end)
-
     it('makes plugin available immediately', function()
-      local plug_basic_src = get_test_src('plug_basic')
       local out = exec_lua(function()
-        vim.pack.add({ { src = plug_basic_src, version = 'feat-branch' } })
-        return require('plug_basic').get()
+        vim.pack.add({ { src = repos_src.basic, version = 'feat-branch' } })
+        return require('basic').get()
       end)
 
-      eq('basic feat branch', out)
+      eq('basic feat-branch', out)
     end)
 
     pending('reports errors after loading', function()
@@ -79,41 +194,25 @@ describe('vim.pack', function()
   end)
 
   describe('get()', function()
-    local plug_basic_spec =
-      { name = 'plug_basic', src = get_test_src('plug_basic'), version = 'feat-branch' }
-    local plug_dummy_spec =
-      { name = 'plug_dummy', src = get_test_src('plug_dummy'), version = 'main' }
-
-    -- TODO: It would be better to use `setup()` / `teardown()` for test plugins.
-    -- The issue is that there is no tested Nvim session at that point.
-    -- Modifying 'pack/testutil.lua' to work not in tested Nvim session also
-    -- has issues (like `vim.system` is not recognized).
-
     before_each(function()
-      n.clear()
-      t_pack.setup_plug_basic()
-      t_pack.setup_plug_dummy()
-
-      -- Ensure tested plugins are installed
+      -- Ensure tested plugins were installed in previous session
       exec_lua(function()
-        vim.pack.add({ plug_dummy_spec, plug_basic_spec })
+        vim.pack.add({ repos_src.dummy, repos_src.basic })
       end)
       n.clear()
-    end)
-
-    after_each(function()
-      t_pack.clean_repos()
     end)
 
     it('returns list of available plugins', function()
       exec_lua(function()
-        vim.pack.add({ plug_basic_spec })
+        vim.pack.add({ repos_src.basic })
       end)
 
       -- Should first return active plugins followed by non-active
+      local plug_basic_spec = { name = 'basic', src = repos_src.basic, version = 'feat-branch' }
+      local plug_dummy_spec = { name = 'dummy', src = repos_src.dummy, version = 'main' }
       local expected = {
-        { active = true, path = get_pack_dir('plug_basic'), spec = plug_basic_spec },
-        { active = false, path = get_pack_dir('plug_dummy'), spec = plug_dummy_spec },
+        { active = true, path = pack_get_plug_path('basic'), spec = plug_basic_spec },
+        { active = false, path = pack_get_plug_path('dummy'), spec = plug_dummy_spec },
       }
       local actual = exec_lua(function()
         return vim.pack.get()
