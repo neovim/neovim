78f4994627b7d9c3b85f30028fe55ff38024a39d
Sathya Pramodh <94102031+sathya-pramodh@users.noreply.github.com>
2025-08-03 11:20:45 +0530

    feat: ":restart [cmd]" can run commands #35045
    Problem:
    Not easy for a user to tell ":restart" to "run this command(s) after restarting".
    
    Solution:
    All ":restart" args following the optional +cmd arg are treated as a big cmdline that is passed as a "-c" CLI arg when restarting nvim.

--- src/nvim/ex_cmds.lua
+++ src/nvim/ex_cmds.lua
@@ -2248,7 +2248,7 @@ M.cmds = {
   },
   {
     command = 'restart',
-    flags = bit.bor(CMDARG, TRLBAR),
+    flags = bit.bor(CMDARG, EXTRA, NOTRLCOM),
     addr_type = 'ADDR_NONE',
     func = 'ex_restart',
   },
