20cc92a5e9a79c7dd19560f1c121443f4cbb38f0
Jaehwang Jung <tomtomjhj@gmail.com>
2025-08-17 02:21:29 +0900

    fix(api): call on_detach before wiping out the buffer
    Problem:
    Buffer-updates on_detach callback is invoked before buf_freeall(), which
    deletes autocmds of the buffer (via apply_autocmds(EVENT_BUFWIPEOUT,
    ...)). Due to this, buffer-local autocmds executed in on_detach (e.g.,
    LspDetach) are not actually invoked.
    
    Solution:
    Call buf_updates_unload() before buf_freeall().

--- src/nvim/buffer.c
+++ src/nvim/buffer.c
@@ -656,6 +656,10 @@ bool close_buffer(win_T *win, buf_T *buf, int action, bool abort_if_last, bool i
 
   buf->b_nwindows = nwindows;
 
+  // Disable buffer-updates for the current buffer.
+  // No need to check `unload_buf`: in that case the function returned above.
+  buf_updates_unload(buf, false);
+
   buf_freeall(buf, ((del_buf ? BFA_DEL : 0)
                     + (wipe_buf ? BFA_WIPE : 0)
                     + (ignore_abort ? BFA_IGNORE_ABORT : 0)));
@@ -677,10 +681,6 @@ bool close_buffer(win_T *win, buf_T *buf, int action, bool abort_if_last, bool i
   if (buf == curbuf && !is_curbuf) {
     return false;
   }
-
-  // Disable buffer-updates for the current buffer.
-  // No need to check `unload_buf`: in that case the function returned above.
-  buf_updates_unload(buf, false);
 
   if (win != NULL  // Avoid bogus clang warning.
       && win_valid_any_tab(win)
