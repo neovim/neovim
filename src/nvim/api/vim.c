63ec441a23ac867aac1e1d8504e33dbb80a9dd38
shadmansaleh <13149513+shadmansaleh@users.noreply.github.com>
2025-08-22 11:43:57 +0600

    update docs

--- src/nvim/api/vim.c
+++ src/nvim/api/vim.c
@@ -758,13 +758,13 @@ void nvim_set_vvar(String name, Object value, Error *err)
 ///               the (optional) name or ID `hl_group`.
 /// @param history  if true, add to |message-history|.
 /// @param opts  Optional parameters.
-///          - id: message-id for updating existing message.
+///          - id: message id for updating existing message.
 ///          - err: Treat the message like `:echoerr`. Sets `hl_group` to |hl-ErrorMsg| by default.
 ///          - kind: Set the |ui-messages| kind with which this message will be emitted.
 ///          - verbose: Message is controlled by the 'verbose' option. Nvim invoked with `-V3log`
 ///            will write the message to the "log" file instead of standard output.
-///          - title: The title for progress message.
-///          - status: Current status of the progress message. Can be
+///          - title: The title for {progress-message|.
+///          - status: Current status of the |progress-message|. Can be
 ///            one of the following values
 ///            - success: The progress item completed successfully
 ///            - running: The progress is ongoing
