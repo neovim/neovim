e7d98afff2ddf59d4c99a135f4416cee9ead7141
Green Committer <137760120+Greenie0701@users.noreply.github.com>
2025-08-29 22:01:36 +0530

    build: add arch-specific vswhere detection for vsdev cmd

--- .github/scripts/env.ps1
+++ .github/scripts/env.ps1
@@ -1,7 +1,12 @@
 # This script enables Developer Command Prompt
 # See https://github.com/microsoft/vswhere/wiki/Start-Developer-Command-Prompt#using-powershell
-$arch = if ($env:BUILD_ARCH -eq "x86_64") { "x64" } else { "arm64" } 
-$installationPath = vswhere.exe -latest -requiresAny -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -requires Microsoft.VisualStudio.Component.VC.Tools.arm64 -property installationPath
+if ($env:BUILD_ARCH -eq "arm64") {
+  $arch = "arm64"
+  $installationPath = vswhere.exe -latest -requires Microsoft.VisualStudio.Component.VC.Tools.arm64 -property installationPath
+} else {
+  $arch = "x64"
+  $installationPath = vswhere.exe -latest -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath
+}
 
 if ($installationPath) {
   & "${env:COMSPEC}" /s /c "`"$installationPath\Common7\Tools\vsdevcmd.bat`" -arch=$arch -no_logo && set" |
