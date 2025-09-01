e415fae42e18657f40ab54c89fde2071cda311ea
Wise Man <137760120+Greenie0701@users.noreply.github.com>
2025-09-01 11:05:48 +0530

    ci: Windows arm64 packages #35345
    Problem:
    Neovim binaries are not provided for Windows ARM64.
    GitHub Actions now offer native CI runners for Windows on ARM devices
    (windows-11-arm), enabling automated builds and testing.
    
    Solution:
    - Modified CMake packaging to include packaging windows on arm binaries.
    - Modified env script to install and initialize vs setup for arm64 arch. 
    
    Co-authored-by: Justin M. Keyes <justinkz@gmail.com>

--- .github/scripts/env.ps1
+++ .github/scripts/env.ps1
@@ -1,9 +1,17 @@
 # This script enables Developer Command Prompt
 # See https://github.com/microsoft/vswhere/wiki/Start-Developer-Command-Prompt#using-powershell
-$installationPath = vswhere.exe -latest -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath
-if ($installationPath -and (Test-Path "$installationPath\Common7\Tools\vsdevcmd.bat")) {
-  & "${env:COMSPEC}" /s /c "`"$installationPath\Common7\Tools\vsdevcmd.bat`" -arch=x64 -no_logo && set" | ForEach-Object {
-    $name, $value = $_ -split '=', 2
-    "$name=$value" >> $env:GITHUB_ENV
-  }
+if ($env:BUILD_ARCH -eq "arm64") {
+  $arch = "arm64"
+  $installationPath = vswhere.exe -latest -requires Microsoft.VisualStudio.Component.VC.Tools.arm64 -property installationPath
+} else {
+  $arch = "x64"
+  $installationPath = vswhere.exe -latest -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath
+}
+
+if ($installationPath) {
+  & "${env:COMSPEC}" /s /c "`"$installationPath\Common7\Tools\vsdevcmd.bat`" -arch=$arch -no_logo && set" |
+    ForEach-Object {
+      $name, $value = $_ -split '=', 2
+      "$name=$value" >> $env:GITHUB_ENV
+    }
 }
