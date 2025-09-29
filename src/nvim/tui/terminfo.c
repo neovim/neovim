6ff973b7aca848991b2913170848402f5270ca42
bfredl <bjorn.linse@gmail.com>
2025-09-29 13:18:59 +0200

    perf(tui): faster implementation of terminfo
    The processing of terminfo can be separated into two steps:
    
    1. The initialization of terminfo, which includes trying to find $TERM
       in a terminfo database file. As a fallback, common terminfo
       definitions are compiled in. After this, we apply a lot of ad-hoc
       patching to cover over limitations of terminfo.
    
    2. While processing updates from nvim, actually using terminfo strings
       and formatting them with runtime values. for this part, terminfo
       essentially is a hyper-enhanced version of snprintf(), including
       a sm0l stack based virtual machine which can manipulate the runtime
       parameters.
    
    This PR completely replaces libuniblium for step 2, with code
    vendored from NetBSD's libtermkey which has been adapted to use typesafe
    input parameters and to write into an output buffer in place.
    
    The most immedatiate effects is a performance enhancement of
    update_attrs() which is a very hot function when profiling the
    TUI-process part of screen updates. In a stupid microbenchmark
    (essentially calling nvim__screenshot over and over in a loop) this
    leads to a speedup of ca 1.5x for redrawing the screen on the TUI-side.
    What this means in practise when using nvim as a text editor is probably
    no noticible effect at all, and when reabusing nvim as idk a full screen
    RGB ASCII art rendrer maybe an increase from 72 to 75 FPS LMAO.
    
    As nice side-effect, reduce the usage of unibilium to initialization only..
    which will make it easier to remove, replace or make unibilium optional,
    adressing #31989. Specifically, the builtin fallback doesn't use
    unibilium at all, so a unibilium-free build is in principle possible
    if the builtin definitions are good enough.
    
    As a caveat, this PR doesn't touch libtermkey at all, which still has a
    conditional dependency on unibilium. This will be investigated in a
    follow-up PR
    
    Note: the check of $TERMCOLOR was moved from tui/tui.c to
    _defaults.lua in d7651b27d54a87c5783c0a579af11da9a16a39aa as we want to
    skip the logic in _defaults.lua if the env var was set. But there
    is no harm to check in the TUI code so that the TUI knows the right
    value before the TUI process dives into _it's_ terminfo
    shenanigans. Also this check is still strictly needed when the neovim TUI
    connects to a `--headless` server later, which came from a completely
    different $ENV-irionment, and thus we will miss the $TERMCOLOR value that
    the actual terminal would tell us.

--- src/nvim/tui/terminfo.c
+++ src/nvim/tui/terminfo.c
@@ -17,6 +17,12 @@
 # include "nvim/os/os.h"
 #endif
 
+typedef struct {
+  long nums[20];
+  char *strings[20];
+  size_t offset;
+} TPSTACK;
+
 #include "tui/terminfo.c.generated.h"
 
 bool terminfo_is_term_family(const char *term, const char *family)
@@ -59,85 +65,130 @@ bool terminfo_is_bsd_console(const char *term)
 /// @param term $TERM value
 /// @param[out,allocated] termname decided builtin 'term' name
 /// @return [allocated] terminfo structure
-static unibi_term *terminfo_builtin(const char *term, char **termname)
+const NeoTerminfo *terminfo_from_builtin(const char *term, char **termname)
 {
   if (terminfo_is_term_family(term, "xterm")) {
     *termname = xstrdup("builtin_xterm");
-    return unibi_from_mem((const char *)xterm_256colour_terminfo,
-                          sizeof xterm_256colour_terminfo);
+    return &xterm_256colour_terminfo;
   } else if (terminfo_is_term_family(term, "screen")) {
     *termname = xstrdup("builtin_screen");
-    return unibi_from_mem((const char *)screen_256colour_terminfo,
-                          sizeof screen_256colour_terminfo);
+    return &screen_256colour_terminfo;
   } else if (terminfo_is_term_family(term, "tmux")) {
     *termname = xstrdup("builtin_tmux");
-    return unibi_from_mem((const char *)tmux_256colour_terminfo,
-                          sizeof tmux_256colour_terminfo);
+    return &tmux_256colour_terminfo;
   } else if (terminfo_is_term_family(term, "rxvt")) {
     *termname = xstrdup("builtin_rxvt");
-    return unibi_from_mem((const char *)rxvt_256colour_terminfo,
-                          sizeof rxvt_256colour_terminfo);
+    return &rxvt_256colour_terminfo;
   } else if (terminfo_is_term_family(term, "putty")) {
     *termname = xstrdup("builtin_putty");
-    return unibi_from_mem((const char *)putty_256colour_terminfo,
-                          sizeof putty_256colour_terminfo);
+    return &putty_256colour_terminfo;
   } else if (terminfo_is_term_family(term, "linux")) {
     *termname = xstrdup("builtin_linux");
-    return unibi_from_mem((const char *)linux_16colour_terminfo,
-                          sizeof linux_16colour_terminfo);
+    return &linux_16colour_terminfo;
   } else if (terminfo_is_term_family(term, "interix")) {
     *termname = xstrdup("builtin_interix");
-    return unibi_from_mem((const char *)interix_8colour_terminfo,
-                          sizeof interix_8colour_terminfo);
+    return &interix_8colour_terminfo;
   } else if (terminfo_is_term_family(term, "iterm")
              || terminfo_is_term_family(term, "iterm2")
              || terminfo_is_term_family(term, "iTerm.app")
              || terminfo_is_term_family(term, "iTerm2.app")) {
     *termname = xstrdup("builtin_iterm");
-    return unibi_from_mem((const char *)iterm_256colour_terminfo,
-                          sizeof iterm_256colour_terminfo);
+    return &iterm_256colour_terminfo;
   } else if (terminfo_is_term_family(term, "st")) {
     *termname = xstrdup("builtin_st");
-    return unibi_from_mem((const char *)st_256colour_terminfo,
-                          sizeof st_256colour_terminfo);
+    return &st_256colour_terminfo;
   } else if (terminfo_is_term_family(term, "gnome")
              || terminfo_is_term_family(term, "vte")) {
     *termname = xstrdup("builtin_vte");
-    return unibi_from_mem((const char *)vte_256colour_terminfo,
-                          sizeof vte_256colour_terminfo);
+    return &vte_256colour_terminfo;
   } else if (terminfo_is_term_family(term, "cygwin")) {
     *termname = xstrdup("builtin_cygwin");
-    return unibi_from_mem((const char *)cygwin_terminfo,
-                          sizeof cygwin_terminfo);
+    return &cygwin_terminfo;
   } else if (terminfo_is_term_family(term, "win32con")) {
     *termname = xstrdup("builtin_win32con");
-    return unibi_from_mem((const char *)win32con_terminfo,
-                          sizeof win32con_terminfo);
+    return &win32con_terminfo;
   } else if (terminfo_is_term_family(term, "conemu")) {
     *termname = xstrdup("builtin_conemu");
-    return unibi_from_mem((const char *)conemu_terminfo,
-                          sizeof conemu_terminfo);
+    return &conemu_terminfo;
   } else if (terminfo_is_term_family(term, "vtpcon")) {
     *termname = xstrdup("builtin_vtpcon");
-    return unibi_from_mem((const char *)vtpcon_terminfo,
-                          sizeof vtpcon_terminfo);
+    return &vtpcon_terminfo;
   } else {
     *termname = xstrdup("builtin_ansi");
-    return unibi_from_mem((const char *)ansi_terminfo,
-                          sizeof ansi_terminfo);
+    return &ansi_terminfo;
   }
 }
 
-/// @param term $TERM value
-/// @param[out,allocated] termname decided builtin 'term' name
-/// @return [allocated] terminfo structure
-unibi_term *terminfo_from_builtin(const char *term, char **termname)
+static ssize_t unibi_find_ext_str(unibi_term *ut, const char *name)
 {
-  unibi_term *ut = terminfo_builtin(term, termname);
-  if (*termname == NULL) {
-    *termname = xstrdup("builtin_?");
+  size_t max = unibi_count_ext_str(ut);
+  for (size_t i = 0; i < max; i++) {
+    const char *n = unibi_get_ext_str_name(ut, i);
+    if (n && 0 == strcmp(n, name)) {
+      return (ssize_t)i;
+    }
   }
-  return ut;
+  return -1;
+}
+
+static int unibi_find_ext_bool(unibi_term *ut, const char *name)
+{
+  size_t max = unibi_count_ext_bool(ut);
+  for (size_t i = 0; i < max; i++) {
+    const char *n = unibi_get_ext_bool_name(ut, i);
+    if (n && 0 == strcmp(n, name)) {
+      return (int)i;
+    }
+  }
+  return -1;
+}
+
+bool terminfo_from_unibilium(NeoTerminfo *ti, char *termname)
+{
+  unibi_term *ut = unibi_from_term(termname);
+  if (!ut) {
+    return false;
+  }
+
+  ti->bce = unibi_get_bool(ut, unibi_back_color_erase);
+  ti->max_colors = unibi_get_num(ut, unibi_max_colors);
+  ti->lines = unibi_get_num(ut, unibi_lines);
+  ti->columns = unibi_get_num(ut, unibi_columns);
+
+  // Check for Tc or RGB
+  ti->has_Tc_or_RGB = false;
+  for (size_t i = 0; i < unibi_count_ext_bool(ut); i++) {
+    const char *n = unibi_get_ext_bool_name(ut, i);
+    if (n && (!strcmp(n, "Tc") || !strcmp(n, "RGB"))) {
+      ti->has_Tc_or_RGB = true;
+      break;
+    }
+  }
+
+  static const enum unibi_string uni_ids[] = {
+#define X(name) unibi_##name,
+    XLIST_TERMINFO_BUILTIN
+#undef X
+  };
+
+  for (size_t i = 0; i < ARRAY_SIZE(uni_ids); i++) {
+    const char *val = unibi_get_str(ut, uni_ids[i]);
+    ti->defs[i] = val ? xstrdup(val) : NULL;
+  }
+
+  static const char *uni_ext[] = {
+#define X(informal_name, terminfo_name) #terminfo_name,
+    XLIST_TERMINFO_EXT
+#undef X
+  };
+
+  for (size_t i = 0; i < ARRAY_SIZE(uni_ext); i++) {
+    ssize_t val = unibi_find_ext_str(ut, uni_ext[i]);
+    ti->defs[kTermExtOffset + i] = val >= 0 ? xstrdup(unibi_get_ext_str(ut, (size_t)val)) : NULL;
+  }
+
+  unibi_destroy(ut);
+  return true;
 }
 
 /// Dumps termcap info to the messages area.
@@ -220,4 +271,430 @@ String terminfo_info_msg(const unibi_term *ut, const char *termname)
   kv_push(data, NUL);
 
   return cbuf_as_string(data.items, data.size - 1);
+}
+
+// The implementation of terminfo_fmt() is based on NetBSD libterminfo,
+// with full license reproduced below
+
+// Copyright (c) 2009, 2011, 2013 The NetBSD Foundation, Inc.
+//
+// This code is derived from software contributed to The NetBSD Foundation
+// by Roy Marples.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+// 1. Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+// IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+// NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+// THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+// nvim modifications:
+// - use typesafe param args instead of va_args and piss
+// - caller provides the output buffer
+
+static int push(long num, char *string, TPSTACK *stack)
+{
+  if (stack->offset >= sizeof(stack->nums)) {
+    return -1;
+  }
+  stack->nums[stack->offset] = num;
+  stack->strings[stack->offset] = string;
+  stack->offset++;
+  return 0;
+}
+
+static int pop(long *num, char **string, TPSTACK *stack)
+{
+  if (stack->offset == 0) {
+    if (num) {
+      *num = 0;
+    }
+    if (string) {
+      *string = NULL;
+    }
+    return -1;
+  }
+  stack->offset--;
+  if (num) {
+    *num = stack->nums[stack->offset];
+  }
+  if (string) {
+    *string = stack->strings[stack->offset];
+  }
+  return 0;
+}
+
+static bool ochar(char **buf, const char *buf_end, int c)
+{
+  if (c == 0) {
+    c = 0200;
+  }
+  // Check we have space and a terminator
+  if (buf_end - *buf < 2) {
+    return 0;
+  }
+  *(*buf)++ = (char)c;
+  return 1;
+}
+
+static bool onum(char **buf, const char *buf_end, const char *fmt, int num, size_t len)
+{
+  const size_t LONG_STR_MAX = 21;
+  len = MAX(len, LONG_STR_MAX);
+
+  if (buf_end - *buf < (ssize_t)(len + 2)) {
+    return 0;
+  }
+  int l = snprintf(*buf, len + 2, fmt, num);
+  if (l == -1) {
+    return 0;
+  }
+  *buf += l;
+  return true;
+}
+
+size_t terminfo_fmt(char *buf_start, char *buf_end, const char *str, TPVAR *params)
+{
+  char c, fmt[64], *fp, *ostr;
+  long val, val2;
+  long dnums[26];  // dynamic variables a-z, not preserved
+  long snums[26];  // static variables a-z, not preserved EITHER HAHA
+  memset(dnums, 0, sizeof snums);
+  memset(snums, 0, sizeof snums);
+
+  char *buf = buf_start;
+
+  size_t l, width, precision, olen;
+  TPSTACK stack;
+  unsigned done, dot, minus;
+
+  memset(&stack, 0, sizeof(stack));
+  while ((c = *str++) != '\0') {
+    if (c != '%' || (c = *str++) == '%') {
+      if (c == '\0') {
+        break;
+      }
+      if (!ochar(&buf, buf_end, c)) {
+        return false;
+      }
+      continue;
+    }
+
+    // Handle formatting.
+    fp = fmt;
+    *fp++ = '%';
+    done = dot = minus = width = precision = 0;
+    val = 0;
+    while (done == 0 && (size_t)(fp - fmt) < sizeof(fmt)) {
+      switch (c) {
+      case 'c':
+      case 's':
+        *fp++ = c;
+        done = 1;
+        break;
+      case 'd':
+      case 'o':
+      case 'x':
+      case 'X':
+        *fp++ = 'l';
+        *fp++ = c;
+        done = 1;
+        break;
+      case '#':
+      case ' ':
+        *fp++ = c;
+        break;
+      case '.':
+        *fp++ = c;
+        if (dot == 0) {
+          dot = 1;
+          width = (size_t)val;
+        } else {
+          done = 2;
+        }
+        val = 0;
+        break;
+      case ':':
+        minus = 1;
+        break;
+      case '-':
+        if (minus) {
+          *fp++ = c;
+        } else {
+          done = 1;
+        }
+        break;
+      default:
+        if (isdigit((unsigned char)c)) {
+          val = (val * 10) + (c - '0');
+          if (val > 10000) {
+            done = 2;
+          } else {
+            *fp++ = c;
+          }
+        } else {
+          done = 1;
+        }
+      }
+      if (done == 0) {
+        c = *str++;
+      }
+    }
+    if (done == 2) {
+      // Found an error in the format
+      fp = fmt + 1;
+      *fp = *str;
+      olen = 0;
+    } else {
+      if (dot == 0) {
+        width = (size_t)val;
+      } else {
+        precision = (size_t)val;
+      }
+      olen = MAX(width, precision);
+    }
+    *fp++ = '\0';
+
+    // Handle commands
+    switch (c) {
+    case 'c':
+      pop(&val, NULL, &stack);
+      if (!ochar(&buf, buf_end, (unsigned char)val)) {
+        return false;
+      }
+      break;
+    case 's':
+      pop(NULL, &ostr, &stack);
+      if (ostr != NULL) {
+        int r;
+
+        l = strlen(ostr);
+        if (l < olen) {
+          l = olen;
+        }
+        if ((size_t)(buf_end - buf) < (l + 1)) {
+          return false;
+        }
+        r = snprintf(buf, l + 1,
+                     fmt, ostr);
+        if (r != -1) {
+          buf += (size_t)r;
+        }
+      }
+      break;
+    case 'l':
+      pop(NULL, &ostr, &stack);
+      if (ostr == NULL) {
+        l = 0;
+      } else {
+        l = strlen(ostr);
+      }
+      push((long)l, NULL, &stack);
+      break;
+    case 'd':
+    case 'o':
+    case 'x':
+    case 'X':
+      pop(&val, NULL, &stack);
+      if (onum(&buf, buf_end, fmt, (int)val, olen) == 0) {
+        return 0;
+      }
+      break;
+    case 'p':
+      if (*str < '1' || *str > '9') {
+        break;
+      }
+      l = (size_t)(*str++ - '1');
+      if (push(params[l].num, params[l].string, &stack)) {
+        return 0;
+      }
+      break;
+    case 'P':
+      pop(&val, NULL, &stack);
+      if (*str >= 'a' && *str <= 'z') {
+        dnums[*str - 'a'] = val;
+      } else if (*str >= 'A' && *str <= 'Z') {
+        snums[*str - 'A'] = val;
+      }
+      break;
+    case 'g':
+      if (*str >= 'a' && *str <= 'z') {
+        if (push(dnums[*str - 'a'], NULL, &stack)) {
+          return 0;
+        }
+      } else if (*str >= 'A' && *str <= 'Z') {
+        if (push(snums[*str - 'A'], NULL, &stack)) {
+          return 0;
+        }
+      }
+      break;
+    case 'i':
+      params[0].num++;
+      params[1].num++;
+      break;
+    case '\'':
+      if (push((long)(unsigned char)*str++, NULL, &stack)) {
+        return 0;
+      }
+      while (*str != '\0' && *str != '\'') {
+        str++;
+      }
+      if (*str == '\'') {
+        str++;
+      }
+      break;
+    case '{':
+      val = 0;
+      for (; isdigit((unsigned char)*str); str++) {
+        val = (val * 10) + (*str - '0');
+      }
+      if (push(val, NULL, &stack)) {
+        return 0;
+      }
+      while (*str != '\0' && *str != '}') {
+        str++;
+      }
+      if (*str == '}') {
+        str++;
+      }
+      break;
+    case '+':
+    case '-':
+    case '*':
+    case '/':
+    case 'm':
+    case 'A':
+    case 'O':
+    case '&':
+    case '|':
+    case '^':
+    case '=':
+    case '<':
+    case '>':
+      pop(&val, NULL, &stack);
+      pop(&val2, NULL, &stack);
+      switch (c) {
+      case '+':
+        val = val + val2;
+        break;
+      case '-':
+        val = val2 - val;
+        break;
+      case '*':
+        val = val * val2;
+        break;
+      case '/':
+        val = val ? val2 / val : 0;
+        break;
+      case 'm':
+        val = val ? val2 % val : 0;
+        break;
+      case 'A':
+        val = val && val2;
+        break;
+      case 'O':
+        val = val || val2;
+        break;
+      case '&':
+        val = val & val2;
+        break;
+      case '|':
+        val = val | val2;
+        break;
+      case '^':
+        val = val ^ val2;
+        break;
+      case '=':
+        val = val == val2;
+        break;
+      case '<':
+        val = val2 < val;
+        break;
+      case '>':
+        val = val2 > val;
+        break;
+      }
+      if (push(val, NULL, &stack)) {
+        return 0;
+      }
+      break;
+    case '!':
+    case '~':
+      pop(&val, NULL, &stack);
+      switch (c) {
+      case '!':
+        val = !val;
+        break;
+      case '~':
+        val = ~val;
+        break;
+      }
+      if (push(val, NULL, &stack)) {
+        return 0;
+      }
+      break;
+    case '?':  // if
+      break;
+    case 't':  // then
+      pop(&val, NULL, &stack);
+      if (val == 0) {
+        l = 0;
+        for (; *str != '\0'; str++) {
+          if (*str != '%') {
+            continue;
+          }
+          str++;
+          if (*str == '?') {
+            l++;
+          } else if (*str == ';') {
+            if (l > 0) {
+              l--;
+            } else {
+              str++;
+              break;
+            }
+          } else if (*str == 'e' && l == 0) {
+            str++;
+            break;
+          }
+        }
+      }
+      break;
+    case 'e':  // else
+      l = 0;
+      for (; *str != '\0'; str++) {
+        if (*str != '%') {
+          continue;
+        }
+        str++;
+        if (*str == '?') {
+          l++;
+        } else if (*str == ';') {
+          if (l > 0) {
+            l--;
+          } else {
+            str++;
+            break;
+          }
+        }
+      }
+      break;
+    case ';':  // fi
+      break;
+    }
+  }
+  return (size_t)(buf - buf_start);
 }
--- src/nvim/tui/terminfo.h
+++ src/nvim/tui/terminfo.h
@@ -3,5 +3,23 @@
 #include <unibilium.h>  // IWYU pragma: keep
 
 #include "nvim/api/private/defs.h"  // IWYU pragma: keep
+#include "nvim/tui/terminfo_enum.h"
+
+typedef struct {
+  bool bce;
+  // these extended booleans indiciate likely 24-color support
+  bool has_Tc_or_RGB;
+  bool Su;
+
+  int max_colors;
+  int lines;
+  int columns;
+  const char *defs[kTermCount];
+} NeoTerminfo;
+
+typedef struct {
+  long num;
+  char *string;
+} TPVAR;
 
 #include "tui/terminfo.h.generated.h"
