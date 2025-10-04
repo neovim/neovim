21f3ae01e399eeb6cccdb473ec32fa9a0d82376d
phanium <91544758+phanen@users.noreply.github.com>
2025-10-05 00:07:54 +0800

    fix(lsp): format_item should return string
    Problem: format_item return two value
    Solution: `:h vim.ui.select()` say format_item should return string

--- runtime/lua/vim/lsp/handlers.lua
+++ runtime/lua/vim/lsp/handlers.lua
@@ -109,7 +109,7 @@ RSC[ms.window_showMessageRequest] = function(_, params, ctx)
         kind = 'lsp_message',
         prompt = params.message .. ': ',
         format_item = function(action)
-          return (action.title:gsub('\r\n', '\\r\\n')):gsub('\n', '\\n')
+          return (action.title:gsub('\r\n', '\\r\\n'):gsub('\n', '\\n'))
         end,
       }
       vim.ui.select(params.actions, opts, function(choice)
