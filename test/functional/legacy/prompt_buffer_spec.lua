e2f7ccc7d899a1951d3e7a70fe7babcca4159a5b
shadmansaleh <13149513+shadmansaleh@users.noreply.github.com>
2025-10-15 17:32:34 +0600

    fix(prompt): left, c-left, home keys forces cursor to end of prompt
    **Problem**:
    Currently, whenever user get's on prompt-text area we move the user to
    end of user-input area. As a result when left/c-left,home keys are
    triggered at start of user-input the cursor get's placed at end of
    user-input. But this behavior can be jarring and unintuitive also it's
    different from previous behavior where it'd just stay at start of
    input-area. Also, previously when insert-mode was triggered in
    prompt-text with n_a for example then cursor was placed at start of
    user-input area not at the end. So, that behavior was also broken.
    
    **Solution:**
    Restore previous behavior. Don't force user to end of user-input when
    entering insert-mode from readonly section.

--- test/functional/legacy/prompt_buffer_spec.lua
+++ test/functional/legacy/prompt_buffer_spec.lua
@@ -302,8 +302,8 @@ describe('prompt buffer', function()
       {5:-- INSERT --}             |
     ]])
 
-    -- ensure cursor gets adjusted to end of user-text to prompt when insert mode
-    -- is entered from readonly region of prompt buffer
+    -- ensure cursor gets placed on first line of user input.
+    -- when insert mode is entered from read-only region of prompt buffer.
     local prompt_pos = api.nvim_buf_get_mark(0, ':')
     feed('<esc>')
     -- works before prompt
@@ -319,8 +319,8 @@ describe('prompt buffer', function()
     feed('<esc>')
     screen:expect([[
       other buffer             |
-      % line1                  |
-      line^2                    |
+      %^ line1                  |
+      line2                    |
       {1:~                        }|*6
                                |
     ]])
@@ -337,10 +337,39 @@ describe('prompt buffer', function()
     feed('<esc>')
     screen:expect([[
       other buffer             |
-      % line1                  |
-      line^2                    |
+      %^ line1                  |
+      line2                    |
       {1:~                        }|*6
                                |
+    ]])
+
+    -- i_<Left> i_<C-Left> i_<Home> i_<End> keys on prompt-line doesn't put cursor
+    -- at end of text
+    feed('a<Left><C-Left>')
+    screen:expect([[
+      other buffer             |
+      % ^line1                  |
+      line2                    |
+      {1:~                        }|*6
+      {5:-- INSERT --}             |
+    ]])
+
+    feed('<End>')
+    screen:expect([[
+      other buffer             |
+      % line1^                  |
+      line2                    |
+      {1:~                        }|*6
+      {5:-- INSERT --}             |
+    ]])
+
+    feed('<Home>')
+    screen:expect([[
+      other buffer             |
+      % ^line1                  |
+      line2                    |
+      {1:~                        }|*6
+      {5:-- INSERT --}             |
     ]])
   end)
 
