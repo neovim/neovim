35224f6d595e8402fd59a54dd74af96cc6d914da
Robert O'Shea <robertosheagamedev@gmail.com>
2025-10-17 05:50:56 +0100

    Run make doc

--- runtime/lua/vim/_meta/vimfn.lua
+++ runtime/lua/vim/_meta/vimfn.lua
@@ -3229,7 +3229,6 @@ function vim.fn.getcursorcharpos(winid) end
 --- Tabs and windows are identified by their respective numbers,
 --- 0 means current tab or window. Missing tab number implies 0.
 --- Thus the following are equivalent: >vim
----   getcwd()
 ---   getcwd(0)
 ---   getcwd(0, 0)
 --- <If {winnr} is -1 it is ignored, only the tab is resolved.
