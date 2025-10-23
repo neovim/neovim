eec629fddcb815f44147a0c269469371a006eac8
zeertzjq <zeertzjq@outlook.com>
2025-10-23 09:17:23 +0800

    fix(statusline): redraw if Visual selection other end changes

--- src/nvim/buffer_defs.h
+++ src/nvim/buffer_defs.h
@@ -1295,6 +1295,7 @@ struct window_S {
   int w_stl_recording;               // reg_recording when last redrawn
   int w_stl_state;                   // get_real_state() when last redrawn
   int w_stl_visual_mode;             // VIsual_mode when last redrawn
+  pos_T w_stl_visual_pos;            // VIsual when last redrawn
 
   int w_alt_fnum;                   // alternate file (for # and CTRL-^)
 
