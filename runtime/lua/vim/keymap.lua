c240ae71665962c559be264a002db5b1e2d37f58
Evgeni Chasnovski <evgeni.chasnovski@gmail.com>
2025-10-30 13:40:33 +0200

    feat(keymap): add `force` to control whether to override present mapping
    Problem: No straightforward way to create a mapping only if there
      doesn't exist one already. This is at least useful for plugins that
      want to be careful to not override user's mappings.
    
      The `vim.fn.maparg()` does not differentiate between buffer and global
      mappings, making it not a reliable source of truth for global mapping.
    
      The potential `vim.keymap.get()` might improve things for users (will
      require only something like `if not vim.keymap.get(...) then` instead
      of having to manually iterate), but being able to `force = false`
      still feels like a good improvement.
    
    Solution: Add `opts.force` which if `false` will not create new mapping
      if there already exists one for the same mode+lhs pair.

--- runtime/lua/vim/keymap.lua
+++ runtime/lua/vim/keymap.lua
@@ -1,5 +1,15 @@
 local keymap = {}
 
+local has_mapping = function(mode, lhs, buf)
+  local all = buf == nil and vim.api.nvim_get_keymap(mode) or vim.api.nvim_buf_get_keymap(buf, mode)
+  for _, map in ipairs(all) do
+    if map.lhs == lhs then
+      return true
+    end
+  end
+  return false
+end
+
 --- Table of |:map-arguments|.
 --- Same as |nvim_set_keymap()| {opts}, except:
 --- - {replace_keycodes} defaults to `true` if "expr" is `true`.
@@ -14,6 +24,10 @@ local keymap = {}
 --- Make the mapping recursive. Inverse of {noremap}.
 --- (Default: `false`)
 --- @field remap? boolean
+---
+--- Override already present mapping if `true`, preserve it if `false`.
+--- (Default: `true`)
+--- @field force? boolean
 
 --- Defines a |mapping| of |keycodes| to a function or keycodes.
 ---
@@ -65,6 +79,9 @@ function keymap.set(mode, lhs, rhs, opts)
     opts.remap = nil ---@type boolean?
   end
 
+  local force = vim.F.if_nil(opts.force, true)
+  opts.force = nil
+
   if type(rhs) == 'function' then
     opts.callback = rhs
     rhs = ''
@@ -74,12 +91,16 @@ function keymap.set(mode, lhs, rhs, opts)
     local bufnr = opts.buffer == true and 0 or opts.buffer --[[@as integer]]
     opts.buffer = nil ---@type integer?
     for _, m in ipairs(mode) do
-      vim.api.nvim_buf_set_keymap(bufnr, m, lhs, rhs, opts)
+      if force or not has_mapping(m, lhs, bufnr) then
+        vim.api.nvim_buf_set_keymap(bufnr, m, lhs, rhs, opts)
+      end
     end
   else
     opts.buffer = nil
     for _, m in ipairs(mode) do
-      vim.api.nvim_set_keymap(m, lhs, rhs, opts)
+      if force or not has_mapping(m, lhs) then
+        vim.api.nvim_set_keymap(m, lhs, rhs, opts)
+      end
     end
   end
 end
