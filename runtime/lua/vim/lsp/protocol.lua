03d6cf7aae4a72c7221a4fb8ebb14a7c8603ba18
Riley Bruins <ribru17@hotmail.com>
2025-11-29 20:38:11 -0800

    feat(lsp): support `version` in `textDocument/publishDiagnostics` #36754
    This commit makes it so that push diagnostics received for an outdated
    document version are ignored.

--- runtime/lua/vim/lsp/protocol.lua
+++ runtime/lua/vim/lsp/protocol.lua
@@ -560,6 +560,7 @@ function protocol.make_client_capabilities()
           valueSet = get_value_set(constants.DiagnosticTag),
         },
         dataSupport = true,
+        versionSupport = true,
       },
       callHierarchy = {
         dynamicRegistration = false,
