neovim/neovim

commit acfb9bc614ed480791e65268dfc4d9ad36a923a3

feat(lsp): graduate ClientConfig `exit_timeout` #36750
Problem:
The `flags` field calls its sub-fields "experimental".
But `exit_timeout` is now used for multiple purposes.

Solution:
Graduate `exit_timeout` to a top-level ClientConfig field.

runtime/lua/vim/lsp/client.lua
@@ -25,12 +25,6 @@ local all_clients = {}
 --- No debounce occurs if `nil`.
 --- (default: `150`)
 --- @field debounce_text_changes integer
----
---- Milliseconds to wait for server to exit cleanly after sending the
---- "shutdown" request before sending kill -15. If set to false, nvim exits
---- immediately after sending the "shutdown" request to the server.
---- (default: `false`)
---- @field exit_timeout integer|false
 
 --- @class vim.lsp.ClientConfig
 ---
@@ -74,6 +68,12 @@ local all_clients = {}
 --- behind orphaned server processes.
 --- (default: `true`)
 --- @field detached? boolean
+---
+--- Milliseconds to wait for server to exit cleanly after sending the "shutdown" request before
+--- sending kill -15. If set to false, waits indefinitely. If set to true, nvim will kill the
+--- server immediately.
+--- (default: `3000`)
+--- @field exit_timeout? integer|boolean
 ---
 --- A table with flags for the client. The current (experimental) flags are:
 --- @field flags? vim.lsp.Client.Flags
@@ -156,6 +156,12 @@ local all_clients = {}
 ---
 --- Capabilities provided at runtime (after startup).
 --- @field dynamic_capabilities lsp.DynamicCapabilities
+---
+--- Milliseconds to wait for server to exit cleanly after sending the "shutdown" request before
+--- sending kill -15. If set to false, waits indefinitely. If set to true, nvim will kill the
+--- server immediately.
+--- (default: `3000`)
+--- @field exit_timeout integer|boolean
 ---
 --- A table with flags for the client. The current (experimental) flags are:
 --- @field flags vim.lsp.Client.Flags
@@ -314,6 +320,7 @@ local function validate_config(config)
   validate('cmd_cwd', config.cmd_cwd, optional_validator(is_dir), 'directory')
   validate('cmd_env', config.cmd_env, 'table', true)
   validate('detached', config.detached, 'boolean', true)
+  validate('exit_timeout', config.exit_timeout, { 'number', 'boolean' }, true)
   validate('name', config.name, 'string', true)
   validate('on_error', config.on_error, 'function', true)
   validate('on_exit', config.on_exit, { 'function', 'table' }, true)
@@ -388,6 +395,7 @@ function Client.create(config)
     commands = config.commands or {},
     settings = config.settings or {},
     flags = config.flags or {},
+    exit_timeout = config.exit_timeout == nil and 3000 or config.exit_timeout --[[@as integer|boolean]],
     get_language_id = config.get_language_id or default_get_language_id,
     capabilities = config.capabilities,
     workspace_folders = lsp._get_workspace_folders(config.workspace_folders or config.root_dir),

runtime/lua/vim/lsp.lua
@@ -550,8 +550,7 @@ end
 ---
 --- @param name string|string[] Name(s) of client(s) to enable.
 --- @param enable? boolean `true|nil` to enable, `false` to disable (actively stops and detaches
---- clients as needed, and force stops them if necessary after `client.flags.exit_timeout`
---- milliseconds, with a default time of 3000 milliseconds)
+--- clients as needed, and force stops them if necessary after `client.exit_timeout` milliseconds)
 function lsp.enable(name, enable)
   validate('name', name, { 'string', 'table' })
 
@@ -588,9 +587,7 @@ function lsp.enable(name, enable)
   else
     for _, nm in ipairs(names) do
       for _, client in ipairs(lsp.get_clients({ name = nm })) do
-        local t = client.flags.exit_timeout
-        local force_timeout = t and tonumber(t) or (t ~= false and 3000 or nil)
-        client:stop(force_timeout)
+        client:stop(client.exit_timeout)
       end
     end
   end
@@ -1147,7 +1144,7 @@ api.nvim_create_autocmd('VimLeavePre', {
     log.info('exit_handler', active_clients)
 
     for _, client in pairs(active_clients) do
-      client:stop(client.flags.exit_timeout)
+      client:stop(client.exit_timeout)
     end
   end,
 })

commit 6e37c573ec70ddb726102c37e382d19bd7ea8714

fix(lsp): close timer when client exits
Also, don't start the timer at all when a previous shutdown failed, as
in this case a forced shutdown is used and no timer is needed.

This fixes most of the delays caused by #36750.
The delays caused by #36378 still seem to remain.

runtime/lua/vim/lsp/client.lua
@@ -219,6 +219,9 @@ local all_clients = {}
 --- Whether on-type formatting is enabled for this client.
 --- @field _otf_enabled boolean?
 ---
+--- Timer for stop() with timeout.
+--- @field private _shutdown_timer uv.uv_timer_t?
+---
 --- Track this so that we can escalate automatically if we've already tried a
 --- graceful shutdown
 --- @field private _graceful_shutdown_failed true?
@@ -882,12 +885,6 @@ end
 ---
 --- @param force? boolean|integer
 function Client:stop(force)
-  if type(force) == 'number' then
-    vim.defer_fn(function()
-      self:stop(true)
-    end, force)
-  end
-
   local rpc = self.rpc
   if rpc.is_closing() then
     return
@@ -900,6 +897,13 @@ function Client:stop(force)
   if force == true or not self.initialized or self._graceful_shutdown_failed then
     rpc.terminate()
     return
+  end
+
+  if type(force) == 'number' then
+    self._shutdown_timer = vim.defer_fn(function()
+      self._shutdown_timer = nil
+      self:stop(true)
+    end, force)
   end
 
   -- Sending a signal after a process has exited is acceptable.
@@ -1309,6 +1313,11 @@ end
 --- @param code integer) exit code of the process
 --- @param signal integer the signal used to terminate (if any)
 function Client:_on_exit(code, signal)
+  if self._shutdown_timer and not self._shutdown_timer:is_closing() then
+    self._shutdown_timer:close()
+    self._shutdown_timer = nil
+  end
+
   vim.schedule(function()
     for bufnr in pairs(self.attached_buffers) do
       self:_on_detach(bufnr)