170992c8d218f28dcffd967ecc2bae3fb3c46545
altermo <107814000+altermo@users.noreply.github.com>
2026-01-01 00:00:05 +0100

    test(tohtml): test syntax, not just treesitter #37181

--- test/functional/plugin/tohtml_spec.lua
+++ test/functional/plugin/tohtml_spec.lua
@@ -260,6 +260,7 @@ describe(':TOhtml', function()
     exec('set termguicolors')
     exec('syntax enable')
     exec('setf lua')
+    exec_lua('vim.treesitter.stop()') -- Ensure that legacy syntax (not just TS) is tested.
     run_tohtml_and_assert(screen)
   end)
 
