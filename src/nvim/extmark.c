a39334767321bf56dc6d0baedb048066fc56e11c
luukvbaal <luukvbaal@gmail.com>
2026-02-16 15:35:53 +0100

    fix(extmark): adjust invalidate range by one for deleted lines #37897
    Problem:  A multi-line extmark that ends exactly at a deleted range end
              is not invalidated.
              Revalidated sign mark is added with incorrect range.
    Solution: Remove questionable invalidation range condition which was
              originally added to avoid deleting a mark that ends below a
              deleted line.
    
              Since splicing for a deleted line, and a replaced range that
              explicitly ends at column 0 beyond a deleted line is identical,
              we can't try to distinguish these two cases. I.e. :1delete 1
              and nvim_buf_set_text(0, 0, 0, 1, 0, {}) yield the same splice
              operation.
    
              This means that a multi-line sign_text mark should now span at
              least one column beyond its end_row, as seen in the adjusted
              test. This is still somewhat unexpected/inconvenient to me
              which is what prompted me to try to avoid it with the original
              condition.
    
              Add revalidated sign mark back to decor with correct range;
              third sign mark added to test exposed a crash.

--- src/nvim/extmark.c
+++ src/nvim/extmark.c
@@ -141,7 +141,7 @@ static void extmark_setraw(buf_T *buf, uint64_t mark, int row, colnr_T col, bool
   }
 
   if (invalid) {
-    buf_put_decor(buf, mt_decor(key), MIN(row, key.pos.row), MAX(row, key.pos.row));
+    buf_put_decor(buf, mt_decor(key), MIN(row, alt.pos.row), MAX(row, alt.pos.row));
   } else if (!mt_invalid(key) && key.flags & MT_FLAG_DECOR_SIGNTEXT && buf->b_signcols.autom) {
     buf_signcols_count_range(buf, row1, MIN(curbuf->b_ml.ml_line_count - 1, row2), 0, kNone);
   }
@@ -398,9 +398,8 @@ void extmark_splice_delete(buf_T *buf, int l_row, colnr_T l_col, int u_row, coln
       // range has been deleted.
       if ((!mt_paired(mark) && mark.pos.row < u_row)
           || (mt_paired(mark)
-              && (endpos.col <= u_col || (!u_col && endpos.row == mark.pos.row))
-              && mark.pos.col >= l_col
-              && mark.pos.row >= l_row && endpos.row <= u_row - (u_col ? 0 : 1))) {
+              && (mark.pos.row > l_row || (mark.pos.row == l_row && mark.pos.col >= l_col))
+              && (endpos.row < u_row || (endpos.row == u_row && endpos.col <= u_col)))) {
         if (mt_no_undo(mark)) {
           extmark_del(buf, itr, mark, true);
           continue;
