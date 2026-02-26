70c8dfc87ec63cf916cf32ca5d87049b7613628c
glacambre <code@lacamb.re>
2026-02-26 21:20:03 +0900

    feat(ext_windows): externalize window management to UI
    Co-authored-by: Utkarsh Maheshwari <coditva@gmail.com>

--- test/functional/api/vim_spec.lua
+++ test/functional/api/vim_spec.lua
@@ -3690,6 +3690,7 @@ describe('API', function()
           ext_tabline = false,
           ext_termcolors = false,
           ext_wildmenu = false,
+          ext_windows = false,
           height = 4,
           override = true,
           rgb = true,
