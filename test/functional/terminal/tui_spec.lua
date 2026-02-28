48dad356632546bf8cc9342e599692a43ec7cc03
sathya-pramodh <sathyapramodh17@gmail.com>
2026-02-28 21:59:41 +0530

    fix: other skipped tests

--- test/functional/terminal/tui_spec.lua
+++ test/functional/terminal/tui_spec.lua
@@ -4181,8 +4181,6 @@ describe('TUI client', function()
   end)
 
   it(':restart works when connecting to remote instance (with its own TUI)', function()
-    t.skip(is_os('win'), 'relies on chan.detach which is currently broken on Windows')
-
     local _, screen_server, screen_client = start_tui_and_remote_client()
 
     -- Run :restart on the remote client.
@@ -4281,8 +4279,6 @@ describe('TUI client', function()
   end)
 
   it(':restart works when connecting to remote instance (--headless)', function()
-    t.skip(is_os('win'), 'relies on chan.detach which is currently broken on Windows')
-
     local _, server_pipe, screen_client = start_headless_server_and_client(false)
 
     -- Run :restart on the client.
