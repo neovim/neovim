46ee96f16f7c4756a842fba92a8381b5401d1e9f
zeertzjq <zeertzjq@outlook.com>
2026-03-02 07:11:51 +0800

    test(terminal/cursor_spec): only delete TermOpen autocmds

--- test/functional/terminal/cursor_spec.lua
+++ test/functional/terminal/cursor_spec.lua
@@ -542,7 +542,7 @@ describe('buffer cursor position is correct in terminal without number column', 
 
   before_each(function()
     clear()
-    command('autocmd! nvim.terminal')
+    command('autocmd! nvim.terminal TermOpen')
   end)
 
   describe('in a line with no multibyte chars or trailing spaces,', function()
@@ -882,7 +882,7 @@ describe('buffer cursor position is correct in terminal with number column', fun
 
   before_each(function()
     clear()
-    command('autocmd! nvim.terminal')
+    command('autocmd! nvim.terminal TermOpen')
     -- 'number' should be set before the terminal process starts, otherwise the resize
     -- from setting 'number' may cause a redraw that removes the "Entering Ex mode".
     command('set number')
