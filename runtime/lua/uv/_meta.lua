e8307000744bbe06ff09a8902a39e5d76996ecc5
Christian Clason <c.clason@uni-graz.at>
2026-03-12 19:17:11 +0100

    build(deps): bump luv to f65fe9d76

--- runtime/lua/uv/_meta.lua
+++ runtime/lua/uv/_meta.lua
@@ -21,7 +21,7 @@ uv.constants = {}
 --- local uv = require("luv") -- "luv" when stand-alone, "uv" in luvi apps
 ---
 --- local server = uv.new_tcp()
---- server:bind("127.0.0.1", 1337)
+--- assert(server:bind("127.0.0.1", 1337))
 --- server:listen(128, function (err)
 ---   assert(not err, err)
 ---   local client = uv.new_tcp()
