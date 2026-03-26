52fe90d4ead510198899e70a7f78aa13f5bbf38f
zeertzjq <zeertzjq@outlook.com>
2026-03-26 09:42:05 +0800

    test: revert startup_spec change

--- test/functional/core/startup_spec.lua
+++ test/functional/core/startup_spec.lua
@@ -1492,7 +1492,7 @@ describe('user config init', function()
         ^{MATCH: +}|
         ~{MATCH: +}|*4
         [No Name]{MATCH: +}0,0-1{MATCH: +}All|
-        {MATCH:.*}|
+        {MATCH: +}|
         -- TERMINAL --{MATCH: +}|
       ]])
 
