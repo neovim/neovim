2e3110eba80278047fd2b9c4e706d34a67641974
marvim <marvim@users.noreply.github.com>
2026-03-30 04:08:19 +0000

    docs: update version.c
    vim-patch:9.1.0333: tests: test_xdg fails on the appimage repo
    vim-patch:9.1.0336: tests: typo in test_xdg
    vim-patch:9.1.0339: tests: xdg test uses screen dumps
    vim-patch:9.1.0347: A few typos in test_xdg when testing gvimrc
    vim-patch:b9897ec27 runtime(xdg): remove // from 'undo' and 'viwedir'
    vim-patch:2e9e7cb8e runtime(xdg): Move viminfofile storage to state dir
    vim-patch:9.2.0255: tests: Test_popup_opacity_vsplit() fails in a wide terminal
    vim-patch:9.2.0260: statusline not redrawn after closing a popup window
    vim-patch:9.2.0261: terminal: redraws are slow
    vim-patch:9.2.0264: Cannot disable kitty keyboard protocol in vim :terminal
    vim-patch:9.2.0268: memory leak in call_oc_method()
    vim-patch:9.2.0269: configure: Link error on Solaris

--- src/nvim/version.c
+++ src/nvim/version.c
@@ -60,7 +60,7 @@ char *version_cflags = "Compilation: " NVIM_VERSION_CFLAGS;
 // clang-format off
 static const int vim_versions[] = { 801, 802, 900, 901, 902 };
 
-static const int num_patches[] = { 2331, 3803, 1574, 1612, 121 };
+static const int num_patches[] = { 2331, 3803, 1574, 1613, 136 };
 
 static const int *included_patchsets[] = {
   (const int[]) {  // 801
@@ -3716,9 +3716,7 @@ static const int *included_patchsets[] = {
     // 333
     332, 331,
     // 330
-    329, 328,
-    // 327
-    326, 325, 324, 323, 322, 321, 320, 319, 318, 317, 316, 315,
+    329, 328, 327, 326, 325, 324, 323, 322, 321, 320, 319, 318, 317, 316, 315,
     // 314
     313, 312, 311, 310,
     // 309
@@ -3816,13 +3814,19 @@ static const int *included_patchsets[] = {
     // 0
   },
   (const int[]) {  // 902
+    271, 270,
+    // 266-269
+    265,
+    // 255-264
+    254, 253, 252,
+    // 248-251
+    247, 246, 245, 244, 243, 242,
+    // 240-241
     239,
     // 238
     237,
     // 236
-    235,
-    // 234
-    233,
+    235, 234, 233,
     // 232
     231, 230,
     // 227-229
@@ -3903,7 +3907,9 @@ static const int *included_patchsets[] = {
     59, 58, 57, 56, 55, 54,
     // 53
     52,
-    // 48-51
+    // 51
+    50,
+    // 48-49
     47, 46,
     // 45
     44,
@@ -3920,9 +3926,7 @@ static const int *included_patchsets[] = {
     // 26
     25, 24, 23, 22,
     // 21
-    20,
-    // 19
-    18,
+    20, 19, 18,
     // 14-17
     13, 12,
     // 11
