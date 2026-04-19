a252c538b5fb4457b690325f383f8ed4c0acc716
Patrick <paddyyates04@gmail.com>
2026-04-19 21:15:01 +0100

    fix: style updates to fix linting error

--- src/nvim/tui/tui.c
+++ src/nvim/tui/tui.c
@@ -2182,7 +2182,7 @@ static void patch_terminfo_bugs(TUIData *tui, const char *term, const char *colo
     terminfo_set_if_empty(tui, kTerm_enter_italics_mode, "\x1b[3m");
   } else if (st) {
     // No bugs in the vanilla terminfo for our purposes.
-  }else {
+  } else {
     // if we have no bugs then we still want title to work
     terminfo_set_if_empty(tui, kTerm_to_status_line, "\x1b]0;");
     terminfo_set_if_empty(tui, kTerm_from_status_line, "\x07");
