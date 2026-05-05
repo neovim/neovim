3858bb68c6dc4a0edb0c7443f050dacb3ba67ba1
Yochem van Rosmalen <git@yochem.nl>
2026-05-05 09:56:34 +0200

    feat(diagnostics): add ui2 msg area as diagnostic handler
    TODO:
    - tests
    - Config
      - format func that returns chunks
      - severity filter and sort
      - single line only mode?
      - msg or cmd
      - add type annotation to diagnostic.lua

--- runtime/lua/vim/diagnostic.lua
+++ runtime/lua/vim/diagnostic.lua
@@ -383,6 +383,7 @@ local builtin_handler_names = {
   underline = true,
   virtual_text = true,
   virtual_lines = true,
+  ui2 = true,
 }
 
 --- @nodoc
@@ -670,6 +671,15 @@ M.handlers.virtual_lines = {
   end,
   hide = function(namespace, bufnr)
     return M._handlers.virtual_lines.hide(namespace, bufnr)
+  end,
+}
+
+M.handlers.ui2 = {
+  show = function(...)
+    return M._handlers.ui2.show(...)
+  end,
+  hide = function(namespace, bufnr)
+    return M._handlers.ui2.hide(namespace, bufnr)
   end,
 }
 
