0189d21725451e07fa496da70bb05104d96499aa
bfredl <bjorn.linse@gmail.com>
2026-05-17 21:31:45 +0200

    fix(api): regressions with lua return values
    fixes #39834
    fixes #39833
    fixes #39851
    
    Co-authored-by: Justin M. Keyes <justinkz@gmail.com>

--- test/functional/api/buffer_spec.lua
+++ test/functional/api/buffer_spec.lua
@@ -188,7 +188,7 @@ describe('api/buf', function()
       eq(0, api.nvim_buf_line_count(bufnr))
     end)
 
-    it('get_lines has defined behaviour for unloaded buffers', function()
+    it('get_lines and get_text has defined behaviour for unloaded buffers', function()
       -- we'll need to know our bufnr for when it gets unloaded
       local bufnr = api.nvim_get_current_buf()
       -- replace the buffer contents with these three lines
@@ -202,6 +202,22 @@ describe('api/buf', function()
       eq({}, api.nvim_buf_get_lines(bufnr, 1, 3, true))
       -- it's impossible to get out-of-bounds errors for an unloaded buffer
       eq({}, api.nvim_buf_get_lines(bufnr, 8888, 9999, true))
+      eq({}, api.nvim_buf_get_text(bufnr, 8888, 1000, 9999, 2000, {}))
+      -- The Lua path (vim.api) must also return an empty table, not nil. #39833 #39851
+      eq(
+        { 'table', {} },
+        exec_lua(function()
+          local r = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+          return { type(r), r }
+        end)
+      )
+      eq(
+        { 'table', {} },
+        exec_lua(function()
+          local r = vim.api.nvim_buf_get_text(bufnr, 0, 0, -1, 0, {})
+          return { type(r), r }
+        end)
+      )
     end)
 
     describe('handles topline', function()
@@ -2494,6 +2510,32 @@ describe('api/buf', function()
           return test(function()
             return nil
           end)
+        end)
+      )
+    end)
+
+    it('propagates return values when called from a coroutine #39834', function()
+      local other = api.nvim_create_buf(false, true)
+      eq(
+        { other, vim.NIL, 42 },
+        exec_lua(function()
+          local out
+          local co = coroutine.create(function()
+            local function pack(...)
+              local r = { ... }
+              for i = 1, select('#', ...) do
+                if r[i] == nil then
+                  r[i] = vim.NIL
+                end
+              end
+              return r
+            end
+            out = pack(vim.api.nvim_buf_call(other, function()
+              return vim.api.nvim_get_current_buf(), nil, 42
+            end))
+          end)
+          assert(coroutine.resume(co))
+          return out
         end)
       )
     end)
