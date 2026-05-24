8f6a0c06e41755775825d0246c5acb9c6e002e16
puneetdixit200 <236133619+puneetdixit200@users.noreply.github.com>
2026-05-24 16:34:10 +0530

    test(ex_cmds): move edit glob test

--- test/functional/ex_cmds/edit_spec.lua
+++ test/functional/ex_cmds/edit_spec.lua
@@ -4,12 +4,25 @@ local n = require('test.functional.testnvim')()
 local eq, command, fn = t.eq, n.command, n.fn
 local ok = t.ok
 local matches = t.matches
+local pcall_err = t.pcall_err
+local write_file = t.write_file
 local clear = n.clear
 local feed = n.feed
 
 describe(':edit', function()
+  local glob_files = { 'Xedit_glob_one.js', 'Xedit_glob_two.js' }
+
   before_each(function()
     clear()
+    for _, file in ipairs(glob_files) do
+      fn.delete(file)
+    end
+  end)
+
+  after_each(function()
+    for _, file in ipairs(glob_files) do
+      fn.delete(file)
+    end
   end)
 
   it('without arguments does not restart :terminal buffer', function()
@@ -26,5 +39,16 @@ describe(':edit', function()
     ok(fn.line('$') > 1)
     eq(bufname_before, bufname_after)
     eq(bufnr_before, bufnr_after)
+  end)
+
+  it('with glob reports that glob is not allowed', function()
+    for _, file in ipairs(glob_files) do
+      write_file(file, '')
+    end
+
+    eq(
+      'Vim(edit):E77: Too many file names (glob not allowed)',
+      pcall_err(command, 'edit Xedit_glob_*.js')
+    )
   end)
 end)
