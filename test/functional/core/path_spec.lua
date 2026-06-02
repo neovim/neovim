neovim/neovim

commit e18a5783080f7c94f408ec5f53dedffdb69789e1

*: Move some dictionary functions to typval.h and use char*
Also fixes buffer reusage in setmatches() and complete().

src/nvim/path.c
@@ -282,48 +282,63 @@ bool dir_of_file_exists(char_u *fname)
   return retval;
 }
 
-/*
- * Versions of fnamecmp() and fnamencmp() that handle '/' and '\' equally
- * and deal with 'fileignorecase'.
- */
-int vim_fnamecmp(char_u *x, char_u *y)
+/// Compare two file names
+///
+/// Handles '/' and '\\' correctly and deals with &fileignorecase option.
+///
+/// @param[in]  fname1  First file name.
+/// @param[in]  fname2  Second file name.
+///
+/// @return 0 if they are equal, non-zero otherwise.
+int path_fnamecmp(const char *fname1, const char *fname2)
+  FUNC_ATTR_NONNULL_ALL FUNC_ATTR_PURE FUNC_ATTR_WARN_UNUSED_RESULT
 {
 #ifdef BACKSLASH_IN_FILENAME
-  return vim_fnamencmp(x, y, MAXPATHL);
+  const size_t len1 = strlen(fname1);
+  const size_t len2 = strlen(fname2);
+  return path_fnamencmp(fname1, fname2, MAX(len1, len2));
 #else
-  if (p_fic)
-    return mb_stricmp(x, y);
-  return STRCMP(x, y);
+  return mb_strcmp_ic((bool)p_fic, fname1, fname2);
 #endif
 }
 
-int vim_fnamencmp(char_u *x, char_u *y, size_t len)
+/// Compare two file names
+///
+/// Handles '/' and '\\' correctly and deals with &fileignorecase option.
+///
+/// @param[in]  fname1  First file name.
+/// @param[in]  fname2  Second file name.
+/// @param[in]  len  Compare at most len bytes.
+///
+/// @return 0 if they are equal, non-zero otherwise.
+int path_fnamencmp(const char *const fname1, const char *const fname2,
+                   const size_t len)
+  FUNC_ATTR_NONNULL_ALL FUNC_ATTR_PURE FUNC_ATTR_WARN_UNUSED_RESULT
 {
 #ifdef BACKSLASH_IN_FILENAME
-  char_u      *px = x;
-  char_u      *py = y;
-  int cx = NUL;
-  int cy = NUL;
+  int c1 = NUL;
+  int c2 = NUL;
 
+  const char *p1 = fname1;
+  const char *p2 = fname2;
   while (len > 0) {
-    cx = PTR2CHAR(px);
-    cy = PTR2CHAR(py);
-    if (cx == NUL || cy == NUL
-        || ((p_fic ? vim_tolower(cx) != vim_tolower(cy) : cx != cy)
-            && !(cx == '/' && cy == '\\')
-            && !(cx == '\\' && cy == '/')))
+    c1 = PTR2CHAR(p1);
+    c2 = PTR2CHAR(p2);
+    if (c1 == NUL || c2 == NUL
+        || (!((c1 == '/' || c1 == '\\') && (c2 == '\\' || c2 == '/')))
+        || (p_fic ? (c1 != c2 && CH_FOLD(c1) != CH_FOLD(c2)) : c1 != c2)) {
       break;
-    len -= MB_PTR2LEN(px);
-    px += MB_PTR2LEN(px);
-    py += MB_PTR2LEN(py);
+    }
+    len -= MB_PTR2LEN(p1);
+    p1 += MB_PTR2LEN(p1);
+    p2 += MB_PTR2LEN(p2);
   }
-  if (len == 0)
-    return 0;
-  return cx - cy;
+  return c1 - c2;
 #else
-  if (p_fic)
-    return mb_strnicmp(x, y, len);
-  return STRNCMP(x, y, len);
+  if (p_fic) {
+    return mb_strnicmp((const char_u *)fname1, (const char_u *)fname2, len);
+  }
+  return strncmp(fname1, fname2, len);
 #endif
 }
 

src/.asan-blacklist
@@ -1,3 +1,3 @@
 # multiqueue.h pointer arithmetic is not accepted by asan
 fun:multiqueue_node_data
-fun:dictwatcher_node_data
+fun:tv_dict_watcher_node_data

src/nvim/api/private/helpers.c
@@ -88,14 +88,13 @@ bool try_end(Error *err)
 /// @param[out] err Details of an error that may have occurred
 Object dict_get_value(dict_T *dict, String key, Error *err)
 {
-  hashitem_T *hi = hash_find(&dict->dv_hashtab, (char_u *)key.data);
+  dictitem_T *const di = tv_dict_find(dict, key.data, (ptrdiff_t)key.size);
 
-  if (HASHITEM_EMPTY(hi)) {
+  if (di == NULL) {
     api_set_error(err, Validation, _("Key not found"));
     return (Object) OBJECT_INIT;
   }
 
-  dictitem_T *di = dict_lookup(hi);
   return vim_to_object(&di->di_tv);
 }
 
@@ -130,7 +129,7 @@ Object dict_set_var(dict_T *dict, String key, Object value, bool del,
     return rv;
   }
 
-  dictitem_T *di = dict_find(dict, (char_u *)key.data, (int)key.size);
+  dictitem_T *di = tv_dict_find(dict, key.data, (ptrdiff_t)key.size);
 
   if (di != NULL) {
     if (di->di_flags & DI_FLAGS_RO) {
@@ -156,9 +155,7 @@ Object dict_set_var(dict_T *dict, String key, Object value, bool del,
         rv = vim_to_object(&di->di_tv);
       }
       // Delete the entry
-      hashitem_T *hi = hash_find(&dict->dv_hashtab, di->di_key);
-      hash_remove(&dict->dv_hashtab, hi);
-      dictitem_free(di);
+      tv_dict_item_remove(dict, di);
     }
   } else {
     // Update the key
@@ -171,8 +168,8 @@ Object dict_set_var(dict_T *dict, String key, Object value, bool del,
 
     if (di == NULL) {
       // Need to create an entry
-      di = dictitem_alloc((uint8_t *) key.data);
-      dict_add(dict, di);
+      di = tv_dict_item_alloc_len(key.data, key.size);
+      tv_dict_add(dict, di);
     } else {
       // Return the old value
       if (retval) {
@@ -706,7 +703,7 @@ bool object_to_vim(Object obj, typval_T *tv, Error *err)
     }
 
     case kObjectTypeDictionary: {
-      dict_T *dict = dict_alloc();
+      dict_T *const dict = tv_dict_alloc();
 
       for (uint32_t i = 0; i < obj.data.dictionary.size; i++) {
         KeyValuePair item = obj.data.dictionary.items[i];
@@ -716,20 +713,20 @@ bool object_to_vim(Object obj, typval_T *tv, Error *err)
           api_set_error(err, Validation,
                         _("Empty dictionary keys aren't allowed"));
           // cleanup
-          dict_free(dict);
+          tv_dict_free(dict);
           return false;
         }
 
-        dictitem_T *di = dictitem_alloc((uint8_t *)key.data);
+        dictitem_T *const di = tv_dict_item_alloc(key.data);
 
         if (!object_to_vim(item.value, &di->di_tv, err)) {
           // cleanup
-          dictitem_free(di);
-          dict_free(dict);
+          tv_dict_item_free(di);
+          tv_dict_free(dict);
           return false;
         }
 
-        dict_add(dict, di);
+        tv_dict_add(dict, di);
       }
       dict->dv_refcount++;
 
@@ -960,11 +957,7 @@ static void set_option_value_err(char *key,
 {
   char *errmsg;
 
-  if ((errmsg = (char *)set_option_value((uint8_t *)key,
-                                         numval,
-                                         (uint8_t *)stringval,
-                                         opt_flags)))
-  {
+  if ((errmsg = set_option_value(key, numval, stringval, opt_flags))) {
     if (try_end(err)) {
       return;
     }

src/nvim/api/vim.c
@@ -185,7 +185,7 @@ Object nvim_eval(String expr, Error *err)
   typval_T *expr_result = eval_expr((char_u *)expr.data, NULL);
 
   if (!expr_result) {
-    api_set_error(err, Exception, _("Failed to evaluate expression"));
+    api_set_error(err, Exception, "Failed to evaluate expression");
   }
 
   if (!try_end(err)) {

src/nvim/ascii.h
@@ -8,9 +8,11 @@
 
 // Definitions of various common control characters.
 
-#define CharOrd(x)      ((x) < 'a' ? (x) - 'A' : (x) - 'a')
-#define CharOrdLow(x)   ((x) - 'a')
-#define CharOrdUp(x)    ((x) - 'A')
+#define CharOrd(x)      ((uint8_t)(x) < 'a' \
+                         ? (uint8_t)(x) - 'A'\
+                         : (uint8_t)(x) - 'a')
+#define CharOrdLow(x)   ((uint8_t)(x) - 'a')
+#define CharOrdUp(x)    ((uint8_t)(x) - 'A')
 #define ROT13(c, a)     (((((c) - (a)) + 13) % 26) + (a))
 
 #define NUL             '\000'

src/nvim/buffer.c
@@ -661,7 +661,7 @@ static void free_buffer(buf_T *buf)
   free_buffer_stuff(buf, true);
   unref_var_dict(buf->b_vars);
   aubuflocal_remove(buf);
-  dict_unref(buf->additional_data);
+  tv_dict_unref(buf->additional_data);
   clear_fmark(&buf->b_last_cursor);
   clear_fmark(&buf->b_last_insert);
   clear_fmark(&buf->b_last_change);
@@ -1481,7 +1481,7 @@ static inline void buf_init_changedtick(buf_T *const buf)
     },
     .di_key = "changedtick",
   };
-  dict_add(buf->b_vars, (dictitem_T *)&buf->changedtick_di);
+  tv_dict_add(buf->b_vars, (dictitem_T *)&buf->changedtick_di);
 }
 
 /// Add a file name to the buffer list.
@@ -1573,7 +1573,7 @@ buf_T * buflist_new(char_u *ffname, char_u *sfname, linenr_T lnum, int flags)
   if (buf != curbuf || curbuf == NULL) {
     buf = xcalloc(1, sizeof(buf_T));
     // init b: variables
-    buf->b_vars = dict_alloc();
+    buf->b_vars = tv_dict_alloc();
     init_var_dict(buf->b_vars, &buf->b_bufvar, VAR_SCOPE);
     buf_init_changedtick(buf);
   }
@@ -5443,8 +5443,8 @@ void buf_open_scratch(handle_T bufnr, char *bufname)
 {
   (void)do_ecmd((int)bufnr, NULL, NULL, NULL, ECMD_ONE, ECMD_HIDE, NULL);
   (void)setfname(curbuf, (char_u *)bufname, NULL, true);
-  set_option_value((char_u *)"bh", 0L, (char_u *)"hide", OPT_LOCAL);
-  set_option_value((char_u *)"bt", 0L, (char_u *)"nofile", OPT_LOCAL);
-  set_option_value((char_u *)"swf", 0L, NULL, OPT_LOCAL);
+  set_option_value("bh", 0L, "hide", OPT_LOCAL);
+  set_option_value("bt", 0L, "nofile", OPT_LOCAL);
+  set_option_value("swf", 0L, NULL, OPT_LOCAL);
   RESET_BINDING(curwin);
 }

src/nvim/buffer.h
@@ -7,6 +7,7 @@
 #include "nvim/screen.h"  // for StlClickRecord
 #include "nvim/func_attr.h"
 #include "nvim/eval.h"
+#include "nvim/macros.h"
 
 // Values for buflist_getfile()
 enum getf_values {
@@ -91,8 +92,8 @@ static inline void buf_set_changedtick(buf_T *const buf, const int changedtick)
 static inline void buf_set_changedtick(buf_T *const buf, const int changedtick)
 {
 #ifndef NDEBUG
-  dictitem_T *const changedtick_di = dict_find(
-      buf->b_vars, (char_u *)"changedtick", sizeof("changedtick") - 1);
+  dictitem_T *const changedtick_di = tv_dict_find(
+      buf->b_vars, S_LEN("changedtick"));
   assert(changedtick_di != NULL);
   assert(changedtick_di->di_tv.v_type == VAR_NUMBER);
   assert(changedtick_di->di_tv.v_lock == VAR_FIXED);

src/nvim/charset.h
@@ -5,6 +5,16 @@
 #include "nvim/pos.h"
 #include "nvim/buffer_defs.h"
 
+/// Return the folded-case equivalent of the given character
+///
+/// @param[in]  c  Character to transform.
+///
+/// @return Folded variant.
+#define CH_FOLD(c) \
+    utf_fold((sizeof(c) == sizeof(char)) \
+             ?((int)(uint8_t)(c)) \
+             :((int)(c)))
+
 #ifdef INCLUDE_GENERATED_DECLARATIONS
 # include "charset.h.generated.h"
 #endif

commit 13b2b1427578bec7714e1a14be44ca72ef18f379

fix(path): prevent infinite loop when re-editing file without drive-letter
Problem:
Edit a file with a drive-letter path, then re-edit it without the drive letter
and colon. This cause `path_fnamencmp` to loop infinitely as `len` never
reaches 0, while `c1` and `c2` are already NUL.

Commit e18a578 accidentally used || before `(p_fic`, commit 4bcee96 fixed that,
but also moved the NUL check into a grouped condition. The bug remained hidden
because there weren't any cases where strings had different lengths and c1 and
c2 could both reach NUL. `c:/foo` vs `/foo` happens to be such a case, which is
why the infinite loop finally showed up.

Solution:
Break the loop when either `c1` or `c2` is NUL.

src/nvim/path.c
@@ -414,9 +414,11 @@ int path_fnamencmp(const char *const fname1, const char *const fname2, size_t le
   while (len > 0) {
     c1 = utf_ptr2char(p1);
     c2 = utf_ptr2char(p2);
-    if ((c1 == NUL || c2 == NUL
-         || (!((c1 == '/' || c1 == '\\') && (c2 == '\\' || c2 == '/'))))
-        && (p_fic ? (c1 != c2 && utf_fold(c1) != utf_fold(c2)) : c1 != c2)) {
+    if (c1 == NUL
+        || c2 == NUL
+        || (c1 != c2
+            && ((c1 != '/' && c1 != '\\') || (c2 != '/' && c2 != '\\'))
+            && (!p_fic || utf_fold(c1) != utf_fold(c2)))) {
       break;
     }
     len -= (size_t)utfc_ptr2len(p1);

test/functional/core/path_spec.lua
@@ -17,7 +17,7 @@ local function join_path(...)
   return table.concat({ ... }, '/')
 end
 
-describe('path collapse', function()
+describe('path', function()
   local targetdir
   local expected_path
 
@@ -58,6 +58,13 @@ describe('path collapse', function()
   it('with ./../ and different starting directory #7117', function()
     command('cd test')
     command('edit ' .. join_path('.', '..', targetdir, 'tty-test.c'))
+    eq(expected_path, eval('expand("%:p")'))
+  end)
+
+  it('with implicit drive letter #40013', function()
+    t.skip(not is_os('win'), 'N/A: only works on Windows')
+    command('edit ' .. expected_path:sub(3))
+    eq(1, #fn.getbufinfo())
     eq(expected_path, eval('expand("%:p")'))
   end)
 end)