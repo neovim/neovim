b26fad2a6946ab4cb325dc25b8b83a95f57c00a1
Riccardo Mazzarini <me@noib3.dev>
2026-06-05 01:20:03 +0200

    feat(terminal): set the terminal's default cursor by reading `guicursor`
    See https://github.com/ghostty-org/ghostty/pull/12900 for the
    corresponding PR that added the libghostty-vt functionality that enables
    this.

--- src/nvim/optionstr.c
+++ src/nvim/optionstr.c
@@ -1325,6 +1325,7 @@ const char *did_set_guicursor(optset_T *args FUNC_ATTR_UNUSED)
     return errmsg;
   }
   terminal_update_colors_all();
+  terminal_update_default_cursor_all();
   if (VIsual_active) {
     // In Visual mode cursor may be drawn differently.
     redrawWinline(curwin, curwin->w_cursor.lnum);
