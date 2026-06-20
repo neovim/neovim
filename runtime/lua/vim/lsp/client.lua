54188fa242582808fc1d0651786a4aa2f6295fa6
jdrouhard <john@drouhard.dev>
2026-06-20 11:46:58 -0500

    fix(lsp): make LspNotify more robust #40332
    Problem: LspNotify never passed a buffer when executing the autocmds, so
    buffer-local LspNotify autocmd subscriptions didn't have the correct buf
    in the event metadata. It was also wrapped in a schedule() so the actual
    autocmd was delayed until after the event loop.
    
    This could result in the wrong buffer receiving the notification if
    multiple LspNotify autocmds with buffer filters were added. Only the
    "latest" one would actually receive non-buffer-filtered autocmds, not
    the matching one. It also caused listeners to receive the notification
    "out of sync" with when the notification is actually sent. If a buffer
    is being deleted (which fires a textDocument/didClose notification), the
    notification is scheduled and fired after the buffer is already gone.
    
    Solution: For LSP notifications that pertain to a particular buffer, set
    it when executing the LspNotify autocmds so the callback functions that
    are filtered on that buffer will get the correct notifications and the
    metadata buf field will be correct. Additionally, there is no need to
    wrap the LspNotify callback in vim.schedule when it can be called inline
    when the notification to the rpc server is fired.
    
    This is tested by removing now-unnecessary autocmds from semantic tokens
    (InsertEnter and BufWinEnter should no longer be necessary now that
    requests are fired by LspNotify). Without this fix, simply modifying a
    buffer doesn't actually trigger LspNotify correctly, and the test for
    that fails.

--- runtime/lua/vim/lsp/client.lua
+++ runtime/lua/vim/lsp/client.lua
@@ -847,9 +847,10 @@ end
 ---
 --- @param method vim.lsp.protocol.Method.ClientToServer.Notification LSP method name.
 --- @param params table? LSP request params.
+--- @param bufnr integer? Buffer associated with notification.
 --- @return boolean status indicating if the notification was successful.
 ---                        If it is false, then the client has shutdown.
-function Client:notify(method, params)
+function Client:notify(method, params, bufnr)
   if method ~= 'textDocument/didChange' then
     changetracking.flush(self)
   end
@@ -857,16 +858,15 @@ function Client:notify(method, params)
   local client_active = self.rpc.notify(method, params)
 
   if client_active then
-    vim.schedule(function()
-      api.nvim_exec_autocmds('LspNotify', {
-        modeline = false,
-        data = {
-          client_id = self.id,
-          method = method,
-          params = params,
-        },
-      })
-    end)
+    api.nvim_exec_autocmds('LspNotify', {
+      buf = bufnr,
+      modeline = false,
+      data = {
+        client_id = self.id,
+        method = method,
+        params = params,
+      },
+    })
   end
 
   return client_active
@@ -1137,14 +1137,14 @@ end
 
 --- Default handler for the 'textDocument/didClose' LSP notification.
 ---
---- @param buf integer Number of the buffer, or 0 for current
-function Client:_text_document_did_close_handler(buf)
+--- @param bufnr integer Number of the buffer, or 0 for current
+function Client:_text_document_did_close_handler(bufnr)
   if not self:supports_method('textDocument/didClose') then
     return
   end
-  local uri = vim.uri_from_bufnr(buf)
+  local uri = vim.uri_from_bufnr(bufnr)
   local params = { textDocument = { uri = uri } }
-  self:notify('textDocument/didClose', params)
+  self:notify('textDocument/didClose', params, bufnr)
 end
 
 --- Default handler for the 'textDocument/didOpen' LSP notification.
@@ -1166,7 +1166,7 @@ function Client:_text_document_did_open_handler(bufnr)
       languageId = self:_get_language_id(bufnr),
       text = lsp._buf_get_full_text(bufnr),
     },
-  })
+  }, bufnr)
 
   -- Next chance we get, we should re-do the diagnostics
   vim.schedule(function()
