a297985d05dafe92260d6733ba30f737b1997099
Curnic Dorin <157069618+dorin-curnic@users.noreply.github.com>
2026-07-16 17:23:27 +0300

    fix(messages): show hint in E77 error #40765

--- test/functional/ex_cmds/edit_spec.lua
+++ test/functional/ex_cmds/edit_spec.lua
@@ -4,12 +4,32 @@ local n = require('test.functional.testnvim')()
 local eq, command, fn = t.eq, n.command, n.fn
 local ok = t.ok
 local matches = t.matches
+local pcall_err = t.pcall_err
 local clear = n.clear
 local feed = n.feed
 
 describe(':edit', function()
   before_each(function()
     clear()
+  end)
+
+  it('E77 message', function()
+    local glob_files = { 'Xedit-glob-one.js', 'Xedit-glob-two.js' }
+
+    finally(function()
+      for _, file in ipairs(glob_files) do
+        os.remove(file)
+      end
+    end)
+
+    for _, file in ipairs(glob_files) do
+      t.write_file(file, '')
+    end
+
+    eq(
+      'Vim(edit):E77: Too many file names (glob not allowed)',
+      pcall_err(command, 'edit Xedit-glob-*.js')
+    )
   end)
 
   it('without arguments does not restart :terminal buffer', function()
