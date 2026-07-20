b9ec7c6141a24786aeacc7a0b5bfb4769e466fc0
Barrett Ruth <br@barrettruth.com>
2026-07-19 20:19:28 -0500

    feat(treesitter): highlight code inside diff hunks

---
+++ runtime/queries/diff/highlights.scm
@@ -0,0 +1,55 @@
+(comment) @comment @spell
+
+[
+  (addition)
+  (new_file)
+] @diff.plus
+
+[
+  (deletion)
+  (old_file)
+] @diff.minus
+
+((addition) @diff.plus.line
+  (#set! priority 90))
+
+((deletion) @diff.minus.line
+  (#set! priority 90))
+
+(commit) @constant
+
+(location) @attribute
+
+(command
+  "diff" @function
+  (argument) @variable.parameter)
+
+(filename) @string.special.path
+
+(mode) @number
+
+([
+  ".."
+  "+"
+  "++"
+  "+++"
+  "++++"
+  "-"
+  "--"
+  "---"
+  "----"
+] @punctuation.special
+  (#set! priority 95))
+
+[
+  (binary_change)
+  (similarity)
+  (file_change)
+] @label
+
+(index
+  "index" @keyword)
+
+(similarity
+  (score) @number
+  "%" @number)
