e9f291664feb1d16e0071e1916c1b5b13104718f
Barrett Ruth <br@barrettruth.com>
2026-07-23 16:44:16 -0700

    test(zip): use local harness

--- test/functional/plugin/zip_spec.lua
+++ test/functional/plugin/zip_spec.lua
@@ -1,6 +1,7 @@
 local n = require('test.functional.testnvim')()
 local t = require('test.testutil')
 
+local describe, it, before_each, after_each = t.describe, t.it, t.before_each, t.after_each
 local api = n.api
 local eq = t.eq
 local exec_capture = n.exec_capture
