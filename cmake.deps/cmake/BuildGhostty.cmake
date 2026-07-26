5f1db09f801f23c6df0ff62aeb1f99d33e3bab78
Riccardo Mazzarini <me@noib3.dev>
2026-07-26 13:24:49 +0200

    build(ghostty): always build in release mode

--- cmake.deps/cmake/BuildGhostty.cmake
+++ cmake.deps/cmake/BuildGhostty.cmake
@@ -1,6 +1,12 @@
 get_externalproject_options(ghostty ${DEPS_IGNORE_SHA})
 
 set(GHOSTTY_CMAKE_ARGS ${DEPS_CMAKE_ARGS})
+
+# Ghostty's Debug mode makes terminal tests slow enough for check_term_rep() in
+# test/functional/terminal/buffer_spec.lua to hit a 30-second timeout on some CI
+# runners, so we always build in release mode.
+list(APPEND GHOSTTY_CMAKE_ARGS -D CMAKE_BUILD_TYPE=RelWithDebInfo)
+
 if(APPLE)
   list(APPEND GHOSTTY_CMAKE_ARGS -DGHOSTTY_ZIG_BUILD_FLAGS=-Demit-xcframework=false)
 elseif(CMAKE_SYSTEM_NAME STREQUAL "Linux")
