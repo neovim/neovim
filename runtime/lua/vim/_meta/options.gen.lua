354484a246e2b261a2af4bebf01039f49900790b
Riccardo Mazzarini <me@noib3.dev>
2026-07-27 18:57:56 +0200

    docs(terminal): lines in scrollback might differ from `scrollback`

--- runtime/lua/vim/_meta/options.gen.lua
+++ runtime/lua/vim/_meta/options.gen.lua
@@ -5531,8 +5531,9 @@ vim.o.scr = vim.o.scroll
 vim.wo.scroll = vim.o.scroll
 vim.wo.scr = vim.wo.scroll
 
---- Maximum number of lines kept beyond the visible screen. Lines at the
---- top are deleted if new lines exceed this limit.
+--- Requested maximum number of lines kept beyond the visible screen.
+--- The actual number may differ because the terminal emulator stores and
+--- discards scrollback in larger chunks.
 --- Minimum is 1, maximum is 1000000.
 --- Only in `terminal` and `prompt-buffer` buffers.
 ---
