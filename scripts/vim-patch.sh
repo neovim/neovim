9fbcb28ea697a116280fbfd4c7f3fb5eaf4c4b46
Jan Edmund Lazo <janedmundlazo@hotmail.com>
2026-08-01 06:23:08 -0400

    build(vim-patch): N/A prop func defs, n/a vim9 type vars (#41089)

--- scripts/vim-patch.sh
+++ scripts/vim-patch.sh
@@ -946,6 +946,7 @@ is_na_patch() {
           '-I^\s+$' \
           '-I^#\s*(ifdef|if.*defined\().*FEAT_' \
           '-I^#\s*(else|endif)' \
+          '-I^EXTERN type_T t_.* INIT[2-9]\(' \
           '-I^EXTERN char e_(abstract|class|enum|interface|type)_' \
           '-I^EXTERN char e_.*def_function' \
           '-I^EXTERN char e_.*enddef' \
@@ -962,6 +963,7 @@ is_na_patch() {
           '-I^\s+$' \
           '-I^#\s*(ifdef|if.*defined\().*FEAT_' \
           '-I^#\s*(else|endif)' \
+          '-I^\s+\{"prop_[a-z]+",.*f_prop_[a-z]+},$' \
           '-I#\s*define.*ex_ni$' \
           '-I[_.>]sc_version = ' \
           '-I[_.>]uf_script_ctx_version = ' \
