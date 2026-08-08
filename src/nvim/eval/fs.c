f3491ac9719259b763890c3223cf80f77af55061
Jan Edmund Lazo <janedmundlazo@hotmail.com>
2026-08-08 00:20:46 -0400

    vim-patch:8.2.1574: Vim9: glob() doesnot take "true" as argument
    Problem:    Vim9: glob() doesnot take "true" as argument.
    Solution:   Use tv_get_bool_chk(). (closes vim/vim#6821)
    
    https://github.com/vim/vim/commit/5892ea151197c8a6363c7ce2322d84277b97353e
    
    Co-authored-by: Bram Moolenaar <Bram@vim.org>

--- src/nvim/eval/fs.c
+++ src/nvim/eval/fs.c
@@ -932,15 +932,15 @@ void f_glob(typval_T *argvars, typval_T *rettv, EvalFuncData fptr)
   // for 'wildignore' and don't put matches for 'suffixes' at the end.
   rettv->v_type = VAR_STRING;
   if (argvars[1].v_type != VAR_UNKNOWN) {
-    if (tv_get_number_chk(&argvars[1], &error)) {
+    if (tv_get_bool_chk(&argvars[1], &error)) {
       options |= WILD_KEEP_ALL;
     }
     if (argvars[2].v_type != VAR_UNKNOWN) {
-      if (tv_get_number_chk(&argvars[2], &error)) {
+      if (tv_get_bool_chk(&argvars[2], &error)) {
         tv_list_set_ret(rettv, NULL);
       }
       if (argvars[3].v_type != VAR_UNKNOWN
-          && tv_get_number_chk(&argvars[3], &error)) {
+          && tv_get_bool_chk(&argvars[3], &error)) {
         options |= WILD_ALLLINKS;
       }
     }
