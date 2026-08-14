f26a59692807f12dfc9538cf57d8af71981c9b53
Barrett Ruth <br@barrettruth.com>
2026-08-14 16:33:08 -0500

    feat(treesitter): injections for the bundled diff parser

---
+++ test/functional/treesitter/parser_diff_spec.lua
@@ -0,0 +1,171 @@
+local t = require('test.testutil')
+local n = require('test.functional.testnvim')()
+local Screen = require('test.functional.ui.screen')
+
+local describe, it, before_each = t.describe, t.it, t.before_each
+local eq = t.eq
+local clear = n.clear
+local api = n.api
+local command = n.command
+local exec_lua = n.exec_lua
+
+local function injected(lines)
+  api.nvim_buf_set_lines(0, 0, -1, false, lines)
+  return exec_lua(function()
+    local parser = vim.treesitter.get_parser(0, 'diff')
+    parser:parse(true)
+    local result = {}
+    for lang, child in pairs(parser:children()) do
+      result[lang] = {}
+      for _, region in ipairs(child:included_regions()) do
+        local texts = {}
+        for _, range in ipairs(region) do
+          texts[#texts + 1] =
+            table.concat(vim.api.nvim_buf_get_text(0, range[1], range[2], range[4], range[5], {}))
+        end
+        result[lang][#result[lang] + 1] = texts
+      end
+      table.sort(result[lang], function(a, b)
+        return #a > #b
+      end)
+    end
+    return result
+  end)
+end
+
+describe('treesitter bundled parser: diff', function()
+  before_each(clear)
+
+  it('highlights hunk content with the language of the file', function()
+    local screen = Screen.new(44, 4)
+    screen:add_extra_attr_ids({
+      [100] = { foreground = Screen.colors.SeaGreen },
+    })
+    api.nvim_buf_set_lines(0, 0, -1, false, {
+      'diff --git a/foo.lua b/foo.lua',
+      '--- a/foo.lua',
+      '+++ b/foo.lua',
+      '@@ -1,2 +1,2 @@',
+      ' local x = 1',
+      '-local y = 2',
+      '+local y = 3',
+    })
+    api.nvim_set_option_value('filetype', 'diff', { buf = 0 })
+    exec_lua(function()
+      vim.treesitter.start(0, 'diff')
+    end)
+    api.nvim_win_set_cursor(0, { 5, 0 })
+    command('normal! zt')
+
+    screen:expect([[
+      ^ {15:local} {25:x} {15:=} {26:1}                                |
+      {19:-}{15:local}{19: }{25:y}{19: }{15:=}{19: }{26:2}                                |
+      {100:+}{15:local}{100: }{25:y}{100: }{15:=}{100: }{26:3}                                |
+                                                  |
+    ]])
+  end)
+
+  it('injects the old and new file languages separately', function()
+    eq(
+      {
+        c = { { 'int x = 1;', 'int y = 2;' } },
+        lua = { { 'int x = 1;' } },
+      },
+      injected({
+        'diff --git a/foo.lua b/foo.c',
+        'rename from foo.lua',
+        'rename to foo.c',
+        '--- a/foo.lua',
+        '+++ b/foo.c',
+        '@@ -1,2 +1,2 @@',
+        ' int x = 1;',
+        '+int y = 2;',
+      })
+    )
+  end)
+
+  it('continues an injection past lines that are not changes', function()
+    eq(
+      {
+        lua = {
+          { 'local x = 1', 'local y = 3', 'local z = 4' },
+          { 'local x = 1', 'local y = 2', 'local z = 4' },
+        },
+      },
+      injected({
+        'diff --git a/foo.lua b/foo.lua',
+        '--- a/foo.lua',
+        '+++ b/foo.lua',
+        '@@ -1,4 +1,4 @@',
+        ' local x = 1',
+        '-local y = 2',
+        '\\ No newline at end of file',
+        '+local y = 3',
+        ' local z = 4',
+      })
+    )
+  end)
+
+  it('resolves a filename followed by a timestamp', function()
+    eq(
+      {
+        lua = { { 'local x = 1', 'local y = 2' }, { 'local x = 1' } },
+      },
+      injected({
+        'diff -u foo.lua bar.lua',
+        '--- foo.lua\t2026-01-01 12:00:00.000000000 +0000',
+        '+++ bar.lua\t2026-01-02 12:00:00.000000000 +0000',
+        '@@ -1,2 +1,2 @@',
+        ' local x = 1',
+        '+local y = 2',
+      })
+    )
+  end)
+
+  it('resolves a quoted filename', function()
+    eq(
+      {
+        lua = { { 'local x = 1', 'local y = 2' }, { 'local x = 1' } },
+      },
+      injected({
+        'diff --git "a/my foo.lua" "b/my foo.lua"',
+        '--- "a/my foo.lua"',
+        '+++ "b/my foo.lua"',
+        '@@ -1,2 +1,2 @@',
+        ' local x = 1',
+        '+local y = 2',
+      })
+    )
+  end)
+
+  it('does not inject when the file has no parser', function()
+    eq(
+      {},
+      injected({
+        'diff --git a/foo.xyzzy b/foo.xyzzy',
+        '--- a/foo.xyzzy',
+        '+++ b/foo.xyzzy',
+        '@@ -1,2 +1,2 @@',
+        ' hello',
+        '+world',
+      })
+    )
+  end)
+
+  it('does not inject the missing side of an added file', function()
+    eq(
+      {
+        lua = { { 'local x = 1', 'local y = 2' } },
+      },
+      injected({
+        'diff --git a/foo.lua b/foo.lua',
+        'new file mode 100644',
+        '--- /dev/null',
+        '+++ b/foo.lua',
+        '@@ -0,0 +1,2 @@',
+        '+local x = 1',
+        '+local y = 2',
+      })
+    )
+  end)
+end)
