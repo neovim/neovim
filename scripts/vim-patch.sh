f88655b527aed3a64172ab74ecb69a3def18cbfc
Jan Edmund Lazo <janedmundlazo@hotmail.com>
2026-08-25 22:59:44 -0400

    build(vim-patch): v8.2.0922 is n/a

--- scripts/vim-patch.sh
+++ scripts/vim-patch.sh
@@ -969,6 +969,14 @@ is_na_patch() {
           "$patch" -- "${file}")
         test -n "$HUNKS" && return 1
         ;;
+      src/testdir/*.vim)
+        HUNKS=$(git -C "${VIM_SOURCE_DIR}" diff-tree --no-commit-id -r -b -U0 \
+          "$patch" -- "${file}")
+        if test -n "$HUNKS"; then
+          HUNK_NUM_FINAL=$(echo "$HUNKS" | grep '^@@ .* @@' | sed 's/^@@ .* @@ //' | grep -cv -e '^func RunVimInTerminal(')
+          test "$HUNK_NUM_FINAL" -ne 0 && return 1
+        fi
+        ;;
       *.h)
         HUNKS=$(git -C "${VIM_SOURCE_DIR}" diff-tree --no-commit-id -r -b -U0 \
           '-I^\s+$' \
