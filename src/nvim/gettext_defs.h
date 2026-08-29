898c18f5cddacb1d09eadc0f4ac634cd08de063e
Christian Clason <c.clason@uni-graz.at>
2026-08-29 19:08:43 +0200

    fixup: revert to old noop macros

--- src/nvim/gettext_defs.h
+++ src/nvim/gettext_defs.h
@@ -1,11 +1,12 @@
 #pragma once
 
 // inlined from https://github.com/sabotage-linux/gettext-tiny/blob/master/include/libintl.h
-
 #define _(X) ((char *)(X))  // gettext()
 #define N_(X) X  // gettext_noop()
 #define NGETTEXT(X, Y, \
                  N) ((char *)((((unsigned long)N) == 1) ? ((void)(Y), (X)) : ((void)(X), (Y))))  // ngettext()
-#define bindtextdomain(X, Y) ((void)(X), (void)(Y), (char *)"/")
-#define bind_textdomain_codeset(dom, codeset) ((void)(dom), (void)(codeset), (char *)0)
-#define textdomain(X) ((void)(X), (char *)"messages")
+
+// empty shims
+#define bindtextdomain(X, Y)
+#define bind_textdomain_codeset(dom, codeset)
+#define textdomain(X)
