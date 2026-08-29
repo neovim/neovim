1265c4703fb16879440262a4a51e85c93db8ab0b
Christian Clason <c.clason@uni-graz.at>
2026-08-29 14:14:19 +0200

    build(deps): vendor gettext-tiny for platforms without system lintl.h

--- src/nvim/gettext_defs.h
+++ src/nvim/gettext_defs.h
@@ -1,25 +1,11 @@
 #pragma once
 
-#ifdef HAVE_WORKING_LIBINTL
-# include <libintl.h>  // IWYU pragma: export
-# define _(x) gettext(x)  // NOLINT(bugprone-reserved-identifier)
-// XXX do we actually need this?
-# ifdef gettext_noop
-#  define N_(x) gettext_noop(x)
-# else
-#  define N_(x) x
-# endif
-# define NGETTEXT(x, xs, n) ngettext(x, xs, (unsigned long)n)
-// On a Mac, gettext's libintl.h defines "setlocale" to be replaced by
-// "libintl_setlocal" which leads to wrong return values. #9789
-# if defined(__APPLE__) && defined(setlocale)
-#  undef setlocale
-# endif
-#else
-# define _(x) ((char *)(x))  // NOLINT(bugprone-reserved-identifier)
-# define N_(x) x
-# define NGETTEXT(x, xs, n) ((n) == 1 ? (x) : (xs))
-# define bindtextdomain(x, y)  // empty
-# define bind_textdomain_codeset(x, y)  // empty
-# define textdomain(x)  // empty
-#endif
+// inlined from https://github.com/sabotage-linux/gettext-tiny/blob/master/include/libintl.h
+
+#define _(X) ((char *)(X))  // gettext()
+#define N_(X) X  // gettext_noop()
+#define NGETTEXT(X, Y, \
+                 N) ((char *)((((unsigned long)N) == 1) ? ((void)(Y), (X)) : ((void)(X), (Y))))  // ngettext()
+#define bindtextdomain(X, Y) ((void)(X), (void)(Y), (char *)"/")
+#define bind_textdomain_codeset(dom, codeset) ((void)(dom), (void)(codeset), (char *)0)
+#define textdomain(X) ((void)(X), (char *)"messages")
