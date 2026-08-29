82ea5a8aac40331579113b961b6cef88865ad5a9
Jan Edmund Lazo <janedmundlazo@hotmail.com>
2026-08-29 19:06:07 -0400

    vim-patch:8.2.4439: accepting "iso8859" 'encoding' as "iso-8859-" (#41545)
    Problem:    Accepting "iso8859" 'encoding' as "iso-8859-".
    Solution:   use "iso8859" as "iso-8859-1".
    
    https://github.com/vim/vim/commit/1349bd712cf7d24dc65408c523dd7deb30224f80
    
    Co-authored-by: Bram Moolenaar <Bram@vim.org>

--- src/nvim/mbyte.c
+++ src/nvim/mbyte.c
@@ -286,6 +286,7 @@ static struct
 enc_alias_table[] = {
   { "ansi",            IDX_LATIN_1 },
   { "iso-8859-1",      IDX_LATIN_1 },
+  { "iso-8859",        IDX_LATIN_1 },
   { "latin2",          IDX_ISO_2 },
   { "latin3",          IDX_ISO_3 },
   { "latin4",          IDX_ISO_4 },
@@ -2399,7 +2400,7 @@ char *enc_canonize(char *enc)
   }
 
   // "iso-8859n" -> "iso-8859-n"
-  if (strncmp(p, "iso-8859", 8) == 0 && p[8] != '-') {
+  if (strncmp(p, "iso-8859", 8) == 0 && isdigit(p[8])) {
     STRMOVE(p + 9, p + 8);
     p[8] = '-';
   }
