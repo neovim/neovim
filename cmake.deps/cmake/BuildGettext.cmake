647de953d799856c8881948b3593f61d3eadb2d0
Christian Clason <c.clason@uni-graz.at>
2026-08-29 11:26:06 +0200

    build(deps): switch to gettext-tiny

--- cmake.deps/cmake/BuildGettext.cmake
+++ cmake.deps/cmake/BuildGettext.cmake
@@ -1,10 +1,10 @@
 if(MSVC)
   get_externalproject_options(gettext ${DEPS_IGNORE_SHA})
   ExternalProject_Add(gettext
-    DOWNLOAD_DIR ${DEPS_DOWNLOAD_DIR}/gettext
+    DOWNLOAD_DIR ${DEPS_DOWNLOAD_DIR}/gettext-tiny
     PATCH_COMMAND ${CMAKE_COMMAND} -E copy
       ${CMAKE_CURRENT_SOURCE_DIR}/cmake/GettextCMakeLists.txt
-      ${DEPS_BUILD_DIR}/src/gettext/CMakeLists.txt
+      ${DEPS_BUILD_DIR}/src/gettext-tiny/CMakeLists.txt
     CMAKE_ARGS ${DEPS_CMAKE_ARGS}
       -D LIBICONV_INCLUDE_DIRS=${DEPS_INSTALL_DIR}/include
       -D LIBICONV_LIBRARIES=${DEPS_LIB_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}libcharset${CMAKE_STATIC_LIBRARY_SUFFIX}$<SEMICOLON>${DEPS_LIB_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}libiconv${CMAKE_STATIC_LIBRARY_SUFFIX}
