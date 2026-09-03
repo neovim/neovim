27f13704e0e1bfa821d4ed7ce07c8848b5b2e5af
Volodymyr Chernetskyi <19735328+chernetskyi@users.noreply.github.com>
2026-09-03 09:47:48 +0200

    fix(filetype): report documented parameter name
    Problem:
    Invalid `args` parameter for `vim.filetype.match()` reports wrong
    parameter name `"arg"`.
    
    Solution:
    Report correct parameter name `"args"` instead.

--- runtime/lua/vim/filetype.lua
+++ runtime/lua/vim/filetype.lua
@@ -3255,7 +3255,7 @@ end
 ---                     (i.e., ".conf"), which indicates the filetype should be set with
 ---                     `:setf FALLBACK conf`. See |:setfiletype|.
 function M.match(args)
-  vim.validate('arg', args, 'table')
+  vim.validate('args', args, 'table')
 
   if not (args.buf or args.filename or args.contents) then
     error('At least one of "buf", "filename", or "contents" must be given')
