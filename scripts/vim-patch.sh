8789214153c6078ca2b136655581d3b135acb0e1
Jan Edmund Lazo <janedmundlazo@hotmail.com>
2026-09-07 14:26:44 -0400

    build(vim-patch): v9.1.1564 is n/a

--- scripts/vim-patch.sh
+++ scripts/vim-patch.sh
@@ -1024,6 +1024,7 @@ is_na_patch() {
           '-I^EXTERN char e_[_a-z]+_channel' \
           '-I^EXTERN char e_cannot_declare_.*variable_str' \
           '-I^EXTERN char e_cannot_define_new_.+_as_static' \
+          '-I^EXTERN char e_cannot_open_a_popup_window_to_a_closing_buffer' \
           '-I^EXTERN char e_cannot_use_a_return_type_with_new' \
           '-I^EXTERN char e_dictionary_not_set' \
           '-I^EXTERN char e_dictnull' \
@@ -1036,6 +1037,7 @@ is_na_patch() {
           '-I\sINIT\(= .+"E1103: Dictionary not set' \
           '-I\sINIT\(= .+"E1365: Cannot use a return type with the \\"new\\" function"' \
           '-I\sINIT\(= .+"E1370: Cannot define a .+ as static' \
+          '-I\sINIT\(= .+"E1551: Cannot open a popup window to a closing buffer' \
           '-I\s(bool|char(|_u))\s+w_popup_image_[_a-zA-Z]+;' \
           '-I\schar(|_u)\s+\*w_popup_title;' \
           '-I\sint\s+ch_[_a-zA-Z]+;' \
