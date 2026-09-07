97660307f5ac10d01febd7ffba63dfeb24d10351
bfredl <bjorn.linse@gmail.com>
2026-09-07 13:39:56 +0200

    fix(strings): non fatal errors for [NULL] safety in vim_snprintf
    context: vim_snprintf provides a "portable" replacement for a system
    vsnprintf(). However we rely on system vsnprintf() in places. This
    is quite arbitrary. I want to use system vsnprintf() only, and
    reduce the machinery in string.c for typval printf only, which will
    delet a lot of duplicated code.
    
    The biggest hurdle here is not "portably" (we only support sane c
    runtimes) but the following discrepancy: standard libc printf()
    considers NULL args to %s to be undefined behavior strictly
    while vim_snprintf() _defacto_ allows this by replacement to "[NULL]"
    
    IMO, this should be seen as "graceful" error handling (nicer than crashing
    on the user), not a valid means to have the string "[NULL]" intentionally
    be shoved in the users face. more robust code should explicitly
    check for NULL and replace it with a _appropriate_ fallback for
    the situation, depending on what a NULL string actually means in the
    context (unnamed buffer? anonymous namespace? global augroup?)
    
    soo, this PR provides the most _gently_ incremental nudge towards
    considering this case again as an error one could possible imagine.
    If sanitizers are complied in, this minor incursion gets printed
    into the sanitizers' log as a "report-and-continue" error with a
    traceback. This doesn't annoy the end user or "stop the world"
    in the test suite, but the CI will see the reported error and
    fail the build, just like other non-fatal CI errors.

--- src/nvim/sign.c
+++ src/nvim/sign.c
@@ -264,7 +264,8 @@ static void sign_list_placed(buf_T *rbuf, char *group)
   while (buf != NULL && !got_int) {
     if (buf_has_signs(buf)) {
       msg_putchar('\n');
-      vim_snprintf(lbuf, MSG_BUF_LEN, _("Signs for %s:"), buf->b_fname);
+      const char *fname = buf->b_fname ? buf->b_fname : _("Untitled");
+      snprintf(lbuf, MSG_BUF_LEN, _("Signs for %s:"), fname);
       msg_puts_hl(lbuf, HLF_D, false);
     }
 
