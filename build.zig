ac03dcf6e8e064d0fe21622474caf621434b1fb6
dundargoc <gocdundar@gmail.com>
2026-09-20 22:25:54 +0200

    build: add libghostty-vt as a terminal dependency
    Co-authored-by: Riccardo Mazzarini <me@noib3.dev>

--- build.zig
+++ build.zig
@@ -105,6 +105,14 @@ pub fn build(b: *std.Build) !void {
     };
     const ci_build = b.option(bool, "ci-build", "CI build") orelse false;
 
+    const ghostty = b.dependency("ghostty", .{
+        .target = target,
+        .optimize = optimize,
+        .@"emit-lib-vt" = true,
+        .@"emit-xcframework" = false,
+    });
+    const ghostty_vt = ghostty.artifact("ghostty-vt-static");
+
     const ziglua = b.dependency("zlua", .{
         .target = target,
         .optimize = optimize_lua,
@@ -347,7 +355,6 @@ pub fn build(b: *std.Build) !void {
         .HAVE_BUILTIN_ADD_OVERFLOW = true,
         .HAVE_WIMPLICIT_FALLTHROUGH_FLAG = true,
         .HAVE_BITSCANFORWARD64 = null,
-
         .VTERM_TEST_FILE = "test/vterm_test_output", // TODO(bfredl): revisit when porting libvterm tests
     });
 
@@ -453,6 +460,7 @@ pub fn build(b: *std.Build) !void {
     if (iconv) |dep| {
         try unittest_include_path.append(b.allocator, dep.artifact("iconv").getEmittedIncludeTree());
     }
+    try unittest_include_path.append(b.allocator, ghostty_vt.getEmittedIncludeTree());
 
     const gen_headers, const funcs_data = try gen.nvim_gen_sources(
         b,
@@ -535,7 +543,9 @@ pub fn build(b: *std.Build) !void {
     if (is_windows) {
         nvim_mod.linkSystemLibrary("netapi32", .{});
     }
+    nvim_mod.linkLibrary(ghostty_vt);
     nvim_mod.addIncludePath(b.path("src"));
+    nvim_mod.addIncludePath(ghostty_vt.getEmittedIncludeTree());
     nvim_mod.addIncludePath(gen_config.getDirectory());
     nvim_mod.addIncludePath(gen_headers.getDirectory());
     try build_lua.add_lua_modules(nvim_mod, use_luajit, false);
@@ -565,6 +575,7 @@ pub fn build(b: *std.Build) !void {
         "-std=gnu99",
         "-DZIG_BUILD",
         "-D_GNU_SOURCE",
+        "-DGHOSTTY_STATIC",
         if (support_unittests) "-DUNIT_TESTING" else "",
         if (use_luajit) "" else "-DNVIM_VENDOR_BIT",
         if (is_windows) "-DMSWIN" else "",
